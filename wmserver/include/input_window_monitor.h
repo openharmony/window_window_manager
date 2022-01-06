95e7315df391fa0185c89865b8c053522a107858
wangxinpeng <wangxinpeng4@huawei.com>
2022-01-06 15:34:42 +0800

    fix updating display resolution to IMS
    Signed-off-by: wangxinpeng <wangxinpeng4@huawei.com>
    Change-Id: Ib4db3a24ec00ca302519758deef41e9628a42eb7

--- wmserver/include/input_window_monitor.h
+++ wmserver/include/input_window_monitor.h
@@ -16,6 +16,7 @@
 #ifndef OHOS_INPUT_WINDOW_MONITOR_H
 #define OHOS_INPUT_WINDOW_MONITOR_H
 
+#include <unordered_set>
 #include <input_manager.h>
 #include <refbase.h>
 
@@ -25,38 +26,7 @@ namespace OHOS {
 namespace Rosen {
 class InputWindowMonitor : public RefBase {
 public:
-    InputWindowMonitor(sptr<WindowRoot>& root) : windowRoot_(root)
-    {
-        MMI::PhysicalDisplayInfo physicalDisplayInfo = {
-            .id = 0, //todo: update when DMS is ready
-            .leftDisplayId = -1, // todo: invalid displayId for testing
-            .upDisplayId = -1, // todo: invalid displayId for testing
-            .topLeftX = 0, // todo: use wgn info for testing
-            .topLeftY = 0, // todo: use wgn info for testing
-            .width = 2560, // todo: use wgn info for testing
-            .height = 1600, // todo: use wgn info for testing
-            .name = "physical_display0", // todo: wait for DMS
-            .seatId = "seat0", // todo: update seatId
-            .seatName = "default0", // todo: update seatId
-            .logicWidth = 2560, // todo: use wgn info for testing
-            .logicHeight = 1600, // todo: use wgn info for testing
-            .direction = MMI::Direction0 // todo: use direction 0 for testing
-        };
-        physicalDisplays_.emplace_back(physicalDisplayInfo);
-        MMI::LogicalDisplayInfo logicalDisplayInfo = {
-            .id = 0, //todo: update when DMS is ready
-            .topLeftX = 0, // todo: use wgn info for testing
-            .topLeftY = 0, // todo: use wgn info for testing
-            .width = 2560, // todo: use wgn info for testing
-            .height = 1600, // todo: use wgn info for testing
-            .name = "logical_display0", // todo: wait for DMS
-            .seatId = "seat0", // todo: update seatId
-            .seatName = "default0", // todo: update seatName
-            .focusWindowId = -1,
-            .windowsInfo_ = {},
-        };
-        logicalDisplays_.emplace_back(logicalDisplayInfo);
-    }
+    InputWindowMonitor(sptr<WindowRoot>& root) : windowRoot_(root) {}
     ~InputWindowMonitor() = default;
     void UpdateInputWindow(uint32_t windowId);
 
@@ -64,8 +34,12 @@ private:
     sptr<WindowRoot> windowRoot_;
     std::vector<MMI::PhysicalDisplayInfo> physicalDisplays_;
     std::vector<MMI::LogicalDisplayInfo> logicalDisplays_;
+    std::unordered_set<WindowType> windowTypeSkipped_ { WindowType::WINDOW_TYPE_POINTER};
+    const int INVALID_DISPLAY_ID = -1;
+    const int INVALID_WINDOW_ID = -1;
     void TraverseWindowNodes(const std::vector<sptr<WindowNode>>& windowNodes,
                               std::vector<MMI::LogicalDisplayInfo>::iterator& iter);
+    void UpdateDisplaysInfo(const sptr<WindowNodeContainer>& container);
 };
 }
 }
