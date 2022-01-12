4c3583093873982c8d916c9db37eb36323d92ea0
Klaus_q <wangqi175@huawei.com>
2022-01-12 11:54:35 +0800

    add window snapshot
    Signed-off-by: Klaus_q <wangqi175@huawei.com>
    Change-Id: I3f7aa4c50e03136bc5da44909f3eb63fb4c33c26

---
+++ wmserver/src/window_snapshot/snapshot_controller.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "snapshot_controller.h"
+#include "window_manager_hilog.h"
+#include "wm_common.h"
+
+namespace OHOS {
+namespace Rosen {
+namespace {
+    constexpr HiviewDFX::HiLogLabel LABEL = {LOG_CORE, 0, "SnapshotController"};
+}
+
+WMError SnapshotController::TakeSnapshot(const std::shared_ptr<RSSurfaceNode>& surfaceNode, Snapshot& snapshot)
+{
+    std::shared_ptr<GetSurfaceCapture> callback = std::make_shared<GetSurfaceCapture>();
+    rsInterface_.TakeSurfaceCapture(surfaceNode, callback);
+
+    int counter = 0;
+    while (!callback->IsPixelMapOk()) {
+        usleep(10000); // 10000us equals to 10ms
+        counter++;
+        if (counter >= 200) { // wait for 200 * 10ms = 2s
+            WLOGFE("Failed to get pixelmap, timeout");
+            return WMError::WM_ERROR_NULLPTR;
+        }
+    }
+    std::shared_ptr<Media::PixelMap> pixelMap = callback->GetPixelMap();
+
+    if (pixelMap == nullptr) {
+        WLOGFE("Failed to get pixelmap, return nullptr!");
+        return WMError::WM_ERROR_NULLPTR;
+    }
+    snapshot.SetPixelMap(pixelMap);
+    return WMError::WM_OK;
+}
+
+int32_t SnapshotController::GetSnapshot(const sptr<IRemoteObject> &token, Snapshot& snapshot)
+{
+    std::shared_ptr<RSSurfaceNode> surfaceNode = windowRoot_->GetSurfaceNodeByAbilityToken(token);
+    if (surfaceNode == nullptr) {
+        WLOGFE("Get surfaceNode failed!");
+        return static_cast<int32_t>(WMError::WM_ERROR_NULLPTR);
+    }
+    TakeSnapshot(surfaceNode, snapshot);
+    return static_cast<int32_t>(TakeSnapshot(surfaceNode, snapshot));
+}
+} // namespace Rosen
+} // namespace OHOS
