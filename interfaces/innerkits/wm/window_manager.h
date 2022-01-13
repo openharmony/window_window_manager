clipsheep6/window_manager

modify callback register impl for systemUI
Change-Id: I467dcc6b27c20dc878fe5c588c1bd975885022e8

interfaces/innerkits/wm/window_manager.h
@@ -35,12 +35,19 @@ public:
         WindowType windowType, int32_t displayId) = 0;
 };
 
+class ISystemBarChangedListener : public RefBase {
+public:
+    virtual void OnSystemBarPropertyChange(uint32_t displayId, const SystemBarProps& props) = 0;
+};
+
 class WindowManager : public RefBase {
 DECLARE_SINGLE_INSTANCE_BASE(WindowManager);
 friend class WindowManagerAgent;
 public:
     void RegisterFocusChangedListener(const sptr<IFocusChangedListener>& listener);
     void UnregisterFocusChangedListener(const sptr<IFocusChangedListener>& listener);
+    void RegisterSystemBarChangedListener(const sptr<ISystemBarChangedListener>& listener);
+    void UnregisterSystemBarChangedListener(const sptr<ISystemBarChangedListener>& listener);
 
 private:
     WindowManager();
@@ -50,6 +57,8 @@ private:
 
     void UpdateFocusStatus(uint32_t windowId, const sptr<IRemoteObject>& abilityToken, WindowType windowType,
         int32_t displayId, bool focused) const;
+    void UpdateSystemBarProperties(uint32_t displayId, const SystemBarProps& props) const;
+
 };
 } // namespace Rosen
 } // namespace OHOS

wm/include/window_manager_agent.h
@@ -27,6 +27,7 @@ public:
 
     void UpdateFocusStatus(uint32_t windowId, const sptr<IRemoteObject>& abilityToken, WindowType windowType,
         int32_t displayId, bool focused) override;
+    void UpdateSystemBarProperties(uint32_t displayId, const SystemBarProps& props) override;
 };
 } // namespace Rosen
 } // namespace OHOS

openharmony/window_window_manager

Signed-off-by: jincanran <jincanran@huawei.com>

interfaces/innerkits/wm/window_manager.h
@@ -34,12 +34,19 @@ public:
         WindowType windowType, int32_t displayId) = 0;
 };
 
+class ISystemBarChangedListener : public RefBase {
+public:
+    virtual void OnSystemBarPropertyChange(uint64_t displayId, const SystemBarProps& props) = 0;
+};
+
 class WindowManager : public RefBase {
 WM_DECLARE_SINGLE_INSTANCE_BASE(WindowManager);
 friend class WindowManagerAgent;
 public:
     void RegisterFocusChangedListener(const sptr<IFocusChangedListener>& listener);
     void UnregisterFocusChangedListener(const sptr<IFocusChangedListener>& listener);
+    void RegisterSystemBarChangedListener(const sptr<ISystemBarChangedListener>& listener);
+    void UnregisterSystemBarChangedListener(const sptr<ISystemBarChangedListener>& listener);
 
 private:
     WindowManager();
@@ -49,6 +56,7 @@ private:
 
     void UpdateFocusStatus(uint32_t windowId, const sptr<IRemoteObject>& abilityToken, WindowType windowType,
         int32_t displayId, bool focused) const;
+    void UpdateSystemBarProperties(uint64_t displayId, const SystemBarProps& props) const;
 };
 } // namespace Rosen
 } // namespace OHOS

wm/include/window_manager_agent.h
@@ -27,6 +27,7 @@ public:
 
     void UpdateFocusStatus(uint32_t windowId, const sptr<IRemoteObject>& abilityToken, WindowType windowType,
         int32_t displayId, bool focused) override;
+    void UpdateSystemBarProperties(uint64_t displayId, const SystemBarProps& props) override;
 };
 } // namespace Rosen
 } // namespace OHOS