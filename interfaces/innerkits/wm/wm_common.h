7ce96267244975ebc724f177acea90dae5df4404
jincanran <jincanran@huawei.com>
2022-02-22 14:36:24 +0800

    bugfix when window change mode from FULL_SCREEN to FLOATING
    Signed-off-by: jincanran <jincanran@huawei.com>
    Change-Id: I28cc3256e27280be3921e9350b112cf31dc64a99
    Signed-off-by: jincanran <jincanran@huawei.com>

--- interfaces/innerkits/wm/wm_common.h
+++ interfaces/innerkits/wm/wm_common.h
@@ -153,7 +153,7 @@ struct SystemBarProperty {
     bool enable_;
     uint32_t backgroundColor_;
     uint32_t contentColor_;
-    SystemBarProperty() : enable_(true), backgroundColor_(SYSTEM_COLOR_WHITE), contentColor_(SYSTEM_COLOR_BLACK) {}
+    SystemBarProperty() : enable_(true), backgroundColor_(SYSTEM_COLOR_BLACK), contentColor_(SYSTEM_COLOR_WHITE) {}
     SystemBarProperty(bool enable, uint32_t background, uint32_t content)
         : enable_(enable), backgroundColor_(background), contentColor_(content) {}
     bool operator == (const SystemBarProperty& a) const
