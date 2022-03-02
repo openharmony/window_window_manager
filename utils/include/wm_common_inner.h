75e06932831a94d99c2565df56aa73df31410809
xingyanan <xingyanan2@huawei.com>
2022-03-02 11:36:19 +0800

    调整自由窗口最小宽度为320vp
    Signed-off-by: xingyanan <xingyanan2@huawei.com>
    Change-Id: I2d56c280ccbb98594ad35310a6d96272ab69a14c

--- utils/include/wm_common_inner.h
+++ utils/include/wm_common_inner.h
@@ -52,7 +52,7 @@ namespace {
     constexpr uint32_t HOTZONE = 40;
     constexpr uint32_t DIV_HOTZONE = 20;
     constexpr uint32_t MIN_VERTICAL_FLOATING_WIDTH = 240;
-    constexpr uint32_t MIN_VERTICAL_FLOATING_HEIGHT = 426;
+    constexpr uint32_t MIN_VERTICAL_FLOATING_HEIGHT = 320;
     constexpr uint32_t MIN_VERTICAL_SPLIT_HEIGHT = 426;
     constexpr uint32_t MIN_HORIZONTAL_SPLIT_WIDTH = 426;
 }
