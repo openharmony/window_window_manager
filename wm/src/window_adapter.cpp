d2c0d18a92eb9eb010f4f7e4e4c9b9ab0ebda28b
l00574490 <liuqi149@huawei.com>
2022-03-04 11:40:34 +0800

    fix code warning
    Change-Id: Ic53021111db56ffaf13681dcab5bc39c0141c31a
    Signed-off-by: l00574490 <liuqi149@huawei.com>

--- wm/src/window_adapter.cpp
+++ wm/src/window_adapter.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -223,7 +223,6 @@ void WMSDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& wptrDeath)
         return;
     }
     SingletonContainer::Get<WindowAdapter>().ClearWindowAdapter();
-    return;
 }
 
 WMError WindowAdapter::GetTopWindowId(uint32_t mainWinId, uint32_t& topWinId)
