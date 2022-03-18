fb9b0fdfea612eb5857bad1465f8c3d2c7b0142b
maojiangping <maojiangping@huawei.com>
2022-03-18 18:59:33 +0800

    Fix an issue that the ime window can not be hidden when lock and unlock screen
    Signed-off-by: maojiangping <maojiangping@huawei.com>
    Change-Id: I8d17857a00d28b86b33e5b95082032432c17b2e4

--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -704,7 +704,7 @@ WMError WindowImpl::Hide(uint32_t reason)
     }
     WindowStateChangeReason stateChangeReason = static_cast<WindowStateChangeReason>(reason);
     if (stateChangeReason == WindowStateChangeReason::KEYGUARD) {
-        state_ = WindowState::STATE_HIDDEN;
+        state_ = WindowState::STATE_FROZEN;
         NotifyAfterBackground();
         return WMError::WM_OK;
     }
@@ -1297,7 +1297,7 @@ void WindowImpl::UpdateWindowState(WindowState state)
                 AAFwk::AbilityManagerClient::GetInstance()->DoAbilityBackground(abilityContext_->GetToken(),
                     static_cast<uint32_t>(WindowStateChangeReason::KEYGUARD));
             } else {
-                state_ = WindowState::STATE_HIDDEN;
+                state_ = WindowState::STATE_FROZEN;
                 NotifyAfterBackground();
             }
             break;
