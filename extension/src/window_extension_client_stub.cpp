3fc79ce5c356680fb3015872bd08591b9316be4e
lu <zhaolu2@huawei.com>
2022-03-30 11:09:33 +0800

    test
    Signed-off-by: lu <zhaolu2@huawei.com>
    Change-Id: I36ed18102317c46faf3340ada66431fadc0ac6be

--- extension/src/window_extension_client_stub.cpp
+++ extension/src/window_extension_client_stub.cpp
@@ -19,7 +19,7 @@
 namespace OHOS {
 namespace Rosen {
 namespace {
-    constexpr HiviewDFX::HiLogLabel LABEL = {LOG_CORE, HILOG_DOMAIN_WINDOW, "WindowExtensionClientStub"};
+    constexpr  HiviewDFX::HiLogLabel LABEL = {LOG_CORE, HILOG_DOMAIN_WINDOW, "WindowExtensionClientStub"};
 }
 
 int WindowExtensionClientStub::OnRemoteRequest(uint32_t code, MessageParcel &data,
