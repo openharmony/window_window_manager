1d320b13f75403d3258c36e204318a40f3770918
xiahaiqin <xiahaiqin1@huawei.com>
2022-04-06 15:11:00 +0800

    add SetCallingWindow interface
    Signed-off-by: xiahaiqin <xiahaiqin1@huawei.com>
    Change-Id: I3a687f34dbf165c3af9e0bb3b10185f59b8c504e

--- utils/include/wm_common_inner.h
+++ utils/include/wm_common_inner.h
@@ -62,6 +62,7 @@ enum class PropertyChangeAction : uint32_t {
     ACTION_UPDATE_OTHER_PROPS = 1 << 3,
     ACTION_UPDATE_FOCUSABLE = 1 << 4,
     ACTION_UPDATE_TOUCHABLE = 1 << 5,
+    ACTION_UPDATE_CALLING_WINDOW = 1 << 6,
 };
 
 namespace {
