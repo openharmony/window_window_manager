005853ca0003d41b58f58075993cd23a4c93084d
刘超 <liuchao92@huawei.com>
2022-04-08 09:45:32 +0000

    update wmserver/src/window_manager_proxy.cpp. Signed-off-by: ustclc <liuchao92@huawei.com> Change-Id: ddb0e69c54c720eebffe0c1798d09c40583b1e50

--- wmserver/src/window_manager_proxy.cpp
+++ wmserver/src/window_manager_proxy.cpp
@@ -361,7 +361,7 @@ WMError WindowManagerProxy::SetWindowAnimationController(const sptr<RSIWindowAni
         return WMError::WM_ERROR_IPC_FAILED;
     }
 
-    auto error = Remote()->SendRequest(TRANS_ID_ANIMATION_SET_CONTROLLER, data, reply, option);
+    auto error = Remote()->SendRequest(WindowManagerMessage::TRANS_ID_ANIMATION_SET_CONTROLLER, data, reply, option);
     if (error != ERR_NONE) {
         WLOGFE("Send request error: %{public}d", error);
         return WMError::WM_ERROR_IPC_FAILED;
