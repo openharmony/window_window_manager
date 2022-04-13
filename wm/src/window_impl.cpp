f268e308e8f88338e047d07191784a0b553e5571
leafly2021 <figo.yefei@huawei.com>
2022-04-13 10:46:55 +0800

    dock window is floating
    Signed-off-by: leafly2021 <figo.yefei@huawei.com>
    Change-Id: I538552bf681b33214559b335fcf347b3fc3275b1

--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -1690,6 +1690,7 @@ void WindowImpl::SetDefaultOption()
         }
         case WindowType::WINDOW_TYPE_TOAST:
         case WindowType::WINDOW_TYPE_FLOAT:
+        case WindowType::WINDOW_TYPE_LAUNCHER_DOCK:
         case WindowType::WINDOW_TYPE_SEARCHING_BAR: {
             property_->SetWindowMode(WindowMode::WINDOW_MODE_FLOATING);
             break;
