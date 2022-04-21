7ef3d3514d3cb805f28f2bfa6eb6807c88fb678e
xiaojianfeng <xiaojianfeng3@huawei.com>
2022-04-21 20:56:16 +0800

    add fuzztest
    Signed-off-by: xiaojianfeng <xiaojianfeng3@huawei.com>
    Change-Id: Id7a67daa4a7d85b7ef8376aa4a7135b39b6e4d63

---
+++ dm/test/fuzztest/screen_fuzzer/screen_fuzzer.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "screen_fuzzer.h"
+
+#include <iostream>
+
+#include "display_manager.h"
+#include "display.h"
+#include "dm_common.h"
+#include "screen.h"
+#include "screen_manager.h"
+
+namespace OHOS::Rosen {
+template<class T>
+size_t GetObject(T &object, const uint8_t *data, size_t size)
+{
+    size_t objectSize = sizeof(object);
+    if (objectSize > size) {
+        return 0;
+    }
+    std::memcpy(&object, data, objectSize);
+    return objectSize;
+}
+
+bool ScreenFuzzTest(const uint8_t *data, size_t size)
+{
+    DisplayManager& displayManager = DisplayManager::GetInstance();
+    sptr<Display> display = displayManager.GetDefaultDisplay();
+    if (display == nullptr) {
+        return false;
+    }
+    ScreenId screenId = display->GetScreenId();
+    sptr<Screen> screen = ScreenManager::GetInstance().GetScreenById(screenId);
+    if (screen == nullptr) {
+        return false;
+    }
+    uint32_t modeId;
+    Orientation orientation;
+    if (data == nullptr || size < sizeof(modeId) + sizeof(orientation)) {
+        return false;
+    }
+    size_t startPos = 0;
+    startPos += GetObject<uint32_t>(modeId, data + startPos, size - startPos);
+    startPos += GetObject<Orientation>(orientation, data + startPos, size - startPos);
+    screen->SetScreenActiveMode(modeId);
+    screen->SetOrientation(orientation);
+    screen->SetScreenActiveMode(0);
+    screen->SetOrientation(Orientation::UNSPECIFIED);
+    return true;
+}
+
+bool ColorGamutsFuzzTest(const uint8_t *data, size_t size)
+{
+    DisplayManager& displayManager = DisplayManager::GetInstance();
+    sptr<Display> display = displayManager.GetDefaultDisplay();
+    if (display == nullptr) {
+        return false;
+    }
+    ScreenId screenId = display->GetScreenId();
+    sptr<Screen> screen = ScreenManager::GetInstance().GetScreenById(screenId);
+    if (screen == nullptr) {
+        return false;
+    }
+    int32_t colorGamutIdx;
+    uint32_t gamutMap;
+    if (data == nullptr || size < sizeof(colorGamutIdx) + sizeof(gamutMap)) {
+        return false;
+    }
+    size_t startPos = 0;
+    startPos += GetObject<int32_t>(colorGamutIdx, data + startPos, size - startPos);
+    startPos += GetObject<uint32_t>(gamutMap, data + startPos, size - startPos);
+    std::vector<ScreenColorGamut> colorGamuts;
+    screen->GetScreenSupportedColorGamuts(colorGamuts);
+    size_t colorGamutsSize = colorGamuts.size();
+    if (colorGamutsSize == 0) {
+        return false;
+    }
+    int32_t index = colorGamutIdx % colorGamutsSize;
+    screen->SetScreenColorGamut(index);
+    ScreenColorGamut colorGamut;
+    screen->GetScreenColorGamut(colorGamut);
+    if (colorGamuts[index] != colorGamut) {
+        std::cout << "colorGamuts not equals." << std::endl;
+    }
+    screen->SetScreenGamutMap(static_cast<ScreenGamutMap>(gamutMap));
+    ScreenGamutMap screenGamutMap;
+    screen->GetScreenGamutMap(screenGamutMap);
+    if (static_cast<ScreenGamutMap>(gamutMap) != screenGamutMap) {
+        std::cout << "screenGamutMaps not equals." << std::endl;
+    }
+    return true;
+}
+} // namespace.OHOS::Rosen
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Rosen::ScreenFuzzTest(data, size);
+    OHOS::Rosen::ColorGamutsFuzzTest(data, size);
+    return 0;
+}
+
