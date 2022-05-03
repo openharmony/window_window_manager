c58a110f64c1bb383f4f474bf42f69fcf4cb48e9
xiahaiqin <xiahaiqin1@huawei.com>
2022-05-03 12:32:20 +0800

    add zidl for wmserver
    Signed-off-by: xiahaiqin <xiahaiqin1@huawei.com>
    Change-Id: Ia565f25e4453072fc5907faf02c7659a45db6c3c

--- wm/include/window_agent.h
+++ wm/include/window_agent.h
@@ -16,7 +16,7 @@
 #ifndef OHOS_WINDOW_AGENT_H
 #define OHOS_WINDOW_AGENT_H
 
-#include "window_stub.h"
+#include "zidl/window_stub.h"
 #include "window_impl.h"
 #include "window_property.h"
 #include "wm_common.h"
