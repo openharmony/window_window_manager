6555758b02d04f4d208f2db680e272b7716efea7
leafly2021 <figo.yefei@huawei.com>
2022-05-09 17:25:56 +0800

    add disablewindowdecor ut
    Signed-off-by: leafly2021 <figo.yefei@huawei.com>
    Change-Id: I59879fd480f1480c8ff9490fae83a41b45894a83

--- wm/test/unittest/window_impl_test.cpp
+++ wm/test/unittest/window_impl_test.cpp
@@ -1299,6 +1299,27 @@ HWTEST_F(WindowImplTest, SetTouchable02, Function | SmallTest | Level3)
     EXPECT_CALL(m->Mock(), DestroyWindow(_)).Times(1).WillOnce(Return(WMError::WM_OK));
     ASSERT_EQ(WMError::WM_OK, window->Destroy());
 }
+
+/**
+ * @tc.name: DisableAppWindowDecor01
+ * @tc.desc: disbale app window decor
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowImplTest, DisableAppWindowDecor01, Function | SmallTest | Level3)
+{
+    std::unique_ptr<Mocker> m = std::make_unique<Mocker>();
+    sptr<WindowOption> option = new WindowOption();
+    option->SetWindowName("DisableAppWindowDecor01");
+    sptr<WindowImpl> window = new WindowImpl(option);
+    EXPECT_CALL(m->Mock(), CreateWindow(_, _, _, _, _)).Times(1).WillOnce(Return(WMError::WM_OK));
+    ASSERT_EQ(WMError::WM_OK, window->Create(""));
+    window->DisableAppWindowDecor();
+    EXPECT_CALL(m->Mock(), AddWindow(_)).Times(1).WillOnce(Return(WMError::WM_OK));
+    ASSERT_EQ(WMError::WM_OK, window->Show());
+    ASSERT_FALSE(window->isAppDecorEnbale_);
+    EXPECT_CALL(m->Mock(), DestroyWindow(_)).Times(1).WillOnce(Return(WMError::WM_OK));
+    ASSERT_EQ(WMError::WM_OK, window->Destroy());
+}
 }
 } // namespace Rosen
 } // namespace OHOS
