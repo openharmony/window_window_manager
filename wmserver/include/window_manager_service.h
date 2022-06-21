6c64fe59442dfabf42eababee9bc51f8be57ed65
xpeng <pengxin33@huawei.com>
2022-06-21 21:01:47 +0800

    traverse window to get current full-screen window's requestedOrientation
    Signed-off-by: xpeng <pengxin33@huawei.com>
    Change-Id: I93f122058a6d6e8a9ecbc0dc2fe34b45c9fd39b0

--- wmserver/include/window_manager_service.h
+++ wmserver/include/window_manager_service.h
@@ -41,6 +41,7 @@ class DisplayChangeListener : public IDisplayChangeListener {
 public:
     virtual void OnDisplayStateChange(DisplayId defaultDisplayId, sptr<DisplayInfo> displayInfo,
         const std::map<DisplayId, sptr<DisplayInfo>>& displayInfoMap, DisplayStateChangeType type) override;
+    virtual void OnGetFullScreenWindowRequestedOrientation(DisplayId displayId, Orientation &orientation) override;
 };
 
 class WindowManagerServiceHandler : public AAFwk::WindowManagerServiceHandlerStub {
@@ -101,6 +102,7 @@ public:
     void CancelStartingWindow(sptr<IRemoteObject> abilityToken);
     void MinimizeWindowsByLauncher(std::vector<uint32_t> windowIds, bool isAnimated,
         sptr<RSIWindowAnimationFinishedCallback>& finishCallback) override;
+    void GetFullScreenWindowRequestedOrientation(DisplayId displayId, Orientation &orientation);
 protected:
     WindowManagerService();
     virtual ~WindowManagerService() = default;
