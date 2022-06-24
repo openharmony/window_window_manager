55ef0382927382a36ed6b661db818cabcb2e87e6
zcdqs <junfeng.lijunfeng@huawei.com>
2022-06-24 12:38:23 +0800

    fixed 3c5b6d2 from https://gitee.com/zcdqs/windowmanager/pulls/1070 add set nullptr before uicontent destroy
    Signed-off-by: zcdqs <junfeng.lijunfeng@huawei.com>
    Change-Id: I968162916966e6e43b3dba400eaf2130b7c25dbf

--- wm/include/window_impl.h
+++ wm/include/window_impl.h
@@ -225,9 +225,13 @@ private:
     {
         CALL_LIFECYCLE_LISTENER(AfterUnfocused);
     }
-    inline void NotifyBeforeDestroy(std::string windowName) const
+    inline void NotifyBeforeDestroy(std::string windowName)
     {
-        CALL_UI_CONTENT(Destroy);
+        if (uiContent_ != nullptr) {
+            auto uiContent = std::move(uiContent_);
+            uiContent_ = nullptr;
+            uiContent->Destroy();
+        }
         if (notifyNativefunc_) {
             notifyNativefunc_(windowName);
         }
