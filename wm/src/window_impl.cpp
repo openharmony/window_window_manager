ccde114fb7043cb6f92072b880117adbd3b7197b
xiaojianfeng <xiaojianfeng3@huawei.com>
2022-06-28 15:12:13 +0800

    fix toggle split window states bug
    Signed-off-by: xiaojianfeng <xiaojianfeng3@huawei.com>
    Change-Id: Iab9fcb834ac0ee71fe3a38efedec4b661de07515

--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -2023,8 +2023,7 @@ void WindowImpl::UpdateWindowState(WindowState state)
                 WLOGFD("MinimizeAbility, id: %{public}u", GetWindowId());
                 AAFwk::AbilityManagerClient::GetInstance()->MinimizeAbility(abilityContext->GetToken(), true);
             } else {
-                state_ = WindowState::STATE_HIDDEN;
-                NotifyAfterBackground();
+                Hide(static_cast<uint32_t>(WindowStateChangeReason::TOGGLING));
             }
             break;
         }
