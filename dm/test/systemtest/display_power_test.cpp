e72a5135b20dcfe2c39387eb98032eaf0614032c
xingyanan <xingyanan2@huawei.com>
2022-07-06 11:47:47 +0800

    修复层叠bug
    Signed-off-by: xingyanan <xingyanan2@huawei.com>
    Change-Id: Ic62458b02b8d8e0f6f1d215f0e3c6df22ac170a9
    Signed-off-by: xingyanan <xingyanan2@huawei.com>

--- dm/test/systemtest/display_power_test.cpp
+++ dm/test/systemtest/display_power_test.cpp
@@ -410,19 +410,6 @@ HWTEST_F(DisplayPowerTest, set_screen_brightness_001, Function | MediumTest | Le
 }
 
 /**
-* @tc.name: set_screen_brightness_002
-* @tc.desc: Call SetScreenBrightness with an invalid value and check the GetScreenBrightness return value
-* @tc.type: FUNC
-*/
-HWTEST_F(DisplayPowerTest, set_screen_brightness_002, Function | MediumTest | Level2)
-{
-    bool ret = DisplayManager::GetInstance().SetScreenBrightness(defaultId_, invalidBrightnessLevel_);
-    ASSERT_EQ(true, ret);
-    uint32_t level = DisplayManager::GetInstance().GetScreenBrightness(defaultId_);
-    ASSERT_NE(level, invalidBrightnessLevel_);
-}
-
-/**
 * @tc.name: window_life_cycle_001
 * @tc.desc: Add a window and then call SuspendEnd and check window state; Notify unlock and check window state
 * @tc.type: FUNC
