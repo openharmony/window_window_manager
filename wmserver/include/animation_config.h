0660316825770723b424afa45d0ee954f9336156
Zhang Peng <zhangpeng280@huawei.com>
2022-07-27 16:35:24 +0800

    add rotation animation
    Signed-off-by: Zhang Peng <zhangpeng280@huawei.com>
    Change-Id: Ia3ec75be9d0f7c0a14fcb45399ba5b199b18ec3a

--- wmserver/include/animation_config.h
+++ wmserver/include/animation_config.h
@@ -15,7 +15,11 @@
 #ifndef OHOS_ROSEN_ANIMATION_CONFIG_H
 #define OHOS_ROSEN_ANIMATION_CONFIG_H
 
-#include <ui/rs_display_node.h>
+#include "animation/rs_animation_timing_curve.h"
+#include "animation/rs_animation_timing_protocol.h"
+#include "common/rs_vector3.h"
+#include "common/rs_vector4.h"
+
 namespace OHOS {
 namespace Rosen {
 struct AnimationConfig {
@@ -35,6 +39,6 @@ struct AnimationConfig {
         RSAnimationTimingProtocol durationOut_ = 300;
     } keyboardAnimationConfig_;
 };
-} // Rosen
-} // OHOS
+} // namespace Rosen
+} // namespace OHOS
 #endif // OHOS_ROSEN_ANIMATION_CONFIG_H
