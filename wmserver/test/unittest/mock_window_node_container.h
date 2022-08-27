9ceea5849152448fc1d3a28e00bc401cc575e418
yangfei <yangfei110@huawei.com>
2022-08-27 14:38:10 +0800

    window node container test ,has error ,need to be commit later
    Signed-off-by: yangfei <yangfei110@huawei.com>
    Change-Id: Ibfd9e0d4a20d052d7e7abc676a48a3479cb91087

--- wmserver/test/unittest/mock_window_node_container.h
+++ wmserver/test/unittest/mock_window_node_container.h
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 
-#ifndef FRAMEWORKS_WM_TEST_UT_MOCK_WINDOW_ADAPTER_H
-#define FRAMEWORKS_WM_TEST_UT_MOCK_WINDOW_ADAPTER_H
+#ifndef FRAMEWORKS_WMS_TEST_UT_MOCK_WINDOW_ADAPTER_H
+#define FRAMEWORKS_WMS_TEST_UT_MOCK_WINDOW_ADAPTER_H
 #include <gmock/gmock.h>
 
 #include "window_node_container.h"
@@ -23,9 +23,9 @@ namespace OHOS {
 namespace Rosen {
 class MockWindowNodeContainer : public WindowNodeContainer {
 public:
-    MOCK_METHOD0(AddWindowNodeOnWindowTree, WMError(sptr<WindowNode>& node, const sptr<WindowNode>& parentNode));
+    MOCK_METHOD2(AddWindowNodeOnWindowTree, WMError(sptr<WindowNode>& node, const sptr<WindowNode>& parentNode));
     MOCK_METHOD1(RemoveWindowNode, WMError(sptr<WindowNode>& node));
-    MOCK_METHOD2(HandleRemoveWindow, WMError(sptr<WindowNode>& node));
+    MOCK_METHOD1(HandleRemoveWindow, WMError(sptr<WindowNode>& node));
 };
 }
 } // namespace OHOS
