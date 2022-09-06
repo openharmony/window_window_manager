41e147208be17ebdd3d1ae15b0978b2527983d44
l00574490 <liuqi149@huawei.com>
2022-09-06 17:22:36 +0800

    update to mmi
    Signed-off-by: l00574490 <liuqi149@huawei.com>
    Change-Id: I885156a79caea5e17192e8937eda9ca68de5e272

--- wmserver/src/window_controller.cpp
+++ wmserver/src/window_controller.cpp
@@ -505,7 +505,7 @@ void WindowController::NotifyDisplayStateChange(DisplayId defaultDisplayId, sptr
         }
         case DisplayStateChangeType::DESTROY: {
             windowRoot_->ProcessDisplayDestroy(defaultDisplayId, displayInfo, displayInfoMap);
-            FlushWindowInfoWithDisplayId(displayInfo->GetDisplayId());
+            FlushWindowInfoWithDisplayId(defaultDisplayId);
             break;
         }
         case DisplayStateChangeType::LAYOUT_COMPRESS:
