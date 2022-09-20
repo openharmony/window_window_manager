b29ffe43436af9488a3238e750e808093b29413c
mingzhu_pearl <limingzhu10@huawei.com>
2022-09-20 12:43:01 +0000

    use SurfaceBufferUsage instead of DisplayBufferUsage
    Signed-off-by: mingzhu_pearl <limingzhu10@huawei.com>
    Change-Id: Iefec36f04bd938eee42066abbe0cafd3f5dc8f2b

--- utils/src/surface_draw.cpp
+++ utils/src/surface_draw.cpp
@@ -145,7 +145,7 @@ sptr<OHOS::SurfaceBuffer> SurfaceDraw::GetSurfaceBuffer(sptr<OHOS::Surface> laye
         .height = bufferHeight,
         .strideAlignment = 0x8,
         .format = PIXEL_FMT_RGBA_8888,
-        .usage = HBM_USE_CPU_READ | HBM_USE_CPU_WRITE | HBM_USE_MEM_DMA,
+        .usage = BUFFER_USAGE_CPU_READ | BUFFER_USAGE_CPU_WRITE | BUFFER_USAGE_MEM_DMA,
     };
 
     OHOS::SurfaceError ret = layer->RequestBuffer(buffer, releaseFence, config);
