e4f4c98669adbaec6494c9c71b2c1bcbafda4225
leafly2021 <figo.yefei@huawei.com>
2022-10-22 10:23:30 +0800

    fixed 767be11 from https://gitee.com/leafly2021/windowmanager/pulls/1730 add window_manager_proxy_test
    Signed-off-by: leafly2021 <figo.yefei@huawei.com>
    Change-Id: I8762c44a90d6e872536c557b5c94722bae371e88

--- wmserver/test/unittest/window_manager_stub_test.cpp
+++ wmserver/test/unittest/window_manager_stub_test.cpp
@@ -14,114 +14,12 @@
  */
 
 #include <gtest/gtest.h>
-#include "window_manager_stub.h"
-#include "window_manager_agent.h"
-#include <rs_iwindow_animation_controller.h>
-#include "window_impl.h"
-#include "window_agent.h"
+#include "window_manager_stub_impl.h"
 
 using namespace testing;
 using namespace testing::ext;
 namespace OHOS {
 namespace Rosen {
-class WindowManagerStubImpl : public WindowManagerStub {
-WMError CreateWindow(sptr<IWindow>& window, sptr<WindowProperty>& property,
-    const std::shared_ptr<RSSurfaceNode>& surfaceNode,
-    uint32_t& windowId, sptr<IRemoteObject> token)
-{
-    return WMError::WM_OK;
-};
-WMError AddWindow(sptr<WindowProperty>& property)
-{
-    return WMError::WM_OK;
-};
-WMError RemoveWindow(uint32_t windowId)
-{
-    return WMError::WM_OK;
-};
-WMError DestroyWindow(uint32_t windowId, bool onlySelf = false)
-{
-    return WMError::WM_OK;
-};
-WMError RequestFocus(uint32_t windowId)
-{
-    return WMError::WM_OK;
-};
-AvoidArea GetAvoidAreaByType(uint32_t windowId, AvoidAreaType type)
-{
-    AvoidArea area;
-    return area;
-};
-WMError GetTopWindowId(uint32_t mainWinId, uint32_t& topWinId)
-{
-    return WMError::WM_OK;
-};
-void NotifyServerReadyToMoveOrDrag(uint32_t windowId, sptr<WindowProperty>& windowProperty,
-    sptr<MoveDragProperty>& moveDragProperty){};
-void ProcessPointDown(uint32_t windowId){};
-void ProcessPointUp(uint32_t windowId){};
-void MinimizeAllAppWindows(DisplayId displayId){};
-WMError ToggleShownStateForAllAppWindows()
-{
-    return WMError::WM_OK;
-};
-WMError SetWindowLayoutMode(WindowLayoutMode mode)
-{
-    return WMError::WM_OK;
-};
-WMError UpdateProperty(sptr<WindowProperty>& windowProperty, PropertyChangeAction action,
-    bool isAsyncTask = false)
-{
-    return WMError::WM_OK;
-};
-void RegisterWindowManagerAgent(WindowManagerAgentType type,
-    const sptr<IWindowManagerAgent>& windowManagerAgent){};
-void UnregisterWindowManagerAgent(WindowManagerAgentType type,
-    const sptr<IWindowManagerAgent>& windowManagerAgent){};
-WMError GetAccessibilityWindowInfo(std::vector<sptr<AccessibilityWindowInfo>>& infos)
-{
-    return WMError::WM_OK;
-};
-WMError GetVisibilityWindowInfo(std::vector<sptr<WindowVisibilityInfo>>& infos)
-{
-    return WMError::WM_OK;
-};
-WMError SetWindowAnimationController(const sptr<RSIWindowAnimationController>& controller)
-{
-    return WMError::WM_OK;
-};
-WMError GetSystemConfig(SystemConfig& systemConfig)
-{
-    return WMError::WM_OK;
-};
-WMError NotifyWindowTransition(sptr<WindowTransitionInfo>& from, sptr<WindowTransitionInfo>& to,
-    bool isFromClient = false)
-{
-    return WMError::WM_OK;
-};
-WMError GetModeChangeHotZones(DisplayId displayId, ModeChangeHotZones& hotZones)
-{
-    return WMError::WM_OK;
-};
-void MinimizeWindowsByLauncher(std::vector<uint32_t> windowIds, bool isAnimated,
-    sptr<RSIWindowAnimationFinishedCallback>& finishCallback){};
-WMError UpdateAvoidAreaListener(uint32_t windowId, bool haveListener)
-{
-    return WMError::WM_OK;
-};
-WMError UpdateRsTree(uint32_t windowId, bool isAdd)
-{
-    return WMError::WM_OK;
-};
-WMError BindDialogTarget(uint32_t& windowId, sptr<IRemoteObject> targetToken)
-{
-    return WMError::WM_OK;
-};
-void SetAnchorAndScale(int32_t x, int32_t y, float scale){};
-void SetAnchorOffset(int32_t deltaX, int32_t deltaY){};
-void OffWindowZoom(){};
-};
-
 class WindowManagerStubTest : public testing::Test {
 public:
     static void SetUpTestCase();
