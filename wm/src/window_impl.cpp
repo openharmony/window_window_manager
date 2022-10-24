70dd8d0d3fc833ea8ad53a5c33586e13648a4de8
xiahaiqin <xiahaiqin1@huawei.com>
2022-10-24 16:39:08 +0800

    add IsFocused interface for window
    Signed-off-by: xiahaiqin <xiahaiqin1@huawei.com>
    Change-Id: If4770cd2fef520714274ea91369bd23cfd5512dd

--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -2461,6 +2461,11 @@ void WindowImpl::UpdateFocusStatus(bool focused)
     UpdateWindowShadowAccordingToSystemConfig();
 }
 
+bool WindowImpl::IsFocused() const
+{
+    return isFocused_;
+}
+
 void WindowImpl::UpdateConfiguration(const std::shared_ptr<AppExecFwk::Configuration>& configuration)
 {
     if (uiContent_ != nullptr) {
