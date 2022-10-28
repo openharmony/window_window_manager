038bdee1215f08b6aeff6af5b56c80e4515754b4
zhirong <yangzhirong6@huawei.com>
2022-10-28 12:39:15 +0800

    fix tile layout bug
    Signed-off-by: zhirong <yangzhirong6@huawei.com>
    Change-Id: I2f9d16d2df6b6f15b4ee84e285dbead79a4b4887
    Signed-off-by: zhirong <yangzhirong6@huawei.com>

--- wmserver/src/window_layout_policy_tile.cpp
+++ wmserver/src/window_layout_policy_tile.cpp
@@ -243,7 +243,7 @@ void WindowLayoutPolicyTile::ForegroundNodeQueuePushBack(const sptr<WindowNode>&
         return;
     }
     WLOGFI("add win in tile, displayId: %{public}" PRIu64", winId: %{public}d", displayId, node->GetWindowId());
-    while (foregroundNodes.size() >= maxTileWinNumMap_[displayId]) {
+    while (!foregroundNodes.empty() && foregroundNodes.size() >= maxTileWinNumMap_[displayId]) {
         auto removeNode = foregroundNodes.front();
         foregroundNodes.pop_front();
         WLOGFI("pop win in queue head for add new win, windowId: %{public}d", removeNode->GetWindowId());
