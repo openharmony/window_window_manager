8c911c39752e84ee239ada249b6795bb0aa76c79
xukunkun <xukunkun1@huawei.com>
2022-11-24 11:04:46 +0800

    Fix "use of undeclared identifier" mistake
    Signed-off-by: xukunkun <xukunkun1@huawei.com>

--- wmserver/test/unittest/window_layout_policy_cascade_test.cpp
+++ wmserver/test/unittest/window_layout_policy_cascade_test.cpp
@@ -15,6 +15,7 @@
 
 #include <gtest/gtest.h>
 #include "display_group_info.h"
+#include "display_manager.h"
 #include "window_layout_policy.h"
 #include "window_layout_policy_cascade.h"
 #include "window_node_container.h"
@@ -29,19 +30,19 @@ class WindowLayoutPolicyCascadeTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
-    virtual void SetUp() override;
-    virtual void TearDown() override;
+    void SetUp() override;
+    void TearDown() override;
 
     static sptr<WindowNodeContainer> container_;
     static sptr<DisplayGroupInfo> displayGroupInfo_;
     static sptr<DisplayInfo> defaultDisplayInfo_;
-    static sptr<WindowLayoutPolicy> layoutPolicy_;
+    static sptr<WindowLayoutPolicyCascade> layoutPolicyCascade_;
 };
 
 sptr<WindowNodeContainer> WindowLayoutPolicyCascadeTest::container_ = nullptr;
 sptr<DisplayGroupInfo> WindowLayoutPolicyCascadeTest::displayGroupInfo_ = nullptr;
 sptr<DisplayInfo> WindowLayoutPolicyCascadeTest::defaultDisplayInfo_ = nullptr;
-sptr<WindowLayoutPolicyCascade> WindowLayoutPolicyCascadeTest::layoutPolicy_ = nullptr;
+sptr<WindowLayoutPolicyCascade> WindowLayoutPolicyCascadeTest::layoutPolicyCascade_ = nullptr;
 
 void WindowLayoutPolicyCascadeTest::SetUpTestCase()
 {
@@ -53,7 +54,7 @@ void WindowLayoutPolicyCascadeTest::SetUpTestCase()
 
     container_ = new WindowNodeContainer(defaultDisplayInfo_, display->GetScreenId());
     displayGroupInfo_ = container_->displayGroupInfo_;
-    layoutPolicy_ = container_->GetLayoutPolicy();
+    layoutPolicyCascade_ = container_->GetLayoutPolicy();
 }
 
 void WindowLayoutPolicyCascadeTest::TearDownTestCase()
@@ -83,19 +84,19 @@ HWTEST_F(WindowLayoutPolicyCascadeTest, InitCascadeRectCfg01, Function | SmallTe
 {
     std::vector<int> numbers {82, 121, 1068, 706};
     WindowLayoutPolicyCascade::SetDefaultFloatingWindow(numbers);
-    ASSERT_EQ(defaultFloatingWindow_.posX_, static_cast<uint32_t>(numbers[0]));
+    ASSERT_EQ(WindowLayoutPolicyCascade::defaultFloatingWindow_.posX_, static_cast<uint32_t>(numbers[0]));
 
     auto displayId = defaultDisplayInfo_->GetDisplayId();
-    ASSERT_EQ(layoutPolicy_->InitCascadeRectCfg(displayId), true);
+    ASSERT_EQ(layoutPolicyCascade_->InitCascadeRectCfg(displayId), true);
 
-    Rect resRect = defaultFloatingWindow_;
+    Rect resRect = WindowLayoutPolicyCascade::defaultFloatingWindow_;
     auto vpr = displayGroupInfo_->GetDisplayVirtualPixelRatio(displayId);
     resRect.width_ = static_cast<uint32_t>(vpr * resRect.width_);
     resRect.height_ = static_cast<uint32_t>(vpr * resRect.height_);
     resRect.posX_ = static_cast<int32_t>(vpr * resRect.posX_);
     resRect.posY_ = static_cast<int32_t>(vpr * resRect.posY_);
 
-    ASSERT_EQ(cascadeRectsMap_[displayId].defaultCascadeRect_, resRect);
+    ASSERT_EQ(layoutPolicyCascade_->cascadeRectsMap_[displayId].defaultCascadeRect_, resRect);
 }
 }
 }
