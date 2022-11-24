2910135a24830eed60084122ab7f13dcdded6e8b
DemonGrady <yangguangwei4@huawei.com>
2022-11-24 16:59:01 +0800

    Avoid deadlock between wms and dms
    Signed-off-by: DemonGrady <yangguangwei4@huawei.com>
    Change-Id: Icff5d021e9a653b0054601671f1f6f68cfc408da

--- wmserver/src/window_root.cpp
+++ wmserver/src/window_root.cpp
@@ -1645,8 +1645,16 @@ void WindowRoot::OnRenderModeChanged(bool isUniRender)
 
 void WindowRoot::SwitchRenderModeIfNeeded()
 {
-    uint32_t rsScreenNum = DisplayManagerServiceInner::GetInstance().GetRSScreenNum();
-    if (rsScreenNum > 1) {
+    if (displayIdMap_.empty()) {
+        WLOGFE("WindowRoot::SwitchRenderModeIfNeeded: displayIdMap_ is empty");
+        return;
+    }
+    if (displayIdMap_.size() != 1) {
+        WLOGFE("WindowRoot::SwitchRenderModeIfNeeded: invalid screenGroup number");
+        return;
+    }
+    uint32_t displayNum = displayIdMap_.begin()->second.size();
+    if (displayNum > 1) {
         // switch to sperate render mode
         ChangeRSRenderModeIfNeeded(false);
         return;
