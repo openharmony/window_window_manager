41b67ccc1e0dffe8eeadc4d38bf1cabfafb407db
ac__fancheyu <liuyangyang58@huawei.com>
2023-01-13 14:52:35 +0800

    solves the multithreading problem when reading WindowState
    Signed-off-by: ac__fancheyu <liuyangyang58@huawei.com>
    Change-Id: I4ffabb3c4e6138f7e2bcbe64a5c5b1977872859a

--- wm/include/window_impl.h
+++ wm/include/window_impl.h
@@ -553,6 +553,7 @@ private:
     std::unique_ptr<Ace::UIContent> uiContent_;
     std::shared_ptr<AbilityRuntime::Context> context_;
     std::recursive_mutex mutex_;
+    std::recursive_mutex windowStateMutex_;
     static std::recursive_mutex globalMutex_;
     const float SYSTEM_ALARM_WINDOW_WIDTH_RATIO = 0.8;
     const float SYSTEM_ALARM_WINDOW_HEIGHT_RATIO = 0.3;
