b02b8803c14a7e6df6f09bac380a34b3bbd4644a
ac__fancheyu <liuyangyang58@huawei.com>
2023-01-13 14:52:35 +0800

    solves the multithreading problem when reading WindowState
    Signed-off-by: ac__fancheyu <liuyangyang58@huawei.com>
    Change-Id: I4ffabb3c4e6138f7e2bcbe64a5c5b1977872859a

--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -1355,7 +1355,8 @@ WMError WindowImpl::Show(uint32_t reason, bool withAnimation)
     if (ret != WMError::WM_OK) {
         return ret;
     }
-    std::lock_guard<std::recursive_mutex> lock(mutex_);
+    // this lock solves the multithreading problem when reading WindowState
+    std::lock_guard<std::recursive_mutex> lock(windowStateMutex_);
     ret = SingletonContainer::Get<WindowAdapter>().AddWindow(property_);
     RecordLifeCycleExceptionEvent(LifeCycleEvent::SHOW_EVENT, ret);
     if (ret == WMError::WM_OK) {
@@ -1428,7 +1429,8 @@ WMError WindowImpl::MoveTo(int32_t x, int32_t y)
     Rect moveRect = { x, y, rect.width_, rect.height_ }; // must keep w/h, which may maintain stashed resize info
     property_->SetRequestRect(moveRect);
     {
-        std::lock_guard<std::recursive_mutex> lock(mutex_);
+        // this lock solves the multithreading problem when reading WindowState
+        std::lock_guard<std::recursive_mutex> lock(windowStateMutex_);
         if (state_ == WindowState::STATE_HIDDEN || state_ == WindowState::STATE_CREATED) {
         WLOGFD("window is hidden or created! id: %{public}u, oriPos: [%{public}d, %{public}d, "
                "movePos: [%{public}d, %{public}d]", property_->GetWindowId(), rect.posX_, rect.posY_, x, y);
@@ -1458,7 +1460,8 @@ WMError WindowImpl::Resize(uint32_t width, uint32_t height)
     property_->SetRequestRect(resizeRect);
     property_->SetDecoStatus(false);
     {
-        std::lock_guard<std::recursive_mutex> lock(mutex_);
+        // this lock solves the multithreading problem when reading WindowState
+        std::lock_guard<std::recursive_mutex> lock(windowStateMutex_);
         if (state_ == WindowState::STATE_HIDDEN || state_ == WindowState::STATE_CREATED) {
         WLOGFD("window is hidden or created! id: %{public}u, oriRect: [%{public}u, %{public}u], "
                "resizeRect: [%{public}u, %{public}u]", property_->GetWindowId(), rect.width_,
