105c3921f769a112557a9c8a26659155574443c8
l00574490 <liuqi149@huawei.com>
2023-02-02 11:08:12 +0800

    fix bug
    Signed-off-by: l00574490 <liuqi149@huawei.com>
    Change-Id: Ie67ad25540902d31bc04c13a305b2f9889945449

--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -1321,7 +1321,6 @@ WMError WindowImpl::PreProcessShow(uint32_t reason, bool withAnimation)
     AdjustWindowAnimationFlag(withAnimation);
 
     if (NeedToStopShowing()) { // true means stop showing
-        NotifyForegroundInvalidWindowMode();
         return WMError::WM_ERROR_INVALID_WINDOW_MODE_OR_SIZE;
     }
 
@@ -1364,6 +1363,7 @@ WMError WindowImpl::Show(uint32_t reason, bool withAnimation)
     }
     WMError ret = PreProcessShow(reason, withAnimation);
     if (ret != WMError::WM_OK) {
+        NotifyForegroundFailed(ret);
         return ret;
     }
     // this lock solves the multithreading problem when reading WindowState
@@ -1372,10 +1372,8 @@ WMError WindowImpl::Show(uint32_t reason, bool withAnimation)
     RecordLifeCycleExceptionEvent(LifeCycleEvent::SHOW_EVENT, ret);
     if (ret == WMError::WM_OK) {
         UpdateWindowStateWhenShow();
-    } else if (ret == WMError::WM_ERROR_INVALID_WINDOW_MODE_OR_SIZE) {
-        NotifyForegroundInvalidWindowMode();
     } else {
-        NotifyForegroundFailed();
+        NotifyForegroundFailed(ret);
         WLOGFE("show window id:%{public}u errCode:%{public}d", property_->GetWindowId(), static_cast<int32_t>(ret));
     }
     return ret;
