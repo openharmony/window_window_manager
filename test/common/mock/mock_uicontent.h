4e9ea469685978b7a7b33b0018829b7ae24fc7d9
zhongjianfei <zhongjianfei@huawei.com>
2023-02-07 20:17:12 +0800

    [ArkTS Card] cherry pick from master.
    Signed-off-by: zhongjianfei <zhongjianfei@huawei.com>
    Change-Id: I1e0a5e7ee600694602b68f9d884842f02aaba5d5

--- test/common/mock/mock_uicontent.h
+++ test/common/mock/mock_uicontent.h
@@ -66,6 +66,13 @@ public:
     MOCK_METHOD1(NotifyMemoryLevel, void(int32_t level));
     MOCK_METHOD1(SetAppWindowTitle, void(const std::string& title));
     MOCK_METHOD1(SetAppWindowIcon, void(const std::shared_ptr<Media::PixelMap>& pixelMap));
+    MOCK_METHOD0(GetCardRootNode, std::shared_ptr<Rosen::RSSurfaceNode>());
+    MOCK_METHOD1(ProcessFormUpdate, void(const std::string& data));
+    MOCK_METHOD1(SetFormWidth, void(const float width));
+    MOCK_METHOD1(SetFormHeight, void(const float height));
+    MOCK_METHOD0(GetFormWidth, float());
+    MOCK_METHOD0(GetFormHeight, float());
+    MOCK_METHOD1(SetActionEventHandler, void(std::function<void(const std::string& action)>&& actionCallback));
 };
 } // namespace Ace
 } // namespace OHOS
