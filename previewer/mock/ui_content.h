c36ea60dc68bb164552e9d6855eeb952224fa463
zhongjianfei <zhongjianfei@huawei.com>
2023-02-15 08:43:56 +0800

    [ArkTS Card] CherryPick Master PR 2268.
    Signed-off-by: zhongjianfei <zhongjianfei@huawei.com>
    Change-Id: If5cd85d68196187bc6ccff0c4f44e4d7bff3803d

--- previewer/mock/ui_content.h
+++ previewer/mock/ui_content.h
@@ -104,19 +104,19 @@ public:
     virtual void SetAppWindowIcon(const std::shared_ptr<OHOS::Media::PixelMap>& pixelMap) = 0;
 
     // ArkTS Form
-    virtual std::shared_ptr<Rosen::RSSurfaceNode> GetCardRootNode()
-    {
-        return nullptr;
-    }
+    virtual std::shared_ptr<Rosen::RSSurfaceNode> GetFormRootNode() = 0;
 
-    virtual void ProcessFormUpdate(const std::string& data) = 0;
+    virtual void UpdateFormDate(const std::string& data) = 0;
+
     virtual void SetFormWidth(const float width) = 0;
     virtual void SetFormHeight(const float height) = 0;
     virtual float GetFormWidth() = 0;
     virtual float GetFormHeight() = 0;
 
     virtual void SetActionEventHandler(
-        std::function<void(const std::string& action)>&& actionCallback) = 0;
+        std::function<void(const std::string&)>&& actionCallback) = 0;
+    virtual void SetErrorEventHandler(
+        std::function<void(const std::string&, const std::string&)>&& errorCallback) = 0;
 };
 } // namespace OHOS::Ace
 
