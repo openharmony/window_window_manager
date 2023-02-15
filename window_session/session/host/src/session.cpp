0117746096df31ae3f399224c3caa49152dd5550
chyyy0213 <chenhaiying3@huawei.com>
2023-02-15 09:37:12 +0000

    change session framework
    Signed-off-by: chyyy0213 <chenhaiying3@huawei.com>
    Change-Id: I5f8672fc88e1b09d1baa3e9f4191720bf423c220

---
+++ window_session/session/host/src/session.cpp
@@ -0,0 +1,276 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "session.h"
+
+#include <ui/rs_surface_node.h>
+
+#include "window_manager_hilog.h"
+
+namespace OHOS::Rosen {
+namespace {
+    constexpr HiviewDFX::HiLogLabel LABEL = {LOG_CORE, HILOG_DOMAIN_WINDOW, "Session"};
+    const std::string UNDEFINED = "undefined";
+}
+
+Session::Session(const WindowSession::AbilityInfo& info) : abilityInfo_(info)
+{
+    surfaceNode_ = CreateSurfaceNode(info.bundleName_);
+    if (surfaceNode_ == nullptr) {
+        WLOGFE("create surface node failed");
+    }
+}
+
+void Session::SetPersistentId(uint32_t persistentId)
+{
+    persistentId_ = persistentId;
+}
+
+uint32_t Session::GetPersistentId() const
+{
+    return persistentId_;
+}
+
+std::shared_ptr<RSSurfaceNode> Session::GetSurfaceNode() const
+{
+    return surfaceNode_;
+}
+
+const WindowSession::AbilityInfo& Session::GetAbilityInfo() const
+{
+    return abilityInfo_;
+}
+
+bool Session::RegisterLifecycleListener(const std::shared_ptr<ILifecycleListener>& listener)
+{
+    return RegisterListenerLocked(lifecycleListeners_, listener);
+}
+
+bool Session::UnregisterLifecycleListener(const std::shared_ptr<ILifecycleListener>& listener)
+{
+    return RegisterListenerLocked(lifecycleListeners_, listener);
+}
+
+template<typename T>
+bool Session::RegisterListenerLocked(std::vector<std::shared_ptr<T>>& holder, const std::shared_ptr<T>& listener)
+{
+    if (listener == nullptr) {
+        WLOGFE("listener is nullptr");
+        return false;
+    }
+    std::lock_guard<std::recursive_mutex> lock(mutex_);
+    if (std::find(holder.begin(), holder.end(), listener) != holder.end()) {
+        WLOGFE("Listener already registered");
+        return true;
+    }
+    holder.emplace_back(listener);
+    return true;
+}
+
+template<typename T>
+bool Session::UnregisterListenerLocked(std::vector<std::shared_ptr<T>>& holder, const std::shared_ptr<T>& listener)
+{
+    if (listener == nullptr) {
+        WLOGFE("listener could not be null");
+        return false;
+    }
+    std::lock_guard<std::recursive_mutex> lock(mutex_);
+    holder.erase(std::remove_if(holder.begin(), holder.end(),
+        [listener](std::shared_ptr<T> registeredListener) {
+            return registeredListener == listener;
+        }), holder.end());
+    return true;
+}
+
+void Session::NotifyForeground()
+{
+    auto lifecycleListeners = GetListeners<ILifecycleListener>();
+    for (auto& listener : lifecycleListeners) {
+        if (!listener.expired()) {
+            listener.lock()->OnForeground();
+        }
+    }
+}
+
+void Session::NotifyBackground()
+{
+    auto lifecycleListeners = GetListeners<ILifecycleListener>();
+    for (auto& listener : lifecycleListeners) {
+        if (!listener.expired()) {
+            listener.lock()->OnBackground();
+        }
+    }
+}
+
+SessionState Session::GetSessionState() const
+{
+    return state_;
+}
+
+void Session::UpdateSessionState(SessionState state)
+{
+    state_ = state;
+}
+
+bool Session::IsSessionValid() const
+{
+    bool res = state_ > SessionState::STATE_DISCONNECT;
+    if (!res) {
+        WLOGFI("session is already destroyed or not created! id: %{public}u state: %{public}u",
+            GetPersistentId(), state_);
+    }
+    return res;
+}
+
+RSSurfaceNode::SharedPtr Session::CreateSurfaceNode(std::string name)
+{
+    if (name.empty()) {
+        WLOGFI("name is empty");
+        name = UNDEFINED + std::to_string(persistentId_++);
+    } else {
+        std::string surfaceNodeName = name + std::to_string(persistentId_++);
+        std::size_t pos = surfaceNodeName.find_last_of('.');
+        name = (pos == std::string::npos) ? surfaceNodeName : surfaceNodeName.substr(pos + 1); // skip '.'
+    }
+    struct RSSurfaceNodeConfig rsSurfaceNodeConfig;
+    rsSurfaceNodeConfig.SurfaceNodeName = name;
+    return RSSurfaceNode::Create(rsSurfaceNodeConfig);
+}
+
+WSError Session::UpdateRect(const WSRect& rect, SizeChangeReason reason)
+{
+    WLOGFI("session update rect: id: %{public}u, rect[%{public}d, %{public}d, %{public}u, %{public}u], "\
+        "reason:%{public}u", GetPersistentId(), rect.posX_, rect.posY_, rect.width_, rect.height_, reason);
+    if (!IsSessionValid()) {
+        winRect_ = rect;
+        return WSError::WS_ERROR_INVALID_SESSION;
+    }
+    sessionStage_->UpdateRect(rect, reason);
+    winRect_ = rect;
+    return WSError::WS_OK;
+}
+
+
+WSError Session::Connect(const sptr<ISessionStage>& sessionStage, const sptr<IWindowEventChannel>& eventChannel)
+{
+    WLOGFI("Connect session, id: %{public}u, state: %{public}u", GetPersistentId(),
+        static_cast<uint32_t>(GetSessionState()));
+    if (GetSessionState() != SessionState::STATE_DISCONNECT) {
+        return WSError::WS_ERROR_INVALID_SESSION;
+    }
+    sessionStage_ = sessionStage;
+    if (sessionStage_ == nullptr) {
+        WLOGFE("session stage is nullptr");
+        return WSError::WS_ERROR_NULLPTR;
+    }
+    windowEventChannel_ = eventChannel;
+    if (sessionStage_ == nullptr || windowEventChannel_ == nullptr) {
+        WLOGFE("session stage or eventChannel is nullptr");
+        return WSError::WS_ERROR_NULLPTR;
+    }
+    UpdateSessionState(SessionState::STATE_CONNECT);
+    // once update rect before connect, update again when connect
+    UpdateRect(winRect_, SizeChangeReason::SHOW);
+    return WSError::WS_OK;
+}
+
+WSError Session::Foreground()
+{
+    SessionState state = GetSessionState();
+    WLOGFI("Foreground session, id: %{public}u, state: %{public}u", GetPersistentId(),
+        static_cast<uint32_t>(state));
+    if (state != SessionState::STATE_CONNECT && state != SessionState::STATE_BACKGROUND) {
+        return WSError::WS_ERROR_INVALID_SESSION;
+    }
+    UpdateSessionState(SessionState::STATE_FOREGROUND);
+    if (!isActive_) {
+        sessionStage_->SetActive(true);
+        UpdateSessionState(SessionState::STATE_ACTIVE);
+    }
+    return WSError::WS_OK;
+}
+
+WSError Session::Background()
+{
+    SessionState state = GetSessionState();
+    WLOGFI("Background session, id: %{public}u, state: %{public}u", GetPersistentId(),
+        static_cast<uint32_t>(state));
+    if (state < SessionState::STATE_INACTIVE) {
+        return WSError::WS_ERROR_INVALID_SESSION;
+    }
+
+    UpdateSessionState(SessionState::STATE_BACKGROUND);
+    return WSError::WS_OK;
+}
+
+WSError Session::Disconnect()
+{
+    SessionState state = GetSessionState();
+    WLOGFI("Disconnect session, id: %{public}u, state: %{public}u", GetPersistentId(),
+        static_cast<uint32_t>(state));
+    Background();
+    if (GetSessionState() == SessionState::STATE_BACKGROUND) {
+        UpdateSessionState(SessionState::STATE_DISCONNECT);
+    }
+    return WSError::WS_OK;
+}
+
+WSError Session::SetActive(bool active)
+{
+    SessionState state = GetSessionState();
+    WLOGFI("Session update active: %{public}d, id: %{public}u, state: %{public}u", active, GetPersistentId(),
+        static_cast<uint32_t>(state));
+    if (!IsSessionValid()) {
+        return WSError::WS_ERROR_INVALID_SESSION;
+    }
+    if (active == isActive_) {
+        WLOGFI("Session active do not change: [%{public}d]", active);
+        return WSError::WS_DO_NOTHING;
+    }
+    isActive_ = active;
+    if (active && GetSessionState() == SessionState::STATE_FOREGROUND) {
+        sessionStage_->SetActive(true);
+        UpdateSessionState(SessionState::STATE_ACTIVE);
+    }
+    if (!active && GetSessionState() == SessionState::STATE_ACTIVE) {
+        sessionStage_->SetActive(false);
+        UpdateSessionState(SessionState::STATE_INACTIVE);
+    }
+    return WSError::WS_OK;
+}
+
+WSError Session::StartAbility(const WindowSession::AbilityInfo& info)
+{
+    if (startSceneFunc_) {
+        startSceneFunc_(info);
+    }
+    return WSError::WS_OK;
+}
+
+void Session::SetStartSceneEventListener(const NotifyStartSceneFunc& func)
+{
+    startSceneFunc_ = func;
+}
+
+WSError Session::Recover()
+{
+    return WSError::WS_OK;
+}
+
+WSError Session::Maximum()
+{
+    return WSError::WS_OK;
+}
+} // namespace OHOS::Rosen
---
+++ window_session/session_manager/src/scene_session_manager.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "scene_session_manager.h"
+
+#include <ability_manager_client.h>
+#include <want.h>
+#include <start_options.h>
+#include <ui_window.h>
+#include "scene_session.h"
+#include "scene_session_info.h"
+#include "window_manager_hilog.h"
+
+namespace OHOS::Rosen {
+namespace {
+    constexpr HiviewDFX::HiLogLabel LABEL = {LOG_CORE, HILOG_DOMAIN_WINDOW, "SceneSessionManager"};
+}
+
+WM_IMPLEMENT_SINGLE_INSTANCE(SceneSessionManager)
+
+sptr<RootSceneSession> SceneSessionManager::GetRootSceneSession()
+{
+    if (rootSceneSession_ != nullptr) {
+        return rootSceneSession_;
+    }
+
+    rootSceneSession_ = new RootSceneSession();
+    rootScene_ = Ace::NG::UIWindow::CreateRootScene();
+    rootSceneSession_->SetLoadContentFunc([rootScene = rootScene_](const std::string& contentUrl,
+        NativeEngine* engine, NativeValue* storage, AbilityRuntime::Context* context) {
+        rootScene->LoadContent(contentUrl, engine, storage, context);
+    });
+    return rootSceneSession_;
+}
+
+sptr<SceneSession> SceneSessionManager::RequestSceneSession(const WindowSession::AbilityInfo& abilityInfo)
+{
+    WLOGFI("abilityInfo: bundleName: %{public}s, abilityName: %{public}s", abilityInfo.bundleName_.c_str(),
+        abilityInfo.abilityName_.c_str());
+    sptr<SceneSession> sceneSession = new (std::nothrow) SceneSession(abilityInfo);
+    ++sessionId_;
+    uint32_t persistentId = pid_ + sessionId_;
+    sceneSession->SetPersistentId(persistentId);
+    std::lock_guard<std::recursive_mutex> lock(mutex_);
+    abilitySceneMap_.insert({persistentId, std::make_pair(sceneSession, nullptr)});
+    WLOGFI("create session persistentId: %{public}u", persistentId);
+    return sceneSession;
+}
+
+WSError SceneSessionManager::RequestSceneSessionActivation(const sptr<SceneSession>& sceneSession)
+{
+    if(sceneSession == nullptr) {
+        WLOGFE("session is nullptr");
+        return WSError::WS_ERROR_NULLPTR;
+    }
+    auto persistentId = sceneSession->GetPersistentId();
+    WLOGFI("active persistentId: %{public}u", persistentId);
+    if (abilitySceneMap_.count(persistentId) == 0) {
+        WLOGFE("session is invalid with %{public}u", persistentId);
+        return WSError::WS_ERROR_INVALID_SESSION;
+    }
+    AAFwk::Want want;
+    auto abilityInfo = sceneSession->GetAbilityInfo();
+    want.SetElementName(abilityInfo.bundleName_, abilityInfo.abilityName_);
+    AAFwk::StartOptions startOptions;
+    sptr<AAFwk::SceneSessionInfo> sceneSessionInfo = new (std::nothrow) AAFwk::SceneSessionInfo();
+    sceneSessionInfo->sceneSessionToken_ = sceneSession;
+    sceneSessionInfo->surfaceNode_ = sceneSession->GetSurfaceNode();
+    sceneSessionInfo->persistentId_ = sceneSession->GetPersistentId();
+    AAFwk::AbilityManagerClient::GetInstance()->StartAbilityByLauncher(want, startOptions, nullptr, sceneSessionInfo);
+    auto newAbilityToken = AAFwk::AbilityManagerClient::GetInstance()->GetTokenBySceneSession(
+        sceneSessionInfo->persistentId_);
+    if (newAbilityToken) {
+        WLOGFW("newAbilityToken is not null");
+    }
+    std::lock_guard<std::recursive_mutex> lock(mutex_);
+    // replace with real token after start ability
+    abilitySceneMap_[persistentId].second = newAbilityToken;
+    return WSError::WS_OK;
+}
+
+WSError SceneSessionManager::RequestSceneSessionBackground(const sptr<SceneSession>& sceneSession)
+{
+    if(sceneSession == nullptr) {
+        WLOGFE("session is invalid");
+        return WSError::WS_ERROR_NULLPTR;
+    }
+    auto persistentId = sceneSession->GetPersistentId();
+    WLOGFI("background session persistentId: %{public}u", persistentId);
+    sceneSession->Background();
+    std::lock_guard<std::recursive_mutex> lock(mutex_);
+    if (abilitySceneMap_.count(persistentId) == 0) {
+        WLOGFE("session is invalid with %{public}u", persistentId);
+        return WSError::WS_ERROR_INVALID_SESSION;
+    }
+    auto abilityToken = abilitySceneMap_[persistentId].second;
+    AAFwk::AbilityManagerClient::GetInstance()->MinimizeAbility(abilityToken);
+    return WSError::WS_OK;
+}
+
+WSError SceneSessionManager::RequestSceneSessionDestruction(const sptr<SceneSession>& sceneSession)
+{
+    if(sceneSession == nullptr) {
+        WLOGFE("session is invalid");
+        return WSError::WS_ERROR_NULLPTR;
+    }
+    auto persistentId = sceneSession->GetPersistentId();
+    WLOGFI("destroy session persistentId: %{public}u", persistentId);
+    sceneSession->Disconnect();
+    std::lock_guard<std::recursive_mutex> lock(mutex_);
+    if (abilitySceneMap_.count(persistentId) == 0) {
+        WLOGFE("session is invalid with %{public}u", persistentId);
+        return WSError::WS_ERROR_INVALID_SESSION;
+    }
+    auto abilityToken = abilitySceneMap_[persistentId].second;
+    AAFwk::Want resultWant;
+    AAFwk::AbilityManagerClient::GetInstance()->TerminateAbility(abilityToken, -1, &resultWant);
+    abilitySceneMap_.erase(persistentId);
+    return WSError::WS_OK;
+}
+} // namespace OHOS::Rosen
