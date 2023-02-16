db73a56203ef5e82123068fdf339ac1dc4b8d19a
chyyy0213 <chenhaiying3@huawei.com>
2023-02-16 14:35:34 +0000

    change window_session to window_scene
    Signed-off-by: chyyy0213 <chenhaiying3@huawei.com>
    Change-Id: I61d51dc10a9e3c5f67c847ddfb2c7aa1eea8c221

---
+++ window_scene/interfaces/kits/napi/screen_session_manager/js_screen_session_manager.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_WINDOW_SCENE_JS_SCREEN_SESSION_MANAGER_H
+#define OHOS_WINDOW_SCENE_JS_SCREEN_SESSION_MANAGER_H
+
+#include <native_engine/native_engine.h>
+#include <native_engine/native_value.h>
+#include <refbase.h>
+
+#include "session_manager/include/screen_session_manager.h"
+
+namespace OHOS::Rosen {
+class JsScreenSessionManager final : public IScreenConnectionListener {
+public:
+    JsScreenSessionManager(NativeEngine& engine);
+    ~JsScreenSessionManager();
+
+    static NativeValue* Init(NativeEngine* engine, NativeValue* exportObj);
+    static void Finalizer(NativeEngine* engine, void* data, void* hint);
+
+    void OnScreenConnect(sptr<ScreenSession>& screenSession) override;
+    void OnScreenDisconnect(sptr<ScreenSession>& screenSession) override;
+
+private:
+    static NativeValue* RegisterCallback(NativeEngine* engine, NativeCallbackInfo* info);
+
+    NativeValue* OnRegisterCallback(NativeEngine& engine, const NativeCallbackInfo& info);
+
+    std::shared_ptr<NativeReference> screenConnectionCallback_;
+    NativeEngine& engine_;
+};
+} // namespace OHOS::Rosen
+
+#endif // OHOS_WINDOW_SCENE_JS_SCREEN_SESSION_MANAGER_H
---
+++ window_scene/session/container/include/zidl/window_event_channel_stub.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_WINDOW_SCENE_WINDOW_EVENT_CHANNEL_STUB_H
+#define OHOS_WINDOW_SCENE_WINDOW_EVENT_CHANNEL_STUB_H
+
+#include <map>
+#include <iremote_stub.h>
+#include "session/container/include/zidl/window_event_channel_interface.h"
+
+namespace OHOS::Rosen {
+class WindowEventChannelStub;
+using WindowEventChannelStubFunc = int (WindowEventChannelStub::*)(MessageParcel& data, MessageParcel& reply);
+
+class WindowEventChannelStub : public IRemoteStub<IWindowEventChannel> {
+public:
+    WindowEventChannelStub() = default;
+    ~WindowEventChannelStub() = default;
+
+    virtual int OnRemoteRequest(uint32_t code, MessageParcel &data, MessageParcel &reply,
+        MessageOption &option) override;
+
+private:
+    static const std::map<uint32_t, WindowEventChannelStubFunc> stubFuncMap_;
+
+    int HandleTransferKeyEvent(MessageParcel& data, MessageParcel& reply);
+    int HandleTransferPointerEvent(MessageParcel& data, MessageParcel& reply);
+};
+}
+#endif // OHOS_WINDOW_SCENE_WINDOW_EVENT_CHANNEL_STUB_H
