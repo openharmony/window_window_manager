cd02d570b02f4dbaa2b942c0fd1f753715ee69e8
chyyy0213 <chenhaiying3@huawei.com>
2023-02-16 14:35:34 +0000

    change window_session to window_scene
    Signed-off-by: chyyy0213 <chenhaiying3@huawei.com>
    Change-Id: I61d51dc10a9e3c5f67c847ddfb2c7aa1eea8c221

---
+++ window_scene/session/container/src/zidl/session_stage_stub.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "session/container/include/zidl/session_stage_stub.h"
+
+#include <ipc_types.h>
+
+#include "window_manager_hilog.h"
+
+namespace OHOS::Rosen {
+namespace {
+    constexpr HiviewDFX::HiLogLabel LABEL = {LOG_CORE, HILOG_DOMAIN_WINDOW, "SessionStageStub"};
+}
+
+const std::map<uint32_t, SessionStageStubFunc> SessionStageStub::stubFuncMap_{
+    std::make_pair(static_cast<uint32_t>(SessionStageMessage::TRANS_ID_SET_ACTIVE),
+        &SessionStageStub::HandleSetActive),
+    std::make_pair(static_cast<uint32_t>(SessionStageMessage::TRANS_ID_NOTIFY_SIZE_CHANGE),
+        &SessionStageStub::HandleUpdateRect),
+};
+
+int SessionStageStub::OnRemoteRequest(uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option)
+{
+    WLOGFD("Scene session stage on remote request!, code: %{public}u", code);
+    if (data.ReadInterfaceToken() != GetDescriptor()) {
+        WLOGFE("Failed to check interface token!");
+        return ERR_INVALID_STATE;
+    }
+
+    const auto func = stubFuncMap_.find(code);
+    if (func == stubFuncMap_.end()) {
+        WLOGFE("Failed to find function handler!");
+        return ERR_UNKNOWN_TRANSACTION;
+    }
+
+    return (this->*(func->second))(data, reply);
+}
+
+int SessionStageStub::HandleSetActive(MessageParcel& data, MessageParcel& reply)
+{
+    WLOGFD("SetActive!");
+    bool active = data.ReadBool();
+    WSError errCode = SetActive(active);
+    reply.WriteUint32(static_cast<uint32_t>(errCode));
+    return ERR_NONE;
+}
+
+int SessionStageStub::HandleUpdateRect(MessageParcel& data, MessageParcel& reply)
+{
+    WLOGFD("UpdateRect!");
+    WSRect rect = { data.ReadInt32(), data.ReadInt32(), data.ReadUint32(), data.ReadUint32() };
+    SizeChangeReason reason = static_cast<SizeChangeReason>(data.ReadUint32());
+    WSError errCode = UpdateRect(rect, reason);
+    reply.WriteUint32(static_cast<uint32_t>(errCode));
+    return ERR_NONE;
+}
+} // namespace OHOS::Rosen
