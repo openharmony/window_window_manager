cfe216335ebe604f5c6175a858feed414ca3aca5
z00514981 <zhengjiangliang@huawei.com>
2023-04-03 15:52:57 +0800

    组件自动避让适配
    Signed-off-by: z00514981 <zhengjiangliang@huawei.com>
    Change-Id: I72594ca16aa63c587e8cd1b43fd365948e38d8ac

--- previewer/mock/parcel.h
+++ previewer/mock/parcel.h
@@ -19,6 +19,7 @@
 #include <string>
 #include <vector>
 #include "refbase.h"
+#include "context.h"
 
 namespace OHOS {
 class Parcel;
--- wm/src/window.cpp
+++ wm/src/window.cpp
@@ -38,6 +38,14 @@ sptr<Window> Window::Create(const std::string& windowName, sptr<WindowOption>& o
             return nullptr;
         }
     }
+    uint32_t version = 0;
+    if ((context != nullptr) && (context->GetApplicationInfo() != nullptr)) {
+        version = context->GetApplicationInfo()->apiCompatibleVersion;
+    }
+    // 10 ArkUI新框架适用版本API10
+    if (version >= 10) {
+        option->RemoveWindowFlag(WindowFlag::WINDOW_FLAG_NEED_AVOID);
+    }
     WindowType type = option->GetWindowType();
     if (!(WindowHelper::IsAppWindow(type) || WindowHelper::IsSystemWindow(type))) {
         WLOGFE("window type is invalid %{public}d", type);
