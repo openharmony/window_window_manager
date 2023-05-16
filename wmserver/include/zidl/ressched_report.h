45a64b438b55ec862ef57e81ae062153d88e756f
cuijiawei14@huawei.com <cuijiawei14@huawei.com>
2023-05-16 14:39:46 +0800

    解决wmserver TDD崩溃问题
    Signed-off-by: cuijiawei14@huawei.com <cuijiawei14@huawei.com>

--- wmserver/include/zidl/ressched_report.h
+++ wmserver/include/zidl/ressched_report.h
@@ -19,26 +19,29 @@
 #include <cstdint>
 #include <string>
 #include <unordered_map>
+#include "res_sched_client.h"
 
 namespace OHOS::Rosen {
 constexpr uint32_t RES_TYPE_SHOW_REMOTE_ANIMATION = 33;
 constexpr int32_t REMOTE_ANIMATION_BEGIN = 0;
 constexpr int32_t REMOTE_ANIMATION_END = 1;
-
-using ReportDataFunc = void (*)(uint32_t resType, int64_t value,
-    const std::unordered_map<std::string, std::string>& payload);
-
-ReportDataFunc __attribute__((visibility("default"))) LoadReportDataFunc();
-
-class __attribute__((visibility("default"))) ResSchedReport final {
+class ResSchedReport final {
 public:
-    static ResSchedReport& GetInstance();
+    static ResSchedReport& GetInstance()
+    {
+        static ResSchedReport instance;
+        return instance;
+    }
     void ResSchedDataReport(uint32_t resType, int32_t value = 0,
-        const std::unordered_map<std::string, std::string>& payload = {});
+        const std::unordered_map<std::string, std::string>& payload = {})
+    {
+#ifdef RESOURCE_SCHEDULE_SERVICE_ENABLE
+        OHOS::ResourceSchedule::ResSchedClient::GetInstance().ReportData(resType, value, payload);
+#endif
+    }
 private:
     ResSchedReport() {}
     ~ResSchedReport() {}
-    ReportDataFunc reportDataFunc_ = nullptr;
 };
 } // namespace OHOS::Rosen
 #endif // OHOS_WINDOW_MANAGER_RESSCHED_REPORT_H
