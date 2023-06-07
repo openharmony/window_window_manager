847d2ca05362edf83427f914fb1d9ebb3493d4c6
zhangkai <zhangkai324@huawei.com>
2023-06-07 15:17:53 +0800

    reset ColorGamut with GraphicColorGamut
    Signed-off-by: zhangkai <zhangkai324@huawei.com>
    Change-Id: Icebb4ea1ada497009a934456494fb6bc0d297847

--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -50,8 +50,8 @@ namespace {
 WM_IMPLEMENT_SINGLE_INSTANCE(ResSchedReport);
 
 const WindowImpl::ColorSpaceConvertMap WindowImpl::colorSpaceConvertMap[] = {
-    { ColorSpace::COLOR_SPACE_DEFAULT, ColorGamut::COLOR_GAMUT_SRGB },
-    { ColorSpace::COLOR_SPACE_WIDE_GAMUT, ColorGamut::COLOR_GAMUT_DCI_P3 },
+    { ColorSpace::COLOR_SPACE_DEFAULT, GraphicColorGamut::GRAPHIC_COLOR_GAMUT_SRGB },
+    { ColorSpace::COLOR_SPACE_WIDE_GAMUT, GraphicColorGamut::GRAPHIC_COLOR_GAMUT_DCI_P3 },
 };
 
 std::map<std::string, std::pair<uint32_t, sptr<Window>>> WindowImpl::windowMap_;
@@ -579,7 +579,7 @@ std::string WindowImpl::GetContentInfo()
     return uiContent_->GetContentInfo();
 }
 
-ColorSpace WindowImpl::GetColorSpaceFromSurfaceGamut(ColorGamut colorGamut)
+ColorSpace WindowImpl::GetColorSpaceFromSurfaceGamut(GraphicColorGamut colorGamut)
 {
     for (auto item: colorSpaceConvertMap) {
         if (item.surfaceColorGamut == colorGamut) {
@@ -589,14 +589,14 @@ ColorSpace WindowImpl::GetColorSpaceFromSurfaceGamut(ColorGamut colorGamut)
     return ColorSpace::COLOR_SPACE_DEFAULT;
 }
 
-ColorGamut WindowImpl::GetSurfaceGamutFromColorSpace(ColorSpace colorSpace)
+GraphicColorGamut WindowImpl::GetSurfaceGamutFromColorSpace(ColorSpace colorSpace)
 {
     for (auto item: colorSpaceConvertMap) {
         if (item.colorSpace == colorSpace) {
             return item.surfaceColorGamut;
         }
     }
-    return ColorGamut::COLOR_GAMUT_SRGB;
+    return GraphicColorGamut::GRAPHIC_COLOR_GAMUT_SRGB;
 }
 
 bool WindowImpl::IsSupportWideGamut()
