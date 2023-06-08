8e67242163d80048f8a314c157efddfef716de8a
zhaoxiaoyu <zhaoxiaoyu7@huawei.com>
2023-06-08 14:23:39 +0800

    remove listenr from uicontent
    Signed-off-by: zhaoxiaoyu <zhaoxiaoyu7@huawei.com>
    Change-Id: I6dab3e1830b586afe3d5c4d46be27e39db20d387

--- test/common/mock/mock_uicontent.h
+++ test/common/mock/mock_uicontent.h
@@ -41,8 +41,8 @@ public:
     MOCK_METHOD1(ProcessAxisEvent, bool(const std::shared_ptr<OHOS::MMI::AxisEvent>& axisEvent));
     MOCK_METHOD1(ProcessVsyncEvent, bool(uint64_t timeStampNanos));
     MOCK_METHOD1(UpdateConfiguration, void(const std::shared_ptr<OHOS::AppExecFwk::Configuration>& config));
-    MOCK_METHOD4(UpdateViewportConfig, void(const ViewportConfig& config, OHOS::Rosen::WindowSizeChangeReason reason,
-        const std::function<void()>& listener, const std::shared_ptr<OHOS::Rosen::RSTransaction>& rsTransaction));
+    MOCK_METHOD3(UpdateViewportConfig, void(const ViewportConfig& config, OHOS::Rosen::WindowSizeChangeReason reason,
+        const std::shared_ptr<OHOS::Rosen::RSTransaction>& rsTransaction));
     MOCK_METHOD2(UpdateWindowMode, void(OHOS::Rosen::WindowMode mode, bool hasDeco));
     MOCK_METHOD3(HideWindowTitleButton, void(bool hideSplit, bool hideMaximize, bool hideMinimize));
     MOCK_METHOD0(GetBackgroundColor, uint32_t());
--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -2268,12 +2268,6 @@ void WindowImpl::UpdateRect(const struct Rect& rect, bool decoStatus, WindowSize
         }
     }
     auto task = [this, reason, rsTransaction, rectToAce, lastOriRect, display]() mutable {
-        auto sizeChange = [this, reason, rsTransaction, rectToAce, lastOriRect]() mutable {
-            if ((rectToAce != lastOriRect) || (reason != lastSizeChangeReason_)) {
-                NotifySizeChange(rectToAce, reason, rsTransaction);
-                lastSizeChangeReason_ = reason;
-            }
-        };
         if (rsTransaction) {
             RSTransaction::FlushImplicitTransaction();
             rsTransaction->Begin();
@@ -2282,22 +2276,29 @@ void WindowImpl::UpdateRect(const struct Rect& rect, bool decoStatus, WindowSize
         protocol.SetDuration(600);
         auto curve = RSAnimationTimingCurve::CreateCubicCurve(0.2, 0.0, 0.2, 1.0);
         RSNode::OpenImplicitAnimation(protocol, curve);
-        UpdateViewportConfig(rectToAce, display, reason, sizeChange, rsTransaction);
-        RSNode::CloseImplicitAnimation();
-        if (rsTransaction) {
-            rsTransaction->Commit();
-        }
-    };
-    ResSchedReport::GetInstance().RequestPerfIfNeed(reason, GetType(), GetMode());
-    handler_ = std::make_shared<AppExecFwk::EventHandler>(AppExecFwk::EventRunner::GetMainEventRunner());
-    if (handler_ != nullptr && reason == WindowSizeChangeReason::ROTATION) {
-        handler_->PostTask(task);
-    } else {
         if ((rectToAce != lastOriRect) || (reason != lastSizeChangeReason_)) {
             NotifySizeChange(rectToAce, reason, rsTransaction);
             lastSizeChangeReason_ = reason;
         }
-        UpdateViewportConfig(rectToAce, display, reason, nullptr, rsTransaction);
+        UpdateViewportConfig(rectToAce, display, reason, rsTransaction);
+        RSNode::CloseImplicitAnimation();
+        if (rsTransaction) {
+            rsTransaction->Commit();
+        }
+        postTaskDone_ = true;
+    };
+    ResSchedReport::GetInstance().RequestPerfIfNeed(reason, GetType(), GetMode());
+    handler_ = std::make_shared<AppExecFwk::EventHandler>(AppExecFwk::EventRunner::GetMainEventRunner());
+    if (handler_ != nullptr && reason == WindowSizeChangeReason::ROTATION) {
+        postTaskDone_ = false;
+        handler_->PostTask(task);
+    } else {
+        if ((rectToAce != lastOriRect) || (reason != lastSizeChangeReason_) || !postTaskDone_) {
+            NotifySizeChange(rectToAce, reason, rsTransaction);
+            lastSizeChangeReason_ = reason;
+            postTaskDone_ = true;
+        }
+        UpdateViewportConfig(rectToAce, display, reason, rsTransaction);
     }
 }
 
@@ -2872,13 +2873,10 @@ void WindowImpl::UpdateAvoidArea(const sptr<AvoidArea>& avoidArea, AvoidAreaType
 }
 
 void WindowImpl::UpdateViewportConfig(const Rect& rect, const sptr<Display>& display, WindowSizeChangeReason reason,
-    const std::function<void()>& listener, const std::shared_ptr<RSTransaction>& rsTransaction)
+    const std::shared_ptr<RSTransaction>& rsTransaction)
 {
     std::lock_guard<std::recursive_mutex> lock(mutex_);
     if (uiContent_ == nullptr) {
-        if (listener) {
-            listener();
-        }
         return;
     }
     Ace::ViewportConfig config;
@@ -2887,7 +2885,7 @@ void WindowImpl::UpdateViewportConfig(const Rect& rect, const sptr<Display>& dis
     if (display) {
         config.SetDensity(display->GetVirtualPixelRatio());
     }
-    uiContent_->UpdateViewportConfig(config, reason, listener, rsTransaction);
+    uiContent_->UpdateViewportConfig(config, reason, rsTransaction);
     WLOGFD("Id:%{public}u, windowRect:[%{public}d, %{public}d, %{public}u, %{public}u]",
         property_->GetWindowId(), rect.posX_, rect.posY_, rect.width_, rect.height_);
 }
