9bd24a62f759890cb1ca1b24daba53e106e27bba
mdquan <mdquan099@163.com>
2023-06-09 11:15:49 +0800

    supply interface for ams
    Signed-off-by: mdquan <mdquan099@163.com>
    Change-Id: I750785cc00eef0456280407a6a810624c4eeb17d

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
@@ -162,7 +162,7 @@ void JsSceneSession::ProcessSessionEventRegister()
 
 void JsSceneSession::ProcessTerminateSessionRegister()
 {
-    WLOGFD("mdquan begin to run ProcessTerminateSessionRegister");
+    WLOGFD("begin to run ProcessTerminateSessionRegister");
     NotifyTerminateSessionFunc func = [this](const SessionInfo& info) {
         this->TerminateSession(info);
     };
@@ -447,7 +447,7 @@ void JsSceneSession::OnBackPressed()
 
 void JsSceneSession::TerminateSession(const SessionInfo& info)
 {
-    WLOGFI("[NAPI]mdquan run TerminateSession, bundleName = %{public}s, id = %{public}s",
+    WLOGFI("[NAPI]run TerminateSession, bundleName = %{public}s, id = %{public}s",
         info.bundleName_.c_str(), info.abilityName_.c_str());
     auto iter = jsCbMap_.find(TERMINATE_SESSION_CB);
     if (iter == jsCbMap_.end()) {
