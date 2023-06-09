922a43a017d080acfaacbb7f89d64ca001a15f7a
y30046716 <yaodixiong@huawei.com>
2023-06-09 09:47:32 +0800

    add connect to sceneboard 10 times
    Signed-off-by: y30046716 <yaodixiong@huawei.com>
    Change-Id: I6221109766f21ee63e59d1d3df40d7f7cf41c238

--- window_scene/session_manager/src/session_manager.cpp
+++ window_scene/session_manager/src/session_manager.cpp
@@ -29,6 +29,8 @@
 namespace OHOS::Rosen {
 namespace {
 constexpr HiviewDFX::HiLogLabel LABEL = {LOG_CORE, HILOG_DOMAIN_DISPLAY, "SessionManager"};
+const int CONNECT_COUNTS = 10;
+const int SLEEP_10MS = 10000;
 }
 
 WM_IMPLEMENT_SINGLE_INSTANCE(SessionManager)
@@ -118,9 +120,17 @@ void SessionManager::ConnectToService()
     AAFwk::Want want;
     want.SetElementName("com.ohos.sceneboard", "com.ohos.sceneboard.MainAbility");
     ErrCode ret = AAFwk::AbilityManagerClient::GetInstance()->ConnectAbility(want, abilityConnection_, nullptr);
-    if (ret != ERR_OK) {
-        WLOGFE("ConnectToService failed, errorcode: %{public}d", ret);
+
+    for (uint8_t count = 0; count < CONNECT_COUNTS; count++) {
+        if (ret == ERR_OK) {
+            break;
+        } else {
+            WLOGFE("ConnectToService failed, errorcode: %{public}d", ret);
+            ret = AAFwk::AbilityManagerClient::GetInstance()->ConnectAbility(want, abilityConnection_, nullptr);
+        }
+        usleep(SLEEP_10MS);
     }
+
     serviceConnected_ = (ret == ERR_OK);
 }
 
