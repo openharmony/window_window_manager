d4d584eb2447155f00f2d6f22c597d61fc391644
胡凯 <mail.hk@163.com>
2023-06-13 08:18:19 +0000

    update interfaces/innerkits/extension/window_extension_connection.h.
    Signed-off-by: 胡凯 <mail.hk@163.com>

--- interfaces/innerkits/extension/window_extension_connection.h
+++ interfaces/innerkits/extension/window_extension_connection.h
@@ -24,7 +24,6 @@
 #include <pointer_event.h>
 #include <memory>
 
-#include "session/host/include/extension_session.h"
 #include "wm_common.h"
 
 namespace OHOS {
@@ -38,6 +37,7 @@ namespace {
 }
 
 class RSSurfaceNode;
+class ExtensionSession;
 /**
  * @class IWindowExtensionCallback
  *
@@ -100,7 +100,7 @@ public:
      */
     int ConnectExtension(const AppExecFwk::ElementName& element, const Rect& rect,
         uint32_t uid, uint32_t windowId, const sptr<IWindowExtensionCallback>& callback,
-        const sptr<ExtensionSession>& extensionSession = nullptr) const;
+        const sptr<ExtensionSession>& extensionSession) const;
     /**
      * @brief Disconnect window extension.
      */
