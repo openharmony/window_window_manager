5ad5d3e8212f0716529b4be29ff084f94c860443
qpzeng <zengqingpeng6@huawei.com>
2023-06-19 15:44:29 +0800

    optimize format
    Signed-off-by: qpzeng <zengqingpeng6@huawei.com>
    Change-Id: I7da6d697e0bb08d1881f98b1ae3cf280c385dfdd

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -755,8 +755,8 @@ WSError WindowSessionImpl::NotifyDestroy()
 {
     auto dialogDeathRecipientListener = GetListeners<IDialogDeathRecipientListener>();
     for (auto& listener : dialogDeathRecipientListener) {
-        if (listener.GetRefPtr() != nullptr) {
-            listener.GetRefPtr()->OnDialogDeathRecipient();
+        if (listener != nullptr) {
+            listener->OnDialogDeathRecipient();
         }
     }
     // destroy dialog in client
@@ -768,8 +768,8 @@ void WindowSessionImpl::NotifyTouchDialogTarget()
 {
     auto dialogTargetTouchListener = GetListeners<IDialogTargetTouchListener>();
     for (auto& listener : dialogTargetTouchListener) {
-        if (listener.GetRefPtr() != nullptr) {
-            listener.GetRefPtr()->OnDialogTargetTouch();
+        if (listener != nullptr) {
+            listener->OnDialogTargetTouch();
         }
     }
 }
@@ -778,8 +778,8 @@ void WindowSessionImpl::NotifySizeChange(Rect rect, WindowSizeChangeReason reaso
 {
     auto windowChangeListeners = GetListeners<IWindowChangeListener>();
     for (auto& listener : windowChangeListeners) {
-        if (listener.GetRefPtr() != nullptr) {
-            listener.GetRefPtr()->OnSizeChange(rect, reason);
+        if (listener != nullptr) {
+            listener->OnSizeChange(rect, reason);
         }
     }
 }
