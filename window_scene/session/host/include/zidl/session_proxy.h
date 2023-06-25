clipsheep6/window_manager

合一服务窗口沉浸式适配
Signed-off-by: z00514981 <zhengjiangliang@huawei.com>
Change-Id: Iff58e485f1aa43ea684d7ce90c1c61062016811f

window_scene/session/host/include/zidl/session_proxy.h
@@ -43,6 +43,8 @@ public:
         const sptr<IWindowEventChannel>& eventChannel, const std::shared_ptr<RSSurfaceNode>& surfaceNode,
         sptr<WindowSessionProperty> property, uint64_t& persistentId, sptr<ISession>& session) override;
     WSError DestroyAndDisconnectSpecificSession(const uint64_t& persistentId) override;
+    WSError OnLayoutFullScreen(bool status) override;
+    AvoidArea GetAvoidAreaByType(AvoidAreaType type) override;
     WSError RequestSessionBack() override;
 private:
     static inline BrokerDelegator<SessionProxy> delegator_;

window_scene/session/host/include/zidl/session_stub.h
@@ -50,6 +50,8 @@ private:
     int HandleDestroyAndDisconnectSpecificSession(MessageParcel& data, MessageParcel& reply);
     int HandleRaiseToAppTop(MessageParcel& data, MessageParcel& reply);
     int HandleBackPressed(MessageParcel& data, MessageParcel& reply);
+    int HandleLayoutFullScreen(MessageParcel& data, MessageParcel& reply);
+    int HandleGetAvoidAreaByType(MessageParcel& data, MessageParcel& reply);
 };
 } // namespace OHOS::Rosen
 #endif // OHOS_ROSEN_WINDOW_SCENE_SESSION_STUB_H

window_scene/session/host/src/zidl/session_stub.cpp
@@ -45,7 +45,11 @@ const std::map<uint32_t, SessionStubFunc> SessionStub::stubFuncMap_{
         &SessionStub::HandleDestroyAndDisconnectSpecificSession),
     std::make_pair(static_cast<uint32_t>(SessionMessage::TRANS_ID_RAISE_TO_APP_TOP),
         &SessionStub::HandleRaiseToAppTop),
-    std::make_pair(static_cast<uint32_t>(SessionMessage::TRANS_ID_BACKPRESSED), &SessionStub::HandleBackPressed)
+    std::make_pair(static_cast<uint32_t>(SessionMessage::TRANS_ID_BACKPRESSED), &SessionStub::HandleBackPressed),
+    std::make_pair(static_cast<uint32_t>(SessionMessage::TRANS_ID_LAYOUT_FULL_SCREEN),
+        &SessionStub::HandleLayoutFullScreen),
+    std::make_pair(static_cast<uint32_t>(SessionMessage::TRANS_ID_GET_AVOID_AREA),
+        &SessionStub::HandleGetAvoidAreaByType),
 };
 
 int SessionStub::OnRemoteRequest(uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option)
@@ -260,6 +264,24 @@ int SessionStub::HandleBackPressed(MessageParcel& data, MessageParcel& reply)
     WLOGFD("HandleBackPressed!");
     WSError errCode = RequestSessionBack();
     reply.WriteUint32(static_cast<uint32_t>(errCode));
+    return ERR_NONE;
+}
+
+int SessionStub::HandleLayoutFullScreen(MessageParcel& data, MessageParcel& reply)
+{
+    bool status = static_cast<bool>(data.ReadUint32());
+    WLOGFD("HandleLayoutFullScreen status:%{public}d", static_cast<int32_t>(status));
+    WSError errCode = OnLayoutFullScreen(status);
+    reply.WriteUint32(static_cast<uint32_t>(errCode));
+    return ERR_NONE;
+}
+
+int SessionStub::HandleGetAvoidAreaByType(MessageParcel& data, MessageParcel& reply)
+{
+    AvoidAreaType type = static_cast<AvoidAreaType>(data.ReadUint32());
+    WLOGFD("HandleGetAvoidArea type:%{public}d", static_cast<int32_t>(type));
+    AvoidArea avoidArea = GetAvoidAreaByType(type);
+    reply.WriteParcelable(&avoidArea);
     return ERR_NONE;
 }
 } // namespace OHOS::Rosen

wm/include/window_session_impl.h
@@ -78,6 +78,8 @@ public:
     WMError UnregisterLifeCycleListener(const sptr<IWindowLifeCycle>& listener) override;
     WMError RegisterWindowChangeListener(const sptr<IWindowChangeListener>& listener) override;
     WMError UnregisterWindowChangeListener(const sptr<IWindowChangeListener>& listener) override;
+    WMError RegisterAvoidAreaChangeListener(sptr<IAvoidAreaChangedListener>& listener) override;
+    WMError UnregisterAvoidAreaChangeListener(sptr<IAvoidAreaChangedListener>& listener) override;
     void RegisterDialogDeathRecipientListener(const sptr<IDialogDeathRecipientListener>& listener) override;
     void UnregisterDialogDeathRecipientListener(const sptr<IDialogDeathRecipientListener>& listener) override;
     WMError RegisterDialogTargetTouchListener(const sptr<IDialogTargetTouchListener>& listener) override;
@@ -93,6 +95,7 @@ public:
     void NotifyAfterBackground(bool needNotifyListeners = true, bool needNotifyUiContent = true);
     void NotifyForegroundFailed(WMError ret);
     WSError NotifyDestroy() override;
+    void NotifyAvoidAreaChange(const sptr<AvoidArea>& avoidArea, AvoidAreaType type);
     void NotifyTouchDialogTarget() override;
 
     WindowState state_ { WindowState::STATE_INITIAL };
@@ -117,6 +120,7 @@ protected:
     WMError WindowSessionCreateCheck();
     void UpdateDecorEnable(bool needNotify = false);
     void NotifyModeChange(WindowMode mode, bool hasDeco = true);
+    WMError UpdateProperty(WSPropertyChangeAction action);
 
     sptr<ISession> hostSession_;
     std::unique_ptr<Ace::UIContent> uiContent_;
@@ -133,12 +137,17 @@ protected:
     static std::map<std::string, std::pair<uint64_t, sptr<WindowSessionImpl>>> windowSessionMap_;
     static std::map<uint64_t, std::vector<sptr<WindowSessionImpl>>> subWindowSessionMap_;
 
+    bool isIgnoreSafeAreaNeedNotify_ = false;
+    bool isIgnoreSafeArea_ = false;
+
 private:
     template<typename T> WMError RegisterListener(std::vector<sptr<T>>& holder, const sptr<T>& listener);
     template<typename T> WMError UnregisterListener(std::vector<sptr<T>>& holder, const sptr<T>& listener);
     template<typename T> EnableIfSame<T, IWindowLifeCycle, std::vector<sptr<IWindowLifeCycle>>> GetListeners();
     template<typename T>
     EnableIfSame<T, IWindowChangeListener, std::vector<sptr<IWindowChangeListener>>> GetListeners();
+    template<typename T>
+    EnableIfSame<T, IAvoidAreaChangedListener, std::vector<sptr<IAvoidAreaChangedListener>>> GetListeners();
     template<typename T>
     EnableIfSame<T, IDialogDeathRecipientListener, std::vector<sptr<IDialogDeathRecipientListener>>> GetListeners();
     template<typename T>
@@ -150,11 +159,11 @@ private:
     void UpdateViewportConfig(const Rect& rect, WindowSizeChangeReason reason);
     void NotifySizeChange(Rect rect, WindowSizeChangeReason reason);
     WMError CheckParmAndPermission();
-    WMError UpdateProperty(WSPropertyChangeAction action);
 
     static std::recursive_mutex globalMutex_;
     static std::map<uint64_t, std::vector<sptr<IWindowLifeCycle>>> lifecycleListeners_;
     static std::map<uint64_t, std::vector<sptr<IWindowChangeListener>>> windowChangeListeners_;
+    static std::map<uint32_t, std::vector<sptr<IAvoidAreaChangedListener>>> avoidAreaChangeListeners_;
     static std::map<uint64_t, std::vector<sptr<IDialogDeathRecipientListener>>> dialogDeathRecipientListeners_;
     static std::map<uint64_t, std::vector<sptr<IDialogTargetTouchListener>>> dialogTargetTouchListener_;
 };

openharmony/window_window_manager

window_scene/session/host/include/zidl/session_proxy.h
@@ -43,6 +43,8 @@ public:
         const sptr<IWindowEventChannel>& eventChannel, const std::shared_ptr<RSSurfaceNode>& surfaceNode,
         sptr<WindowSessionProperty> property, uint64_t& persistentId, sptr<ISession>& session) override;
     WSError DestroyAndDisconnectSpecificSession(const uint64_t& persistentId) override;
+    WSError OnNeedAvoid(bool status) override;
+    AvoidArea GetAvoidAreaByType(AvoidAreaType type) override;
     WSError RequestSessionBack() override;
     WSError SetGlobalMaximizeMode(MaximizeMode mode) override;
     WSError GetGlobalMaximizeMode(MaximizeMode& mode) override;

window_scene/session/host/include/zidl/session_stub.h
@@ -52,6 +52,8 @@ private:
     int HandleBackPressed(MessageParcel& data, MessageParcel& reply);
     int HandleSetGlobalMaximizeMode(MessageParcel& data, MessageParcel& reply);
     int HandleGetGlobalMaximizeMode(MessageParcel& data, MessageParcel& reply);
+    int HandleNeedAvoid(MessageParcel& data, MessageParcel& reply);
+    int HandleGetAvoidAreaByType(MessageParcel& data, MessageParcel& reply);
 };
 } // namespace OHOS::Rosen
 #endif // OHOS_ROSEN_WINDOW_SCENE_SESSION_STUB_H

window_scene/session/host/src/zidl/session_stub.cpp
@@ -49,7 +49,11 @@ const std::map<uint32_t, SessionStubFunc> SessionStub::stubFuncMap_{
     std::make_pair(static_cast<uint32_t>(SessionMessage::TRANS_ID_SET_MAXIMIZE_MODE),
         &SessionStub::HandleSetGlobalMaximizeMode),
     std::make_pair(static_cast<uint32_t>(SessionMessage::TRANS_ID_GET_MAXIMIZE_MODE),
-        &SessionStub::HandleGetGlobalMaximizeMode)
+        &SessionStub::HandleGetGlobalMaximizeMode),
+    std::make_pair(static_cast<uint32_t>(SessionMessage::TRANS_ID_NEED_AVOID),
+        &SessionStub::HandleNeedAvoid),
+    std::make_pair(static_cast<uint32_t>(SessionMessage::TRANS_ID_GET_AVOID_AREA),
+        &SessionStub::HandleGetAvoidAreaByType),
 };
 
 int SessionStub::OnRemoteRequest(uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option)
@@ -283,6 +287,24 @@ int SessionStub::HandleGetGlobalMaximizeMode(MessageParcel &data, MessageParcel 
     WSError errCode = GetGlobalMaximizeMode(mode);
     reply.WriteUint32(static_cast<uint32_t>(mode));
     reply.WriteUint32(static_cast<uint32_t>(errCode));
+    return ERR_NONE;
+}
+
+int SessionStub::HandleNeedAvoid(MessageParcel& data, MessageParcel& reply)
+{
+    bool status = static_cast<bool>(data.ReadUint32());
+    WLOGFD("HandleNeedAvoid status:%{public}d", static_cast<int32_t>(status));
+    WSError errCode = OnNeedAvoid(status);
+    reply.WriteUint32(static_cast<uint32_t>(errCode));
+    return ERR_NONE;
+}
+
+int SessionStub::HandleGetAvoidAreaByType(MessageParcel& data, MessageParcel& reply)
+{
+    AvoidAreaType type = static_cast<AvoidAreaType>(data.ReadUint32());
+    WLOGFD("HandleGetAvoidArea type:%{public}d", static_cast<int32_t>(type));
+    AvoidArea avoidArea = GetAvoidAreaByType(type);
+    reply.WriteParcelable(&avoidArea);
     return ERR_NONE;
 }
 } // namespace OHOS::Rosen

wm/include/window_session_impl.h
@@ -80,6 +80,8 @@ public:
     WMError UnregisterLifeCycleListener(const sptr<IWindowLifeCycle>& listener) override;
     WMError RegisterWindowChangeListener(const sptr<IWindowChangeListener>& listener) override;
     WMError UnregisterWindowChangeListener(const sptr<IWindowChangeListener>& listener) override;
+    WMError RegisterAvoidAreaChangeListener(sptr<IAvoidAreaChangedListener>& listener) override;
+    WMError UnregisterAvoidAreaChangeListener(sptr<IAvoidAreaChangedListener>& listener) override;
     void RegisterDialogDeathRecipientListener(const sptr<IDialogDeathRecipientListener>& listener) override;
     void UnregisterDialogDeathRecipientListener(const sptr<IDialogDeathRecipientListener>& listener) override;
     WMError RegisterDialogTargetTouchListener(const sptr<IDialogTargetTouchListener>& listener) override;
@@ -95,6 +97,7 @@ public:
     void NotifyAfterBackground(bool needNotifyListeners = true, bool needNotifyUiContent = true);
     void NotifyForegroundFailed(WMError ret);
     WSError NotifyDestroy() override;
+    void NotifyAvoidAreaChange(const sptr<AvoidArea>& avoidArea, AvoidAreaType type);
     void NotifyTouchDialogTarget() override;
 
     WindowState state_ { WindowState::STATE_INITIAL };
@@ -125,12 +128,17 @@ protected:
     static std::map<std::string, std::pair<uint64_t, sptr<WindowSessionImpl>>> windowSessionMap_;
     static std::map<uint64_t, std::vector<sptr<WindowSessionImpl>>> subWindowSessionMap_;
 
+    bool isIgnoreSafeAreaNeedNotify_ = false;
+    bool isIgnoreSafeArea_ = false;
+
 private:
     template<typename T> WMError RegisterListener(std::vector<sptr<T>>& holder, const sptr<T>& listener);
     template<typename T> WMError UnregisterListener(std::vector<sptr<T>>& holder, const sptr<T>& listener);
     template<typename T> EnableIfSame<T, IWindowLifeCycle, std::vector<sptr<IWindowLifeCycle>>> GetListeners();
     template<typename T>
     EnableIfSame<T, IWindowChangeListener, std::vector<sptr<IWindowChangeListener>>> GetListeners();
+    template<typename T>
+    EnableIfSame<T, IAvoidAreaChangedListener, std::vector<sptr<IAvoidAreaChangedListener>>> GetListeners();
     template<typename T>
     EnableIfSame<T, IDialogDeathRecipientListener, std::vector<sptr<IDialogDeathRecipientListener>>> GetListeners();
     template<typename T>
@@ -145,6 +153,7 @@ private:
     static std::recursive_mutex globalMutex_;
     static std::map<uint64_t, std::vector<sptr<IWindowLifeCycle>>> lifecycleListeners_;
     static std::map<uint64_t, std::vector<sptr<IWindowChangeListener>>> windowChangeListeners_;
+    static std::map<uint64_t, std::vector<sptr<IAvoidAreaChangedListener>>> avoidAreaChangeListeners_;
     static std::map<uint64_t, std::vector<sptr<IDialogDeathRecipientListener>>> dialogDeathRecipientListeners_;
     static std::map<uint64_t, std::vector<sptr<IDialogTargetTouchListener>>> dialogTargetTouchListener_;
 };