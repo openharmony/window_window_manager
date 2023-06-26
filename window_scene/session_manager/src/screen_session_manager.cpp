0328d93e23684a44ece7853101e786176beb85dc
nickyhey <nizhiguang1@huawei.com>
2023-06-26 20:50:54 +0800

    modify ScreenSessionManager
    Signed-off-by: nickyhey <nizhiguang1@huawei.com>

--- window_scene/session_manager/src/screen_session_manager.cpp
+++ window_scene/session_manager/src/screen_session_manager.cpp
@@ -45,6 +45,7 @@ ScreenSessionManager::ScreenSessionManager() : rsInterface_(RSInterfaces::GetIns
     taskScheduler_ = std::make_shared<TaskScheduler>(SCREEN_SESSION_MANAGER_THREAD);
     RegisterScreenChangeListener();
     LoadScreenSceneXml();
+    screenCutoutController_ = new ScreenCutoutController();
 }
 
 void ScreenSessionManager::RegisterScreenConnectionListener(sptr<IScreenConnectionListener>& screenConnectionListener)
@@ -1401,5 +1402,10 @@ void ScreenSessionManager::OnScreenshot(sptr<ScreenshotInfo> info)
     for (auto& agent : agents) {
         agent->OnScreenshot(info);
     }
+}
+
+sptr<CutoutInfo> ScreenSessionManager::GetCutoutInfo(DisplayId displayId)
+{
+    return screenCutoutController_ ? screenCutoutController_->GetScreenCutoutInfo() : nullptr;
 }
 } // namespace OHOS::Rosen
