8843d7604d6278b20f78629f0850ee672f1f346e
z30042987 <zhouminghui8@huawei.com>
2023-06-30 09:13:30 +0800

    scene_session_window_test
    Signed-off-by: z30042987 <zhouminghui8@huawei.com>

--- window_scene/test/mock/mock_session.h
+++ window_scene/test/mock/mock_session.h
@@ -27,15 +27,26 @@ public:
     MOCK_METHOD6(Connect, WSError(const sptr<ISessionStage>& sessionStage,
         const sptr<IWindowEventChannel>& eventChannel, const std::shared_ptr<RSSurfaceNode>& surfaceNode,
         SystemSessionConfig& systemConfig, sptr<WindowSessionProperty> property, sptr<IRemoteObject> token));
+    MOCK_METHOD6(CreateAndConnectSpecificSession, WSError(const sptr<ISessionStage>& sessionStage,
+        const sptr<IWindowEventChannel>& eventChannel, const std::shared_ptr<RSSurfaceNode>& surfaceNode,
+        sptr<WindowSessionProperty> property, uint64_t& persistentId, sptr<ISession>& session));
+
     MOCK_METHOD0(Foreground, WSError(void));
     MOCK_METHOD0(Background, WSError(void));
     MOCK_METHOD0(Disconnect, WSError(void));
 
+    MOCK_METHOD2(UpdateSessionRect, WSError(const WSRect& rect, const SizeChangeReason& reason));
+
     MOCK_METHOD0(Recover, WSError(void));
     MOCK_METHOD0(Maximize, WSError(void));
     MOCK_METHOD1(PendingSessionActivation, WSError(const sptr<AAFwk::SessionInfo> info));
-    MOCK_METHOD1(UpdateActiveStatus, WSError(bool isActive));
+    MOCK_METHOD1(UpdateActiveStatus, WSError(SessionEvent event));
+    MOCK_METHOD1(OnSessionEvent, WSError(bool isActive));
     MOCK_METHOD0(RequestSessionBack, WSError(void));
+    MOCK_METHOD0(UpdateSessionRect, WSError(const WSRect& rect, const SizeChangeReason& reason));
+
+
+
 };
 } // namespace Rosen
 } // namespace OHOS
