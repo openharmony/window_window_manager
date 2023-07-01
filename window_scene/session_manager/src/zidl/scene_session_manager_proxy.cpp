clipsheep6/window_manager

Scene Session适配无障碍接口
Signed-off-by: keveinhu <mail.hk@163.com>

window_scene/session_manager/src/zidl/scene_session_manager_proxy.cpp
@@ -20,6 +20,8 @@
 #include <message_parcel.h>
 #include <ui/rs_surface_node.h>
 
+#include "marshalling_helper.h"
+#include "window_manager.h"
 #include "window_manager_hilog.h"
 
 namespace OHOS::Rosen {
@@ -215,5 +217,29 @@ void SceneSessionManagerProxy::GetFocusWindowInfo(FocusChangeInfo& focusInfo)
     }
     sptr<FocusChangeInfo> info = reply.ReadParcelable<FocusChangeInfo>();
     focusInfo = *info;
+}
+
+WSError SceneSessionManagerProxy::GetWindowInfo(std::vector<sptr<AccessibilityWindowInfo>>& infos)
+{
+    MessageOption option;
+    MessageParcel reply;
+    MessageParcel data;
+    if (!data.WriteInterfaceToken(GetDescriptor())) {
+        WLOGFE("Write InterfaceToken failed");
+        return WMError::WM_ERROR_IPC_FAILED;
+    }
+
+    if (Remote()->SendRequest(static_cast<uint32_t>(
+        SceneSessionManagerMessage::TRANS_ID_GET_WINDOW_INFO),
+        data, reply, option) != ERR_NONE) {
+        WLOGFE("SendRequest failed");
+        return WMError::WM_ERROR_IPC_FAILED;
+    }
+
+    if (!MarshallingHelper::UnmarshallingVectorParcelableObj<AccessibilityWindowInfo>(reply, infos)) {
+        WLOGFE("read window info failed.");
+        return WMError::WM_ERROR_IPC_FAILED;
+    }
+    return static_cast<WSError>(reply.ReadUint32());
 }
 } // namespace OHOS::Rosen

window_scene/session_manager/src/zidl/scene_session_manager_stub.cpp
@@ -17,7 +17,9 @@
 
 #include <ipc_types.h>
 #include <ui/rs_surface_node.h>
+#include "marshalling_helper.h"
 #include "session/host/include/scene_session.h"
+#include "window_manager.h"
 #include "window_manager_agent_proxy.h"
 #include "window_manager_hilog.h"
 
@@ -106,6 +108,16 @@ int SceneSessionManagerStub::OnRemoteRequest(uint32_t code,
             FocusChangeInfo focusInfo;
             GetFocusWindowInfo(focusInfo);
             reply.WriteParcelable(&focusInfo);
+            break;
+        }
+        case SceneSessionManagerMessage::TRANS_ID_GET_WINDOW_INFO: {
+            std::vector<sptr<AccessibilityWindowInfo>> infos;
+            WSError errCode = GetWindowInfo(infos);
+            if (!MarshallingHelper::MarshallingVectorParcelableObj<AccessibilityWindowInfo>(reply, infos)) {
+                WLOGFE("Write window infos failed.");
+                return ERR_TRANSACTION_FAILED;
+            }
+            reply.WriteInt32(static_cast<int32_t>(errCode));
             break;
         }
         default:

openharmony/window_window_manager

window_scene/session_manager/src/zidl/scene_session_manager_proxy.cpp
@@ -20,6 +20,8 @@
 #include <message_parcel.h>
 #include <ui/rs_surface_node.h>
 
+#include "marshalling_helper.h"
+#include "window_manager.h"
 #include "window_manager_hilog.h"
 
 namespace OHOS::Rosen {
@@ -319,4 +321,27 @@ void SceneSessionManagerProxy::UnregisterSessionListener()
     }
 }
 
+WMError SceneSessionManagerProxy::GetAccessibilityWindowInfo(std::vector<sptr<AccessibilityWindowInfo>>& infos)
+{
+    MessageOption option;
+    MessageParcel reply;
+    MessageParcel data;
+    if (!data.WriteInterfaceToken(GetDescriptor())) {
+        WLOGFE("Write InterfaceToken failed");
+        return WMError::WM_ERROR_IPC_FAILED;
+    }
+
+    if (Remote()->SendRequest(static_cast<uint32_t>(
+        SceneSessionManagerMessage::TRANS_ID_GET_ACCESSIBILITY_WINDOW_INFO),
+        data, reply, option) != ERR_NONE) {
+        WLOGFE("SendRequest failed");
+        return WMError::WM_ERROR_IPC_FAILED;
+    }
+
+    if (!MarshallingHelper::UnmarshallingVectorParcelableObj<AccessibilityWindowInfo>(reply, infos)) {
+        WLOGFE("read window info failed.");
+        return WMError::WM_ERROR_IPC_FAILED;
+    }
+    return static_cast<WSError>(reply.ReadUint32());
+}
 } // namespace OHOS::Rosen

window_scene/session_manager/src/zidl/scene_session_manager_stub.cpp
@@ -17,7 +17,9 @@
 
 #include <ipc_types.h>
 #include <ui/rs_surface_node.h>
+#include "marshalling_helper.h"
 #include "session/host/include/scene_session.h"
+#include "window_manager.h"
 #include "window_manager_agent_proxy.h"
 #include "window_manager_hilog.h"
 
@@ -135,6 +137,16 @@ int SceneSessionManagerStub::OnRemoteRequest(uint32_t code,
         }
         case SceneSessionManagerMessage::TRANS_ID_UNREGISTER_SESSION_LISTENER: {
             UnregisterSessionListener();
+            break;
+        }
+        case SceneSessionManagerMessage::TRANS_ID_GET_ACCESSIBILITY_WINDOW_INFO: {
+            std::vector<sptr<AccessibilityWindowInfo>> infos;
+            WMError errCode = GetAccessibilityWindowInfo(infos);
+            if (!MarshallingHelper::MarshallingVectorParcelableObj<AccessibilityWindowInfo>(reply, infos)) {
+                WLOGFE("Write window infos failed.");
+                return ERR_TRANSACTION_FAILED;
+            }
+            reply.WriteInt32(static_cast<int32_t>(errCode));
             break;
         }
         default: