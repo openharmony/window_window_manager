134c358559da9cbb38650c14f20fc8e41194e6b4
胡凯 <mail.hk@163.com>
2023-07-02 06:34:06 +0000

    update window_scene/session_manager/include/zidl/scene_session_manager_stub.h.
    Signed-off-by: 胡凯 <mail.hk@163.com>

--- window_scene/session_manager/include/zidl/scene_session_manager_stub.h
+++ window_scene/session_manager/include/zidl/scene_session_manager_stub.h
@@ -27,9 +27,9 @@ public:
     virtual ~SceneSessionManagerStub() = default;
 
     int OnRemoteRequest(uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option) override;
-    void HandleCreateAndConnectSpecificSession(MessageParcel &data, MessageParcel &reply);
+    int HandleCreateAndConnectSpecificSession(MessageParcel &data, MessageParcel &reply);
     void HandleUpdateProperty(MessageParcel &data, MessageParcel &reply);
-    void HandleGetAccessibilityWindowInfo(MessageParcel &data, MessageParcel &reply);
+    int HandleGetAccessibilityWindowInfo(MessageParcel &data, MessageParcel &reply);
 };
 } // namespace OHOS::Rosen
 
