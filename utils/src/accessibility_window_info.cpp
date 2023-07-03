clipsheep6/window_manager

notify accessibility window info
Signed-off-by: keveinhu <mail.hk@163.com>

utils/src/accessibility_window_info.cpp
@@ -1,0 +1,52 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef OHOS_ROSEN_AGENT_DEATH_RECIPIENT_H
+#define OHOS_ROSEN_AGENT_DEATH_RECIPIENT_H
+
+#include "accessibility_window_info.h"
+
+namespace OHOS {
+namespace Rosen {
+bool AccessibilityWindowInfo::Marshalling(Parcel& parcel) const
+{
+    return parcel.WriteInt32(wid_) && parcel.WriteUint32(windowRect_.width_) &&
+        parcel.WriteUint32(windowRect_.height_) && parcel.WriteInt32(windowRect_.posX_) &&
+        parcel.WriteInt32(windowRect_.posY_) && parcel.WriteBool(focused_) && parcel.WriteBool(isDecorEnable_) &&
+        parcel.WriteUint64(displayId_)  && parcel.WriteUint32(layer_) &&
+        parcel.WriteUint32(static_cast<uint32_t>(mode_)) && parcel.WriteUint32(static_cast<uint32_t>(type_));
+}
+
+static AccessibilityWindowInfo* AccessibilityWindowInfo::Unmarshalling(Parcel& parcel)
+{
+    auto info = new (std::nothrow) AccessibilityWindowInfo();
+    if (info == nullptr) {
+        return nullptr;
+    }
+    bool res = parcel.ReadInt32(info->wid_) && parcel.ReadUint32(info->windowRect_.width_) &&
+        parcel.ReadUint32(info->windowRect_.height_) && parcel.ReadInt32(info->windowRect_.posX_) &&
+        parcel.ReadInt32(info->windowRect_.posY_) && parcel.ReadBool(info->focused_) &&
+        parcel.ReadBool(info->isDecorEnable_) && parcel.ReadUint64(info->displayId_) &&
+        parcel.ReadUint32(info->layer_);
+    if (!res) {
+        delete info;
+        return nullptr;
+    }
+    info->mode_ = static_cast<WindowMode>(parcel.ReadUint32());
+    info->type_ = static_cast<WindowType>(parcel.ReadUint32());
+    return info;
+}
+}
+}
+#endif // OHOS_ROSEN_AGENT_DEATH_RECIPIENT_H

window_scene/session_manager/include/session_manager_agent_controller.h
@@ -16,6 +16,7 @@
 #ifndef OHOS_ROSEN_SESSION_MANAGER_AGENT_CONTROLLER_H
 #define OHOS_ROSEN_SESSION_MANAGER_AGENT_CONTROLLER_H
 
+#include "accessibility_window_info.h"
 #include "client_agent_container.h"
 #include "wm_single_instance.h"
 #include "zidl/window_manager_agent_interface.h"
@@ -32,6 +33,8 @@ public:
 
     void UpdateCameraFloatWindowStatus(uint32_t accessTokenId, bool isShowing);
     void UpdateFocusChangeInfo(const sptr<FocusChangeInfo>& focusChangeInfo, bool focused);
+    void NotifyAccessibilityWindowInfo(const std::vector<sptr<AccessibilityWindowInfo>>& infos,
+        WindowUpdateType type);
 private:
     SessionManagerAgentController() {}
     virtual ~SessionManagerAgentController() = default;

window_scene/session_manager/src/scene_session_manager.cpp
@@ -1334,7 +1334,7 @@ void SceneSessionManager::NotifyWindowInfoChange(uint64_t persistentId, WindowUp
 
     const auto& sceneSession = iter->second;
     FillWindowInfo(infos, sceneSession);
-    // SingletonContainer::Get<WindowAdapter>().NotifyAccessibilityWindowInfo(infos, type);
+    WindowManagerAgentController::GetInstance().NotifyAccessibilityWindowInfo(infos, type);
 }
 
 void SceneSessionManager::FillWindowInfo(std::vector<sptr<AccessibilityWindowInfo>>& infos,

wm/src/window_manager.cpp
@@ -20,6 +20,7 @@
 
 #include "marshalling_helper.h"
 #include "scene_board_judgement.h"
+#include "scene_session_manager.h"
 #include "session_manager.h"
 #include "window_adapter.h"
 #include "window_manager_agent.h"
@@ -372,8 +373,13 @@ WMError WindowManager::RegisterWindowUpdateListener(const sptr<IWindowUpdateList
     WMError ret = WMError::WM_OK;
     if (pImpl_->windowUpdateListenerAgent_ == nullptr) {
         pImpl_->windowUpdateListenerAgent_ = new WindowManagerAgent();
-        ret = SingletonContainer::Get<WindowAdapter>().RegisterWindowManagerAgent(
-            WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        if (SceneBoardJudgement::IsSceneBoardEnabled()) {
+            ret = SingletonContainer::Get<SceneSessionManager>().RegisterWindowManagerAgent(
+                WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        } else {
+            ret = SingletonContainer::Get<WindowAdapter>().RegisterWindowManagerAgent(
+                WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        }
     }
     if (ret != WMError::WM_OK) {
         WLOGFW("RegisterWindowManagerAgent failed !");
@@ -404,8 +410,13 @@ WMError WindowManager::UnregisterWindowUpdateListener(const sptr<IWindowUpdateLi
     pImpl_->windowUpdateListeners_.erase(iter);
     WMError ret = WMError::WM_OK;
     if (pImpl_->windowUpdateListeners_.empty() && pImpl_->windowUpdateListenerAgent_ != nullptr) {
-        ret = SingletonContainer::Get<WindowAdapter>().UnregisterWindowManagerAgent(
-            WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        if (SceneBoardJudgement::IsSceneBoardEnabled()) {
+            ret = SingletonContainer::Get<SceneSessionManager>().UnregisterWindowManagerAgent(
+                WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        } else {
+            ret = SingletonContainer::Get<WindowAdapter>().UnregisterWindowManagerAgent(
+                WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        }
         if (ret == WMError::WM_OK) {
             pImpl_->windowUpdateListenerAgent_ = nullptr;
         }

openharmony/window_window_manager

utils/src/accessibility_window_info.cpp
@@ -1,0 +1,57 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef OHOS_ROSEN_AGENT_DEATH_RECIPIENT_H
+#define OHOS_ROSEN_AGENT_DEATH_RECIPIENT_H
+
+#include <refbase.h>
+#include <iremote_object.h>
+
+
+#include "window_manager.h"
+#include "wm_common.h"
+
+namespace OHOS {
+namespace Rosen {
+bool AccessibilityWindowInfo::Marshalling(Parcel& parcel) const
+{
+    return parcel.WriteInt32(wid_) && parcel.WriteUint32(windowRect_.width_) &&
+        parcel.WriteUint32(windowRect_.height_) && parcel.WriteInt32(windowRect_.posX_) &&
+        parcel.WriteInt32(windowRect_.posY_) && parcel.WriteBool(focused_) && parcel.WriteBool(isDecorEnable_) &&
+        parcel.WriteUint64(displayId_)  && parcel.WriteUint32(layer_) &&
+        parcel.WriteUint32(static_cast<uint32_t>(mode_)) && parcel.WriteUint32(static_cast<uint32_t>(type_));
+}
+
+AccessibilityWindowInfo* AccessibilityWindowInfo::Unmarshalling(Parcel& parcel)
+{
+    auto info = new (std::nothrow) AccessibilityWindowInfo();
+    if (info == nullptr) {
+        return nullptr;
+    }
+    bool res = parcel.ReadInt32(info->wid_) && parcel.ReadUint32(info->windowRect_.width_) &&
+        parcel.ReadUint32(info->windowRect_.height_) && parcel.ReadInt32(info->windowRect_.posX_) &&
+        parcel.ReadInt32(info->windowRect_.posY_) && parcel.ReadBool(info->focused_) &&
+        parcel.ReadBool(info->isDecorEnable_) && parcel.ReadUint64(info->displayId_) &&
+        parcel.ReadUint32(info->layer_);
+    if (!res) {
+        delete info;
+        return nullptr;
+    }
+    info->mode_ = static_cast<WindowMode>(parcel.ReadUint32());
+    info->type_ = static_cast<WindowType>(parcel.ReadUint32());
+    return info;
+}
+}
+}
+#endif // OHOS_ROSEN_AGENT_DEATH_RECIPIENT_H

window_scene/session_manager/include/session_manager_agent_controller.h
@@ -17,6 +17,7 @@
 #define OHOS_ROSEN_SESSION_MANAGER_AGENT_CONTROLLER_H
 
 #include "client_agent_container.h"
+#include "window_manager.h"
 #include "wm_single_instance.h"
 #include "zidl/window_manager_agent_interface.h"
 
@@ -32,6 +33,8 @@ public:
 
     void UpdateCameraFloatWindowStatus(uint32_t accessTokenId, bool isShowing);
     void UpdateFocusChangeInfo(const sptr<FocusChangeInfo>& focusChangeInfo, bool focused);
+    void NotifyAccessibilityWindowInfo(const std::vector<sptr<AccessibilityWindowInfo>>& infos,
+        WindowUpdateType type);
 private:
     SessionManagerAgentController() {}
     virtual ~SessionManagerAgentController() = default;

window_scene/session_manager/src/scene_session_manager.cpp
@@ -1334,7 +1334,7 @@ void SceneSessionManager::NotifyWindowInfoChange(uint64_t persistentId, WindowUp
 
     const auto& sceneSession = iter->second;
     FillWindowInfo(infos, sceneSession);
-    // SingletonContainer::Get<WindowAdapter>().NotifyAccessibilityWindowInfo(infos, type);
+    SessionManagerAgentController::GetInstance().NotifyAccessibilityWindowInfo(infos, type);
 }
 
 void SceneSessionManager::FillWindowInfo(std::vector<sptr<AccessibilityWindowInfo>>& infos,

wm/src/window_manager.cpp
@@ -372,8 +372,13 @@ WMError WindowManager::RegisterWindowUpdateListener(const sptr<IWindowUpdateList
     WMError ret = WMError::WM_OK;
     if (pImpl_->windowUpdateListenerAgent_ == nullptr) {
         pImpl_->windowUpdateListenerAgent_ = new WindowManagerAgent();
-        ret = SingletonContainer::Get<WindowAdapter>().RegisterWindowManagerAgent(
-            WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        if (SceneBoardJudgement::IsSceneBoardEnabled()) {
+            ret = SingletonContainer::Get<SessionManager>().RegisterWindowManagerAgent(
+                WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        } else {
+            ret = SingletonContainer::Get<WindowAdapter>().RegisterWindowManagerAgent(
+                WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        }
     }
     if (ret != WMError::WM_OK) {
         WLOGFW("RegisterWindowManagerAgent failed !");
@@ -404,8 +409,13 @@ WMError WindowManager::UnregisterWindowUpdateListener(const sptr<IWindowUpdateLi
     pImpl_->windowUpdateListeners_.erase(iter);
     WMError ret = WMError::WM_OK;
     if (pImpl_->windowUpdateListeners_.empty() && pImpl_->windowUpdateListenerAgent_ != nullptr) {
-        ret = SingletonContainer::Get<WindowAdapter>().UnregisterWindowManagerAgent(
-            WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        if (SceneBoardJudgement::IsSceneBoardEnabled()) {
+            ret = SingletonContainer::Get<SessionManager>().UnregisterWindowManagerAgent(
+                WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        } else {
+            ret = SingletonContainer::Get<WindowAdapter>().UnregisterWindowManagerAgent(
+                WindowManagerAgentType::WINDOW_MANAGER_AGENT_TYPE_WINDOW_UPDATE, pImpl_->windowUpdateListenerAgent_);
+        }
         if (ret == WMError::WM_OK) {
             pImpl_->windowUpdateListenerAgent_ = nullptr;
         }