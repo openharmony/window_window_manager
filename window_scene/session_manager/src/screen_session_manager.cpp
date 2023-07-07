22e9b7aa6f5d9c410cc933a689e5b16eb86fad74
gaoguanghui <gaoguanghui1@huawei.com>
2023-07-07 16:24:22 +0800

    sensor and rotation
    Signed-off-by: gaoguanghui <gaoguanghui1@huawei.com>
    Change-Id: I79aa2f100ca12c1f021ce4ad876e72fda55755ba

--- window_scene/session_manager/src/screen_session_manager.cpp
+++ window_scene/session_manager/src/screen_session_manager.cpp
@@ -25,6 +25,8 @@
 #include "surface_capture_future.h"
 #include "sys_cap_util.h"
 #include "window_manager_hilog.h"
+#include "screen_rotation_property.h"
+#include "screen_sensor_connector.h"
 
 namespace OHOS::Rosen {
 namespace {
@@ -588,6 +590,7 @@ DMError ScreenSessionManager::IsScreenRotationLocked(bool& isLocked)
         WLOGFE("SCB: ScreenSessionManager is screen rotation locked permission denied!");
         return DMError::DM_ERROR_NOT_SYSTEM_APP;
     }
+    isLocked = ScreenRotationProperty::IsScreenRotationLocked();
     WLOGFI("SCB: IsScreenRotationLocked:isLocked: %{public}u", isLocked);
     return DMError::DM_OK;
 }
@@ -599,7 +602,7 @@ DMError ScreenSessionManager::SetScreenRotationLocked(bool isLocked)
         return DMError::DM_ERROR_NOT_SYSTEM_APP;
     }
     WLOGFI("SCB: SetScreenRotationLocked: isLocked: %{public}u", isLocked);
-    return DMError::DM_OK;
+    return ScreenRotationProperty::SetScreenRotationLocked(isLocked);
 }
 
 DMError ScreenSessionManager::SetOrientation(ScreenId screenId, Orientation orientation)
@@ -645,8 +648,7 @@ DMError ScreenSessionManager::SetOrientationController(ScreenId screenId, Orient
         return DMError::DM_OK;
     }
     if (isFromWindow) {
-        // I will debug it in the future
-        // ScreenRotationProperty::ProcessOrientationSwitch(newOrientation);
+        ScreenRotationProperty::ProcessOrientationSwitch(newOrientation);
     } else {
         Rotation rotationAfter = screenSession->CalcRotation(newOrientation);
         SetRotation(screenId, rotationAfter, false);
@@ -658,8 +660,7 @@ DMError ScreenSessionManager::SetOrientationController(ScreenId screenId, Orient
     return DMError::DM_OK;
 }
 
-bool ScreenSessionManager::SetRotation(ScreenId screenId, Rotation rotationAfter,
-    bool isFromWindow)
+bool ScreenSessionManager::SetRotation(ScreenId screenId, Rotation rotationAfter, bool isFromWindow)
 {
     WLOGFI("Enter SetRotation, screenId: %{public}" PRIu64 ", rotation: %{public}u, isFromWindow: %{public}u,",
         screenId, rotationAfter, isFromWindow);
@@ -677,6 +678,57 @@ bool ScreenSessionManager::SetRotation(ScreenId screenId, Rotation rotationAfter
     screenSession->PropertyChange(screenSession->GetScreenProperty());
     NotifyScreenChanged(screenSession->ConvertToScreenInfo(), ScreenChangeEvent::UPDATE_ROTATION);
     return true;
+}
+
+void ScreenSessionManager::SetSensorSubscriptionEnabled()
+{
+    isAutoRotationOpen_ = system::GetParameter("persist.display.ar.enabled", "1") == "1";
+    if (!isAutoRotationOpen_) {
+        WLOGFE("autoRotation is not open");
+        ScreenRotationProperty::Init();
+        return;
+    }
+    ScreenSensorConnector::SubscribeRotationSensor();
+}
+
+bool ScreenSessionManager::SetRotationFromWindow(DisplayId displayId, Rotation targetRotation)
+{
+    sptr<DisplayInfo> displayInfo = GetDisplayInfoById(displayId);
+    if (displayInfo == nullptr) {
+        return false;
+    }
+    return SetRotation(displayInfo->GetScreenId(), targetRotation, true);
+}
+
+sptr<SupportedScreenModes> ScreenSessionManager::GetScreenModesByDisplayId(DisplayId displayId)
+{
+    auto displayInfo = GetDisplayInfoById(displayId);
+    if (displayInfo == nullptr) {
+        WLOGFE("can not get display.");
+        return nullptr;
+    }
+    auto screenInfo = GetScreenInfoById(displayInfo->GetScreenId());
+    if (screenInfo == nullptr) {
+        WLOGFE("can not get screen.");
+        return nullptr;
+    }
+    auto modes = screenInfo->GetModes();
+    auto id = screenInfo->GetModeId();
+    if (id >= modes.size()) {
+        WLOGFE("can not get screenMode.");
+        return nullptr;
+    }
+    return modes[id];
+}
+
+sptr<ScreenInfo> ScreenSessionManager::GetScreenInfoByDisplayId(DisplayId displayId)
+{
+    auto displayInfo = GetDisplayInfoById(displayId);
+    if (displayInfo == nullptr) {
+        WLOGFE("can not get displayInfo.");
+        return nullptr;
+    }
+    return GetScreenInfoById(displayInfo->GetScreenId());
 }
 
 void ScreenSessionManager::RegisterDisplayChangeListener(sptr<IDisplayChangeListener> listener)
