c5da80738bcdb08084b0433d3b16cfb919c4bbd8
yaodixiong <yaodixiong@huawei.com>
2023-07-18 19:08:39 +0800

    修复关闭窗口后窗口可见性不变的BUG
    Signed-off-by: yaodixiong <yaodixiong@huawei.com>
    Change-Id: Ibcfafd7daeacf04cb9cf1513f1ab171874c5d11e

--- window_scene/session/host/include/session.h
+++ window_scene/session/host/include/session.h
@@ -167,6 +167,8 @@ public:
     bool GetFocusable() const;
     WSError SetTouchable(bool touchable);
     bool GetTouchable() const;
+    WSError SetVisible(bool isVisible);
+    bool GetVisible() const;
     WSError SetGlobalMaximizeMode(MaximizeMode mode) override;
     WSError GetGlobalMaximizeMode(MaximizeMode& mode) override;
     AvoidArea GetAvoidAreaByType(AvoidAreaType type) override;
@@ -268,6 +270,7 @@ private:
 
     int32_t callingPid_ = { 0 };
     int32_t callingUid_ = { 0 };
+    bool isVisible_ {false};
     sptr<IRemoteObject> abilityToken_ = nullptr;
 };
 } // namespace OHOS::Rosen
