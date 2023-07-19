edbcbad650cd4529efdf9fdee9c100a061eaeb3d
wuliangdong <wuliangdong1@huawei.com>
2023-07-19 04:04:15 +0000

    修改检视
    Signed-off-by: wuliangdong <wuliangdong1@huawei.com>
    Change-Id: I109bb7049596bc0e0a488ded4e66cf06b810225a

--- window_scene/intention_event/service/anr_manager/src/anr_manager.cpp
+++ window_scene/intention_event/service/anr_manager/src/anr_manager.cpp
@@ -27,7 +27,7 @@ namespace OHOS {
 namespace Rosen {
 namespace {
 constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, HILOG_DOMAIN_WINDOW, "ANRManager" };
-constexpr int32_t MAX_ANR_TIMER_COUNT = 64;
+constexpr int32_t MAX_ANR_TIMER_COUNT { 64 };
 } // namespace
 
 ANRManager::ANRManager() {}
