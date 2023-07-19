60d775ec78c35808957e948bc6b8a122ec5cba59
chenhantao <chenhantao3@huawei.com>
2023-07-19 07:37:45 +0000

    FUZZ
    Signed-off-by: chenhantao <chenhantao3@huawei.com>
    Change-Id: If11982703f7b82efd95ecfbe44d476b282a4729d

--- test/fuzztest/wms/windowimpl_fuzzer/windowimpl_fuzzer.cpp
+++ test/fuzztest/wms/windowimpl_fuzzer/windowimpl_fuzzer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,6 +18,7 @@
 #include <securec.h>
 
 #include "marshalling_helper.h"
+#include "window.h"
 #include "window_manager.h"
 #include "window_impl.h"
 #include <refbase.h>
@@ -82,6 +83,20 @@ public:
 class DialogDeathRecipientListener : public IDialogDeathRecipientListener {
 public:
     void OnDialogDeathRecipient() const override
+    {
+    }
+};
+
+class WindowDragListener : public IWindowDragListener {
+public:
+    void OnDrag(int32_t x, int32_t y, DragEvent event) override
+    {
+    }
+};
+
+class DisplayMoveListener : public IDisplayMoveListener {
+public:
+    void OnDisplayMove(DisplayId from, DisplayId to) override
     {
     }
 };
@@ -151,6 +166,89 @@ bool WindowImplFuzzTest(const uint8_t* data, size_t size)
     
     return true;
 }
+
+bool WindowImplFuzzTest01(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size < DATA_MIN_SIZE) {
+        return false;
+    }
+    sptr<WindowOption> option = new WindowOption();
+    sptr<WindowImpl> window = new WindowImpl(option);
+    std::vector<std::string> params{"-h"};
+    std::vector<std::string> info{""};
+    window->DumpInfo(params, info);
+    params.push_back("");
+    window->DumpInfo(params, info);
+
+    size_t startPos = 0;
+    auto keyEvent = MMI::KeyEvent::Create();
+    window->ConsumeKeyEvent(keyEvent);
+    keyEvent->SetKeyCode(MMI::KeyEvent::KEYCODE_BACK);
+    keyEvent->SetKeyAction(MMI::KeyEvent::KEY_ACTION_UP);
+    window->ConsumeKeyEvent(keyEvent);
+
+    auto pointerEvent = MMI::PointerEvent::Create();
+    MMI::PointerEvent::PointerItem item;
+    int32_t pointerId = 0;
+    startPos += GetObject<int32_t>(pointerId, data + startPos, size - startPos);
+    pointerEvent->SetPointerId(pointerId);
+    pointerEvent->SetPointerAction(MMI::PointerEvent::POINTER_ACTION_DOWN);
+    window->ConsumePointerEvent(pointerEvent);
+    item.SetPointerId(pointerId);
+    item.SetDisplayX(15); // 15 : position x
+    item.SetDisplayY(15); // 15 : position y
+    pointerEvent->AddPointerItem(item);
+    window->ConsumePointerEvent(pointerEvent);
+    int32_t x = 5;
+    int32_t y = 5;
+    startPos += GetObject<int32_t>(x, data + startPos, size - startPos);
+    startPos += GetObject<int32_t>(y, data + startPos, size - startPos);
+    item.SetDisplayX(x); // 5 : position x
+    item.SetDisplayY(y); // 5 : position y
+    pointerEvent->UpdatePointerItem(pointerId, item);
+    window->ConsumePointerEvent(pointerEvent);
+    return true;
+}
+
+bool WindowImplFuzzTest02(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size < DATA_MIN_SIZE) {
+        return false;
+    }
+    sptr<WindowOption> option = new WindowOption();
+    sptr<WindowImpl> window = new WindowImpl(option);
+    size_t startPos = 0;
+    
+    std::shared_ptr<IInputEventConsumer> iInputEventConsumer = std::make_shared<IInputEventConsumer>();
+    window->SetInputEventConsumer(iInputEventConsumer);
+    std::shared_ptr<VsyncCallback> callback;
+    window->RequestVsync(callback);
+    std::shared_ptr<AppExecFwk::Configuration> configuration;
+    window->UpdateConfiguration(configuration);
+    sptr<IWindowLifeCycle> windowLifeCycleListener = new IWindowLifeCycle();
+    window->RegisterLifeCycleListener(windowLifeCycleListener);
+    window->UnregisterLifeCycleListener(windowLifeCycleListener);
+    sptr<IWindowChangeListener> windowChangeListener = new IWindowChangeListener;
+    window->RegisterWindowChangeListener(sptr<IWindowChangeListener>(windowChangeListener));
+    window->UnregisterWindowChangeListener(sptr<IWindowChangeListener>(windowChangeListener));
+    sptr<IAvoidAreaChangedListener> avoidAreaChangedListener = new IAvoidAreaChangedListener;
+    window->RegisterAvoidAreaChangeListener(avoidAreaChangedListener);
+    window->UnregisterAvoidAreaChangeListener(avoidAreaChangedListener);
+    sptr<IWindowDragListener> windowDragListener = new WindowDragListener;
+    window->RegisterDragListener(windowDragListener);
+    window->UnregisterDragListener(windowDragListener);
+    sptr<IDisplayMoveListener> displayMoveListener = new DisplayMoveListener;
+    window->RegisterDisplayMoveListener(displayMoveListener);
+    window->UnregisterDisplayMoveListener(displayMoveListener);
+    const std::string contentInfo;
+    bool isdistributed;
+    startPos += GetObject<bool>(isdistributed, data + startPos, size - startPos);
+    window->SetUIContent(contentInfo, nullptr, nullptr, isdistributed, nullptr);
+    AAFwk::Want want;
+    window->OnNewWant(want);
+    return true;
+}
+
 } // namespace.OHOS
 
 /* Fuzzer entry point */
@@ -158,6 +256,8 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     /* Run your code on data */
     OHOS::WindowImplFuzzTest(data, size);
+    OHOS::WindowImplFuzzTest01(data, size);
+    OHOS::WindowImplFuzzTest02(data, size);
     return 0;
 }
 
