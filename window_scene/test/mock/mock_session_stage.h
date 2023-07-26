9c4ee84e07ba98211cd9e25d04d4c9cbd76bb8d7
姚迪雄 <yaodixiong@huawei.com>
2023-07-26 12:04:29 +0000

    update window_scene/test/mock/mock_session_stage.h.
    Signed-off-by: 姚迪雄 <yaodixiong@huawei.com>

--- window_scene/test/mock/mock_session_stage.h
+++ window_scene/test/mock/mock_session_stage.h
@@ -36,7 +36,7 @@ public:
     MOCK_METHOD1(NotifyOccupiedAreaChangeInfo, void(sptr<OccupiedAreaChangeInfo> info));
     MOCK_METHOD2(UpdateAvoidArea, WSError(const sptr<AvoidArea>& avoidArea, AvoidAreaType type));
     MOCK_METHOD1(DumpSessionElementInfo, void(const std::vector<std::string>& params));
-    // MOCK_METHOD0(NotifyScreenshot, void(void));
+    MOCK_METHOD0(NotifyScreenshot, void(void));
 };
 } // namespace Rosen
 } // namespace OHOS
