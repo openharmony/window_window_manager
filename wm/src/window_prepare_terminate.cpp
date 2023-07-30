414a4e2f2200c12efda2d0cb416f554537cf12b5
leafly2021 <figo.yefei@huawei.com>
2023-07-30 15:32:21 +0800

    add window prepare terminate in sceneboard
    Signed-off-by: leafly2021 <figo.yefei@huawei.com>
    Change-Id: I416fe4a2b34eef75c7328c8ffb84dfd6689457c4

---
+++ wm/src/window_prepare_terminate.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "window_prepare_terminate.h"
+#include "window_manager_hilog.h"
+
+namespace OHOS {
+namespace Rosen {
+namespace {
+constexpr HiviewDFX::HiLogLabel LABEL = {LOG_CORE, HILOG_DOMAIN_WINDOW, "WindowPrepareTerminateHandler"};
+}
+WindowPrepareTerminateHandler::WindowPrepareTerminateHandler()
+{
+}
+WindowPrepareTerminateHandler::~WindowPrepareTerminateHandler()
+{
+}
+void WindowPrepareTerminateHandler::DoPrepareTerminate()
+{
+    WLOGFI("do pending close by ability");
+    if (func_) {
+        func_();
+    }
+}
+void SetPrepareTerminateFun(const PrepareTerminateFunc& func)
+{
+    func_ = func;
+}
+} // namespace Rosen
+} // namespace OHOS
