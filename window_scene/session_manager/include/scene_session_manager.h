b61e09d521e71d589e9844911429b6a0aa183a80
yaodixiong <yaodixiong@huawei.com>
2023-08-11 14:39:39 +0800

    修复WindowVisibilityChangeCallback稳定性问题
    Signed-off-by: yaodixiong <yaodixiong@huawei.com>
    Change-Id: I7dd9cdc82871c1f6f462b17f194c21202ac1ae82

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -207,6 +207,7 @@ private:
     std::vector<std::pair<uint64_t, bool>> GetWindowVisibilityChangeInfo(
         std::shared_ptr<RSOcclusionData> occlusionData);
     void WindowVisibilityChangeCallback(std::shared_ptr<RSOcclusionData> occlusiontionData);
+    sptr<SceneSession> SelectSesssionFromMap(const uint64_t& surfaceId);
     void WindowDestroyNotifyVisibility(const sptr<SceneSession>& sceneSession);
     void RegisterInputMethodShownFunc(const sptr<SceneSession>& sceneSession);
     void OnInputMethodShown(const int32_t& persistentId);
