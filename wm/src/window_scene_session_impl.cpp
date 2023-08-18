95402419370e804d71dcbe7402734b551d46bf8a
z00514981 <zhengjiangliang@huawei.com>
2023-08-18 15:02:57 +0800

    sceneBoard添加输入法窗口类型
    Signed-off-by: z00514981 <zhengjiangliang@huawei.com>
    Change-Id: Id5b0900e9078da15120b421c9bc441db5bd2c02a

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -82,7 +82,7 @@ bool WindowSceneSessionImpl::IsValidSystemWindowType(const WindowType& type)
         type == WindowType::WINDOW_TYPE_VOICE_INTERACTION || type == WindowType::WINDOW_TYPE_POINTER ||
         type == WindowType::WINDOW_TYPE_TOAST || type == WindowType::WINDOW_TYPE_DRAGGING_EFFECT ||
         type == WindowType::WINDOW_TYPE_SEARCHING_BAR || type == WindowType::WINDOW_TYPE_PANEL ||
-        type == WindowType::WINDOW_TYPE_VOLUME_OVERLAY)) {
+        type == WindowType::WINDOW_TYPE_VOLUME_OVERLAY || type == WindowType::WINDOW_TYPE_INPUT_METHOD_STATUS_BAR)) {
         WLOGFW("Invalid type: %{public}u", GetType());
         return false;
     }
@@ -487,6 +487,10 @@ void WindowSceneSessionImpl::SetDefaultProperty()
 {
     switch (property_->GetWindowType()) {
         case WindowType::WINDOW_TYPE_INPUT_METHOD_FLOAT:{
+            property_->SetFocusable(false);
+            break;
+        }
+        case WindowType::WINDOW_TYPE_INPUT_METHOD_STATUS_BAR:{
             property_->SetFocusable(false);
             break;
         }
