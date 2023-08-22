clipsheep6/window_manager

report to HiSysEvent when application startup request window
Signed-off-by: liuanguang <liuanguang@huawei.com>
Change-Id: I5c854c74cda72b7422694e920561aca553fad3e3

utils/include/perform_reporter.h
@@ -64,6 +64,8 @@ public:
 
     void ReportBackButtonInfoImmediately();
     void ReportZeroOpacityInfoImmediately(const std::string& bundleName, const std::string& packageName);
+    // report when application startup request window
+    void ReportStartWindow(const std::string& bundleName, const std::string& windowNams);
     void ReportRecordedInfos();
 
 private:

utils/src/perform_reporter.cpp
@@ -26,6 +26,9 @@ namespace {
 }
 WM_IMPLEMENT_SINGLE_INSTANCE(WindowInfoReporter)
 
+constexpr char EVENT_KEY_BUNDLE_NAME[] = "BUNDLE_NAME";
+constexpr char EVENT_KEY_WINDOW_NAME[] = "WINDOW_NAME";
+
 /**
  * @brief Construct a new Perform Reporter:: Perform Reporter object
  *
@@ -228,6 +231,19 @@ void WindowInfoReporter::ReportZeroOpacityInfoImmediately(const std::string& bun
     std::ostringstream oss;
     oss << "{ PROCESS_NAME:" << bundleName.c_str() << ", PACKAGE_NAME:" << "" << packageName.c_str() << " }";
     Report("WM_REPORT_WINDOW_OPACITY_ZERO", oss.str());
+}
+
+void WindowInfoReporter::ReportStartWindow(const std::string& bundleName, const std::string& windowNams)
+{
+    std::string eventName = "START_WINDOW";
+    int32_t ret = HiSysEventWrite(
+        OHOS::HiviewDFX::HiSysEvent::Domain::WINDOW_MANAGER, eventName,
+        OHOS::HiviewDFX::HiSysEvent::EventType::BEHAVIOR,
+        EVENT_KEY_BUNDLE_NAME, bundleName,
+        EVENT_KEY_WINDOW_NAME, windowName);
+    if (ret != 0) {
+        WLOGFE("Write HiSysEvent error, ret:%{public}d", ret);
+    }
 }
 
 void WindowInfoReporter::ReportRecordedInfos()

openharmony/window_window_manager

utils/include/perform_reporter.h
@@ -64,6 +64,8 @@ public:
 
     void ReportBackButtonInfoImmediately();
     void ReportZeroOpacityInfoImmediately(const std::string& bundleName, const std::string& packageName);
+    // report when application startup request window
+    void ReportStartWindow(const std::string& bundleName, const std::string& windowName);
     void ReportRecordedInfos();
 
 private:

utils/src/perform_reporter.cpp
@@ -26,6 +26,9 @@ namespace {
 }
 WM_IMPLEMENT_SINGLE_INSTANCE(WindowInfoReporter)
 
+constexpr char EVENT_KEY_BUNDLE_NAME[] = "BUNDLE_NAME";
+constexpr char EVENT_KEY_WINDOW_NAME[] = "WINDOW_NAME";
+
 /**
  * @brief Construct a new Perform Reporter:: Perform Reporter object
  *
@@ -228,6 +231,19 @@ void WindowInfoReporter::ReportZeroOpacityInfoImmediately(const std::string& bun
     std::ostringstream oss;
     oss << "{ PROCESS_NAME:" << bundleName.c_str() << ", PACKAGE_NAME:" << "" << packageName.c_str() << " }";
     Report("WM_REPORT_WINDOW_OPACITY_ZERO", oss.str());
+}
+
+void WindowInfoReporter::ReportStartWindow(const std::string& bundleName, const std::string& windowName)
+{
+    std::string eventName = "START_WINDOW";
+    int32_t ret = HiSysEventWrite(
+        OHOS::HiviewDFX::HiSysEvent::Domain::WINDOW_MANAGER, eventName,
+        OHOS::HiviewDFX::HiSysEvent::EventType::BEHAVIOR,
+        EVENT_KEY_BUNDLE_NAME, bundleName,
+        EVENT_KEY_WINDOW_NAME, windowName);
+    if (ret != 0) {
+        WLOGFE("Write HiSysEvent error, ret:%{public}d", ret);
+    }
 }
 
 void WindowInfoReporter::ReportRecordedInfos()