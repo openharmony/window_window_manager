a35a5b31a93d0c22144b5927b35b305a96f6b871
chenhantao <chenhantao3@huawei.com>
2023-08-22 11:40:37 +0000

    tdd test
    Signed-off-by: chenhantao <chenhantao3@huawei.com>
    Change-Id: I6cabd146d4ee3a8c643edc76bbc3caaacf25f79a

--- window_scene/session_manager/src/session_manager.cpp
+++ window_scene/session_manager/src/session_manager.cpp
@@ -90,6 +90,10 @@ void SessionManager::InitSessionManagerServiceProxy()
         return;
     }
     mockSessionManagerServiceProxy_ = iface_cast<IMockSessionManagerInterface>(remoteObject);
+    if (!mockSessionManagerServiceProxy_) {
+        WLOGFW("Get mock session manager service proxy failed, nullptr");
+        return;
+    }
     sessionManagerServiceProxy_ = iface_cast<ISessionManagerService>(
             mockSessionManagerServiceProxy_->GetSessionManagerService());
     if (!sessionManagerServiceProxy_) {
@@ -149,6 +153,14 @@ void SessionManager::InitSceneSessionManagerProxy()
     }
 }
 
+void SessionManager::Clear()
+{
+    std::lock_guard<std::recursive_mutex> lock(mutex_);
+    if ((sceneSessionManagerProxy_ != nullptr) && (sceneSessionManagerProxy_->AsObject() != nullptr)) {
+        sceneSessionManagerProxy_->AsObject()->RemoveDeathRecipient(ssmDeath_);
+    }
+}
+
 void SSMDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& wptrDeath)
 {
     if (wptrDeath == nullptr) {
@@ -162,6 +174,7 @@ void SSMDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& wptrDeath)
         return;
     }
     WLOGI("ssm OnRemoteDied");
+    SingletonContainer::Get<SessionManager>().Clear();
     SingletonContainer::Get<SessionManager>().ClearSessionManagerProxy();
 }
 
