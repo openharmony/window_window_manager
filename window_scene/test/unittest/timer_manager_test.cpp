45693632ec9389b9f514095ae8d9f6d849c5066d
zwx1199995 <zhangying339@h-partners.com>
2023-08-25 16:10:09 +0800

    add ws_timer_manager_test
    Signed-off-by: zwx1199995 <zhangying339@h-partners.com>
    Change-Id: I08741cb9736191383b348d00291d9d42b8b05a32

---
+++ window_scene/test/unittest/timer_manager_test.cpp
@@ -0,0 +1,202 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "intention_event/service/timer_manager/include/timer_manager.h"
+#include <algorithm>
+#include <cinttypes>
+#include "window_manager_hilog.h"
+
+using namespace testing;
+using namespace testing::ext;
+namespace OHOS {
+namespace Rosen {
+class TimerManagerTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+};
+
+void TimerManagerTest::SetUpTestCase()
+{
+}
+
+void TimerManagerTest::TearDownTestCase()
+{
+}
+
+void TimerManagerTest::SetUp()
+{
+}
+
+void TimerManagerTest::TearDown()
+{
+}
+
+namespace {
+/**
+ * @tc.name: Init
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(TimerManagerTest, Init, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TimerManagerTest::Init start";
+    TimerManager* timermanager = new TimerManager();
+    timermanager->Init();
+    GTEST_LOG_(INFO) << "TimerManagerTest::Init end";
+}
+
+/**
+ * @tc.name: Stop
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(TimerManagerTest, Stop, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TimerManagerTest::Stop start";
+    TimerManager* timermanager = new TimerManager();
+    timermanager->Init();
+    timermanager->Stop();
+    timermanager->Init();
+    GTEST_LOG_(INFO) << "TimerManagerTest::Stop end";
+}
+
+/**
+ * @tc.name: RemoveTimer
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(TimerManagerTest, RemoveTimer, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TimerManagerTest::RemoveTimer start";
+    TimerManager* timermanager = new TimerManager();
+    int32_t res = timermanager->RemoveTimer(0);
+    ASSERT_EQ(-1, res);
+    GTEST_LOG_(INFO) << "TimerManagerTest::RemoveTimer end";
+}
+
+/**
+ * @tc.name: CalcNextDelay
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(TimerManagerTest, CalcNextDelay, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TimerManagerTest::CalcNextDelay start";
+    TimerManager* timermanager = new TimerManager();
+    int32_t res = timermanager->CalcNextDelay();
+    ASSERT_EQ(5000, res);
+    GTEST_LOG_(INFO) << "TimerManagerTest::CalcNextDelay end";
+}
+
+/**
+ * @tc.name: ProcessTimers
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(TimerManagerTest, ProcessTimers, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TimerManagerTest::ProcessTimers start";
+    TimerManager* timermanager = new TimerManager();
+    timermanager->ProcessTimers();
+    GTEST_LOG_(INFO) << "TimerManagerTest::ProcessTimers start";
+}
+
+/**
+ * @tc.name: OnThread
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(TimerManagerTest, OnThread, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TimerManagerTest::OnThread start";
+    TimerManager* timermanager = new TimerManager();
+    timermanager->OnThread();
+    GTEST_LOG_(INFO) << "TimerManagerTest::OnThread start";
+}
+
+/**
+ * @tc.name: OnStop
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(TimerManagerTest, OnStop, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TimerManagerTest::OnStop start";
+    TimerManager* timermanager = new TimerManager();
+    timermanager->OnStop();
+    GTEST_LOG_(INFO) << "TimerManagerTest::OnStop start";
+}
+
+/**
+ * @tc.name: TakeNextTimerId
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(TimerManagerTest, TakeNextTimerId, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TimerManagerTest::TakeNextTimerId start";
+    TimerManager* timermanager = new TimerManager();
+    int32_t res = timermanager->TakeNextTimerId();
+    ASSERT_EQ(res, 0);
+    GTEST_LOG_(INFO) << "TimerManagerTest::TakeNextTimerId start";
+}
+
+/**
+ * @tc.name: RemoveTimerInternal
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(TimerManagerTest, RemoveTimerInternal, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TimerManagerTest::RemoveTimerInternal start";
+    TimerManager* timermanager = new TimerManager();
+    int32_t res = timermanager->RemoveTimerInternal(0);
+    ASSERT_EQ(res, -1);
+    GTEST_LOG_(INFO) << "TimerManagerTest::RemoveTimerInternal start";
+}
+
+/**
+ * @tc.name: CalcNextDelayInternal
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(TimerManagerTest, CalcNextDelayInternal, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TimerManagerTest::CalcNextDelayInternal start";
+    TimerManager* timermanager = new TimerManager();
+    int32_t res = timermanager->CalcNextDelayInternal();
+    ASSERT_EQ(res, 5000);
+    GTEST_LOG_(INFO) << "TimerManagerTest::CalcNextDelayInternal start";
+}
+
+/**
+ * @tc.name: ProcessTimersInternal
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(TimerManagerTest, ProcessTimersInternal, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "TimerManagerTest::ProcessTimersInternal start";
+    TimerManager* timermanager = new TimerManager();
+    timermanager->ProcessTimersInternal();
+    GTEST_LOG_(INFO) << "TimerManagerTest::ProcessTimersInternal start";
+}
+}
+}
+}
