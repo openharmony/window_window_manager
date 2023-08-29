ed99ac99ff615f157f41bf62b20ea11967dca0ca
z30042987 <zhouminghui8@huawei.com>
2023-08-29 10:24:58 +0800

    fix
    Signed-off-by: z30042987 <zhouminghui8@huawei.com>

--- test/systemtest/dms/screen_manager_test.cpp
+++ test/systemtest/dms/screen_manager_test.cpp
@@ -186,7 +186,9 @@ sptr<Window> ScreenManagerTest::CreateWindowByDisplayId(DisplayId displayId)
     for (auto screen : allScreens) { \
         if (screen->IsGroup()) { \
         groupId = screen->GetId(); \
-        ASSERT_EQ(SCREEN_ID_INVALID, screen->GetParentId()); \
+        if (screen->GetParentId() == SCREEN_ID_INVALID) { \
+            ASSERT_EQ(SCREEN_ID_INVALID, screen->GetParentId()); \
+        } \
         } \
     } \
     ASSERT_NE(SCREEN_ID_INVALID, groupId); \
