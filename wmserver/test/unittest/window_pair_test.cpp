e34372b98275e6f25d4e23b67e84249b8d20bcd8
许静怡 <xujingyi19@h-partners.com>
2023-09-15 08:53:43 +0000

    update wmserver/test/unittest/window_pair_test.cpp.
    Signed-off-by: 许静怡 <xujingyi19@h-partners.com>

--- wmserver/test/unittest/window_pair_test.cpp
+++ wmserver/test/unittest/window_pair_test.cpp
@@ -1290,21 +1290,22 @@ HWTEST_F(WindowPairTest, Clear02, Function | SmallTest | Level2)
     ASSERT_EQ(windowPair->StatusSupprtedWhenRecentUpdate(node0), false);
 }
 
-// /**
-//  * @tc.name: StatusSupprtedWhenRecentUpdate
-//  * @tc.desc: check function StatusSupprtedWhenRecentUpdate
-//  * @tc.type: FUNC
-//  */
-// HWTEST_F(WindowPairTest, StatusSupprtedWhenRecentUpdate, Function | SmallTest | Level2)
-// {
-//     WindowPairStatus status_ = WindowPairStatus::SINGLE_PRIMARY;
-//     sptr<WindowPair> windowPair = new WindowPair(0);
-//     windowPair->primary_->SetWindowMode(WindowMode::WINDOW_MODE_SPLIT_PRIMARY);
-//     sptr<WindowNode> node = new WindowNode();
-//     ASSERT_EQ(status_, WindowPairStatus::SINGLE_PRIMARY);
-//     auto result = windowPair->StatusSupprtedWhenRecentUpdate(node);
-//     ASSERT_EQ(result, false);
-// }
+/**
+ * @tc.name: StatusSupprtedWhenRecentUpdate
+ * @tc.desc: check function StatusSupprtedWhenRecentUpdate
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowPairTest, StatusSupprtedWhenRecentUpdate, Function | SmallTest | Level2)
+{
+    sptr<WindowProperty> property1 = new WindowProperty();
+    WindowPairStatus status_ = WindowPairStatus::SINGLE_PRIMARY;
+    sptr<WindowPair> windowPair = new WindowPair(0);
+    property1->SetWindowMode(WindowMode::WINDOW_MODE_SPLIT_PRIMARY);
+    sptr<WindowNode> node = new WindowNode();
+    ASSERT_EQ(status_, WindowPairStatus::SINGLE_PRIMARY);
+    auto result = windowPair->StatusSupprtedWhenRecentUpdate(node);
+    ASSERT_EQ(result, false);
+}
 
 /**
  * @tc.name: CheckOrderedPairZorder
@@ -1354,7 +1355,6 @@ HWTEST_F(WindowPairTest, GetOrderedPair08, Function | SmallTest | Level2)
 HWTEST_F(WindowPairTest, UpdateIfSplitRelated01, Function | SmallTest | Level2)
 {
     sptr<OHOS::Rosen::WindowNode> node = new WindowNode();
-    node = nullptr;
     sptr<WindowPair> windowPair = new WindowPair(0);
     windowPair->UpdateIfSplitRelated(node);
     auto result = windowPair->StatusSupprtedWhenRecentUpdate(node);
