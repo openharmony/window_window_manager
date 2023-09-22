43c69f7c9944c0a0edbb9dd193828ed89266ff83
xiatongchen <xiatongchen@163.com>
2023-09-22 09:52:13 +0800

    snapshot changed注册
    Signed-off-by: xiatongchen <xiatongchen@163.com>
    Change-Id: I8591413481cfc030713fb0e2a3fb132a30d0705f

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -278,6 +278,7 @@ private:
     void OnInputMethodShown(const int32_t& persistentId);
     void RegisterInputMethodHideFunc(const sptr<SceneSession>& sceneSession);
     void RegisterSessionExceptionFunc(const sptr<SceneSession>& sceneSession);
+    void RegisterSessionSnapshotFunc(const sptr<SceneSession>& sceneSession);
     void NotifySessionForCallback(const sptr<SceneSession>& scnSession, const bool needRemoveSession);
     bool IsSessionVisible(const sptr<SceneSession>& session);
     void DumpSessionInfo(const sptr<SceneSession>& session, std::ostringstream& oss);
