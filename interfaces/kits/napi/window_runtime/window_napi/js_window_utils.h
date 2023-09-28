b719c0cb4eb550f8126d3a8cd0a008ec9310ae2b
ineoui <xiazhiqi1@huawei.com>
2023-09-28 09:42:44 +0000

    update window_scene/interfaces/kits/napi/scene_session_manager/js_scene_utils.cpp.
    Change-Id: I79b4ddc68649e1caab7bd5b8e648b423e421527f
    Signed-off-by: ineoui <xiazhiqi1@huawei.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window_utils.h
+++ interfaces/kits/napi/window_runtime/window_napi/js_window_utils.h
@@ -83,7 +83,7 @@ const std::map<WindowType, ApiWindowType> NATIVE_JS_TO_WINDOW_TYPE_MAP {
     { WindowType::WINDOW_TYPE_VOICE_INTERACTION,   ApiWindowType::TYPE_VOICE_INTERACTION },
     { WindowType::WINDOW_TYPE_POINTER,             ApiWindowType::TYPE_POINTER           },
     { WindowType::WINDOW_TYPE_SCREENSHOT,          ApiWindowType::TYPE_SCREENSHOT        },
-    { WindowType::WINDOW_TYPE_SYSTEM_TOAST,           ApiWindowType::TYPE_SYSTEM_TOAST         },
+    { WindowType::WINDOW_TYPE_SYSTEM_TOAST,        ApiWindowType::TYPE_SYSTEM_TOAST      },
 };
 
 const std::map<ApiWindowType, WindowType> JS_TO_NATIVE_WINDOW_TYPE_MAP {
@@ -105,7 +105,7 @@ const std::map<ApiWindowType, WindowType> JS_TO_NATIVE_WINDOW_TYPE_MAP {
     { ApiWindowType::TYPE_VOICE_INTERACTION,   WindowType::WINDOW_TYPE_VOICE_INTERACTION   },
     { ApiWindowType::TYPE_POINTER,             WindowType::WINDOW_TYPE_POINTER             },
     { ApiWindowType::TYPE_SCREENSHOT,          WindowType::WINDOW_TYPE_SCREENSHOT          },
-    { ApiWindowType::TYPE_SYSTEM_TOAST,           WindowType::WINDOW_TYPE_SYSTEM_TOAST           },
+    { ApiWindowType::TYPE_SYSTEM_TOAST,        WindowType::WINDOW_TYPE_SYSTEM_TOAST        },
 };
 
 enum class ApiWindowMode : uint32_t {
