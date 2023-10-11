c1b0f20853b5fe8e613a5ea74278112210ed2a0c
zhouchengchao <zhouchengchao4@huawei.com>
2023-10-11 10:59:11 +0800

    register shutdown callback
    Signed-off-by: zhouchengchao <zhouchengchao4@huawei.com>

--- window_scene/interfaces/kits/napi/screen_session_manager/js_screen_session_manager.h
+++ window_scene/interfaces/kits/napi/screen_session_manager/js_screen_session_manager.h
@@ -19,9 +19,11 @@
 #include <native_engine/native_engine.h>
 #include <native_engine/native_value.h>
 #include "session_manager/include/screen_session_manager.h"
+#include "shutdown/takeover_shutdown_callback_stub.h"
 
 namespace OHOS::Rosen {
-class JsScreenSessionManager final : public IScreenConnectionListener {
+class JsScreenSessionManager final : public IScreenConnectionListener,
+    public PowerMgr::TakeOverShutdownCallbackStub {
 public:
     explicit JsScreenSessionManager(napi_env env);
     ~JsScreenSessionManager();
@@ -31,17 +33,23 @@ public:
 
     void OnScreenConnect(sptr<ScreenSession>& screenSession) override;
     void OnScreenDisconnect(sptr<ScreenSession>& screenSession) override;
+    bool OnTakeOverShutdown(bool isReboot) override;
 
 private:
     static napi_value RegisterCallback(napi_env env, napi_callback_info info);
     static napi_value UpdateScreenRotationProperty(napi_env env, napi_callback_info info);
     static napi_value GetCurvedCompressionArea(napi_env env, napi_callback_info info);
+    static napi_value RegisterShutdownCallback(napi_env env, napi_callback_info info);
+    static napi_value UnRegisterShutdownCallback(napi_env env, napi_callback_info info);
 
     napi_value OnRegisterCallback(napi_env env, const napi_callback_info info);
     napi_value OnUpdateScreenRotationProperty(napi_env env, const napi_callback_info info);
     napi_value OnGetCurvedCompressionArea(napi_env env, const napi_callback_info info);
+    napi_value OnRegisterShutdownCallback(napi_env env, const napi_callback_info info);
+    napi_value OnUnRegisterShutdownCallback(napi_env env, const napi_callback_info info);
 
     std::shared_ptr<NativeReference> screenConnectionCallback_;
+    std::shared_ptr<NativeReference> shutdownCallback_;
     napi_env env_;
 };
 } // namespace OHOS::Rosen
