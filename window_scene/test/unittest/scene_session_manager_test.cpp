953d1d725dc2feb4366c17984844f5f433619b22
yu <zhangyusi3@huawei.com>
2023-10-23 10:59:25 +0000

    add ut test
    Signed-off-by: yu <zhangyusi3@huawei.com>
    Change-Id: I7f0b15747f24f06bb7e0603f173f4785f583db81

--- window_scene/test/unittest/scene_session_manager_test.cpp
+++ window_scene/test/unittest/scene_session_manager_test.cpp
@@ -2402,6 +2402,25 @@ HWTEST_F(SceneSessionManagerTest, SetFocusedSession, Function | SmallTest | Leve
 }
 
 /**
+ * @tc.name: RequestFocusStatus
+ * @tc.desc: SceneSesionManager request focus status
+ * @tc.type: FUNC
+*/
+HWTEST_F(SceneSessionManagerTest, RequestFocusStatus, Function | SmallTest | Level3)
+{
+    int32_t focusedSession_ = ssm_->GetFocusedSession();
+    EXPECT_EQ(focusedSession_, INVALID_SESSION_ID);
+    int32_t persistentId_ = INVALID_SESSION_ID;
+    WSError result01 = ssm_->RequestFocusStatus(persistentId_, true);
+    EXPECT_EQ(result01, WSError::WS_ERROR_INVALID_SESSION);
+    persistentId_ = 10000;
+    WSError result02 = ssm_->RequestFocusStatus(persistentId_, true);
+    EXPECT_EQ(result02, WSError::WS_ERROR_INVALID_SESSION);
+    WSError result03 = ssm_->RequestFocusStatus(persistentId_, false);
+    EXPECT_EQ(result03, WSError::WS_DO_NOTHING);
+}
+
+/**
  * @tc.name: RegisterSessionExceptionFunc
  * @tc.desc: SceneSesionManager register session expection func
  * @tc.type: FUNC
