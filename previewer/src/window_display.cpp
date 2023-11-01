48e1a526faf4756440bc87d0816e7409d88d93e8
t30053014 <callBack>
2023-11-01 22:07:36 +0800

    Signed-off-by: t30053014 <1180300204@stu.hit.edu.cn>
    new file:   previewer/include/window_display.h
    new file:   previewer/src/window_display.cpp

---
+++ previewer/src/window_display.cpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "window_display.h"
+
+namespace OHOS {
+namespace Previewer {
+
+PreviewerDisplay& PreviewerDisplay::GetInstance()
+{
+    static PreviewerDisplay instance;
+    return instance;
+}
+
+void PreviewerDisplay::SetFoldable(const bool value)
+{
+    WLOGFI("SetFoldable value: %s",value ? "true" : "false");
+    foldable = value;
+}
+
+void PreviewerDisplay::SetFoldStatus(const FoldStatus value)
+{
+    foldStatus = value;
+}
+
+void PreviewerDisplay::ExecStatusChangedCallback()
+{
+    if(!displayCallback_){
+        return;
+    }
+    displayCallback_();
+}
+
+bool PreviewerDisplay::IsFoldable() const
+{
+    return foldable;
+}
+
+FoldStatus PreviewerDisplay::GetFoldStatus() const
+{
+    return foldStatus;
+}
+
+void RegisterStatusChangedCallback(const DisplayCallback& callBack)
+{
+    displayCallback_ = callBack;
+}
+
+} // namespace Previewer
+} // namespace OHOS
