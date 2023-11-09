d1f2bf92a33bc459176bbdf21a36fda51ac412ea
dengbiao <dengbiao9@huawei.com>
2023-11-09 19:58:12 +0800

    fix
    Signed-off-by: dengbiao <dengbiao9@huawei.com>

--- window_scene/session_manager/include/screen_session_manager.h
+++ window_scene/session_manager/include/screen_session_manager.h
@@ -169,7 +169,6 @@ public:
     sptr<CutoutInfo> GetCutoutInfo(DisplayId displayId) override;
     DMError HasImmersiveWindow(bool& immersive) override;
     void SetDisplayBoundary(const sptr<ScreenSession> screenSession);
-    void SetImmersiveState(bool immersive);
 
     //Fold Screen
     void SetFoldDisplayMode(const FoldDisplayMode displayMode) override;
@@ -269,7 +268,6 @@ private:
     bool screenPrivacyStates = false;
     bool keyguardDrawnDone_ = true;
     bool needScreenOnWhenKeyguardNotify_ = false;
-    bool isImmersive_ = false;
 
     //Fold Screen
     std::map<ScreenId, ScreenProperty> phyScreenPropMap_;
