819b9dcb2ce766302680d7acddeb410a6579b4c0
s30052613 <shejun1@huawei.com>
2023-11-14 17:05:57 +0800

    支持层层模态
    Signed-off-by: s30052613 <shejun1@huawei.com>

--- window_scene/session/host/include/session.h
+++ window_scene/session/host/include/session.h
@@ -306,6 +306,7 @@ protected:
     bool CheckDialogOnForeground();
     bool CheckPointerEventDispatch(const std::shared_ptr<MMI::PointerEvent>& pointerEvent) const;
     bool CheckKeyEventDispatch(const std::shared_ptr<MMI::KeyEvent>& keyEvent) const;
+    bool IsTopDialog() const;
 
     using Task = std::function<void()>;
     void PostTask(Task&& task, int64_t delayTime = 0);
