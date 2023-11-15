3facaccd1e1b15ca014b1927be26ab007bb736f1
xiaomin1017 <yanminpeng@huawei.com>
2023-11-15 08:03:48 +0000

    add new api to set virtual screen refresh rate
    Signed-off-by: xiaomin1017 <yanminpeng@huawei.com>

--- dm/src/screen_manager.cpp
+++ dm/src/screen_manager.cpp
@@ -553,6 +553,13 @@ DMError ScreenManager::ResizeVirtualScreen(ScreenId screenId, uint32_t width, ui
     return DMError::DM_OK;
 }
 
+DMError ScreenManager::SetVirtualScreenRefreshRate(ScreenId screenId, uint32_t refreshRate)
+{
+    WLOGFI("SetVirtualScreenRefreshRate, screenId: %{public}" PRIu64", refreshRate: %{public}u.",
+        screenId, refreshRate);
+    return DMError::DM_OK;
+}
+
 DMError ScreenManager::SetVirtualMirrorScreenBufferRotation(ScreenId screenId, bool rotation)
 {
     return SingletonContainer::Get<ScreenManagerAdapter>().SetVirtualMirrorScreenBufferRotation(screenId, rotation);
