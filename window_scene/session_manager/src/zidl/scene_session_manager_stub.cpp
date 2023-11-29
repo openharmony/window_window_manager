8dbb5b3792cca77a133f7f42ccba2fd02717b9c6
h00833914 <hongxiaoyu2@huawei.com>
2023-11-29 10:20:38 +0000

    整改日志重复率及超限
    Signed-off-by: h00833914 <hongxiaoyu2@huawei.com>
    Change-Id: I3b72b0f60014fe8c4334901ca626df1af2b79eb4

--- window_scene/session_manager/src/zidl/scene_session_manager_stub.cpp
+++ window_scene/session_manager/src/zidl/scene_session_manager_stub.cpp
@@ -191,7 +191,7 @@ int SceneSessionManagerStub::HandleDestroyAndDisconnectSpcificSession(MessagePar
 
 int SceneSessionManagerStub::HandleUpdateProperty(MessageParcel &data, MessageParcel &reply)
 {
-    WLOGFI("run HandleUpdateProperty!");
+    WLOGFD("run HandleUpdateProperty!");
     auto action = static_cast<WSPropertyChangeAction>(data.ReadUint32());
     sptr<WindowSessionProperty> property = nullptr;
     if (data.ReadBool()) {
@@ -415,7 +415,7 @@ int SceneSessionManagerStub::HandleTerminateSessionNew(MessageParcel& data, Mess
 
 int SceneSessionManagerStub::HandleGetFocusSessionToken(MessageParcel &data, MessageParcel &reply)
 {
-    WLOGFI("run HandleGetFocusSessionToken!");
+    WLOGFD("run HandleGetFocusSessionToken!");
     sptr<IRemoteObject> token = nullptr;
     const WSError& errCode = GetFocusSessionToken(token);
     reply.WriteRemoteObject(token);
