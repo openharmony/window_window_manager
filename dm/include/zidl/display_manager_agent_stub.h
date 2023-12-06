004b2c80a3a4f14f6529fa5928cadfbaadba083d
wangmiaoliang <wangmiaoliang1@huawei.com>
2023-12-06 12:31:25 +0000

    larger function split
    Signed-off-by: wangmiaoliang <wangmiaoliang1@huawei.com>
    Change-Id: Iafc7c135181effb274604bcb4f828c5a27361e4c

--- dm/include/zidl/display_manager_agent_stub.h
+++ dm/include/zidl/display_manager_agent_stub.h
@@ -32,21 +32,21 @@ public:
     virtual int32_t OnRemoteRequest(uint32_t code, MessageParcel& data, MessageParcel& reply,
         MessageOption& option) override;
 private:
-    void NotifyDisplaypowerevent();
-    void NotifyDisplaystateChanged();
-    void ScreenConnect();
-    void ScreenDisconnect();
-    void ScreenChanged();
-    void ScreenGroupChanged();
-    void DisplayConnect();
-    void DisplayDisconnect();
-    void DisplayChanged();
-    void ScreenShot();
-    void PrivateWindow();
-    void FoldStatusChanged();
-    void DisplayChangeInfoChanged();
-    void DisplayModechanged();
-    void AvailableAreaChanged();
+    int32_t NotifyDisplaypowerevent();
+    int32_t NotifyDisplaystateChanged();
+    int32_t ScreenConnect();
+    int32_t ScreenDisconnect();
+    int32_t ScreenChanged();
+    int32_t ScreenGroupChanged();
+    int32_t DisplayConnect();
+    int32_t DisplayDisconnect();
+    int32_t DisplayChanged();
+    int32_t ScreenShot();
+    int32_t PrivateWindow();
+    int32_t FoldStatusChanged();
+    int32_t DisplayChangeInfoChanged();
+    int32_t DisplayModechanged();
+    int32_t AvailableAreaChanged();
 };
 } // namespace OHOS::Rosen
 #endif // OHOS_ROSEN_DISPLAY_MANAGER_AGENT_STUB_H
