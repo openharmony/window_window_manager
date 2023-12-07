f13d1a72a928f692c5b3c24cc129ccfed4b3d803
zhangshixiong7 <zhangshixiong7@huawei.com>
2023-12-07 19:51:43 +0800

    notify app that window destroyed
    Signed-off-by: zhangshixiong7 <zhangshixiong7@huawei.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
+++ interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
@@ -51,13 +51,14 @@ static int finalizerCnt = 0;
 JsWindow::JsWindow(const sptr<Window>& window)
     : windowToken_(window), registerManager_(std::make_unique<JsWindowRegisterManager>())
 {
-    NotifyNativeWinDestroyFunc func = [](std::string windowName) {
+    NotifyNativeWinDestroyFunc func = [this](std::string windowName) {
         std::lock_guard<std::recursive_mutex> lock(g_mutex);
         if (windowName.empty() || g_jsWindowMap.count(windowName) == 0) {
             WLOGFE("Can not find window %{public}s ", windowName.c_str());
             return;
         }
         g_jsWindowMap.erase(windowName);
+        windowToken_ = nullptr;
         WLOGI("Destroy window %{public}s in js window", windowName.c_str());
     };
     windowToken_->RegisterWindowDestroyedListener(func);
