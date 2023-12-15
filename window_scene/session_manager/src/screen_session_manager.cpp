ccddddd27bb0cb55980c6b309fbe1cfed53bf692
t30053014 <1180300204@stu.hit.edu.cn>
2023-12-15 19:59:01 +0800

    Signed-off-by: t30053014 <1180300204@stu.hit.edu.cn>
    modified:   window_scene/session_manager/src/screen_session_manager.cpp

--- window_scene/session_manager/src/screen_session_manager.cpp
+++ window_scene/session_manager/src/screen_session_manager.cpp
@@ -854,7 +854,6 @@ bool ScreenSessionManager::SetScreenPower(ScreenPowerStatus status, PowerStateCh
     }
 
     if (foldScreenController_ != nullptr) {
-        WLOGFI("SetScreenPower fold screenid:%{public}u" PRIu64".", foldScreenController_->GetCurrentScreenId());
         rsInterface_.SetScreenPowerStatus(foldScreenController_->GetCurrentScreenId(), status);
     } else {
         for (auto screenId : screenIds) {
