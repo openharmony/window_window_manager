3ad33ee36e52ea79557c80144e37e77074afacad
yangruixin <444807624@qq.com>
2023-12-15 17:23:48 +0800

    fix fuzz
    Signed-off-by: yangruixin <444807624@qq.com>

--- test/fuzztest/wms/pip_fuzzer/pip_fuzzer.cpp
+++ test/fuzztest/wms/pip_fuzzer/pip_fuzzer.cpp
@@ -53,8 +53,7 @@ bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
     option->SetContext(static_cast<void*>(context));
     std::string navigationId = "nav_id";
     option->SetNavigationId(navigationId);
-    std::shared_ptr<XComponentController> xComponentController;
-    startPos += GetObject(xComponentController, data + startPos, size - startPos);
+    std::shared_ptr<XComponentController> xComponentController = nullptr;
     option->SetXComponentController(xComponentController);
     uint32_t templateType = 0;
     startPos += GetObject<uint32_t>(templateType, data + startPos, size - startPos);
