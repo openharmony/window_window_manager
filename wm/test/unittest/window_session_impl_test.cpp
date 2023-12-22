5f26983506751a616558b8e675a64f85357c8ff7
yangningning <yangningning8@huawei.com>
2023-12-22 19:11:21 +0800

    FixWindowBad
    Signed-off-by: yangningning <yangningning8@huawei.com>
    Change-Id: I4d1630d857c9931e10a800f18ae53e189ddc3908

--- wm/test/unittest/window_session_impl_test.cpp
+++ wm/test/unittest/window_session_impl_test.cpp
@@ -207,7 +207,7 @@ HWTEST_F(WindowSessionImplTest, SetResizeByDragEnabled01, Function | SmallTest |
     window->hostSession_ = session;
     window->state_ = WindowState::STATE_CREATED;
     retCode = window->SetResizeByDragEnabled(true);
-    ASSERT_EQ(retCode, WMError::WM_DO_NOTHING);
+    ASSERT_EQ(retCode, WMError::WM_OK);
 }
 
 /**
@@ -1386,7 +1386,7 @@ HWTEST_F(WindowSessionImplTest, SetRaiseByClickEnabled01, Function | SmallTest |
     window->hostSession_ = session;
     window->state_ = WindowState::STATE_CREATED;
     retCode = window->SetRaiseByClickEnabled(true);
-    ASSERT_EQ(retCode, WMError::WM_DO_NOTHING);
+    ASSERT_EQ(retCode, WMError::WM_OK);
 }
 
 /**
@@ -1409,7 +1409,7 @@ HWTEST_F(WindowSessionImplTest, HideNonSystemFloatingWindows01, Function | Small
     window->hostSession_ = session;
     window->state_ = WindowState::STATE_CREATED;
     retCode = window->HideNonSystemFloatingWindows(false);
-    ASSERT_EQ(retCode, WMError::WM_DO_NOTHING);
+    ASSERT_EQ(retCode, WMError::WM_OK);
 }
 
 /**
@@ -1578,7 +1578,7 @@ HWTEST_F(WindowSessionImplTest, SetDecorVisible, Function | SmallTest | Level2)
     ASSERT_NE(window, nullptr);
     bool isVisble = true;
     WMError res = window->SetDecorVisible(isVisble);
-    ASSERT_EQ(res, WMError::WM_OK);
+    ASSERT_EQ(res, WMError::WM_ERROR_NULLPTR);
     GTEST_LOG_(INFO) << "WindowSessionImplTest: SetDecorVisibletest01 end";
 }
 
@@ -1597,7 +1597,7 @@ HWTEST_F(WindowSessionImplTest, SetDecorHeight, Function | SmallTest | Level2)
     ASSERT_NE(window, nullptr);
     int32_t height = 50;
     WMError res = window->GetDecorHeight(height);
-    ASSERT_EQ(res, WMError::WM_OK);
+    ASSERT_EQ(res, WMError::WM_ERROR_NULLPTR);
     GTEST_LOG_(INFO) << "WindowSessionImplTest: SetDecorHeighttest01 end";
 }
 
@@ -1616,7 +1616,7 @@ HWTEST_F(WindowSessionImplTest, GetDecorHeight, Function | SmallTest | Level2)
     ASSERT_NE(window, nullptr);
     int32_t height = 0;
     WMError res = window->GetDecorHeight(height);
-    ASSERT_EQ(res, WMError::WM_OK);
+    ASSERT_EQ(res, WMError::WM_ERROR_NULLPTR);
     GTEST_LOG_(INFO) << "WindowSessionImplTest: GetDecorHeighttest01 end";
 }
 
@@ -1635,7 +1635,7 @@ HWTEST_F(WindowSessionImplTest, GetTitleButtonArea, Function | SmallTest | Level
     ASSERT_NE(window, nullptr);
     TitleButtonRect titleButtonRect;
     WMError res = window->GetTitleButtonArea(titleButtonRect);
-    ASSERT_EQ(res, WMError::WM_OK);
+    ASSERT_EQ(res, WMError::WM_ERROR_NULLPTR);
     GTEST_LOG_(INFO) << "WindowSessionImplTest: GetDecorHeighttest01 end";
 }
 }
