65205513d0ef9369de0c469fa02d55b52f441a25
nickyhey <nizhiguang1@huawei.com>
2023-12-26 20:35:41 +0800

    add unit test
    Signed-off-by: nickyhey <nizhiguang1@huawei.com>

--- window_scene/screen_session_manager/src/screen_session_manager_client.cpp
+++ window_scene/screen_session_manager/src/screen_session_manager_client.cpp
@@ -18,11 +18,13 @@
 #include <iservice_registry.h>
 #include <system_ability_definition.h>
 
+#include "permission.h"
 #include "window_manager_hilog.h"
 
 namespace OHOS::Rosen {
 namespace {
 constexpr HiviewDFX::HiLogLabel LABEL = { LOG_CORE, HILOG_DOMAIN_DISPLAY, "ScreenSessionManagerClient" };
+const std::string SCREEN_CAPTURE_PERMISSION = "ohos.permission.CAPTURE_SCREEN";
 } // namespace
 
 WM_IMPLEMENT_SINGLE_INSTANCE(ScreenSessionManagerClient)
@@ -307,12 +309,16 @@ void ScreenSessionManagerClient::UpdateAvailableArea(ScreenId screenId, DMRect a
 std::shared_ptr<Media::PixelMap> ScreenSessionManagerClient::GetScreenSnapshot(ScreenId screenId,
     float scaleX, float scaleY)
 {
+    if (!Permission::IsSystemCalling() || !Permission::CheckCallingPermission(SCREEN_CAPTURE_PERMISSION)) {
+        WLOGFE("get screen session is not system calling, permission denied!");
+        return nullptr;
+    }
+
     auto screenSession = GetScreenSession(screenId);
     if (!screenSession) {
         WLOGFE("get screen session is null");
         return nullptr;
     }
-
     return screenSession->GetScreenSnapshot(scaleX, scaleY);
 }
 } // namespace OHOS::Rosen
--- window_scene/session_manager/include/screen_session_manager.h
+++ window_scene/session_manager/include/screen_session_manager.h
@@ -216,7 +216,7 @@ public:
     void SetScreenPrivacyState(bool hasPrivate) override;
     void UpdateAvailableArea(ScreenId screenId, DMRect area) override;
     DMError GetAvailableArea(DisplayId displayId, DMRect& area) override;
-    void NotifyAvailableAreaChanged(DMRect area);  
+    void NotifyAvailableAreaChanged(DMRect area);
 protected:
     ScreenSessionManager();
     virtual ~ScreenSessionManager() = default;
