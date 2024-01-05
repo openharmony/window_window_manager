944dc4bcc67a5d93ac9a2c0c3a24329ab1d6c952
yu <zhangyusi3@huawei.com>
2024-01-05 11:32:06 +0000

    post task setZOrder
    Signed-off-by: yu <zhangyusi3@huawei.com>
    Change-Id: Ic87dac765600f8b35e1bd8667a29737b39047183

--- window_scene/session/host/src/session.cpp
+++ window_scene/session/host/src/session.cpp
@@ -2028,7 +2028,15 @@ WSError Session::UpdateMaximizeMode(bool isMaximize)
 
 void Session::SetZOrder(uint32_t zOrder)
 {
-    zOrder_ = zOrder;
+    auto task = [weakThis = wptr(this), zOrder]() {
+        auto session = weakThis.promote();
+        if (session == nullptr) {
+            WLOGFE("session is null");
+            return;
+        }
+        session->zOrder_ = zOrder;
+    };
+    PostTask(task, "SetZOrder");
     NotifySessionInfoChange();
 }
 
