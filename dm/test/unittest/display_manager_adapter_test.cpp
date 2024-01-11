1ba0a774216b085c7ff6c119bf08fc3282deb6ad
许杨杨 <xuyangyang29@huawei.com>
2024-01-11 08:42:35 +0000

    update dm/test/unittest/display_manager_adapter_test.cpp.
    Signed-off-by: 许杨杨 <xuyangyang29@huawei.com>

--- dm/test/unittest/display_manager_adapter_test.cpp
+++ dm/test/unittest/display_manager_adapter_test.cpp
@@ -370,8 +370,8 @@ HWTEST_F(DisplayManagerAdapterTest, DestroyVirtualScreen, Function | SmallTest |
  */
 HWTEST_F(DisplayManagerAdapterTest, SetVirtualScreenSurface, Function | SmallTest | Level2)
 {
-    sptr<Surface> surfa;
-    DMError err = SingletonContainer::Get<ScreenManagerAdapter>().SetVirtualScreenSurface(0, surfa);
+    sptr<Surface> sur_face;
+    DMError err = SingletonContainer::Get<ScreenManagerAdapter>().SetVirtualScreenSurface(0, sur_face);
     ASSERT_EQ(err, DMError::DM_ERROR_NULLPTR);
 }
 
