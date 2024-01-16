de7443f186ddb4e395bc9e16b298cdebc8b2d948
hujinzhu <hujinzhu@huawei.com>
2024-01-16 07:23:46 +0000

    hicar
    Signed-off-by: hujinzhu <hujinzhu@huawei.com>
    Change-Id: Idcf0da846ff9ac4ad7eaf0e4e00bc68023d10806

--- window_scene/screen_session_manager/include/screen_session_manager_client.h
+++ window_scene/screen_session_manager/include/screen_session_manager_client.h
@@ -71,6 +71,7 @@ protected:
 
 private:
     void ConnectToServer();
+    bool CheckIfNeedCennectScreen(ScreenId screenId, ScreenId rsId, const std::string& name);
     void OnScreenConnectionChanged(ScreenId screenId, ScreenEvent screenEvent,
         ScreenId rsId, const std::string& name) override;
     void OnPropertyChanged(ScreenId screenId,
