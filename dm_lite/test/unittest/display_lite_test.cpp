fad781b88843cc397d281a416c1010affd8ee095
liuyufeng <liuyufeng52@huawei.com>
2024-01-29 14:32:37 +0800

    add some tdd_test
    Signed-off-by: liuyufeng <liuyufeng52@huawei.com>

--- dm_lite/test/unittest/display_lite_test.cpp
+++ dm_lite/test/unittest/display_lite_test.cpp
@@ -62,6 +62,188 @@ HWTEST_F(DisplayTestLite, UpdateDisplayInfo01, Function | SmallTest | Level1)
     display->UpdateDisplayInfo(displayInfo);
     ASSERT_EQ(displayInfo, nullptr);
 }
+
+/**
+ * @tc.name: UpdateDisplayInfopImpl_
+ * @tc.desc: UpdateDisplayInfo with nullptr
+ * @tc.type: FUNC
+ * @tc.require: issueI5K0JP
+ */
+HWTEST_F(DisplayTestLite, UpdateDisplayInfopImpl_, Function | SmallTest | Level1)
+{
+    GTEST_LOG_(INFO) << "DisplayLite::UpdateDisplayInfo start";
+    sptr<DisplayInfo> displayInfo = new DisplayInfo();
+    sptr<DisplayLite> display = new DisplayLite("", displayInfo);
+    display->UpdateDisplayInfo(displayInfo);
+    ASSERT_NE(displayInfo, nullptr);
+    GTEST_LOG_(INFO) << "DisplayLite::UpdateDisplayInfo end";
+}
+
+/**
+ * @tc.name: UpdateDisplayInfo03
+ * @tc.desc: UpdateDisplayInfo with nullptr
+ * @tc.type: FUNC
+ * @tc.require: issueI5K0JP
+ */
+HWTEST_F(DisplayTestLite, UpdateDisplayInfo03, Function | SmallTest | Level1)
+{
+    GTEST_LOG_(INFO) << "DisplayLite::UpdateDisplayInfo start";
+    sptr<DisplayInfo> displayInfo = nullptr;
+    sptr<DisplayLite> display = new DisplayLite("", displayInfo);
+    display->UpdateDisplayInfo();
+    ASSERT_EQ(displayInfo, nullptr);
+    GTEST_LOG_(INFO) << "DisplayLite::UpdateDisplayInfo end";
+}
+
+/**
+ * @tc.name: GetId01
+ * @tc.desc: UpdateDisplayInfo with nullptr
+ * @tc.type: FUNC
+ * @tc.require: issueI5K0JP
+ */
+HWTEST_F(DisplayTestLite, GetId01, Function | SmallTest | Level1)
+{
+    GTEST_LOG_(INFO) << "DisplayLite::GetId start";
+    sptr<DisplayInfo> displayInfo = nullptr;
+    sptr<DisplayLite> display = new DisplayLite("", displayInfo);
+    uint64_t id;
+    id = display->GetId();
+    ASSERT_EQ(id, 0);
+    GTEST_LOG_(INFO) << "DisplayLite::GetId end";
+}
+
+/**
+ * @tc.name: GetId02
+ * @tc.desc: UpdateDisplayInfo with nullptr
+ * @tc.type: FUNC
+ * @tc.require: issueI5K0JP
+ */
+HWTEST_F(DisplayTestLite, GetId02, Function | SmallTest | Level1)
+{
+    GTEST_LOG_(INFO) << "DisplayLite::GetId start";
+    DisplayInfo *displayInfo = new DisplayInfo();
+    sptr<DisplayLite> display = new DisplayLite("", displayInfo);
+    uint64_t id;
+    id = display->GetId();
+    ASSERT_NE(id, 0);
+    GTEST_LOG_(INFO) << "DisplayLite::GetId end";
+}
+
+/**
+ * @tc.name: GetDisplayInfo01
+ * @tc.desc: UpdateDisplayInfo with nullptr
+ * @tc.type: FUNC
+ * @tc.require: issueI5K0JP
+ */
+HWTEST_F(DisplayTestLite, GetDisplayInfo01, Function | SmallTest | Level1)
+{
+    GTEST_LOG_(INFO) << "DisplayLite::GetId start";
+    sptr<DisplayLite> display = nullptr;
+    sptr<DisplayInfo> displayInfo = new DisplayLite("", displayInfo);;
+    displayInfo = display->GetDisplayInfo();
+    ASSERT_NE(displayInfo, nullptr);
+    GTEST_LOG_(INFO) << "DisplayLite::GetId end";
+}
+
+/**
+ * @tc.name: GetDisplayInfo02
+ * @tc.desc: UpdateDisplayInfo with nullptr
+ * @tc.type: FUNC
+ * @tc.require: issueI5K0JP
+ */
+HWTEST_F(DisplayTestLite, GetDisplayInfo02, Function | SmallTest | Level1)
+{
+    GTEST_LOG_(INFO) << "DisplayLite::GetId start";
+    DisplayInfo *displayInfo = new DisplayInfo();
+    sptr<DisplayLite> display = new DisplayLite("", displayInfo);;
+    displayInfo = display->GetDisplayInfo();
+    ASSERT_NE(displayInfo, nullptr);
+    GTEST_LOG_(INFO) << "DisplayLite::GetId end";
+}
+
+/**
+ * @tc.name: GetWidth01
+ * @tc.desc: UpdateDisplayInfo with nullptr
+ * @tc.type: FUNC
+ * @tc.require: issueI5K0JP
+ */
+HWTEST_F(DisplayTestLite, GetWidth01, Function | SmallTest | Level1)
+{
+    GTEST_LOG_(INFO) << "DisplayLite::GetWidth start";
+    sptr<DisplayLite> display = nullptr;
+    sptr<DisplayInfo> displayInfo = new DisplayLite("", displayInfo);
+    int32_t width;
+    width = display->GetWidth();
+    ASSERT_NE(width, 0);
+    GTEST_LOG_(INFO) << "DisplayLite::GetWidth end";
+}
+
+/**
+ * @tc.name: GetWidth02
+ * @tc.desc: UpdateDisplayInfo with nullptr
+ * @tc.type: FUNC
+ * @tc.require: issueI5K0JP
+ */
+HWTEST_F(DisplayTestLite, GetWidth02, Function | SmallTest | Level1)
+{
+    GTEST_LOG_(INFO) << "DisplayLite::GetWidth start";
+    sptr<DisplayLite> display = new DisplayInfo();
+    sptr<DisplayInfo> displayInfo = new DisplayLite("", displayInfo);
+    int32_t width;
+    width = display->GetWidth();
+    ASSERT_EQ(width, 0);
+    GTEST_LOG_(INFO) << "DisplayLite::GetWidth end";
+}
+
+/**
+ * @tc.name: GetHeight01
+ * @tc.desc: UpdateDisplayInfo with nullptr
+ * @tc.type: FUNC
+ * @tc.require: issueI5K0JP
+ */
+HWTEST_F(DisplayTestLite, GetHeight01, Function | SmallTest | Level1)
+{
+    GTEST_LOG_(INFO) << "DisplayLite::GetWidth start";
+    sptr<DisplayLite> display = nullptr;
+    sptr<DisplayInfo> displayInfo = new DisplayLite("", displayInfo);
+    int32_t Height;
+    Height = display->GetWidth();
+    ASSERT_NE(Height, 0);
+    GTEST_LOG_(INFO) << "DisplayLite::GetWidth end";
+}
+
+/**
+ * @tc.name: GetHeight02
+ * @tc.desc: UpdateDisplayInfo with nullptr
+ * @tc.type: FUNC
+ * @tc.require: issueI5K0JP
+ */
+HWTEST_F(DisplayTestLite, GetHeight02, Function | SmallTest | Level1)
+{
+    GTEST_LOG_(INFO) << "DisplayLite::GetWidth start";
+    sptr<DisplayLite> display = new DisplayInfo();
+    sptr<DisplayInfo> displayInfo = new DisplayLite("", displayInfo);
+    int32_t Height;
+    Height = display->GetWidth();
+    ASSERT_EQ(Height, 0);
+    GTEST_LOG_(INFO) << "DisplayLite::GetWidth end";
+}
+
+/**
+ * @tc.name: GetCutoutInfo
+ * @tc.desc: UpdateDisplayInfo with nullptr
+ * @tc.type: FUNC
+ * @tc.require: issueI5K0JP
+ */
+HWTEST_F(DisplayTestLite, GetCutoutInfo, Function | SmallTest | Level1)
+{
+    GTEST_LOG_(INFO) << "DisplayLite::GetCutoutInfo start";
+    sptr<DisplayLite> display = new DisplayInfo();
+    sptr<DisplayInfo> displayInfo = new DisplayLite("", displayInfo);
+    sptr<CutoutInfo> info = display->GetCutoutInfo();
+    ASSERT_EQ(info, nullptr);
+    GTEST_LOG_(INFO) << "DisplayLite::GetCutoutInfo end";
+}
 }
 } // namespace Rosen
 } // namespace OHOS
