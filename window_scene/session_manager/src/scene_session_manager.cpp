05af24a04b3aa6d2c5afde7900291701c5bb572e
db <dengbiao9@huawei.com>
2024-01-30 11:44:25 +0800

    fix
    Signed-off-by: db <dengbiao9@huawei.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -1280,8 +1280,8 @@ WSError SceneSessionManager::RequestSceneSessionActivationInner(
         scnSessionInfo->want.SetParam(AncoConsts::ANCO_MISSION_ID, scnSessionInfo->persistentId);
         scnSessionInfo->collaboratorType = scnSession->GetCollaboratorType();
     }
-    WLOGFI("[WMSLife]RequestSceneSessionActivationInner: want info sent to AMS - deviceId: %{public}s", \
-        abilityName: %{public}s, bundleName: %{public}s, moduleName: %{public}s, uri: %{public}s,
+    WLOGFI("[WMSLife]RequestSceneSessionActivationInner: want info sent to AMS - deviceId: %{public}s, \
+        abilityName: %{public}s, bundleName: %{public}s, moduleName: %{public}s, uri: %{public}s",
         scnSessionInfo->want.GetElement().GetDeviceID().c_str(),
         scnSessionInfo->want.GetElement().GetAbilityName().c_str(),
         scnSessionInfo->want.GetElement().GetBundleName().c_str(),
