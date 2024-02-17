38f2140b5bb4a85bab148bffafdf5c9a5347d6b7
cuifeihe <cuifeihe@huawei.com>
2024-02-17 19:47:34 +0800

    Description: 增加ScreenSessionManagerClient::SetScreenPrivacyState()中的维测日志 IssueNo: https://gitee.com/openharmony/window_window_manager/issues/I91RM9 Signed-off-by: cuifeihe <cuifeihe@huawei.com>

--- window_scene/session_manager/src/screen_session_manager.cpp
+++ window_scene/session_manager/src/screen_session_manager.cpp
@@ -2674,6 +2674,7 @@ void ScreenSessionManager::NotifyPrivateSessionStateChanged(bool hasPrivate)
 
 void ScreenSessionManager::SetScreenPrivacyState(bool hasPrivate)
 {
+    WLOGFI("SetScreenPrivacyState enter, hasPrivate: %{public}d", hasPrivate);
     ScreenId id = GetDefaultScreenId();
     auto screenSession = GetScreenSession(id);
     if (screenSession == nullptr) {
