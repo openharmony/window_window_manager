1ffc966615a2026022e6da6e7672771feb962e61
z30045483 <zhangzhongqing2@huawei.com>
2024-02-18 23:50:53 +0800

    Signed-off-by: z30045483 <zhangzhongqing2@huawei.com>
    	modified:   window_scene/session/host/src/scene_session.cpp

--- window_scene/session/host/src/scene_session.cpp
+++ window_scene/session/host/src/scene_session.cpp
@@ -801,7 +801,7 @@ void SceneSession::GetKeyboardAvoidArea(WSRect& rect, AvoidArea& avoidArea)
     if (((Session::GetWindowMode() == WindowMode::WINDOW_MODE_FLOATING &&
           WindowHelper::IsMainWindow(Session::GetWindowType())) ||
          (WindowHelper::IsSubWindow(Session::GetWindowType()) &&
-          GetParentSession()->GetWindowMode == WindowMode::WINDOW_MODE_FLOATING)) &&
+          GetParentSession()->GetWindowMode() == WindowMode::WINDOW_MODE_FLOATING)) &&
         (system::GetParameter("const.product.devicetype", "unknown") == "phone" ||
          system::GetParameter("const.product.devicetype", "unknown") == "tablet")) {
         return;
