887eda91783bbdbaf481b6889b33fa7abcb878ab
wangmiaoliang <wangmiaoliang1@huawei.com>
2024-02-18 03:12:12 +0000

    fix warn
    Signed-off-by: wangmiaoliang <wangmiaoliang1@huawei.com>
    Change-Id: I80f23b3498e962b2ec2d43e4764af994ae0f9c2d

--- window_scene/interfaces/include/ws_common.h
+++ window_scene/interfaces/include/ws_common.h
@@ -24,7 +24,6 @@
 
 #include <iremote_broker.h>
 #include <want.h>
-#include "../../interfaces/innerkits/dm/dm_common.h"
 
 namespace OHOS::AAFwk {
 class AbilityStartSetting;
@@ -167,7 +166,7 @@ struct SessionInfo {
     uint32_t windowType_ = 1; // WINDOW_TYPE_APP_MAIN_WINDOW
     sptr<IRemoteObject> callerToken_ = nullptr;
     sptr<IRemoteObject> rootToken_ = nullptr;
-    uint64_t screenId_ = SCREEN_ID_INVALID;
+    uint64_t screenId_ = -1ULL; // -1ULL：SCREEN_ID_INVALID
     bool isPersistentRecover_ = false;
 
     mutable std::shared_ptr<AAFwk::Want> want; // want for ability start
