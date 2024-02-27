1b8d518cda94c6b6c039288d1d14ab8f7b0f7cbe
Ning Yuanfeng <Nyuanfeng@163.com>
2024-02-27 09:00:42 +0800

    修复潜在NPE问题
    Signed-off-by: Ning Yuanfeng <Nyuanfeng@163.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -6499,11 +6499,11 @@ WSError SceneSessionManager::RecoveryPullPiPMainWindow(const int32_t& persistent
         WLOGFE("not pip window");
         return WSError::WS_DO_NOTHING;
     }
-    if (!showPiPMainWindowFunc_) {
-        WLOGFE("showPiPMainWindowFunc_ init error, persistentId: %{public}d", persistentId);
-        return WSError::WS_DO_NOTHING;
-    }
     auto task = [this, scnSession, rect]() {
+        if (!showPiPMainWindowFunc_) {
+            WLOGFE("showPiPMainWindowFunc_ init error, persistentId: %{public}d", persistentId);
+            return WSError::WS_DO_NOTHING;
+        }
         showPiPMainWindowFunc_(scnSession->GetParentPersistentId());
         WSRect rectPos = SessionHelper::TransferToWSRect(rect);
         scnSession->UpdateSessionRect(rectPos, SizeChangeReason::RECOVER);
