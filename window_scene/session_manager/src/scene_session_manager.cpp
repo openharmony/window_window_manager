5ef8d393f5aac9d44ba3eed31757edf7f5614fa8
Ning Yuanfeng <Nyuanfeng@163.com>
2024-02-27 10:22:52 +0800

    修复编译问题
    Signed-off-by: Ning Yuanfeng <Nyuanfeng@163.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -6499,7 +6499,7 @@ WSError SceneSessionManager::RecoveryPullPiPMainWindow(const int32_t& persistent
         WLOGFE("not pip window");
         return WSError::WS_DO_NOTHING;
     }
-    auto task = [this, scnSession, rect]() {
+    auto task = [this, scnSession, rect, persistentId]() {
         if (!showPiPMainWindowFunc_) {
             WLOGFE("showPiPMainWindowFunc_ init error, persistentId: %{public}d", persistentId);
             return WSError::WS_DO_NOTHING;
