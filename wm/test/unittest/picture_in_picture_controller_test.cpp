ac7ebf662066d0e0b568768c48e7819fa1dfb60f
Ning Yuanfeng <Nyuanfeng@163.com>
2024-03-08 14:46:13 +0800

    修改测试用例
    Signed-off-by: Ning Yuanfeng <Nyuanfeng@163.com>

--- wm/test/unittest/picture_in_picture_controller_test.cpp
+++ wm/test/unittest/picture_in_picture_controller_test.cpp
@@ -248,27 +248,6 @@ HWTEST_F(PictureInPictureControllerTest, StartMove, Function | SmallTest | Level
 }
 
 /**
- * @tc.name: PipDisplayListener::OnCreate
- * @tc.desc: PipDisplayListener::OnCreate
- * @tc.type: FUNC
- */
-HWTEST_F(PictureInPictureControllerTest, OnCreate, Function | SmallTest | Level2)
-{
-    sptr<MockWindow> mw = new MockWindow();
-    sptr<PipOption> option = new PipOption();
-    DisplayId displayId = 0;
-    wptr<PictureInPictureController> pipController = nullptr;
-    sptr<PictureInPictureController> pipControl = new PictureInPictureController(option, mw, 100, nullptr);
-
-    sptr<PictureInPictureController::PipDisplayListener> pipDisplayListener =
-        new PictureInPictureController::PipDisplayListener(pipController);
-    pipDisplayListener->OnCreate(displayId);
-    pipDisplayListener->OnDestroy(displayId);
-    pipDisplayListener->OnChange(displayId);
-    ASSERT_NE(WMError::WM_OK, pipControl->CreatePictureInPictureWindow());
-}
-
-/**
  * @tc.name: DoActionEvent
  * @tc.desc: DoActionEvent
  * @tc.type: FUNC
@@ -276,11 +255,12 @@ HWTEST_F(PictureInPictureControllerTest, OnCreate, Function | SmallTest | Level2
 HWTEST_F(PictureInPictureControllerTest, DoActionEvent, Function | SmallTest | Level2)
 {
     std::string actionName = " ";
+    int32_t status = 0;
     sptr<MockWindow> mw = new MockWindow();
     sptr<PipOption> option = new PipOption();
     sptr<PictureInPictureController> pipControl = new PictureInPictureController(option, mw, 100, nullptr);
 
-    pipControl->DoActionEvent(actionName);
+    pipControl->DoActionEvent(actionName, status);
     pipControl->RestorePictureInPictureWindow();
     GTEST_LOG_(INFO) << "TearDownCasecccccc5";
     pipControl->ResetExtController();
