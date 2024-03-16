154cccbd5a1a047291ddb6d97b15c1739f9b7976
chehang <chehang1@huawei.com>
2024-03-16 15:45:11 +0800

    feat: send hover event to extension
    Signed-off-by: chehang <chehang1@huawei.com>
    Change-Id: Id522003cea67c85d537a72f3414172e357e243c3

--- window_scene/session/container/include/zidl/session_stage_interface.h
+++ window_scene/session/container/include/zidl/session_stage_interface.h
@@ -39,6 +39,45 @@ class ISessionStage : public IRemoteBroker {
 public:
     DECLARE_INTERFACE_DESCRIPTOR(u"OHOS.ISessionStage");
 
+    // IPC interface
+    virtual WSError SetActive(bool active) = 0;
+    virtual WSError UpdateRect(const WSRect& rect, SizeChangeReason reason,
+        const std::shared_ptr<RSTransaction>& rsTransaction = nullptr) = 0;
+    virtual void UpdateDensity() = 0;
+    virtual WSError HandleBackEvent() = 0;
+    virtual WSError MarkProcessed(int32_t eventId) = 0;
+    virtual WSError UpdateFocus(bool isFocused) = 0;
+    virtual WSError NotifyDestroy() = 0;
+    virtual WSError NotifyCloseExistPipWindow() = 0;
+    virtual WSError NotifyTransferComponentData(const AAFwk::WantParams& wantParams)
+    {
+        return WSError::WS_OK;
+    }
+    virtual WSErrorCode NotifyTransferComponentDataSync(const AAFwk::WantParams& wantParams,
+        AAFwk::WantParams& reWantParams)
+    {
+        return WSErrorCode::WS_OK;
+    }
+    virtual void NotifyOccupiedAreaChangeInfo(sptr<OccupiedAreaChangeInfo> info) = 0;
+    virtual WSError UpdateAvoidArea(const sptr<AvoidArea>& avoidArea, AvoidAreaType type) = 0;
+    virtual void NotifyScreenshot() = 0;
+    virtual void DumpSessionElementInfo(const std::vector<std::string>& params) = 0;
+    virtual WSError NotifyTouchOutside() = 0;
+    virtual WSError NotifyWindowVisibility(bool isVisible) = 0;
+    virtual WSError UpdateWindowMode(WindowMode mode) = 0;
+    virtual void NotifyForegroundInteractiveStatus(bool interactive) = 0;
+    virtual WSError UpdateMaximizeMode(MaximizeMode mode) = 0;
+    virtual void NotifySessionForeground(uint32_t reason, bool withAnimation) = 0;
+    virtual void NotifySessionBackground(uint32_t reason, bool withAnimation, bool isFromInnerkits) = 0;
+    virtual WSError UpdateTitleInTargetPos(bool isShow, int32_t height) = 0;
+    virtual void NotifyTransformChange(const Transform& transform) = 0;
+    virtual WSError NotifyDialogStateChange(bool isForeground)
+    {
+        return WSError::WS_OK;
+    }
+
+
+    // **Non** IPC interface
     virtual void NotifyBackpressedEvent(bool& isConsumed) {}
     virtual void NotifyPointerEvent(const std::shared_ptr<MMI::PointerEvent>& pointerEvent) {}
     virtual void NotifyKeyEvent(const std::shared_ptr<MMI::KeyEvent>& keyEvent, bool& isConsumed,
@@ -48,10 +87,6 @@ public:
     virtual int32_t GetPersistentId() const
     {
         return -1;
-    }
-    virtual WSError NotifyTransferComponentData(const AAFwk::WantParams& wantParams)
-    {
-        return WSError::WS_OK;
     }
     virtual WSError NotifySearchElementInfoByAccessibilityId(int64_t elementId, int32_t mode, int64_t baseParent,
         std::list<Accessibility::AccessibilityElementInfo>& infos)
@@ -78,37 +113,11 @@ public:
     {
         return WSError::WS_OK;
     }
-    virtual WSErrorCode NotifyTransferComponentDataSync(const AAFwk::WantParams& wantParams,
-        AAFwk::WantParams& reWantParams)
-    {
-        return WSErrorCode::WS_OK;
-    }
-    virtual WSError NotifyDialogStateChange(bool isForeground)
+    virtual WSError NotifyAccessibilityHoverEvent(float pointX, float pointY, int32_t sourceType, int32_t eventType,
+        int64_t timeMs)
     {
         return WSError::WS_OK;
     }
-    virtual WSError SetActive(bool active) = 0;
-    virtual WSError UpdateRect(const WSRect& rect, SizeChangeReason reason,
-        const std::shared_ptr<RSTransaction>& rsTransaction = nullptr) = 0;
-    virtual void UpdateDensity() = 0;
-    virtual WSError HandleBackEvent() = 0;
-    virtual WSError MarkProcessed(int32_t eventId) = 0;
-    virtual WSError UpdateFocus(bool isFocused) = 0;
-    virtual WSError NotifyDestroy() = 0;
-    virtual WSError NotifyCloseExistPipWindow() = 0;
-    virtual void NotifyOccupiedAreaChangeInfo(sptr<OccupiedAreaChangeInfo> info) = 0;
-    virtual WSError UpdateAvoidArea(const sptr<AvoidArea>& avoidArea, AvoidAreaType type) = 0;
-    virtual void NotifyScreenshot() = 0;
-    virtual void DumpSessionElementInfo(const std::vector<std::string>& params) = 0;
-    virtual WSError NotifyTouchOutside() = 0;
-    virtual WSError NotifyWindowVisibility(bool isVisible) = 0;
-    virtual WSError UpdateWindowMode(WindowMode mode) = 0;
-    virtual void NotifyForegroundInteractiveStatus(bool interactive) = 0;
-    virtual WSError UpdateMaximizeMode(MaximizeMode mode) = 0;
-    virtual void NotifySessionForeground(uint32_t reason, bool withAnimation) = 0;
-    virtual void NotifySessionBackground(uint32_t reason, bool withAnimation, bool isFromInnerkits) = 0;
-    virtual WSError UpdateTitleInTargetPos(bool isShow, int32_t height) = 0;
-    virtual void NotifyTransformChange(const Transform& transform) = 0;
 };
 } // namespace OHOS::Rosen
 #endif // OHOS_WINDOW_SCENE_SESSION_STAGE_INTERFACE_H
