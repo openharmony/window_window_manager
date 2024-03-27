c640b797985c2c6068e71572872a8b19c86f3d95
qintongtong <qintongtong@huawei.com>
2024-03-27 11:27:24 +0800

    修复keyEvent锁阻塞问题
    Signed-off-by: qintongtong <qintongtong@huawei.com>
    Change-Id: I47e3174af9ec408a8e40e2d95f0e7bf6b149dc31

--- window_scene/session/host/include/session.h
+++ window_scene/session/host/include/session.h
@@ -496,7 +496,7 @@ protected:
     sptr<Session> parentSession_;
 
     mutable std::mutex pointerEventMutex_;
-    mutable std::mutex keyEventMutex_;
+    mutable std::shared_mutex keyEventMutex_;
 
 private:
     void HandleDialogForeground();
