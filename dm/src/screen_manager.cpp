abb66d7aa30ff110f7968c397e7585ebfa7ec1c7
liubo <liubo419@huawei.com>
2024-03-29 15:36:09 +0800

    Description: DMS日志降级 IssueNo:https://gitee.com/openharmony/window_window_manager/issues/I9CP4X Feature or Bugfix: Bugfix Binary Source:No Signed-off-by: liubo419 <liubo419@huawei.com>

--- dm/src/screen_manager.cpp
+++ dm/src/screen_manager.cpp
@@ -639,7 +639,7 @@ bool ScreenManager::Impl::UpdateScreenInfoLocked(sptr<ScreenInfo> screenInfo)
         return false;
     }
     ScreenId screenId = screenInfo->GetScreenId();
-    WLOGFI("screenId:%{public}" PRIu64".", screenId);
+    WLOGFD("screenId:%{public}" PRIu64".", screenId);
     if (screenId == SCREEN_ID_INVALID) {
         WLOGFE("displayId is invalid.");
         return false;
@@ -662,7 +662,7 @@ bool ScreenManager::Impl::isAllListenersRemoved() const
 
 void ScreenManager::Impl::OnRemoteDied()
 {
-    WLOGFI("dms is died");
+    WLOGFD("dms is died");
     std::lock_guard<std::recursive_mutex> lock(mutex_);
     screenManagerListener_ = nullptr;
     virtualScreenAgent_ = nullptr;
