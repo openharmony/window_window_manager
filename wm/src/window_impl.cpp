d7921d400af7cf0aec0676cbe497bbcad4b234f4
zhangkai <zhangkai324@huawei.com>
2024-03-29 11:04:24 +0800

    log bug fix
    Signed-off-by: zhangkai <zhangkai324@huawei.com>
    Change-Id: I98a0a9712072bba95ce2f2f26639ce93e34f04c1

--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -1169,7 +1169,7 @@ void WindowImpl::SetDefaultDisplayIdIfNeed()
             SingletonContainer::Get<DisplayManager>().GetDefaultDisplayId();
         defaultDisplayId = (defaultDisplayId == DISPLAY_ID_INVALID)? 0 : defaultDisplayId;
         property_->SetDisplayId(defaultDisplayId);
-        WLOGFI("Reset displayId to %{public}llu", defaultDisplayId);
+        WLOGFI("Reset displayId to %{public}" PRIu64, defaultDisplayId);
     }
 }
 
@@ -1180,6 +1180,7 @@ WMError WindowImpl::Create(uint32_t parentId, const std::shared_ptr<AbilityRunti
     if (ret != WMError::WM_OK) {
         return ret;
     }
+    property_->SetDisplayId(DISPLAY_ID_INVALID);
     SetDefaultDisplayIdIfNeed();
     context_ = context;
     sptr<WindowImpl> window(this);
