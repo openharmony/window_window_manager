7d97c8045dc4c24950d96f15b00416515e3426aa
孙健 <sunjian89@huawei.com>
2024-03-30 15:36:17 +0000

    update wmserver/include/zidl/window_manager_lite_interface.h.
    Signed-off-by: 孙健 <sunjian89@huawei.com>

--- wmserver/include/zidl/window_manager_lite_interface.h
+++ wmserver/include/zidl/window_manager_lite_interface.h
@@ -18,10 +18,10 @@
 
 #include <iremote_broker.h>
 
+#include "interfaces/include/ws_common.h"
 #include "window_property.h"
 #include "window_transition_info.h"
 #include "zidl/window_manager_agent_interface.h"
-#include "interfaces/include/ws_common.h"
 
 namespace OHOS {
 namespace MMI {
