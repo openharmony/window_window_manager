0b13c1a79e0f8fdac0fc18d2109c9da7171d6f06
chengyiyi <chengyiyi1@huawei.com>
2024-03-30 06:51:29 +0000

    update window_scene/session_manager/src/scene_session_dirty_manager.cpp.
    Signed-off-by: chengyiyi <chengyiyi1@huawei.com>

--- window_scene/session_manager/src/scene_session_dirty_manager.cpp
+++ window_scene/session_manager/src/scene_session_dirty_manager.cpp
@@ -438,8 +438,8 @@ MMI::WindowInfo SceneSessionDirtyManager::GetWindowInfo(const sptr<SceneSession>
         .zOrder = zOrder,
         .transform = transformData
     };
-    if (sceneSession->GetSessionProperty()->GetWindowFlags() &
-        static_cast<uint32_t>(WindowFlag::WINDOW_FLAG_HANDWRITING)) {
+    if (sceneSession->GetSessionProperty() != nullptr && (sceneSession->GetSessionProperty()->GetWindowFlags() &
+        static_cast<uint32_t>(WindowFlag::WINDOW_FLAG_HANDWRITING))) {
         WLOGFI("Add handwrite flag for session, id: %{public}d", windowId);
         windowInfo.flags |= MMI::WindowInfo::FLAG_BIT_HANDWRITING;
     }
