clipsheep6/window_manager

Keyboard Session Refactor
Signed-off-by: wulehui<wulehui@huawei.com>

window_scene/session/host/include/zidl/session_stub.h
@@ -66,6 +66,8 @@ private:
     int HandleSendPointerEvenForMoveDrag(MessageParcel& data, MessageParcel& reply);
     int HandleSetLandscapeMultiWindow(MessageParcel& data, MessageParcel& reply);
     int HandleUpdateRectChangeListenerRegistered(MessageParcel& data, MessageParcel& reply);
+    int HandleSetSessionGravity(MessageParcel& data, MessageParcel& reply);
+    int HandleSetCallingWindowId(MessageParcel& data, MessageParcel& reply);
 
     // extension extension
     int HandleTransferAbilityResult(MessageParcel& data, MessageParcel& reply);

window_scene/session/host/src/zidl/session_proxy.cpp
@@ -1097,4 +1097,49 @@ WSError SessionProxy::UpdateRectChangeListenerRegistered(bool isRegister)
     int32_t ret = reply.ReadInt32();
     return static_cast<WSError>(ret);
 }
+
+WSError SessionProxy::SetSessionGravity(SessionGravity gravity, uint32_t percent)
+{
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    if (!data.WriteInterfaceToken(GetDescriptor())) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "WriteInterfaceToken failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    if (!data.WriteUint32(static_cast<uint32_t>(gravity))) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "Write gravity failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    if (!data.WriteUint32(percent)) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "Write percent failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    if (Remote()->SendRequest(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_SET_SESSION_GRAVITY),
+        data, reply, option) != ERR_NONE) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "SendRequest failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    return static_cast<WSError>(reply.ReadInt32());
+}
+
+void SessionProxy::SetCallingWindowId(const uint32_t callingWindowId)
+{
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    if (!data.WriteInterfaceToken(GetDescriptor())) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "writeInterfaceToken failed");
+        return;
+    }
+    if (!data.WriteUint32(callingWindowId)) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "Write callingWindowId failed.");
+        return;
+    }
+    if (Remote()->SendRequest(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_SET_CALLING_WINDOW_ID),
+        data, reply, option) != ERR_NONE) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "SendRequest failed");
+        return;
+    }
+}
 } // namespace OHOS::Rosen

window_scene/session/host/src/zidl/session_stub.cpp
@@ -97,6 +97,10 @@ const std::map<uint32_t, SessionStubFunc> SessionStub::stubFuncMap_ {
         &SessionStub::HandleProcessPointDownSession),
     std::make_pair(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_SEND_POINTEREVENT_FOR_MOVE_DRAG),
         &SessionStub::HandleSendPointerEvenForMoveDrag),
+    std::make_pair(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_SET_SESSION_GRAVITY),
+        &SessionStub::HandleSetSessionGravity),
+    std::make_pair(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_SET_CALLING_WINDOW_ID),
+        &SessionStub::HandleSetCallingWindowId),
 
     std::make_pair(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_TRANSFER_ABILITY_RESULT),
         &SessionStub::HandleTransferAbilityResult),
@@ -624,4 +628,25 @@ int SessionStub::HandleUpdateRectChangeListenerRegistered(MessageParcel& data, M
     reply.WriteUint32(static_cast<uint32_t>(errCode));
     return ERR_NONE;
 }
+
+int SessionStub::HandleSetSessionGravity(MessageParcel &data, MessageParcel &reply)
+{
+    TLOGD(WmsLogTag::WMS_KEYBOARD, "run HandleSetSessionGravity!");
+    SessionGravity gravity = static_cast<SessionGravity>(data.ReadUint32());
+    uint32_t percent = data.ReadUint32();
+    WSError ret = SetSessionGravity(gravity, percent);
+    reply.WriteInt32(static_cast<int32_t>(ret));
+    return ERR_NONE;
+}
+
+int SessionStub::HandleSetCallingWindowId(MessageParcel& data, MessageParcel& reply)
+{
+    TLOGE(WmsLogTag::WMS_KEYBOARD, "run HandleSetCallingWindowId!");
+    uint32_t callingWindowId = data.ReadUint32();
+
+    SetCallingWindowId(callingWindowId);
+    reply.WriteInt32(static_cast<int32_t>(WSError::WS_OK));
+    return ERR_NONE;
+}
+
 } // namespace OHOS::Rosen

openharmony/window_window_manager

window_scene/session/host/include/zidl/session_stub.h
@@ -66,6 +66,8 @@ private:
     int HandleSendPointerEvenForMoveDrag(MessageParcel& data, MessageParcel& reply);
     int HandleSetLandscapeMultiWindow(MessageParcel& data, MessageParcel& reply);
     int HandleUpdateRectChangeListenerRegistered(MessageParcel& data, MessageParcel& reply);
+    int HandleSetKeyboardSessionGravity(MessageParcel& data, MessageParcel& reply);
+    int HandleSetCallingSessionId(MessageParcel& data, MessageParcel& reply);
 
     // extension extension
     int HandleTransferAbilityResult(MessageParcel& data, MessageParcel& reply);

window_scene/session/host/src/zidl/session_proxy.cpp
@@ -1097,4 +1097,49 @@ WSError SessionProxy::UpdateRectChangeListenerRegistered(bool isRegister)
     int32_t ret = reply.ReadInt32();
     return static_cast<WSError>(ret);
 }
+
+WSError SessionProxy::SetKeyboardSessionGravity(SessionGravity gravity, uint32_t percent)
+{
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option(MessageOption::TF_ASYNC);
+    if (!data.WriteInterfaceToken(GetDescriptor())) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "WriteInterfaceToken failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    if (!data.WriteUint32(static_cast<uint32_t>(gravity))) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "Write gravity failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    if (!data.WriteUint32(percent)) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "Write percent failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    if (Remote()->SendRequest(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_SET_KEYBOARD_SESSION_GRAVITY),
+        data, reply, option) != ERR_NONE) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "SendRequest failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    return static_cast<WSError>(reply.ReadInt32());
+}
+
+void SessionProxy::SetCallingSessionId(const uint32_t callingSessionId)
+{
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option(MessageOption::TF_ASYNC);
+    if (!data.WriteInterfaceToken(GetDescriptor())) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "writeInterfaceToken failed");
+        return;
+    }
+    if (!data.WriteUint32(callingSessionId)) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "Write callingSessionId failed.");
+        return;
+    }
+    if (Remote()->SendRequest(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_SET_CALLING_SESSION_ID),
+        data, reply, option) != ERR_NONE) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "SendRequest failed");
+        return;
+    }
+}
 } // namespace OHOS::Rosen

window_scene/session/host/src/zidl/session_stub.cpp
@@ -97,6 +97,10 @@ const std::map<uint32_t, SessionStubFunc> SessionStub::stubFuncMap_ {
         &SessionStub::HandleProcessPointDownSession),
     std::make_pair(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_SEND_POINTEREVENT_FOR_MOVE_DRAG),
         &SessionStub::HandleSendPointerEvenForMoveDrag),
+    std::make_pair(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_SET_KEYBOARD_SESSION_GRAVITY),
+        &SessionStub::HandleSetKeyboardSessionGravity),
+    std::make_pair(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_SET_CALLING_SESSION_ID),
+        &SessionStub::HandleSetCallingSessionId),
 
     std::make_pair(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_TRANSFER_ABILITY_RESULT),
         &SessionStub::HandleTransferAbilityResult),
@@ -622,6 +626,26 @@ int SessionStub::HandleUpdateRectChangeListenerRegistered(MessageParcel& data, M
     bool isRegister = data.ReadBool();
     WSError errCode = UpdateRectChangeListenerRegistered(isRegister);
     reply.WriteUint32(static_cast<uint32_t>(errCode));
+    return ERR_NONE;
+}
+
+int SessionStub::HandleSetKeyboardSessionGravity(MessageParcel &data, MessageParcel &reply)
+{
+    TLOGD(WmsLogTag::WMS_KEYBOARD, "run HandleSetKeyboardSessionGravity!");
+    SessionGravity gravity = static_cast<SessionGravity>(data.ReadUint32());
+    uint32_t percent = data.ReadUint32();
+    WSError ret = SetKeyboardSessionGravity(gravity, percent);
+    reply.WriteInt32(static_cast<int32_t>(ret));
+    return ERR_NONE;
+}
+
+int SessionStub::HandleSetCallingSessionId(MessageParcel& data, MessageParcel& reply)
+{
+    TLOGD(WmsLogTag::WMS_KEYBOARD, "run HandleSetCallingSessionId!");
+    uint32_t callingSessionId = data.ReadUint32();
+
+    SetCallingSessionId(callingSessionId);
+    reply.WriteInt32(static_cast<int32_t>(WSError::WS_OK));
     return ERR_NONE;
 }
 } // namespace OHOS::Rosen