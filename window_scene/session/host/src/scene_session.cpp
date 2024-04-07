da39a957da2730599e87b07a461ca63b6f42612f
yangwenxin <yangwenxin9@huawei.com>
2024-04-07 01:53:19 +0000

    update window_scene/session/host/src/scene_session.cpp.
    Signed-off-by: yangwenxin <yangwenxin9@huawei.com>

--- window_scene/session/host/src/scene_session.cpp
+++ window_scene/session/host/src/scene_session.cpp
@@ -1976,9 +1976,7 @@ WSError SceneSession::PendingSessionActivation(const sptr<AAFwk::SessionInfo> ab
             info.windowMode = info.want->GetIntParam(AAFwk::Want::PARAM_RESV_WINDOW_MODE, 0);
             info.sessionAffinity = info.want->GetStringParam(Rosen::PARAM_KEY::PARAM_MISSION_AFFINITY_KEY);
             info.screenId_ = info.want->GetIntParam(AAFwk::Want::PARAM_RESV_DISPLAY_ID, -1);
-            TLOGI(WmsLogTag::WMS_LIFE, "PendingSessionActivation: want: windowMode %{public}d, \
-                want: sessionAffinity %{public}, want: screenId %{public}",
-                info.windowMode, info.sessionAffinity, info.screenId_);
+            TLOGI(WmsLogTag::WMS_LIFE, "PendingSessionActivation: want: screenId %{public}" PRIu64, info.screenId_);
         }
 
         TLOGI(WmsLogTag::WMS_LIFE, "PendingSessionActivation: bundleName %{public}s, moduleName:%{public}s, \
