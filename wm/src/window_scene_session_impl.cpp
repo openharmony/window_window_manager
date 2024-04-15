7488be4fbfe55f85a1eb89e3d4e5eafef95bf380
chengzhen <chengzhen25@huawei.com>
2024-04-15 03:08:30 +0000

    update wm/src/window_scene_session_impl.cpp.
    Signed-off-by: chengzhen <chengzhen25@huawei.com>

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -195,8 +195,7 @@ WMError WindowSceneSessionImpl::CreateAndConnectSpecificSession()
         }
         PreProcessCreate();
         auto abilityContext = AbilityRuntime::Context::ConvertTo<AbilityRuntime::AbilityContext>(context_);
-        if (property_ && context_ && context_->GetHapModuleInfo() && abilityContext &&
-            abilityContext->GetAbilityInfo()) {
+        if (property_ && abilityContext && abilityContext->GetAbilityInfo()) {
             auto info = property_->GetSessionInfo();
             info.bundleName_ = abilityContext->GetAbilityInfo()->bundleName;
             property_->SetSessionInfo(info);
