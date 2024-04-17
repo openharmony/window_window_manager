c781dbaafc2f87a7db4d3429cc88e8a1090a7567
yang-yuanzhou <yangyuanzhou@huawei.com>
2024-04-17 08:27:35 +0800

    子窗添加bundleName
    Signed-off-by: yang-yuanzhou <yangyuanzhou@huawei.com>

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -177,6 +177,12 @@ WMError WindowSceneSessionImpl::CreateAndConnectSpecificSession()
                 property_->GetWindowName().c_str(), type);
             return WMError::WM_ERROR_NULLPTR;
         }
+        auto abilityContext = AbilityRuntime::Context::ConvertTo<AbilityRuntime::AbilityContext>(context_);
+        if (property_ && abilityContext && abilityContext->GetAbilityInfo()) {
+            auto info = property_->GetSessionInfo();
+            info.bundleName_ = abilityContext->GetAbilityInfo()->bundleName;
+            property_->SetSessionInfo(info);
+        }
         // set parent persistentId
         property_->SetParentPersistentId(parentSession->GetPersistentId());
         // creat sub session by parent session
