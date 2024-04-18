1adf497883c368b94b5624a5d77b166a2fc728e6
zhudaodong <zhudaodong1@huawei.com>
2024-04-18 21:27:38 +0800

    调用电源改为异步
    Signed-off-by: zhudaodong <zhudaodong1@huawei.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -2977,24 +2977,20 @@ WSError SceneSessionManager::SetBrightness(const sptr<SceneSession>& sceneSessio
     }
     sceneSession->SetBrightness(brightness);
 #ifdef POWERMGR_DISPLAY_MANAGER_ENABLE
-    if (GetDisplayBrightness() != brightness) {
+    if (GetDisplayBrightness() != brightness && eventHandler_ != nullptr) {
         bool setBrightnessRet = false;
         if (std::fabs(brightness - UNDEFINED_BRIGHTNESS) < std::numeric_limits<float>::min()) {
-            if (eventHandler_ != nullptr) {
-                auto task = [this]() {
-                    DisplayPowerMgr::DisplayPowerMgrClient::GetInstance().RestoreBrightness();
-                };
-                setBrightnessRet = eventHandler_->PostTask(task, "DisplayPowerMgr:RestoreBrightness", 0);
-            }
+            auto task = [this]() {
+                DisplayPowerMgr::DisplayPowerMgrClient::GetInstance().RestoreBrightness();
+            };
+            setBrightnessRet = eventHandler_->PostTask(task, "DisplayPowerMgr:RestoreBrightness", 0);
             SetDisplayBrightness(UNDEFINED_BRIGHTNESS); // UNDEFINED_BRIGHTNESS means system default brightness
         } else {
-            if (eventHandler_ != nullptr) {
-                auto task = [this, brightness]() {
-                    DisplayPowerMgr::DisplayPowerMgrClient::GetInstance().OverrideBrightness(
-                        static_cast<uint32_t>(brightness * MAX_BRIGHTNESS));
-                };
-                setBrightnessRet = eventHandler_->PostTask(task, "DisplayPowerMgr:OverrideBrightness", 0);
-            }
+            auto task = [this, brightness]() {
+                DisplayPowerMgr::DisplayPowerMgrClient::GetInstance().OverrideBrightness(
+                    static_cast<uint32_t>(brightness * MAX_BRIGHTNESS));
+            };
+            setBrightnessRet = eventHandler_->PostTask(task, "DisplayPowerMgr:OverrideBrightness", 0);
             SetDisplayBrightness(brightness);
         }
         if (!setBrightnessRet) {
