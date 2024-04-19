92621e169e08a6354f219f83375b28b870bd4a6a
zhangminrui <zhangminrui1@huawei.com>
2024-04-19 16:07:09 +0800

    新需求:沉浸式能力增强，补充设置状态栏标志位
    Signed-off-by: zhangminrui <zhangminrui1@huawei.com>

--- window_scene/common/src/window_session_property.cpp
+++ window_scene/common/src/window_session_property.cpp
@@ -487,7 +487,8 @@ bool WindowSessionProperty::MarshallingSystemBarMap(Parcel& parcel) const
             return false;
         }
         if (!(parcel.WriteBool(it.second.enable_) && parcel.WriteUint32(it.second.backgroundColor_) &&
-              parcel.WriteUint32(it.second.contentColor_) && parcel.WriteBool(it.second.enableAnimation_))) {
+              parcel.WriteUint32(it.second.contentColor_) && parcel.WriteBool(it.second.enableAnimation_) &&
+              parcel.WriteUint32(static_cast<uint32_t>(it.second.settingFlag_)))) {
             return false;
         }
     }
@@ -504,7 +505,8 @@ void WindowSessionProperty::UnMarshallingSystemBarMap(Parcel& parcel, WindowSess
 
     for (uint32_t i = 0; i < size; i++) {
         WindowType type = static_cast<WindowType>(parcel.ReadUint32());
-        SystemBarProperty prop = { parcel.ReadBool(), parcel.ReadUint32(), parcel.ReadUint32(), parcel.ReadBool() };
+        SystemBarProperty prop = { parcel.ReadBool(), parcel.ReadUint32(), parcel.ReadUint32(), parcel.ReadBool(),
+            static_cast<SystemBarSettingFlag>(parcel.ReadUint32()) };
         property->SetSystemBarProperty(type, prop);
     }
 }
