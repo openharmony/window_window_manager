0084dfa207e5662556843f7a35ef860c78e5497c
黄四平 <huangsiping3@huawei.com>
2024-04-30 03:02:40 +0000

    update window_scene/session_manager/include/zidl/scene_session_manager_lite_proxy.h.
    Signed-off-by: 黄四平 <huangsiping3@huawei.com>

--- window_scene/session_manager/include/zidl/scene_session_manager_lite_proxy.h
+++ window_scene/session_manager/include/zidl/scene_session_manager_lite_proxy.h
@@ -59,7 +59,7 @@ public:
         const sptr<IWindowManagerAgent>& windowManagerAgent) override;
     WMError CheckWindowId(int32_t windowId, int32_t &pid) override;
     WMError GetVisibilityWindowInfo(std::vector<sptr<WindowVisibilityInfo>>& infos) override;
-    WMError GetWindowBackHomeStatus(bool &isBackHome) override;
+    WMError GetWindowModStatus(WindowModeType &windowMod) override;
     WMError GetMainWindowInfos(int32_t topNum, std::vector<MainWindowInfo>& topNInfo) override;
 
 private:
