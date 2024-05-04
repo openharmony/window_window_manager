8d5286cb7dcc51e02242692998b1be7c76a0ad8a
鄢吉傲 <914927840@qq.com>
2024-05-04 15:55:11 +0800

    增加窗口拉起拦截：限制非PC设备只允许可交互窗口、有后台拉起权限、以及长时任务拉起其他窗口
    Signed-off-by: 鄢吉傲 <914927840@qq.com>

--- window_scene/session/host/src/zidl/session_stub.cpp
+++ window_scene/session/host/src/zidl/session_stub.cpp
@@ -341,6 +341,7 @@ int SessionStub::HandlePendingSessionActivation(MessageParcel& data, MessageParc
     abilitySessionInfo->uiAbilityId = data.ReadInt64();
     abilitySessionInfo->callingTokenId = data.ReadUint32();
     abilitySessionInfo->reuse = data.ReadBool();
+    abilitySessionInfo->hasContinuousTask = data.ReadBool();
     abilitySessionInfo->processOptions.reset(data.ReadParcelable<AAFwk::ProcessOptions>());
     if (data.ReadBool()) {
         abilitySessionInfo->callerToken = data.ReadRemoteObject();
