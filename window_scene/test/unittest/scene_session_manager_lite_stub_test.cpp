72e3b44d2d36c7e20d6a757a7bc200f92831d731
yangningning <yangningning8@huawei.com>
2024-05-05 09:13:51 +0000

    update window_scene/test/unittest/scene_session_manager_lite_stub_test.cpp.
    Signed-off-by: yangningning <yangningning8@huawei.com>

--- window_scene/test/unittest/scene_session_manager_lite_stub_test.cpp
+++ window_scene/test/unittest/scene_session_manager_lite_stub_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,6 +14,7 @@
  */
 
 #include <gtest/gtest.h>
+#include <ipc_types.h>
 
 #include "session_manager/include/zidl/scene_session_manager_lite_stub.h"
 
@@ -22,13 +23,104 @@ using namespace testing::ext;
 
 namespace OHOS {
 namespace Rosen {
+class MockSceneSessionManagerLiteStub : public SceneSessionManagerLiteStub {
+    WSError SetSessionLabel(const sptr<IRemoteObject>& token, const std::string& label) override {
+        return WSError::WS_OK;
+    }
+    WSError SetSessionIcon(const sptr<IRemoteObject>& token, const std::shared_ptr<Media::PixelMap>& icon) override {
+        return WSError::WS_OK;
+    }
+    WSError IsValidSessionIds(const std::vector<int32_t>& sessionIds, std::vector<bool>& results) override {
+        return WSError::WS_OK;
+    }
+    WSError PendingSessionToForeground(const sptr<IRemoteObject>& token) override {
+        return WSError::WS_OK;
+    }
+    WSError PendingSessionToBackgroundForDelegator(const sptr<IRemoteObject>& token) override {
+        return WSError::WS_OK;
+    }
+    WSError GetFocusSessionToken(sptr<IRemoteObject>& token) override {
+        return WSError::WS_OK;
+    }
+    WSError GetFocusSessionElement(AppExecFwk::ElementName& element) override {
+        return WSError::WS_OK;
+    }
+    WSError RegisterSessionListener(const sptr<ISessionListener>& listener) override {
+        return WSError::WS_OK;
+    }
+    WSError UnRegisterSessionListener(const sptr<ISessionListener>& listener) override {
+        return WSError::WS_OK;
+    }
+    WSError GetSessionInfos(const std::string& deviceId,
+                                    int32_t numMax, std::vector<SessionInfoBean>& sessionInfos) override {
+                                        return WSError::WS_OK;
+    }
+    WSError GetSessionInfo(const std::string& deviceId, int32_t persistentId, SessionInfoBean& sessionInfo) override {
+        return WSError::WS_OK;
+    }
+    WSError GetSessionInfoByContinueSessionId(const std::string& continueSessionId,
+        SessionInfoBean& sessionInfo) override {
+            return WSError::WS_OK;
+    }
+    WSError SetSessionContinueState(const sptr<IRemoteObject>& token, const ContinueState& continueState) override {
+        return WSError::WS_OK;
+    }
+    WSError TerminateSessionNew(
+        const sptr<AAFwk::SessionInfo> info, bool needStartCaller, bool isFromBroker = false) override {
+            return WSError::WS_OK;
+    }
+    WSError GetSessionSnapshot(const std::string& deviceId, int32_t persistentId,
+                                       SessionSnapshot& snapshot, bool isLowResolution) override {
+                                          return WSError::WS_OK;
+    }
+    WSError ClearSession(int32_t persistentId) override {
+        return WSError::WS_OK;
+    }
+    WSError ClearAllSessions() override {
+        return WSError::WS_OK;
+    }
+    WSError LockSession(int32_t sessionId) override {
+        return WSError::WS_OK;
+    }
+    WSError UnlockSession(int32_t sessionId) override {
+        return WSError::WS_OK;
+    }
+    WSError MoveSessionsToForeground(const std::vector<std::int32_t>& sessionIds, int32_t topSessionId) override {
+        return WSError::WS_OK;
+    }
+    WSError MoveSessionsToBackground(const std::vector<std::int32_t>& sessionIds,
+        std::vector<std::int32_t>& result) override {
+            return WSError::WS_OK;
+    }
+    WMError RegisterWindowManagerAgent(WindowManagerAgentType type,
+        const sptr<IWindowManagerAgent>& windowManagerAgent) override {
+            return WMError::WM_OK;
+    }
+    WMError UnregisterWindowManagerAgent(WindowManagerAgentType type,
+        const sptr<IWindowManagerAgent>& windowManagerAgent) override {
+            return WMError::WM_OK;
+    }
+    void GetFocusWindowInfo(FocusChangeInfo& focusInfo) override {
+
+    }
+    WMError CheckWindowId(int32_t windowId, int32_t &pid) override {
+        return WMError::WM_OK;
+    }
+    WMError GetMainWindowInfos(int32_t topNum, std::vector<MainWindowInfo>& topNInfo) override {
+        return WMError::WM_OK;
+    }
+    sptr<IRemoteObject> AsObject() override {
+        return nullptr;
+    }
+};
+
 class SceneSessionManagerLiteStubTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
     void SetUp() override;
     void TearDown() override;
-    sptr<SceneSessionManagerLiteStubTest> sceneSessionManagerLiteStub_ = nullptr;
+    sptr<SceneSessionManagerLiteStub> sceneSessionManagerLiteStub_ = nullptr;
 };
 
 void SceneSessionManagerLiteStubTest::SetUpTestCase()
@@ -41,7 +133,8 @@ void SceneSessionManagerLiteStubTest::TearDownTestCase()
 
 void SceneSessionManagerLiteStubTest::SetUp()
 {
-    sceneSessionManagerLiteStub_ = new SceneSessionManagerLiteStubTest();
+    sceneSessionManagerLiteStub_ = new MockSceneSessionManagerLiteStub();
+    EXPECT_NE(nullptr, sceneSessionManagerLiteStub_);
 }
 
 void SceneSessionManagerLiteStubTest::TearDown()
@@ -52,17 +145,20 @@ void SceneSessionManagerLiteStubTest::TearDown()
 namespace {
 /**
  * @tc.name: OnRemoteRequest
- * @tc.desc: OnRemoteRequest test
+ * @tc.desc: test function : OnRemoteRequest
  * @tc.type: FUNC
  */
 HWTEST_F(SceneSessionManagerLiteStubTest, OnRemoteRequest, Function | SmallTest | Level1)
 {
-    uint32_t code = static_cast<uint32_t>(SceneSessionManagerLiteStub::SceneSessionManagerLiteMessage
-        ::TRANS_ID_SET_SESSION_LABEL);
+    uint32_t code = static_cast<uint32_t>(SceneSessionManagerLiteStub::
+        SceneSessionManagerLiteMessage::TRANS_ID_SET_SESSION_LABEL);
     MessageParcel data;
     MessageParcel reply;
     MessageOption option;
-    data.WritenInterfaceToken(u"OpenHameny");
+    data.WriteInterfaceToken(u"OpenHarmeny");
+    auto res = sceneSessionManagerLiteStub_->
+        SceneSessionManagerLiteStub::OnRemoteRequest(code, data, reply, option);
+    EXPECT_EQ(ERR_INVALID_STATE, res);
 }
 
 }
