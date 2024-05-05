3059c7428225c1c1ec9bd3a5cd150592ae7ca06e
xiongyonglong <xiongyonglong@huawei.com>
2024-05-05 13:49:56 +0800

    提供SCB窗口创建时默认不获取焦点的功能
    Signed-off-by: xiongyonglong <xiongyonglong@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session_manager.h
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session_manager.h
@@ -65,6 +65,7 @@ public:
     static napi_value AddWindowDragHotArea(napi_env env, napi_callback_info info);
     static napi_value PreloadInLakeApp(napi_env env, napi_callback_info info);
     static napi_value RequestFocusStatus(napi_env env, napi_callback_info info);
+    static napi_value ResetFocusedOnShow(napi_env env, napi_callback_info info);
     static napi_value RequestAllAppSessionUnfocus(napi_env env, napi_callback_info info);
     static napi_value SetScreenLocked(napi_env env, napi_callback_info info);
     static napi_value UpdateTitleInTargetPos(napi_env env, napi_callback_info info);
@@ -116,6 +117,7 @@ private:
     napi_value OnAddWindowDragHotArea(napi_env env, napi_callback_info info);
     napi_value OnPreloadInLakeApp(napi_env env, napi_callback_info info);
     napi_value OnRequestFocusStatus(napi_env env, napi_callback_info info);
+    napi_value OnResetFocusedOnShow(napi_env env, napi_callback_info info);
     napi_value OnRequestAllAppSessionUnfocus(napi_env env, napi_callback_info info);
     napi_value OnSetScreenLocked(napi_env env, napi_callback_info info);
     napi_value OnUpdateMaximizeMode(napi_env env, napi_callback_info info);
