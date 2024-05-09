a9e08bbd7a181653de2aa5abc45984ada96c4023
wangjian10 <wangjian10@huawei.com>
2024-05-09 17:04:58 +0800

    add window manager fuzz test
    Signed-off-by: wangjian10 <wangjian10@huawei.com>

---
+++ test/fuzztest/wms/windowmanageragentstubstate_fuzzer/windowmanageragentstubstate_fuzzer.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cstddef>
+#include <cstdint>
+#include <parcel.h>
+#include <securec.h>
+
+#include <iremote_stub.h>
+#include "message_option.h"
+#include "message_parcel.h"
+#include "marshalling_helper.h"
+#include "window_manager_agent.h"
+#include "windowmanageragentstubstate_fuzzer.h"
+
+using namespace OHOS::Rosen;
+
+namespace OHOS {
+namespace {
+    constexpr size_t DATA_MIN_SIZE = 2;
+}
+
+template<class T>
+size_t GetObject(T &object, const uint8_t *data, size_t size)
+{
+    size_t objectSize = sizeof(object);
+    if (objectSize > size) {
+        return 0;
+    }
+    return memcpy_s(&object, objectSize, data, objectSize) == EOK ? objectSize : 0;
+}
+
+bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size < DATA_MIN_SIZE) {
+        return false;
+    }
+    
+    MessageParcel parcel;
+    MessageParcel reply;
+    MessageOption option;
+
+    parcel.WriteInterfaceToken(Rosen::WindowManagerAgentStub::GetDescriptor());
+    parcel.WriteBuffer(data, size);
+    parcel.RewindRead(0);
+    std::shared_ptr<WindowManagerAgent> wmStub = std::make_shared<WindowManagerAgent>();
+    wmStub->OnRemoteRequest(
+        static_cast<uint32_t>(Rosen::IWindowManagerAgent::WindowManagerAgentMsg::TRANS_ID_UPDATE_WINDOW_DRAWING_STATE),
+        parcel, reply, option);
+    return true;
+}
+} // namespace.OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
