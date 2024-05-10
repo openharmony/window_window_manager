256da449d3681ce6574ef56256b557bf71964637
toptao2020 <toptao1993@163.com>
2024-05-10 09:33:02 +0800

    UpdateProperty按照flag增量更新
    Signed-off-by: toptao2020 <toptao1993@163.com>

--- window_scene/common/include/window_session_property.h
+++ window_scene/common/include/window_session_property.h
@@ -160,8 +160,8 @@ public:
     void SetIsLayoutFullScreen(bool isLayoutFullScreen);
     int32_t GetCollaboratorType() const;
     void SetCollaboratorType(int32_t collaboratorType);
-    bool Write(Parcel& parcel, PropertyChangeAction action);
-    void Read(Parcel& parcel, PropertyChangeAction action);
+    bool Write(Parcel& parcel, WSPropertyChangeAction action);
+    void Read(Parcel& parcel, WSPropertyChangeAction action);
 
 private:
     bool MarshallingTouchHotAreas(Parcel& parcel) const;
