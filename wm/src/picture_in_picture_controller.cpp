384f6f22bcb3be33af7fda511314418397c11ed6
Ning Yuanfeng <Nyuanfeng@163.com>
2024-05-14 10:55:40 +0800

    修复编译问题
    Signed-off-by: Ning Yuanfeng <Nyuanfeng@163.com>

--- wm/src/picture_in_picture_controller.cpp
+++ wm/src/picture_in_picture_controller.cpp
@@ -512,7 +512,7 @@ void PictureInPictureController::RestorePictureInPictureWindow()
         curState_ = PiPWindowState::STATE_RESTORING;
         auto stopTask = [weakThis = wptr(this)]() {
             auto controller = weakThis.promote();
-            curState_ = PiPWindowState::STATE_UNDEFINED;
+            controller->curState_ = PiPWindowState::STATE_UNDEFINED;
             if (!controller) {
                 TLOGE(WmsLogTag::WMS_PIP, "controller is nullptr");
                 return;
