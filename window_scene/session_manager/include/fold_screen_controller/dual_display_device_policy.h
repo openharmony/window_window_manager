7a858c51c138088268d6804f9f53bbff9d653845
t30053014 <1180300204@alu.hit.edu.cn>
2024-05-17 15:05:26 +0800

    Signed-off-by: t30053014 <1180300204@alu.hit.edu.cn>
    modified:   window_scene/session_manager/include/fold_screen_controller/dual_display_device_policy.h
    modified:   window_scene/session_manager/src/fold_screen_controller/dual_display_device_policy.cpp

--- window_scene/session_manager/include/fold_screen_controller/dual_display_device_policy.h
+++ window_scene/session_manager/include/fold_screen_controller/dual_display_device_policy.h
@@ -39,7 +39,7 @@ private:
     void ChangeScreenDisplayModeToFull(sptr<ScreenSession> screenSession);
     void ChangeScreenDisplayModeToMainOnBootAnimation(sptr<ScreenSession> screenSession);
     void ChangeScreenDisplayModeToFullOnBootAnimation(sptr<ScreenSession> screenSession);
-    void ChangeScreenDisplayModePower(bool isStartUp, ScreenPowerStatus screenPowerStatus);
+    void ChangeScreenDisplayModePower(ScreenPowerStatus screenPowerStatus);
     void RecoverWhenBootAnimationExit();
     void TriggerScreenDisplayModeUpdate(FoldDisplayMode displayMode);
     FoldDisplayMode GetModeMatchStatus();
