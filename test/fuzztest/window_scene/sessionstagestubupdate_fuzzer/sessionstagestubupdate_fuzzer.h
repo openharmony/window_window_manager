fecbe955562c128e32dbbd84323f55833a069beb
wangjian10 <wangjian10@huawei.com>
2024-05-18 23:38:01 +0800

    add seesion stage code test
    Signed-off-by: wangjian10 <wangjian10@huawei.com>

--- test/fuzztest/window_scene/sessionstagestubupdate_fuzzer/sessionstagestubupdate_fuzzer.h
+++ test/fuzztest/window_scene/sessionstagestubupdate_fuzzer/sessionstagestubupdate_fuzzer.h
@@ -13,9 +13,9 @@
  * limitations under the License.
  */
 
-#ifndef TEST_FUZZTEST_SESSION_STAGE_STUB_SIZE_FUZZER_H
-#define TEST_FUZZTEST_SESSION_STAGE_STUB_SIZE_FUZZER_H
+#ifndef TEST_FUZZTEST_SESSION_STAGE_STUB_UPDATE_FUZZER_H
+#define TEST_FUZZTEST_SESSION_STAGE_STUB_UPDATE_FUZZER_H
 
-#define FUZZ_PROJECT_NAME "sessionstagestubsize_fuzzer"
+#define FUZZ_PROJECT_NAME "sessionstagestubupdate_fuzzer"
 
 #endif
