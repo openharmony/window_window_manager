2d3a7048245d672a514b41629acf50fedb33b406
eager1 <chenyige2@huawei.com>
2024-05-20 20:55:58 +0800

    Window transmit vsyncId to arkui
    Signed-off-by: eager1 <chenyige2@huawei.com>
    Change-Id: I73674bd86118a890ec0e4c9dd70958c6bb54bb45

--- wm/include/vsync_station.h
+++ wm/include/vsync_station.h
@@ -58,8 +58,8 @@ public:
     }
 
 private:
-    static void OnVsync(int64_t nanoTimestamp, void* client);
-    void VsyncCallbackInner(int64_t nanoTimestamp);
+    static void OnVsync(int64_t nanoTimestamp, int64_t frameCount, void* client);
+    void VsyncCallbackInner(int64_t nanoTimestamp, int64_t frameCount);
     void OnVsyncTimeOut();
     void Init();
 
@@ -76,7 +76,7 @@ private:
     std::unordered_set<std::shared_ptr<VsyncCallback>> vsyncCallbacks_;
     VSyncReceiver::FrameCallback frameCallback_ = {
         .userData_ = this,
-        .callback_ = OnVsync,
+        .callbackWithId_ = OnVsync,
     };
     std::shared_ptr<AppExecFwk::EventHandler> vsyncHandler_ = nullptr;
     AppExecFwk::EventHandler::Callback vsyncTimeoutCallback_ = std::bind(&VsyncStation::OnVsyncTimeOut, this);
