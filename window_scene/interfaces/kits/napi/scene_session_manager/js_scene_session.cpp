0bcb54a72b5e9c35e717478e4957b11f515419ec
dengbing <dengbing9@huawei.com>
2024-05-20 11:06:26 +0800

    拖拽全屏恢复自由窗概率性未恢复自由窗大小
    Signed-off-by: dengbing <dengbing9@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
@@ -2614,7 +2614,7 @@ napi_value JsSceneSession::SetWindowLastRect(napi_env env, napi_callback_info in
 napi_value JsSceneSession::OnSetWindowLastRect(napi_env env, napi_callback_info info)
 {
     size_t argc = 4;
-    napi_value argv[argc] = {nullptr};
+    napi_value argv[4] = {nullptr};
     napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
     if (argc < SCALE_ARG_COUNT) {
         TLOGE(WmsLogTag::WMS_LAYOUT, "[NAPI]Argc is invalid: %{public}zu", argc);
