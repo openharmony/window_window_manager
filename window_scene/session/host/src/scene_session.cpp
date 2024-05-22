abb36e8512e02237faf2dcb944a6553c35278f06
zhudaodong <zhudaodong1@huawei.com>
2024-05-22 17:23:06 +0800

    devicetype整改
    Signed-off-by: zhudaodong <zhudaodong1@huawei.com>

--- window_scene/session/host/src/scene_session.cpp
+++ window_scene/session/host/src/scene_session.cpp
@@ -1003,8 +1003,8 @@ void SceneSession::GetSystemAvoidArea(WSRect& rect, AvoidArea& avoidArea)
          Session::GetWindowMode() == WindowMode::WINDOW_MODE_SPLIT_PRIMARY ||
          Session::GetWindowMode() == WindowMode::WINDOW_MODE_SPLIT_SECONDARY) &&
          WindowHelper::IsMainWindow(Session::GetWindowType()) &&
-        (systemConfig_.uiType_ == "phone" || systemConfig_.uiType_ == "pad") &&
-        (!screenSession || screenSession->GetName() != "HiCar")) {
+         (systemConfig_.uiType_ == "phone" || systemConfig_.uiType_ == "pad") &&
+         (!screenSession || screenSession->GetName() != "HiCar")) {
         float miniScale = 0.316f; // Pressed mini floating Scale with 0.001 precision
         if (Session::GetFloatingScale() <= miniScale) {
             return;
