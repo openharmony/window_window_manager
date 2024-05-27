7bb935735fbac69e056e375e621b319baf01b4eb
白钰胜 <baiyusheng@h-partners.com>
2024-05-27 14:30:32 +0800

    Add TDD UT
    Signed-off-by: 白钰胜 <baiyusheng@h-partners.com>

---
+++ window_scene/test/unittest/scene_session_manager_lite_test.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "session_manager/include/scene_session_manager.h"
+#include "session_manager/include/scene_session_manager_lite.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Rosen {
+class SceneSessionManagerLiteTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+};
+
+void SceneSessionManagerLiteTest::SetUpTestCase()
+{
+}
+
+void SceneSessionManagerLiteTest::TearDownTestCase()
+{
+}
+
+void SceneSessionManagerLiteTest::SetUp()
+{
+}
+
+void SceneSessionManagerLiteTest::TearDown()
+{
+}
+
+namespace {
+/**
+ * @tc.name: GetInstance
+ * @tc.desc: test function : GetInstance
+ * @tc.type: FUNC
+ */
+HWTEST_F(SceneSessionManagerLiteTest, GetInstance, Function | SmallTest | Level1)
+{
+    SceneSessionManagerLite& instance1 = SceneSessionManagerLite::GetInstance();
+    EXPECT_NE(nullptr, &instance1);
+    SceneSessionManagerLite& instance2 = SceneSessionManagerLite::GetInstance();
+    EXPECT_EQ(&instance1, &instance2);
+}
+}
+}
+}
