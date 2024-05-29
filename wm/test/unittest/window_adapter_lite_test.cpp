ea77cec949fa7347edf5396608a5b4346fbf9eff
许静怡 <xujingyi19@huawei.com>
2024-05-29 05:45:42 +0000

    update wm/test/unittest/window_adapter_lite_test.cpp.
    Signed-off-by: 许静怡 <xujingyi19@huawei.com>

--- wm/test/unittest/window_adapter_lite_test.cpp
+++ wm/test/unittest/window_adapter_lite_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -85,7 +85,10 @@ HWTEST_F(WindowAdapterLiteTest, UnregisterWindowManagerAgent01, Function | Small
     windowAdapterLite_->OnUserSwitch();
     windowAdapterLite_->ClearWindowAdapter();
 
-    sptr<WMSDeathRecipient> wmSDeathRecipient = new WMSDeathRecipient();
+    sptr<WMSDeathRecipient> wmSDeathRecipient = new (std::nothrow) WMSDeathRecipient();
+    if (!wmSDeathRecipient) {
+        return;
+    }
     wptr<IRemoteObject> wptrDeath;
     wmSDeathRecipient->OnRemoteDied(wptrDeath);
 
