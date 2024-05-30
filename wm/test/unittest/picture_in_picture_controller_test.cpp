7182353175b8a63e169bc0fd8be3696d4aa015e7
XX <mapengfei20@huawei.com>
2024-05-30 20:09:07 +0800

    修改TDD用例
    Signed-off-by: XX <mapengfei20@huawei.com>

--- wm/test/unittest/picture_in_picture_controller_test.cpp
+++ wm/test/unittest/picture_in_picture_controller_test.cpp
@@ -43,6 +43,7 @@ class MockWindow : public Window {
 public:
     MockWindow() {};
     ~MockWindow() {};
+    MOCK_METHOD02(Show, WMError(uint32_t reason, bool withAnimation));
     MOCK_METHOD0(Destroy, WMError());
 };
 
