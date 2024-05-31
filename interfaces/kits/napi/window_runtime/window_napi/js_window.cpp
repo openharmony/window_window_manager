d665351887db56eee1425b772a2e3c6a93b75264
wulei <19072970815@163.com>
2024-05-31 14:31:47 +0800

    屏蔽CreateJsWindowObject函数的cfi检查
    Signed-off-by: wulei <19072970815@163.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
+++ interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
@@ -4800,6 +4800,7 @@ std::shared_ptr<NativeReference> FindJsWindowObject(std::string windowName)
     return g_jsWindowMap[windowName];
 }
 
+__attribute__((no_sanitize("cfi")))
 napi_value CreateJsWindowObject(napi_env env, sptr<Window>& window)
 {
     std::string windowName = window->GetWindowName();
