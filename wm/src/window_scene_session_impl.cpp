d1d45184bc28957cd5b334663193193fac2f711e
yuanxiaojie <yuanxiaojie1@huawei.com>
2024-05-31 17:06:41 +0800

    换接口位置
    Signed-off-by: yuanxiaojie <yuanxiaojie1@huawei.com>

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -878,7 +878,7 @@ WMError WindowSceneSessionImpl::Show(uint32_t reason, bool withAnimation)
     }
 
     if (ret == WMError::WM_OK) {
-        if (state_ == WindowState::STATE_HIDDEN){
+        if (state_ == WindowState::STATE_HIDDEN) {
             UpdateBufferAvailableCallbackFlag(true);
         }
         // update sub window state if this is main window
