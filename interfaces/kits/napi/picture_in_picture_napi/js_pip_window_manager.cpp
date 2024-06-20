fce5e528d5658ef12e217521e219f7a01a3d334a
XX <mapengfei20@huawei.com>
2024-06-20 10:18:44 +0800

    画中画控制面板样式调整
    Signed-off-by: XX <mapengfei20@huawei.com>

--- interfaces/kits/napi/picture_in_picture_napi/js_pip_window_manager.cpp
+++ interfaces/kits/napi/picture_in_picture_napi/js_pip_window_manager.cpp
@@ -91,16 +91,16 @@ static int32_t checkControlsRules(PipOption& option, uint32_t pipTemplateType,
     return 0;
 }
 
-static std::vector<std::uint32_t> uniqueControls(std::vector<std::uint32_t>& controlGroups) 
+static std::vector<std::uint32_t> uniqueControls(std::vector<std::uint32_t>& controlGroups)
 {
     auto item = controlGroups.begin();
     std::set<uint32_t> result;
     while (item != controlGroups.end()) {
         if (result.find(*item) != result.end()) {
-            TLOGE(WmsLogTag::WMS_PIP, "item not find in result, item: %{public}u", *item);
+            TLOGE(WmsLogTag::WMS_PIP, "item  is found in result, item: %{public}u", *item);
             item = controlGroups.erase(item);
         } else {
-            TLOGE(WmsLogTag::WMS_PIP, "item id found in result, item: %{public}u", *item);
+            TLOGE(WmsLogTag::WMS_PIP, "item not find in result, item: %{public}u", *item);
             result.insert(*item);
             item++;
         }
