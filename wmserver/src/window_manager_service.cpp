9164360bdb2f42030a54ed5ec88cb7d992ef2c72
z30052579 <zhonglinjie@huawei.com>
2024-06-24 21:11:56 +0800

    supplement impliment for api GetSnapshot at old framework
    Signed-off-by: z30052579 <zhonglinjie@huawei.com>

--- wmserver/src/window_manager_service.cpp
+++ wmserver/src/window_manager_service.cpp
@@ -1399,6 +1399,10 @@ WmErrorCode WindowManagerService::RaiseToAppTop(uint32_t windowId)
 
 std::shared_ptr<Media::PixelMap> WindowManagerService::GetSnapshot(int32_t windowId)
 {
+    if (!Permission::IsSystemCalling() && !Permission::IsStartByHdcd()) {
+        WLOGFE("window raise to app top permission denied!");
+        return WmErrorCode::WM_ERROR_NOT_SYSTEM_APP;
+    }
     auto task = [this, windowId]() {
         return windowController_->GetSnapshot(windowId);
     };
