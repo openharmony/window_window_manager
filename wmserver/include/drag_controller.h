972c583a321559b8c1666ec48dd3e28ce3a7556b
鄢吉傲 <914927840@qq.com>
2024-06-26 10:35:11 +0000

    update wmserver/include/drag_controller.h.
    Signed-off-by: 鄢吉傲 <914927840@qq.com>

--- wmserver/include/drag_controller.h
+++ wmserver/include/drag_controller.h
@@ -64,8 +64,7 @@ class MoveDragController : public RefBase {
 public:
     MoveDragController() : windowProperty_(new WindowProperty()), moveDragProperty_(new MoveDragProperty())
     {
-        vsyncCallback_->onCallback = 
-            [this](int64_t timeStamp) { this->OnReceiveVsync(timeStamp); };
+        vsyncCallback_->onCallback = std::bind(&MoveDragController::OnReceiveVsync, this, std::placeholders::_1);
     }
     ~MoveDragController() = default;
 
