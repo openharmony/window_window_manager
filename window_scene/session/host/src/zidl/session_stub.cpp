e2c404e460a83f4a8089ae71fcb46bd0520a7eb3
李聪 <licong113@huawei.com>
2024-06-26 09:09:13 +0000

    手机应用上PC
    Signed-off-by: 李聪 <licong113@huawei.com>

--- window_scene/session/host/src/zidl/session_stub.cpp
+++ window_scene/session/host/src/zidl/session_stub.cpp
@@ -273,6 +273,8 @@ int SessionStub::HandleConnect(MessageParcel& data, MessageParcel& reply)
         reply.WriteUint32(winRect.width_);
         reply.WriteUint32(winRect.height_);
         reply.WriteInt32(property->GetCollaboratorType());
+        reply.WriteBool(property->GetCompatibleMode());
+        reply.WriteBool(property->GetIsSupportRotation());
     }
     reply.WriteUint32(static_cast<uint32_t>(errCode));
     return ERR_NONE;
