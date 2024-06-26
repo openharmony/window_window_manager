69e5c2c3fdb4b4eeb439fd790617e3d3b55f1452
鄢吉傲 <914927840@qq.com>
2024-06-26 07:58:20 +0000

    update interfaces/innerkits/wm/wm_common.h.
    Signed-off-by: 鄢吉傲 <914927840@qq.com>

--- interfaces/innerkits/wm/wm_common.h
+++ interfaces/innerkits/wm/wm_common.h
@@ -872,7 +872,7 @@ struct PiPTemplateInfo {
     std::vector<uint32_t> controlGroup;
 };
 
-using OnCallback = std::function<void(int64_t, int64_t)>;
+using OnCallback = std::function<void()>;
 
 /**
  * @struct VsyncCallback
