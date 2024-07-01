b49fc3db89c79fa3c2638d7fe8d524956cdf66f5
h00833914 <hongxiaoyu2@huawei.com>
2024-07-01 07:06:08 +0000

    增加JsSessionType::TYPE_DIVIDER
    Signed-off-by: h00833914 <hongxiaoyu2@huawei.com>
    Change-Id: Ie657a70b774effa5de6aed20388368891fb82115

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_utils.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_utils.cpp
@@ -1083,6 +1083,7 @@ napi_value SessionTypeInit(napi_env env)
     SetTypeProperty(objValue, env, "TYPE_THEME_EDITOR", JsSessionType::TYPE_THEME_EDITOR);
     SetTypeProperty(objValue, env, "TYPE_NAVIGATION_INDICATOR", JsSessionType::TYPE_NAVIGATION_INDICATOR);
     SetTypeProperty(objValue, env, "TYPE_HANDWRITE", JsSessionType::TYPE_HANDWRITE);
+    SetTypeProperty(objValue, env, "TYPE_DIVIDER", JsSessionType::TYPE_DIVIDER);
     return objValue;
 }
 
