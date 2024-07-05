09d1bccfc8f7065d0a1d5cea17508838827f0c4d
马健 <majian59@huawei.com>
2024-07-05 07:58:31 +0000

    update wm/src/window_scene_session_impl.cpp.
    Signed-off-by: 马健 <majian59@huawei.com>

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -676,9 +676,7 @@ void WindowSceneSessionImpl::GetConfigurationFromAbilityInfo()
         UpdateProperty(WSPropertyChangeAction::ACTION_UPDATE_MODE_SUPPORT_INFO);
         auto isPhone = windowSystemConfig_.uiType_ == "phone";
         auto isPad = windowSystemConfig_.uiType_ == "pad";
-        auto isCompatibleModeInPc = property_->GetCompatibleModeInPc();
-        if (modeSupportInfo == WindowModeSupport::WINDOW_MODE_SUPPORT_FULLSCREEN && !isPhone && !isPad
-            && !isCompatibleModeInPc) {
+        if (modeSupportInfo == WindowModeSupport::WINDOW_MODE_SUPPORT_FULLSCREEN && !isPhone && !isPad) {
             SetFullScreen(true);
         }
         // get orientation configuration
