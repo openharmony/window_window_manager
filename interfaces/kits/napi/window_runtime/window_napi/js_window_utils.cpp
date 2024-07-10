f887907a312e31e2c76f026eeeeea613b50e4ae6
qiwenchao <q30045973@china.huawei.com>
2024-07-10 20:14:45 +0800

    update
    Signed-off-by: qiwenchao <q30045973@china.huawei.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window_utils.cpp
+++ interfaces/kits/napi/window_runtime/window_napi/js_window_utils.cpp
@@ -398,6 +398,7 @@ napi_value GetRectAndConvertToJsValue(napi_env env, const Rect& rect)
 {
     napi_value objValue = nullptr;
     CHECK_NAPI_CREATE_OBJECT_RETURN_IF_NULL(env, objValue);
+
     napi_set_named_property(env, objValue, "left", CreateJsValue(env, rect.posX_));
     napi_set_named_property(env, objValue, "top", CreateJsValue(env, rect.posY_));
     napi_set_named_property(env, objValue, "width", CreateJsValue(env, rect.width_));
@@ -481,25 +482,6 @@ napi_value CreateJsSystemBarPropertiesObject(napi_env env, sptr<Window>& window)
     return objValue;
 }
 
-napi_value CreateJsWindowInfoObject(napi_env env, sptr<WindowVisibilityInfo>& info)
-{
-    napi_value objValue = nullptr;
-    CHECK_NAPI_CREATE_OBJECT_RETURN_IF_NULL(env, objValue);
-    napi_set_named_property(env, objValue, "rect", GetRectAndConvertToJsValue(env, info->GetRect()));
-    napi_set_named_property(env, objValue, "bundleName", CreateJsValue(env, info->GetBundleName()));
-    napi_set_named_property(env, objValue, "abilityName", CreateJsValue(env, info->GetAbilityName()));
-    napi_set_named_property(env, objValue, "windowId", CreateJsValue(env, info->GetWindowId()));
-    napi_set_named_property(env, objValue, "windowStatusType",
-        CreateJsValue(env, static_cast<int32_t>(info->GetWindowStatus())));
-    auto windowType = info->GetWindowType();
-    if (NATIVE_JS_TO_WINDOW_TYPE_MAP.count(windowType) != 0) {
-        napi_set_named_property(env, objValue, "windowType",
-            CreateJsValue(env, NATIVE_JS_TO_WINDOW_TYPE_MAP.at(windowType)));
-    } else {
-        napi_set_named_property(env, objValue, "windowType", CreateJsValue(env, windowType));
-    }
-    return objValue;
-}
 
 static napi_value CreateJsSystemBarRegionTintObject(napi_env env, const SystemBarRegionTint& tint)
 {
@@ -526,12 +508,12 @@ static napi_value CreateJsSystemBarRegionTintObject(napi_env env, const SystemBa
     return objValue;
 }
 
-napi_value CreateJsWindowInfoArrayObject(napi_env env, std::vector<sptr<WindowVisibilityInfo>> infos)
+napi_value CreateJsWindowInfoArrayObject(napi_env env, const std::vector<sptr<WindowVisibilityInfo>>& infos)
 {
     napi_value arrayValue = nullptr;
     napi_create_array_with_length(env, infos.size(), &arrayValue);
     if (arrayValue == nullptr) {
-        WLOGFE("[NAPI]Failed to convert subWinVec to jsArrayObject");
+        WLOGFE("[NAPI]Failed to convert windowVisibilityInfo to jsArrayObject");
         return nullptr;
     }
     uint32_t index = 0;
@@ -539,6 +521,26 @@ napi_value CreateJsWindowInfoArrayObject(napi_env env, std::vector<sptr<WindowVi
         napi_set_element(env, arrayValue, index++, CreateJsWindowInfoObject(env, infos[i]));
     }
     return arrayValue;
+}
+
+napi_value CreateJsWindowInfoObject(napi_env env, const sptr<WindowVisibilityInfo>& info)
+{
+    napi_value objValue = nullptr;
+    CHECK_NAPI_CREATE_OBJECT_RETURN_IF_NULL(env, objValue);
+    napi_set_named_property(env, objValue, "rect", GetRectAndConvertToJsValue(env, info->GetRect()));
+    napi_set_named_property(env, objValue, "bundleName", CreateJsValue(env, info->GetBundleName()));
+    napi_set_named_property(env, objValue, "abilityName", CreateJsValue(env, info->GetAbilityName()));
+    napi_set_named_property(env, objValue, "windowId", CreateJsValue(env, info->GetWindowId()));
+    napi_set_named_property(env, objValue, "windowStatusType",
+        CreateJsValue(env, static_cast<int32_t>(info->GetWindowStatus())));
+    auto windowType = info->GetWindowType();
+    if (NATIVE_JS_TO_WINDOW_TYPE_MAP.count(windowType) != 0) {
+        napi_set_named_property(env, objValue, "windowType",
+            CreateJsValue(env, NATIVE_JS_TO_WINDOW_TYPE_MAP.at(windowType)));
+    } else {
+        napi_set_named_property(env, objValue, "windowType", CreateJsValue(env, windowType));
+    }
+    return objValue;
 }
 
 napi_value CreateJsSystemBarRegionTintArrayObject(napi_env env, const SystemBarRegionTints& tints)
