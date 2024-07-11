ada3425f0e196afdea61d3cb6db858b1dbdf60a2
cuifeihe <cuifeihe@huawei.com>
2024-07-11 21:11:54 +0800

    Description: 增加维测 IssueNo: https://gitee.com/openharmony/drivers_hdf_core/issues/IACDWJ Signed-off-by: cuifeihe <cuifeihe@huawei.com>

--- window_scene/session_manager/src/screen_session_manager.cpp
+++ window_scene/session_manager/src/screen_session_manager.cpp
@@ -4357,7 +4357,10 @@ void ScreenSessionManager::GetCurrentScreenPhyBounds(float& phyWidth, float& phy
             isReset = false;
         }
     } else {
+        int id = HiviewDFX::XCollie::GetInstance().SetTimer("GetCurrentScreenPhyBounds", XCOLLIE_TIMEOUT_S, nullptr,
+            nullptr, HiviewDFX::XCOLLIE_FLAG_RECOVERY);
         auto remoteScreenMode = rsInterface_.GetScreenActiveMode(screenid);
+        HiviewDFX::XCollie::GetInstance().CancelTimer(id);
         phyWidth = remoteScreenMode.GetScreenWidth();
         phyHeight = remoteScreenMode.GetScreenHeight();
     }
