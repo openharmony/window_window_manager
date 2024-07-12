4754d027ec7e531cdd1c9acc84d271ffc1fdfbd0
神气龙 <shenqilong1@huawei.com>
2024-07-12 02:05:58 +0000

    set unique density dpi for window
    Signed-off-by: 神气龙 <shenqilong1@huawei.com>

--- window_scene/session/container/include/zidl/session_stage_stub.h
+++ window_scene/session/container/include/zidl/session_stage_stub.h
@@ -66,6 +66,7 @@ private:
     int HandleSwitchFreeMultiWindow(MessageParcel& data, MessageParcel& reply);
     int HandleGetUIContentRemoteObj(MessageParcel& data, MessageParcel& reply);
     int HandleNotifyKeyboardPanelInfoChange(MessageParcel& data, MessageParcel& reply);
+    int HandleSetUniqueVirtualPixelRatio(MessageParcel& data, MessageParcel& reply);
 };
 } // namespace OHOS::Rosen
 #endif // OHOS_WINDOW_SCENE_SESSION_STAGE_STUB_H
