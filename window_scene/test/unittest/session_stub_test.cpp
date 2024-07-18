8fad801524609ac08a133c7f8ae822d27d49a05d
闫瀚林0424 <yanhanlin6@huawei.com>
2024-07-18 12:05:25 +0800

    Update tdd session_stub
    Signed-off-by: 闫瀚林0424 <yanhanlin6@huawei.com>
    Change-Id: I688059f82be5d05392af537dc440d74ae5b78b02

--- window_scene/test/unittest/session_stub_test.cpp
+++ window_scene/test/unittest/session_stub_test.cpp
@@ -315,7 +315,7 @@ HWTEST_F(SessionStubTest, HandleShow009, Function | SmallTest | Level2)
 {
     MessageParcel data;
     MessageParcel reply;
-    data.WriteBool(true);
+    data.WriteBool(false);
     auto res = session_->HandleShow(data, reply);
     ASSERT_EQ(0, res);
 }
