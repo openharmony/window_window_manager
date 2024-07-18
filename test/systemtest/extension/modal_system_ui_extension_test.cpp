359fc9484f141f3e43f51e4d1ec63b33d22d387a
闫瀚林0424 <yanhanlin6@huawei.com>
2024-07-18 07:14:01 +0000

    update test/systemtest/extension/modal_system_ui_extension_test.cpp.
    Signed-off-by: 闫瀚林0424 <yanhanlin6@huawei.com>

--- test/systemtest/extension/modal_system_ui_extension_test.cpp
+++ test/systemtest/extension/modal_system_ui_extension_test.cpp
@@ -77,7 +77,7 @@ HWTEST_F(ModalSystemUiExtensionTest, ModalSystemUiExtensionConnection01, Functio
 HWTEST_F(ModalSystemUiExtensionTest, ToString, Function | SmallTest | Level2)
 {
     AAFwk::WantParams wantParams;
-    std::string ret = ModalSystemUiExtension::ToString(wantParams_);
+    std::string ret = ModalSystemUiExtension::ToString(wantParams);
     ASSERT_EQ("{}", ret);
 }
 }
