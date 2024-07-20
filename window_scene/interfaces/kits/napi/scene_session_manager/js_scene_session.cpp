078f4356c045933c03b9c61299056129ca53bab6
韦国宁 <weiguoning2@huawei.com>
2024-07-20 18:16:30 +0800

    支持应用启动时指定窗口以全屏模式启动和显示
    Signed-off-by: 韦国宁 <weiguoning2@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
@@ -2055,8 +2055,11 @@ sptr<SceneSession> JsSceneSession::GenSceneSession(SessionInfo& info)
 void JsSceneSession::PendingSessionActivation(SessionInfo& info)
 {
     TLOGI(WmsLogTag::WMS_LIFE, "[NAPI]bundleName %{public}s, moduleName %{public}s, abilityName %{public}s, "
-        "appIndex %{public}d, reuse %{public}d", info.bundleName_.c_str(), info.moduleName_.c_str(),
-        info.abilityName_.c_str(), info.appIndex_, info.reuse);
+        "appIndex %{public}d, reuse %{public}d, windowMode %{public}d", info.bundleName_.c_str(),
+        info.moduleName_.c_str(), info.abilityName_.c_str(), info.appIndex_, info.reuse, info.windowMode);
+    if (info.windowMode == static_cast<int32_t>(WindowMode::WINDOW_MODE_FULLSCREEN)) {
+        info.fullScreenStart_ = true;
+    }
     auto sceneSession = GenSceneSession(info);
     if (sceneSession == nullptr) {
         TLOGE(WmsLogTag::WMS_LIFE, "GenSceneSession failed");
