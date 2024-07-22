d14c0bfab3eb0435cb6289a4bfbce558619832a6
guanweixiang <guanweixiang1@huawei.com>
2024-07-22 15:53:03 +0800

    add interface permission check, add context log
    Signed-off-by: guanweixiang <guanweixiang1@huawei.com>

--- window_scene/session_manager/src/scene_input_manager.cpp
+++ window_scene/session_manager/src/scene_input_manager.cpp
@@ -156,6 +156,16 @@ bool IsEqualUiExtentionWindowInfo(const std::vector<MMI::WindowInfo>& a, const s
     return true;
 }
 
+std::string DumpTransformInDisplayInfo(const std::vector<float> &transform)
+{
+    std::stringstream stream("[");
+    for (float transfromItem : transform) {
+        stream << transformItem << ",";
+    }
+    stream << "]";
+    return stream.str();
+}
+
 std::string DumpDisplayInfo(const MMI::DisplayInfo& info)
 {
     std::string infoStr =  "DisplayInfo: ";
@@ -163,7 +173,8 @@ std::string DumpDisplayInfo(const MMI::DisplayInfo& info)
         "y: " + std::to_string(info.y) + " width: " + std::to_string(info.width) + 
         "height: " + std::to_string(info.height) + " dpi: " + std::to_string(info.dpi) + " name:" + info.name +
         " uniq: " + info.uniq + " displayMode: " + std::to_string(static_cast<int>(info.displayMode)) +
-        " direction : " + std::to_string( static_cast<int>(info.direction));
+        " direction : " + std::to_string( static_cast<int>(info.direction)) +
+        " transform: " + DumpTransformInDisplayInfo(info.transform);
     return infoStr;
 }
 } //namespace
