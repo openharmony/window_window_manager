eb2b7050ccb7698fe095b3e1545bc2c29b27ebcb
韦国宁 <weiguoning2@huawei.com>
2024-07-25 20:22:42 +0800

    支持应用启动时指定窗口以全屏模式启动和显示
    Signed-off-by: 韦国宁 <weiguoning2@huawei.com>

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -1679,9 +1679,8 @@ WMError WindowSceneSessionImpl::SetLayoutFullScreen(bool status)
     }
 
     if (WindowHelper::IsMainWindow(GetType()) &&
-        ((windowSystemConfig_.uiType_ != "phone" &&
-        windowSystemConfig_.uiType_ != "pad") ||
-        IsFreeMultiWindowMode())) {
+        ((windowSystemConfig_.uiType_ != "phone" && windowSystemConfig_.uiType_ != "pad") ||
+         IsFreeMultiWindowMode())) {
         if (!WindowHelper::IsWindowModeSupported(property_->GetModeSupportInfo(), WindowMode::WINDOW_MODE_FULLSCREEN)) {
             TLOGE(WmsLogTag::WMS_IMMS, "fullscreen window mode is not supported");
             return WMError::WM_ERROR_INVALID_WINDOW;
