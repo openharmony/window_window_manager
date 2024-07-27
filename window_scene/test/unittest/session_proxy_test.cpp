873dbc9680c5dbde965b784ca039f44ddaa08a6c
cq_0418 <chenqian73@huawei.com>
2024-07-27 15:09:02 +0800

    分栏特性补充UT测试用例
    Signed-off-by: cq_0418 <chenqian73@huawei.com>

--- window_scene/test/unittest/session_proxy_test.cpp
+++ window_scene/test/unittest/session_proxy_test.cpp
@@ -553,6 +553,24 @@ HWTEST_F(SessionProxyTest, GetStatusBarHeight, Function | SmallTest | Level2)
     ASSERT_EQ(res, 0);
     GTEST_LOG_(INFO) << "SessionProxyTest: GetStatusBarHeight end";
 }
+
+/**
+ * @tc.name: GetAppForceLandscapeConfig
+ * @tc.desc: normal function
+ * @tc.type: FUNC
+ */
+HWTEST_F(SessionProxyTest, GetAppForceLandscapeConfig, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "SessionProxyTest: GetAppForceLandscapeConfig start";
+    auto iRemoteObjectMocker = sptr<IRemoteObjectMocker>::MakeSptr();
+    ASSERT_NE(iRemoteObjectMocker, nullptr);
+    auto sProxy = sptr<SessionProxy>::MakeSptr(iRemoteObjectMocker);
+    ASSERT_NE(sProxy, nullptr);
+    AppForceLandscapeConfig config = {};
+    auto res = sProxy->GetAppForceLandscapeConfig(config);
+    ASSERT_EQ(res, WMError::WM_OK);
+    GTEST_LOG_(INFO) << "SessionProxyTest: GetAppForceLandscapeConfig end";
+}
 } // namespace
 } // namespace Rosen
 } // namespace OHOS
