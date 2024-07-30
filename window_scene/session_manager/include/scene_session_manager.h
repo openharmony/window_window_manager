c87c9f716bc4f324be52fa7a6c6e66be39745a79
yyuehao <yangyuehao@huawei.com>
2024-07-30 07:16:40 +0000

    update window_scene/session_manager/include/scene_session_manager.h. 回退错误提交空格
    Signed-off-by: yyuehao <yangyuehao@huawei.com>

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -427,7 +427,7 @@ private:
     bool CheckParentSessionVisible(const sptr<SceneSession>& session);
     void InitSceneSession(sptr<SceneSession>& sceneSession, const SessionInfo& sessionInfo,
         const sptr<WindowSessionProperty>& property);
-    
+
     sptr<SceneSession> GetNextFocusableSession(int32_t persistentId);
     sptr<SceneSession> GetTopNearestBlockingFocusSession(uint32_t zOrder, bool includingAppSession);
     sptr<SceneSession> GetTopFocusableNonAppSession();
