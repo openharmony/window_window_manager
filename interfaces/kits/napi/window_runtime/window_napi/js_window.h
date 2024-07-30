e53b6a8a765477fa668031d980392c1654bc461e
wupengbo <wupengbo0013@163.com>
2024-07-30 08:33:10 +0000

    update interfaces/kits/napi/window_runtime/window_napi/js_window.h.
    Signed-off-by: wupengbo <wupengbo0013@163.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window.h
+++ interfaces/kits/napi/window_runtime/window_napi/js_window.h
@@ -31,6 +31,7 @@
 
 namespace OHOS {
 namespace Rosen {
+using namespace AbilityRuntime;
 napi_value CreateJsWindowObject(napi_env env, sptr<Window>& window);
 std::shared_ptr<NativeReference> FindJsWindowObject(const std::string& windowName);
 void BindFunctions(napi_env env, napi_value object, const char *moduleName);
@@ -155,6 +156,7 @@ public:
     static napi_value SetWindowGrayScale(napi_env env, napi_callback_info info);
     static napi_value SetImmersiveModeEnabledState(napi_env env, napi_callback_info info);
     static napi_value GetImmersiveModeEnabledState(napi_env env, napi_callback_info info);
+    static napi_value EnableDrag(napi_env env, napi_callback_info info);
 
 private:
     std::string GetWindowName();
@@ -278,10 +280,15 @@ private:
     napi_value OnSetWindowMask(napi_env env, napi_callback_info info);
     napi_value OnSetHandwritingFlag(napi_env env, napi_callback_info info);
     napi_value OnSetWindowGrayScale(napi_env env, napi_callback_info info);
+    napi_value OnEnableDrag(napi_env env, napi_callback_info info); 
 
     sptr<Window> windowToken_ = nullptr;
     std::unique_ptr<JsWindowRegisterManager> registerManager_ = nullptr;
     std::shared_ptr<NativeReference> jsTransControllerObj_ = nullptr;
+
+    NapiAsyncTask::CompleteCallback GetCompleteTask(bool enableDrag, const wptr<Window> &weakToken) const;
+
+    NapiAsyncTask
 };
 }  // namespace Rosen
 }  // namespace OHOS
