clipsheep6/window_manager

增加移动系统窗口的接口 Signed-off-by: h00855061 <540716134@qq.com>

window_scene/session/host/src/scene_session.cpp
@@ -382,6 +382,25 @@ WSError SceneSession::OnSessionEvent(SessionEvent event)
     return WSError::WS_OK;
 }
 
+WSError SceneSession::OnSystemSessionEvent(SessionEvent event)
+{
+    auto weakThis = wptr(this);
+    auto session = weakThis.promote();
+    if (!session) {
+        TLOGW(WmsLogTag::WMS_SYSTEM, "IPC communicate failed since hostSession is nullptr");
+        return WSError::WS_ERROR_NULLPTR;
+    }
+    else if (!SessionPermission::IsSystemCalling()) {
+        TLOGW(WmsLogTag::WMS_SYSTEM, "This is not system window, permission denied!");
+        return WSError::WM_ERROR_NOT_SYSTEM_APP;
+    }
+    else if (session->moveDragController_->GetStartDragFlag()) {
+        TLOGW(WmsLogTag::WMS_SYSTEM, "Repeat operation, window is moving");
+        return WSError::WM_ERROR_REPEAT_OPERATION;
+    }
+    WSError errorCode = OnSessionEvent(event);
+    return errorCode;
+}
 uint32_t SceneSession::GetWindowDragHotAreaType(uint32_t type, int32_t pointerX, int32_t pointerY)
 {
     std::shared_lock<std::shared_mutex> lock(windowDragHotAreaMutex_);

openharmony/window_window_manager

window_scene/session/host/src/scene_session.cpp
@@ -397,6 +397,32 @@ WSError SceneSession::OnSessionEvent(SessionEvent event)
     return WSError::WS_OK;
 }
 
+WSError SceneSession::OnSystemSessionEvent(SessionEvent event)
+{
+    if (event != SessionEvent::EVENTT_START_MOVE) {
+        TLOGW(WmsLogTag::WMS_SYSTEM, "This is not start move event");
+        return WSError::WS_ERROR_NULLPTR; 
+    }
+    if (!SessionPermission::IsSystemCalling()) {
+        TLOGW(WmsLogTag::WMS_SYSTEM, "This is not system window, permission denied!");
+        return WSError::WS_ERROR_NOT_SYSTEM_APP;
+    }
+    auto task = [weakThis = wptr(this), event, this]() {
+        auto session = weakThis.promote();
+        if (!session || !session->moveDragController_) {
+            TLOGW(WmsLogTag::WMS_SYSTEM, "IPC communicate failed since hostSession is nullptr");
+            return WSError::WS_ERROR_NULLPTR;
+        } 
+        if (session->moveDragController_->GetStartDragFlag()) {
+            TLOGW(WmsLogTag::WMS_SYSTEM, "Repeat operation, window is moving");
+            return WSError::WS_ERROR_REPEAT_OPERATION;
+        }
+        OnSessionEvent(event);
+        return WSError::WS_OK;
+    };
+    return PostSyncTask(task, "OnSystemSessionEvent");
+}
+
 uint32_t SceneSession::GetWindowDragHotAreaType(uint32_t type, int32_t pointerX, int32_t pointerY)
 {
     std::shared_lock<std::shared_mutex> lock(windowDragHotAreaMutex_);