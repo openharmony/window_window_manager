aa38b02acbf61e4fa9456f7d021cb4d855dd8b57
hfwang0318 <wanghaofan@huawei.com>
2024-08-06 02:21:50 +0000

    clean code
    Signed-off-by: hfwang0318 <wanghaofan@huawei.com>

--- interfaces/innerkits/wm/wm_common.h
+++ interfaces/innerkits/wm/wm_common.h
@@ -253,8 +253,8 @@ enum class SystemBarSettingFlag : uint32_t {
     DEFAULT_SETTING = 0,
     COLOR_SETTING = 1,
     ENABLE_SETTING = 1 << 1,
-    FOLLOW_SETTING = 1 << 2,
-    ALL_SETTING = 0b011
+    ALL_SETTING = 0b011,
+    FOLLOW_SETTING = 1 << 2
 };
 
 /**
