bc21f1c98936e57083bd8585471fa7c27b67e338
gina_h <hongxiaoyu2@huawei.com>
2024-08-08 07:10:39 +0000

    AdjustKeyboardLayout接口合一
    Signed-off-by: gina_h <hongxiaoyu2@huawei.com>
    Change-Id: Ibd7a3dcbbfdae1b555d43f17e2bde519aa22cb4c

--- wm/include/window_scene_session_impl.h
+++ wm/include/window_scene_session_impl.h
@@ -159,6 +159,7 @@ public:
     WSError UpdateOrientation() override;
     WSError UpdateDisplayId(uint64_t displayId) override;
     WMError AdjustKeyboardLayout(const KeyboardLayoutParams& params) override;
+    WMError MoveAndResizeKeyboard(const KeyboardLayoutParams& params);
 
     WSError SwitchFreeMultiWindow(bool enable) override;
     virtual bool GetFreeMultiWindowModeEnabledState() override;
