d67a6999ed35d62d73d608d678b71734ec78a1a7
yyuehao <yangyuehao@huawei.com>
2024-08-08 02:50:15 +0000

    update window_scene/session_manager/include/scene_session_manager.h. 修改圈复杂度规范
    Signed-off-by: yyuehao <yangyuehao@huawei.com>

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -424,6 +424,8 @@ private:
     bool CheckRequestFocusImmdediately(sptr<SceneSession>& sceneSession);
     bool CheckFocusIsDownThroughBlockingType(sptr<SceneSession>& requestSceneSession,
         sptr<SceneSession>& focusedSession, bool includingAppSession);
+    bool CheckClickFocusIsDownThroughFullScreen(sptr<SceneSession>& focusedSession,
+        sptr<SceneSession>& sceneSession, FocusChangeReason reason);
     bool CheckParentSessionVisible(const sptr<SceneSession>& session);
     void InitSceneSession(sptr<SceneSession>& sceneSession, const SessionInfo& sessionInfo,
         const sptr<WindowSessionProperty>& property);
