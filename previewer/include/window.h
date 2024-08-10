7e8ce805fb11d2e31e021093702f1fc6b2527145
wupengbo <wupengbo0013@163.com>
2024-08-10 08:12:39 +0000

    update previewer/include/window.h.
    Signed-off-by: wupengbo <wupengbo0013@163.com>

--- previewer/include/window.h
+++ previewer/include/window.h
@@ -299,7 +299,7 @@ public:
     virtual WMError UnregisterWindowVisibilityChangeListener(const WindowVisibilityListenerSptr& listener) = 0;
     virtual WMError SetWindowLimits(WindowLimits& windowLimits) { return WMError::WM_OK; };
     virtual WMError GetWindowLimits(WindowLimits& windowLimits) { return WMError::WM_OK; };
-    virtual WMError EnableDrag(bool enableDrag) { return WMError::WM_OK; };
+    virtual WMError EnableDrag(bool enableDrag) { return WMError::WM_ERROR_DEVICE_NOT_SUPPORT; };
     virtual WMError RegisterWindowNoInteractionListener(const IWindowNoInteractionListenerSptr& listener)
     {
         return WMError::WM_ERROR_DEVICE_NOT_SUPPORT;
