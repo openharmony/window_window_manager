8f1f4e13da1fc14d32343967935f6a3356d3da7b
MirMort <chenjunliang26@h-partners.com>
2024-08-11 14:21:18 +0800

    add
    Signed-off-by: MirMort <chenjunliang26@h-partners.com>

--- window_scene/session_manager/include/zidl/scene_session_manager_interface.h
+++ window_scene/session_manager/include/zidl/scene_session_manager_interface.h
@@ -262,7 +262,7 @@ public:
     WMError GetWindowStyleType(WindowStyleType& windowStyleType) override { return WMError::WM_OK; }
 
     virtual WMError GetProcessSurfaceNodeIdByPersistentId(const int32_t pid,
-        const std::vector<int32_t>& persistentIds, std::vector<uint64_t>& surfaceNodeIds) = 0
+        const std::vector<int32_t>& persistentIds, std::vector<uint64_t>& surfaceNodeIds) = 0;
 };
 } // namespace OHOS::Rosen
 #endif // OHOS_ROSEN_WINDOW_SCENE_SESSION_MANAGER_INTERFACE_H
