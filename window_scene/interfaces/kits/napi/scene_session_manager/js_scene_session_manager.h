3f5c8775dc5db7740042a26da524d88e16fbd64b
李聪 <licong113@huawei.com>
2024-08-12 02:59:50 +0000

    新增接口updateAppHookDisplayInfo(迭代替换老接口updateDisplayHookInfo)
    Signed-off-by: 李聪 <licong113@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session_manager.h
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session_manager.h
@@ -95,6 +95,7 @@ public:
     static napi_value GetCustomDecorHeight(napi_env env, napi_callback_info info);
     static napi_value NotifyEnterRecentTask(napi_env env, napi_callback_info info);
     static napi_value UpdateDisplayHookInfo(napi_env env, napi_callback_info info);
+    static napi_value UpdateAppHookDisplayInfo(napi_env env, napi_callback_info info);
     static napi_value InitScheduleUtils(napi_env env, napi_callback_info info);
     static napi_value SetAppForceLandscapeConfig(napi_env env, napi_callback_info info);
     static napi_value SwitchFreeMultiWindow(napi_env env, napi_callback_info info);
@@ -157,6 +158,7 @@ private:
     napi_value OnGetIsLayoutFullScreen(napi_env env, napi_callback_info info);
     napi_value OnNotifyEnterRecentTask(napi_env env, napi_callback_info info);
     napi_value OnUpdateDisplayHookInfo(napi_env env, napi_callback_info info);
+    napi_value OnUpdateAppHookDisplayInfo(napi_env env, napi_callback_info info);
     napi_value OnInitScheduleUtils(napi_env env, napi_callback_info info);
     napi_value OnSetAppForceLandscapeConfig(napi_env env, napi_callback_info info);
     napi_value OnIsScbCoreEnabled(napi_env env, napi_callback_info info);
