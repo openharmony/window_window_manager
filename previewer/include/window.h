cfd19684c10e971519c33128e196cd29962d6406
yangfei110@huawei.com <yangfei110@huawei.com>
2024-08-12 09:38:25 +0800

    add displayid in properties （cherry picked commit from <gitee.com//openharmony/window_window_manager/commit/c249daf6fe114ea452194e3a6bbbac4f0c2daa12>
    Signed-off-by: yangfei110@huawei.com <yangfei110@huawei.com>
    Change-Id: I0236a9ca94d00bb893fabd62b5d047ffd1e405e0

--- previewer/include/window.h
+++ previewer/include/window.h
@@ -137,6 +137,7 @@ public:
     virtual float GetAlpha() const = 0;
     virtual const std::string& GetWindowName() const = 0;
     virtual uint32_t GetWindowId() const = 0;
+    virtual uint64_t GetDisplayId() const = 0;
     virtual uint32_t GetWindowFlags() const = 0;
     virtual WindowState GetWindowState() const = 0;
     virtual WMError SetFocusable(bool isFocusable) = 0;
