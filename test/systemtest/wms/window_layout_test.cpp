8935ac2279f46bc20d201b6afa954ca81c8ad96b
lpf <liaopengfei@h-partners.com>
2024-08-13 14:01:31 +0800

    Signed-off-by: lpf <liaopengfei@h-partners.com>
    moveWindowTo   与   resize 接口测试

--- test/systemtest/wms/window_layout_test.cpp
+++ test/systemtest/wms/window_layout_test.cpp
@@ -667,38 +667,38 @@ HWTEST_F(WindowLayoutTest, moveWindowTo01, Function | MediumTest | Level3)
     Rect rect;
     WMError ret;
     ret = window->Create(abilityContext_, nullptr);
-    ASSERT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
 
     ret = window->Show();
-    ASSERT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
 
     ret = window->MoveTo(-500, -500);
     usleep(100000);
-    ASSERT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     rect = window->property_->GetWindowRect();
-    ASSERT_EQ(-500, rect.posX_);
-    ASSERT_EQ(-500, rect.posY_);
+    EXPECT_EQ(-500, rect.posX_);
+    EXPECT_EQ(-500, rect.posY_);
 
     ret = window->MoveTo(0, 0);
     usleep(100000);
-    ASSERT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     rect = window->property_->GetWindowRect();
-    ASSERT_EQ(0, rect.posX_);
-    ASSERT_EQ(0, rect.posY_);
+    EXPECT_EQ(0, rect.posX_);
+    EXPECT_EQ(0, rect.posY_);
 
     ret = window->MoveTo(500, 500);
     usleep(100000);
-    ASSERT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     rect = window->property_->GetWindowRect();
-    ASSERT_EQ(500, rect.posX_);
-    ASSERT_EQ(500, rect.posY_);
+    EXPECT_EQ(500, rect.posX_);
+    EXPECT_EQ(500, rect.posY_);
 
     ret = window->MoveTo(20000, 20000);
     usleep(100000);
-    ASSERT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     rect = window->property_->GetWindowRect();
-    ASSERT_EQ(20000, rect.posX_);
-    ASSERT_EQ(20000, rect.posY_);
+    EXPECT_EQ(20000, rect.posX_);
+    EXPECT_EQ(20000, rect.posY_);
 }
 
 /**
@@ -954,38 +954,38 @@ HWTEST_F(WindowLayoutTest, resize01, Function | MediumTest | Level3)
     Rect rect;
     WMError ret;
     ret = window->Create(abilityContext_, nullptr);
-    EXCEPT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     window->property_->SetPersistentId(10008);
     ret = window->Show();
-    EXCEPT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
 
     ret = window->Resize(-500, -500);
-    EXCEPT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     usleep(100000);
     rect = window->property_->GetWindowRect();
-    EXCEPT_EQ(-500, rect.width_);
-    EXCEPT_EQ(-500, rect.height_);
+    EXPECT_EQ(-500, rect.width_);
+    EXPECT_EQ(-500, rect.height_);
 
     ret = window->Resize(0, 0);
-    EXCEPT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     usleep(100000);
     rect = window->property_->GetWindowRect();
-    EXCEPT_EQ(0, rect.width_);
-    EXCEPT_EQ(0, rect.height_);
+    EXPECT_EQ(0, rect.width_);
+    EXPECT_EQ(0, rect.height_);
 
     ret = window->Resize(500, 500);
-    EXCEPT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     usleep(100000);
     rect = window->property_->GetWindowRect();
-    EXCEPT_EQ(500, rect.width_);
-    EXCEPT_EQ(500, rect.height_);
+    EXPECT_EQ(500, rect.width_);
+    EXPECT_EQ(500, rect.height_);
 
     ret = window->Resize(20000, 20000);
-    EXCEPT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     usleep(100000);
     rect = window->property_->GetWindowRect();
-    EXCEPT_EQ(20000, rect.width_);
-    EXCEPT_EQ(20000, rect.height_);
+    EXPECT_EQ(20000, rect.width_);
+    EXPECT_EQ(20000, rect.height_);
 }
 
 /**
@@ -1124,27 +1124,27 @@ HWTEST_F(WindowLayoutTest, resize05, Function | MediumTest | Level3)
     Rect rect;
     WMError ret;
     ret = window->Create(abilityContext_, nullptr);
-    EXCEPT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     window->property_->SetPersistentId(10012);
     ret = window->Show();
-    EXCEPT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
 
     WindowLimits windowLimits;
     ret = window->GetWindowLimits(windowLimits);
 
     ret = window->Resize(windowLimits.maxWidth_ - 100, windowLimits.maxHeight_ - 100);
-    EXCEPT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     usleep(100000);
     rect = window->property_->GetWindowRect();
-    EXCEPT_EQ(windowLimits.maxWidth_ - 100, rect.width_);
-    EXCEPT_EQ(windowLimits.maxHeight_ - 100, rect.height_);
+    EXPECT_EQ(windowLimits.maxWidth_ - 100, rect.width_);
+    EXPECT_EQ(windowLimits.maxHeight_ - 100, rect.height_);
 
     ret = window->Resize(windowLimits.maxWidth_ + 100, windowLimits.maxHeight_ + 100);
-    EXCEPT_EQ(WMError::WM_OK, ret);
+    EXPECT_EQ(WMError::WM_OK, ret);
     usleep(100000);
     rect = window->property_->GetWindowRect();
-    EXCEPT_EQ(windowLimits.maxWidth_ + 100, rect.width_);
-    EXCEPT_EQ(windowLimits.maxHeight_ + 100, rect.height_);
+    EXPECT_EQ(windowLimits.maxWidth_ + 100, rect.width_);
+    EXPECT_EQ(windowLimits.maxHeight_ + 100, rect.height_);
 }
 
 
