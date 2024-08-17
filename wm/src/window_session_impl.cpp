093780e544374582c2c13d5044fafa32fa237942
wupengbo <wupengbo0013@163.com>
2024-08-17 03:22:26 +0000

    add
    Signed-off-by: wupengbo <wupengbo0013@163.com>

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -1590,7 +1590,7 @@ WMError WindowSessionImpl::EnableDrag(bool enableDrag)
     CHECK_HOST_SESSION_RETURN_ERROR_IF_NULL(hostSession, WMError::WM_ERROR_INVALID_WINDOW);
     WSError errorCode = hostSession->SetSystemWindowEnableDrag(enableDrag);
     TLOGI(WmsLogTag::WMS_EVENT, "IPC, sessionId: %{public}d ,errcode: %{public}d", GetPersistentId(),
-        static_cast<int>(errcode));
+        static_cast<int>(errorcode));
     return static_cast<WMError>(errcode;
 }
 
