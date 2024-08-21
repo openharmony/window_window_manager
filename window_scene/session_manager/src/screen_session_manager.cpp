27026792bd8c67d6e5a5c772f11dfc169314a890
licong <licong113@huawei.com>
2024-08-21 09:31:59 +0800

    添加TDD测试用例
    Signed-off-by: licong <licong113@huawei.com>

--- window_scene/session_manager/src/screen_session_manager.cpp
+++ window_scene/session_manager/src/screen_session_manager.cpp
@@ -695,9 +695,7 @@ sptr<DisplayInfo> ScreenSessionManager::HookDisplayInfoByUid(sptr<DisplayInfo> d
                 DisplayOrientation targetOrientation = screenSession->CalcDisplayOrientation(targetRotation,
                     FoldDisplayMode::UNKNOWN);
                 TLOGI(WmsLogTag::DMS, "tR: %{public}u, tO: %{public}u", targetRotation, targetOrientation);
-                if (targetOrientation != DisplayOrientation::UNKNOWN) {
-                    displayInfo->SetDisplayOrientation(targetOrientation);
-                }            
+                displayInfo->SetDisplayOrientation(targetOrientation);
             }
         }
     }
