d4d0ea049d66a28ae8298b7f77dbcecbd9af8c6d
flybear <lichao436@huawei.com>
2024-08-23 08:36:52 +0000

    pc 扩展屏需求层级适配
    Signed-off-by: flybear <lichao436@huawei.com>

--- window_scene/session/host/include/scene_session.h
+++ window_scene/session/host/include/scene_session.h
@@ -392,6 +392,12 @@ public:
     void SetMinimizedFlagByUserSwitch(bool isMinimized);
     bool IsMinimizedByUserSwitch() const;
     void UnregisterSessionChangeListeners() override;
+    bool GetPcScenePanel(){
+        return isPcScenePanel_;
+    }
+    void SetPcScenePanel(bool isPcScenePanel){
+        isPcScenePanel_ = isPcScenePanel;
+    }
 
 protected:
     void NotifySessionRectChange(const WSRect& rect, const SizeChangeReason& reason = SizeChangeReason::UNDEFINED);
@@ -573,6 +579,9 @@ private:
 
     // Multi User
     bool isMinimizedByUserSwitch_ { false };
+
+    // pc scene panel is used to determine whether the freshPcZOrder method is used to process the layer
+    bool isPcScenePanel_ {false};
 };
 } // namespace OHOS::Rosen
 #endif // OHOS_ROSEN_WINDOW_SCENE_SCENE_SESSION_H
