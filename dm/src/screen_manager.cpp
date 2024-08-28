a8d4b132010ecb014c98572dc4ae7a1335f99c82
j30052480 <jiangweisheng1@huawei.com>
2024-08-28 16:15:57 +0800

    fix: add lock for virtualScreenAgent_
    Signed-off-by: j30052480 <jiangweisheng1@huawei.com>

--- dm/src/screen_manager.cpp
+++ dm/src/screen_manager.cpp
@@ -551,7 +551,7 @@ ScreenId ScreenManager::CreateVirtualScreen(VirtualScreenOption option)
 ScreenId ScreenManager::Impl::CreateVirtualScreen(VirtualScreenOption option)
 {
     //  After the process creating the virtual screen is killed, DMS needs to delete the virtual screen
-    std::lock_guard agentLock(virtualScreenAgentMutex_);
+    std::lock_guard<std::mutex> agentLock(virtualScreenAgentMutex_);
     if (virtualScreenAgent_ == nullptr) {
         virtualScreenAgent_ = new DisplayManagerAgentDefault();
     }
@@ -706,7 +706,7 @@ void ScreenManager::Impl::OnRemoteDied()
 {
     WLOGFD("dms is died");
     {
-        std::lock_guard agentLock(virtualScreenAgentMutex_);
+        std::lock_guard<std::mutex> agentLock(virtualScreenAgentMutex_);
         virtualScreenAgent_ = nullptr;
     }
     std::lock_guard<std::recursive_mutex> lock(mutex_);
