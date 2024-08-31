7b954b270fb0707eecf427ea21f506b00920a7e2
白钰胜 <baiyusheng@h-partners.com>
2024-08-31 19:45:26 +0800

    蓝黄代码同步
    Signed-off-by: 白钰胜 <baiyusheng@h-partners.com>

--- window_scene/session/host/src/scene_session.cpp
+++ window_scene/session/host/src/scene_session.cpp
@@ -18,6 +18,7 @@
 
 #include <ability_manager_client.h>
 #include <algorithm>
+#include <climits>
 #include <hitrace_meter.h>
 #ifdef IMF_ENABLE
 #include <input_method_controller.h>
@@ -61,6 +62,16 @@ namespace {
 constexpr HiviewDFX::HiLogLabel LABEL = { LOG_CORE, HILOG_DOMAIN_WINDOW, "SceneSession" };
 const std::string DLP_INDEX = "ohos.dlp.params.index";
 constexpr const char* APP_CLONE_INDEX = "ohos.extra.param.key.appCloneIndex";
+
+bool CheckIfRectElementIsTooLarge(const WSRect& rect)
+{
+    int32_t largeNumber = static_cast<int32_t>(SHRT_MAX);
+    if (rect.posX_ >= largeNumber || rect.posY_ >= largeNumber ||
+        rect.width_ >= largeNumber || rect.height_ >= largeNumber) {
+        return true;
+    }
+    return false;
+}
 } // namespace
 
 MaximizeMode SceneSession::maximizeMode_ = MaximizeMode::MODE_RECOVER;
@@ -1015,8 +1026,10 @@ WSError SceneSession::UpdateSessionRect(const WSRect& rect, const SizeChangeReas
         auto parentSession = GetParentSession();
         if (parentSession) {
             auto parentRect = parentSession->GetSessionRect();
-            newRect.posX_ -= parentRect.posX_;
-            newRect.posY_ -= parentRect.posY_;
+            if (!CheckIfRectElementIsTooLarge(parentRect)) {
+                newRect.posX_ -= parentRect.posX_;
+                newRect.posY_ -= parentRect.posY_;
+            }
         }
     }
     Session::RectCheckProcess();
