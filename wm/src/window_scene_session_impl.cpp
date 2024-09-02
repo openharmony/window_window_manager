a9819233d8e7cf745721dd2af499bb35b0d7da99
liaopengfei <liaopengfei@h-partners.com>
2024-09-02 16:37:24 +0800

    Signed-off-by: liaopengfei <liaopengfei@h-partners.com>
    fix resize00

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -1583,7 +1583,8 @@ WMError WindowSceneSessionImpl::Resize(uint32_t width, uint32_t height)
 {
     TLOGI(WmsLogTag::WMS_LAYOUT, "Id:%{public}d resize %{public}u %{public}u",
         property_->GetPersistentId(), width, height);
-    if (0 == width || 0 == height) {
+    const uint32_t zero = 0u;
+    if (zero == width || zero == height) {
         TLOGI(WmsLogTag::WMS_LAYOUT, "width or height should greater than 0!");
         return WMError::WM_ERROR_INVALID_PARAM;
     }
