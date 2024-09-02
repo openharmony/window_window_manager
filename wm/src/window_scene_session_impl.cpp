6e91683bdbb98d01247c5d347985bc7f5ec79288
h30026439 <huangzhizhong6@huawei.com>
2024-09-02 18:18:24 +0800

    TLOGD修改
    Signed-off-by: h30026439 <huangzhizhong6@huawei.com>

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -3043,7 +3043,7 @@ WMError WindowSceneSessionImpl::SetTouchHotAreas(const std::vector<Rect>& rects)
         return result;
     }
     for (uint32_t i = 0; i < rects.size(); i++) {
-        WLOGFD("Set areas: %{public}u [x: %{public}d y:%{public}d w:%{public}u h:%{public}u]",
+        TLOGD(WmsLogTag::WMS_EVENT, "id:%{public}u xywh[%{public}d %{public}d %{public}u %{public}u]",
             i, rects[i].posX_, rects[i].posY_, rects[i].width_, rects[i].height_);
     }
     return result;
