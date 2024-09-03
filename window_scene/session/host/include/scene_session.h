55d0a7bd13c107e7bf1783c662737ff16a0d0a5a
reccoder <jianhao2@h-partners.com>
2024-09-03 21:41:25 +0800

    refactor getWindowAvoidArea
    Signed-off-by: reccoder <jianhao2@h-partners.com>

--- window_scene/session/host/include/scene_session.h
+++ window_scene/session/host/include/scene_session.h
@@ -218,6 +218,7 @@ public:
     virtual bool IsTopmost() const { return false; }
     virtual bool IsModal() const { return false; }
     WSError SetSystemBarProperty(WindowType type, SystemBarProperty systemBarProperty);
+    void SetIsStatusBarVisible(bool isVisible) { isStatusBarVisible_ = isVisible; }
     void SetAbilitySessionInfo(std::shared_ptr<AppExecFwk::AbilityInfo> abilityInfo);
     void SetWindowDragHotAreaListener(const NotifyWindowDragHotAreaFunc& func);
     void SetSessionEventParam(SessionEventParam param);
@@ -523,6 +524,7 @@ private:
     SessionChangeByActionNotifyManagerFunc sessionChangeByActionNotifyManagerFunc_;
     bool isAddBlank_ = false;
     bool bufferAvailableCallbackEnable_ = false;
+    bool isStatusBarVisible_ = true;
 
     // Session recover
     bool isRecovered_ = false;
