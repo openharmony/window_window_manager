d83a446a27e584d4b1830cafbd1cc245c38015fe
imp9527 <liumingxin4@h-partners.com>
2024-09-05 16:23:42 +0800

    黄蓝区同步
    Signed-off-by: imp9527 <liumingxin4@h-partners.com>

--- window_scene/interfaces/include/ws_common.h
+++ window_scene/interfaces/include/ws_common.h
@@ -282,7 +282,7 @@ struct SessionInfo {
     uint32_t windowType_ = 1; // WINDOW_TYPE_APP_MAIN_WINDOW
     sptr<IRemoteObject> callerToken_ = nullptr;
     sptr<IRemoteObject> rootToken_ = nullptr;
-    uint64_t screenId_ = -1;
+    uint64_t screenId_ = -1ULL; // -1ULL: SCREEN_ID_INVALID
     bool isPersistentRecover_ = false;
 
     mutable std::shared_ptr<AAFwk::Want> want; // want for ability start
