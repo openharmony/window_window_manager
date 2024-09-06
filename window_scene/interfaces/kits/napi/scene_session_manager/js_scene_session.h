103589142ec6d72dc244b71ff94f5a209c2ca5a6
xiongyonglong <xiongyonglong@huawei.com>
2024-09-06 09:46:16 +0800

    删除无效代码
    Signed-off-by: xiongyonglong <xiongyonglong@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.h
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.h
@@ -129,7 +129,6 @@ private:
     static napi_value SetUniqueDensityDpiFromSCB(napi_env env, napi_callback_info info);
     static napi_value SetBlankFlag(napi_env env, napi_callback_info info);
     static napi_value SetBufferAvailableCallbackEnable(napi_env env, napi_callback_info info);
-    static napi_value IsDeviceWakeupByApplication(napi_env env, napi_callback_info info);
     static napi_value SetIsPcAppInPad(napi_env env, napi_callback_info info);
     static napi_value SetStartingWindowExitAnimationFlag(napi_env env, napi_callback_info info);
     static napi_value CompatibleFullScreenRecover(napi_env env, napi_callback_info info);
@@ -174,7 +173,6 @@ private:
     napi_value OnSetUniqueDensityDpiFromSCB(napi_env env, napi_callback_info info);
     napi_value OnSetBlankFlag(napi_env env, napi_callback_info info);
     napi_value OnSetBufferAvailableCallbackEnable(napi_env env, napi_callback_info info);
-    napi_value OnIsDeviceWakeupByApplication(napi_env env, napi_callback_info info);
     napi_value OnSetIsPcAppInPad(napi_env env, napi_callback_info info);
     napi_value OnSetStartingWindowExitAnimationFlag(napi_env env, napi_callback_info info);
     napi_value OnCompatibleFullScreenRecover(napi_env env, napi_callback_info info);
