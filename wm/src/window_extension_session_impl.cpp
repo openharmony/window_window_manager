4f81a2b683d57b86eb46f00440912eddeeb37fa6
崔洪斌 <cuihongbin@huawei.com>
2024-09-06 17:31:32 +0800

    toast实现调整，由于property_没有置空机会，影响性能
    Signed-off-by: 崔洪斌 <cuihongbin@huawei.com>

--- wm/src/window_extension_session_impl.cpp
+++ wm/src/window_extension_session_impl.cpp
@@ -1160,17 +1160,11 @@ void WindowExtensionSessionImpl::NotifyExtensionTimeout(int32_t errorCode)
 
 int32_t WindowExtensionSessionImpl::GetRealParentId() const
 {
-    if (property_ == nullptr) {
-        return static_cast<int32_t>(INVALID_WINDOW_ID);
-    }
     return property_->GetRealParentId();
 }
 
 WindowType WindowExtensionSessionImpl::GetParentWindowType() const
 {
-    if (property_ == nullptr) {
-        return WindowType::WINDOW_TYPE_APP_MAIN_WINDOW;
-    }
     return property_->GetParentWindowType();
 }
 
