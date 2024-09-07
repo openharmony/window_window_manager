bb680d484a8d9c709e5d04c410b58de29068caa1
liusensen <liusen43@huawei.com>
2024-09-07 16:23:15 +0800

    JsSceneSession文件中针对PendingSessionActivation中this指针捕获修改
    Signed-off-by: liusensen <liusen43@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
@@ -1140,7 +1140,12 @@ void JsSceneSession::OnTouchOutside()
 void JsSceneSession::Finalizer(napi_env env, void* data, void* hint)
 {
     WLOGI("[NAPI]Finalizer");
-    static_cast<JsSceneSession*>(data)->DecStrongRef(nullptr);
+    auto jsSceneSession = static_cast<JsSceneSession*>(data);
+    if (jsSceneSession == nullptr) {
+        TLOGE(WmsLogTag::WMS_LIFE, "JsSceneSession is nullptr");
+        return;
+    }
+    jsSceneSession->DecStrongRef(nullptr);
 }
 
 napi_value JsSceneSession::RegisterCallback(napi_env env, napi_callback_info info)
