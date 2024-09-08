dfe2a6ddf7a652842ce0533322b2a3329511f1ed
yyuehao <yangyuehao@huawei.com>
2024-09-08 06:07:22 +0000

    update window_scene/session/host/include/zidl/session_proxy.h. 修改方法返回值类型
    Signed-off-by: yyuehao <yangyuehao@huawei.com>

--- window_scene/session/host/include/zidl/session_proxy.h
+++ window_scene/session/host/include/zidl/session_proxy.h
@@ -90,7 +90,7 @@ public:
     WSError NotifyFrameLayoutFinishFromApp(bool notifyListener, const WSRect& rect) override;
     WSError SetDialogSessionBackGestureEnabled(bool isEnabled) override;
     WMError SetSystemWindowEnableDrag(bool enableDrag) override;
-    WMError RequestFocus(bool isFocused) override;
+    WSError RequestFocus(bool isFocused) override;
     void NotifyExtensionEventAsync(uint32_t notifyEvent) override;
 
 private:
