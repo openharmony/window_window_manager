3340f2611ad8017c5dd0f6f91711c30b7f69826f
zoujian <jan.zou@huawei.com>
2024-09-09 21:11:55 +0800

    DFX for first vsync request
    Signed-off-by: zoujian <jan.zou@huawei.com>

--- wm/src/vsync_station.cpp
+++ wm/src/vsync_station.cpp
@@ -124,11 +124,20 @@ __attribute__((no_sanitize("cfi"))) void VsyncStation::RequestVsync(
         }
         hasRequestedVsync_ = true;
 
+        if (isFirstVsync_) {
+            isFirstVsync_ = false;
+            TLOGI(WmsLogTag::WMS_MAIN, "First vsync has requested, nodeId: %{public}" PRIu64, nodeId_);
+        }
+
         // post timeout task for a new vsync
         vsyncHandler_->RemoveTask(vsyncTimeoutTaskName_);
         auto task = [weakThis = weak_from_this()] {
             if (auto sp = weakThis.lock()) {
                 sp->OnVsyncTimeOut();
+                if (sp->isFirstVsyncBack_) {
+                    sp->isFirstVsyncBack_ = false;
+                    TLOGI(WmsLogTag::WMS_MAIN, "First vsync has done, nodeId: %{public}" PRIu64, sp->nodeId_);
+                }
             }
         };
         vsyncHandler_->PostTask(task, vsyncTimeoutTaskName_, VSYNC_TIME_OUT_MILLISECONDS);
