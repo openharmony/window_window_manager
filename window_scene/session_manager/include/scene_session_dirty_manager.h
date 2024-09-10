3d89b545e1cd2706300cd39dfc02fbe05cdf872d
hejunfei <hejunfei@huawei.com>
2024-09-10 10:18:51 +0800

    DMS 目录迁移
    Signed-off-by: hejunfei <hejunfei@huawei.com>

--- window_scene/session_manager/include/scene_session_dirty_manager.h
+++ window_scene/session_manager/include/scene_session_dirty_manager.h
@@ -23,7 +23,7 @@
 #include "display_manager.h"
 #include "session/host/include/scene_session.h"
 #include "session/screen/include/screen_session.h"
-#include "session_manager/include/screen_session_manager.h"
+#include "screen_session_manager/include/screen_session_manager.h"
 #include "input_manager.h"
 
 namespace OHOS::Rosen {
