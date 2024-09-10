fe16f781a296e25344bc8fc98f919f706ae66cda
reccoder <jianhao2@h-partners.com>
2024-09-10 14:42:05 +0800

    fix: indents
    Signed-off-by: reccoder <jianhao2@h-partners.com>

--- wm/src/window_impl.cpp
+++ wm/src/window_impl.cpp
@@ -3317,7 +3317,7 @@ void WindowImpl::UpdateAvoidArea(const sptr<AvoidArea>& avoidArea, AvoidAreaType
 {
     WLOGI("Update AvoidArea, id: %{public}u", property_->GetWindowId());
     auto display = SingletonContainer::IsDestroyed() ? nullptr :
-            SingletonContainer::Get<DisplayManager>().GetDisplayById(property_->GetDisplayId());
+        SingletonContainer::Get<DisplayManager>().GetDisplayById(property_->GetDisplayId());
     UpdateViewportConfig(GetRect(), display, WindowSizeChangeReason::UNDEFINED, nullptr, {{type, *avoidArea}});
     NotifyAvoidAreaChange(avoidArea, type);
 }
