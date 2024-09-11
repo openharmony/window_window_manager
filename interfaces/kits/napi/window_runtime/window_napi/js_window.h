d92e2d9f9234b00520affd0464133314d2f39476
tanchenghao <tanchenghao@h-partners.com>
2024-09-11 15:22:42 +0800

    窗口新增隐藏/显示最大化最小化按钮接口
    Signed-off-by: tanchenghao <tanchenghao@h-partners.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window.h
+++ interfaces/kits/napi/window_runtime/window_napi/js_window.h
@@ -161,6 +161,7 @@ public:
     static napi_value GetTitleButtonRect(napi_env env, napi_callback_info info);
     static napi_value SetWindowMask(napi_env env, napi_callback_info info);
     static napi_value SetTitleButtonVisible(napi_env env, napi_callback_info info);
+    static napi_value SetWindowTitleButtonVisible(napi_env env, napi_callback_info info);
     static napi_value SetWindowGrayScale(napi_env env, napi_callback_info info);
     static napi_value SetImmersiveModeEnabledState(napi_env env, napi_callback_info info);
     static napi_value GetImmersiveModeEnabledState(napi_env env, napi_callback_info info);
@@ -244,6 +245,7 @@ private:
     napi_value OnGetWindowDecorHeight(napi_env env, napi_callback_info info);
     napi_value OnGetTitleButtonRect(napi_env env, napi_callback_info info);
     napi_value OnSetTitleButtonVisible(napi_env env, napi_callback_info info);
+    napi_value OnSetWindowTitleButtonVisible(napi_env env, napi_callback_info info);
     napi_value OnSetImmersiveModeEnabledState(napi_env env, napi_callback_info info);
     napi_value OnGetImmersiveModeEnabledState(napi_env env, napi_callback_info info);
     napi_value OnGetWindowStatus(napi_env env, napi_callback_info info);
