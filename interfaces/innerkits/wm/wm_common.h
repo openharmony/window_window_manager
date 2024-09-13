f5bf6d4e8131f89a1cbd1d71223f89dbca5d0592
wangzilin <wangzilin17@huawei.com>
2024-09-13 19:57:36 +0800

    横屏状态栏显示setSpecificSystemBarEnabled接口未设置settingFlag导致应用内配置未生效
    Signed-off-by: wangzilin <wangzilin17@huawei.com>

--- interfaces/innerkits/wm/wm_common.h
+++ interfaces/innerkits/wm/wm_common.h
@@ -272,6 +272,11 @@ enum class SystemBarSettingFlag : uint32_t {
     FOLLOW_SETTING = 1 << 2
 };
 
+inline SystemBarSettingFlag operator|(SystemBarSettingFlag lhs, SystemBarSettingFlag rhs) {
+    using T = std::underlying_type<SystemBarSettingFlag>::type;
+    return static_cast<SystemBarSettingFlag>(static_cast<T>(lhs) | static_cast<T>(rhs));
+}
+
 /**
  * @brief Enumerates flag of multiWindowUIType.
  */
