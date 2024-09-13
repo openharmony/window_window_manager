2473081dda21188adccbf394652720db38abe8cd
c30063259 <chenjunliang26@h-partners.com>
2024-09-13 10:35:38 +0800

    update
    Signed-off-by: c30063259 <chenjunliang26@h-partners.com>

--- window_scene/test/unittest/scene_session_manager_stub_test.cpp
+++ window_scene/test/unittest/scene_session_manager_stub_test.cpp
@@ -2052,7 +2052,7 @@ HWTEST_F(SceneSessionManagerStubTest, HandleSetProcessWatermark, Function | Smal
     MessageParcel reply;
 
     data.WriteInt32(123);
-    data.WriteString("SetProcessWatermarkWatermarkName");
+    data.WriteString("SetProcessWatermarkName");
     data.WriteBool(true);
 
     int res = stub_->HandleSetProcessWatermark(data, reply);
