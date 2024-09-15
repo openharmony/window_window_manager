b4d430950cf61beee7d45ee50c4b814b7b6ed237
rsyys <renzhao5@h-partners.com>
2024-09-15 09:48:54 +0800

    fix
    Signed-off-by: rsyys <renzhao5@h-partners.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -4284,7 +4284,7 @@ WSError SceneSessionManager::RequestSessionUnfocus(int32_t persistentId, FocusCh
     needBlockNotifyFocusStatusUntilForeground_ = false;
 
     if (CheckLastFocusedAppSessionFocus(focusedSession, nextSession)) {
-        return WSError::WS_OK; 
+        return WSError::WS_OK;
     }
 
     return ShiftFocus(nextSession, reason);
@@ -4337,7 +4337,7 @@ bool SceneSessionManager::CheckLastFocusedAppSessionFocus(
     TLOGI(WmsLogTag::WMS_FOCUS, "lastFocusedAppSessionId: %{public}d, nextSceneSession: %{public}d",
         lastFocusedAppSessionId_, nextSession->GetPersistentId());
     
-    if (lastFocusedAppSessionId_ == INVALID_SESSION_ID || nextSession->GetPersistentId() == lastFocusedAppSessionId_ ) {
+    if (lastFocusedAppSessionId_ == INVALID_SESSION_ID || nextSession->GetPersistentId() == lastFocusedAppSessionId_) {
         return false;
     }
 
@@ -4351,7 +4351,7 @@ bool SceneSessionManager::CheckLastFocusedAppSessionFocus(
         (mode == WindowMode::WINDOW_MODE_SPLIT_PRIMARY || mode == WindowMode::WINDOW_MODE_SPLIT_SECONDARY ||
          mode == WindowMode::WINDOW_MODE_FLOATING)) {
         if (RequestSessionFocus(lastFocusedAppSessionId_, false, FocusChangeReason::LAST_FOCUSED_APP) ==
-            WSError::WS_OK){
+            WSError::WS_OK) {
             return true;
         }
         lastFocusedAppSessionId_ = INVALID_SESSION_ID;
