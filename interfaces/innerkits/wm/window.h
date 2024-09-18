ea9793ada63e61b3a7121aa8bb37fe7b9669b7ab
luochenjian <luochenjian1@h-partners.com>
2024-09-18 22:41:01 +0800

    不持支的蓝区接口 返回WM_ERROR_DEVICE_NOT_SUPPORT
    Signed-off-by: luochenjian <luochenjian1@h-partners.com>

--- interfaces/innerkits/wm/window.h
+++ interfaces/innerkits/wm/window.h
@@ -2040,7 +2040,7 @@ public:
      */
     virtual WMError RegisterWindowRectChangeListener(const sptr<IWindowRectChangeListener>& listener)
     {
-        return WMError::WM_OK;
+        return WMError::WM_ERROR_DEVICE_NOT_SUPPORT;
     }
 
     /**
@@ -2051,7 +2051,7 @@ public:
      */
     virtual WMError UnregisterWindowRectChangeListener(const sptr<IWindowRectChangeListener>& listener)
     {
-        return WMError::WM_OK;
+        return WMError::WM_ERROR_DEVICE_NOT_SUPPORT;
     }
 
     /**
