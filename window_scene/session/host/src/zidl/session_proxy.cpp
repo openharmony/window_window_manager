209d2fb680fe604a6876eb16e63f857562894b73
zzhcharmer <zhouzhenhui3@h-partners.com>
2024-09-20 17:25:25 +0800

    支持startAbility启动时定制启动页
    Signed-off-by: zzhcharmer <zhouzhenhui3@h-partners.com>

--- window_scene/session/host/src/zidl/session_proxy.cpp
+++ window_scene/session/host/src/zidl/session_proxy.cpp
@@ -28,6 +28,7 @@
 #include "process_options.h"
 #include "session/host/include/zidl/session_ipc_interface_code.h"
 #include "window_manager_hilog.h"
+#include "start_window_option.h"
 namespace OHOS::Rosen {
 namespace {
 constexpr HiviewDFX::HiLogLabel LABEL = { LOG_CORE, HILOG_DOMAIN_WINDOW, "SessionProxy" };
@@ -427,6 +428,16 @@ WSError SessionProxy::PendingSessionActivation(sptr<AAFwk::SessionInfo> abilityS
         if (!data.WriteBool(false)) {
             WLOGFE("Write has not startSetting failed");
             return WSError::WS_ERROR_IPC_FAILED;
+        }
+    }
+    if (abilitySessionInfo->startWindowOption) {
+        if (!data.WriteBool(true) || !data.WriteParcelable(abilitySessionInfo->startWindowOption.get())) {
+             WLOGFE("Write startWindowOption failed");
+        } else {
+            if (!data.WriteBool(false)) {
+                WLOGFE("Write has not startWindowOption failed");
+                return WSError::WS_ERROR_IPC_FAILED;
+            }
         }
     }
     sptr<IRemoteObject> remote = Remote();
