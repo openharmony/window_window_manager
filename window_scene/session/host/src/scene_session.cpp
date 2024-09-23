4c743e66940057483b7b85c0df9b9038749c2f61
ReCoder <jianhao2@h-partners.com>
2024-09-23 10:07:59 +0800

    fix: notify avoid area on isVisible change
    Signed-off-by: ReCoder <jianhao2@h-partners.com>

--- window_scene/session/host/src/scene_session.cpp
+++ window_scene/session/host/src/scene_session.cpp
@@ -1048,6 +1048,17 @@ WSError SceneSession::SetSystemBarProperty(WindowType type, SystemBarProperty sy
     return WSError::WS_OK;
 }
 
+void SceneSession::SetIsStatusBarVisible(bool isVisible)
+{
+    bool isNeedNotify = isStatusBarVisible_ != isVisible;
+    TLOGI(WmsLogTag::WMS_IMMS, "Window [%{public}d, %{public}s] status bar visible %{public}u, need notify %{public}u",
+          GetPersistentId(), GetWindowName().c_str(), isVisible, isNeedNotify);
+    isStatusBarVisible_ = isVisible;
+    if (isNeedNotify && specificCallback_ && specificCallback_->onUpdateAvoidAreaByType_) {
+        specificCallback_->onUpdateAvoidAreaByType_(GetPersistentId(), AvoidAreaType::TYPE_SYSTEM);
+    }
+}
+
 void SceneSession::NotifyPropertyWhenConnect()
 {
     WLOGFI("Notify property when connect.");
