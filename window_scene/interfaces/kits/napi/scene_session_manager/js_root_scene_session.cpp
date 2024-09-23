55bb3fcb98070d54a7f6e67421f8f90759166c8f
hubijie <hubijie@huawei.com>
2024-09-23 20:17:54 +0800

    元服务session复用逻辑修改 Signed-off-by: <hubijie@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_root_scene_session.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_root_scene_session.cpp
@@ -342,13 +342,14 @@ sptr<SceneSession> JsRootSceneSession::GenSceneSession(SessionInfo& info)
             return nullptr;
         }
 
-        if (info.reuse) {
+        if (info.reuse || info.isAtomicService_) {
             if (SceneSessionManager::GetInstance().CheckCollaboratorType(info.collaboratorType_)) {
                 sceneSession = SceneSessionManager::GetInstance().FindSessionByAffinity(
                     info.sessionAffinity);
             } else {
-                sceneSession = SceneSessionManager::GetInstance().GetSceneSessionByName(info.bundleName_,
-                    info.moduleName_, info.abilityName_, info.appIndex_, info.appInstanceKey_, info.windowType_);
+                ComparedSessionInfo compareSessionInfo = { info.bundleName_, info.moduleName_, info.abilityName_,
+                    info.appIndex_, info.appInstanceKey_, info.windowType_, info.isAtomicService_};
+                sceneSession = SceneSessionManager::GetInstance().GetSceneSessionByName(compareSessionInfo);
             }
         }
         if (sceneSession == nullptr) {
