8f2a0f774a9b8ea44b4fd74efb5a36021ee8d027
flybear <lichao436@huawei.com>
2024-09-24 02:51:54 +0000

    回退https://gitee.com/openharmony/window_window_manager/pulls/8234 中部分代码
    Signed-off-by: flybear <lichao436@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
@@ -2905,9 +2905,7 @@ void JsSceneSession::PendingSessionActivation(SessionInfo& info)
 
     auto callerSession = SceneSessionManager::GetInstance().GetSceneSession(info.callerPersistentId_);
     if (callerSession != nullptr) {
-        bool isSceneBoardBundle = SessionPermission::IsSameAppAsCalling("", "");
-        bool isCalledRightlyByCallerId = ((info.callerToken_ == callerSession->GetAbilityToken()) &&
-            isSceneBoardBundle);
+        bool isCalledRightlyByCallerId = ((info.callerToken_ == callerSession->GetAbilityToken()));
         TLOGI(WmsLogTag::WMS_SCB,
             "isCalledRightlyByCallerId result is: %{public}d", isCalledRightlyByCallerId);
         info.isCalledRightlyByCallerId_ = isCalledRightlyByCallerId;
