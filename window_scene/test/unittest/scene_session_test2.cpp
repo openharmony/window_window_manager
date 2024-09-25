36c3fe6d96705a96c18e20572aa9691fbaa5aa52
liusensen <liusen43@huawei.com>
2024-09-25 09:55:18 +0800

    SceneSessionManagerTest5 modify
    Signed-off-by: liusensen <liusen43@huawei.com>

--- window_scene/test/unittest/scene_session_test2.cpp
+++ window_scene/test/unittest/scene_session_test2.cpp
@@ -1668,14 +1668,13 @@ HWTEST_F(SceneSessionTest2, TransferPointerEvent03, Function | SmallTest | Level
     EXPECT_NE(property, nullptr);
     property->SetWindowType(WindowType::WINDOW_TYPE_FLOAT);
     sceneSession->SetSessionProperty(property);
-    ASSERT_EQ(WSError::WS_ERROR_INVALID_TYPE, sceneSession->SetPipActionEvent("pointerEvent", 0));
+    ASSERT_EQ(WSError::WS_ERROR_NULLPTR, sceneSession->SetPipActionEvent("pointerEvent", 0));
 
     sceneSession->sessionStage_ = sptr<SessionStageMocker>::MakeSptr();
     property->SetWindowType(WindowType::WINDOW_TYPE_PIP);
     property->SetWindowMode(WindowMode::WINDOW_MODE_PIP);
-    ASSERT_EQ(WSError::WS_OK, sceneSession->SetPipActionEvent("pointerEvent", 0));
-
     sceneSession->FixRectByLimits(limits, rect, ratio, false, vpr);
+    ASSERT_EQ(WSError::WS_OK, sceneSession->SetPipActionEvent("pointerEvent", 0));
 }
 
 /**
