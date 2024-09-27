ee76c1c86c1952a97203169c772f7520e7a85059
yyuehao <yangyuehao@huawei.com>
2024-09-27 11:06:00 +0000

    update window_scene/session/host/src/zidl/session_proxy.cpp. 修改IPC接口为异步任务
    Signed-off-by: yyuehao <yangyuehao@huawei.com>

--- window_scene/session/host/src/zidl/session_proxy.cpp
+++ window_scene/session/host/src/zidl/session_proxy.cpp
@@ -1854,7 +1854,7 @@ WSError SessionProxy::SetFocusableOnShow(bool isFocusableOnShow)
 {
     MessageParcel data;
     MessageParcel reply;
-    MessageOption option(MessageOption::TF_SYNC);
+    MessageOption option(MessageOption::TF_ASYNC);
     if (!data.WriteInterfaceToken(GetDescriptor())) {
         TLOGE(WmsLogTag::WMS_FOCUS, "WriteInterfaceToken failed");
         return WSError::WS_ERROR_IPC_FAILED;
