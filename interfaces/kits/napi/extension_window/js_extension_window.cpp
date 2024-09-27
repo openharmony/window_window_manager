560240887351c8b1505642233ea978d9e5d72739
c30063259 <chenjunliang26@h-partners.com>
2024-09-27 14:19:55 +0800

    错误捕获This指针对应日志调整
    Signed-off-by: haoxinpeng <haoxinpeng1@huawei.com>

--- interfaces/kits/napi/extension_window/js_extension_window.cpp
+++ interfaces/kits/napi/extension_window/js_extension_window.cpp
@@ -457,7 +457,7 @@ napi_value JsExtensionWindow::OnDestroyWindow(napi_env env, napi_callback_info i
     NapiAsyncTask::CompleteCallback complete =
         [extwin = extensionWindow_](napi_env env, NapiAsyncTask& task, int32_t status) {
             if (extwin == nullptr) {
-                TLOGE(WmsLogTag::WMS_UIEXT, "extensionWindow is null");
+                TLOGNE(WmsLogTag::WMS_UIEXT, "extensionWindow is null");
                 task.Reject(env,
                     CreateJsError(env, static_cast<int32_t>(WmErrorCode::WM_ERROR_STATE_ABNORMALLY)));
                 return;
@@ -510,7 +510,7 @@ napi_value JsExtensionWindow::OnShowWindow(napi_env env, napi_callback_info info
     NapiAsyncTask::CompleteCallback complete =
         [extwin = extensionWindow_](napi_env env, NapiAsyncTask& task, int32_t status) {
             if (extwin == nullptr) {
-                TLOGE(WmsLogTag::WMS_UIEXT, "extensionWindow is null");
+                TLOGNE(WmsLogTag::WMS_UIEXT, "extensionWindow is null");
                 task.Reject(env, CreateJsError(env,
                     static_cast<int32_t>(WmErrorCode::WM_ERROR_STATE_ABNORMALLY)));
                 return;
