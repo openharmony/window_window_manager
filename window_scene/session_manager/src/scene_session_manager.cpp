c2594861877fd2b3d6d4841f5aabd0f082ae8f77
ReCoder <jianhao2@h-partners.com>
2024-09-29 11:39:11 +0800

    fix: conflicts
    Signed-off-by: ReCoder <jianhao2@h-partners.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -7551,16 +7551,19 @@ void SceneSessionManager::UpdateAvoidAreaByType(int32_t persistentId, AvoidAreaT
 {
     auto task = [this, persistentId, type] {
         auto sceneSession = GetSceneSession(persistentId);
-        if (sceneSession == nullptr || !IsSessionVisibleForeground(sceneSession)) {
-            TLOGND(WmsLogTag::WMS_IMMS, "window %{public}d is nullptr or invisible", persistentId);
+        if (sceneSession == nullptr || !IsSessionVisible(sceneSession)) {
+            TLOGD(WmsLogTag::WMS_IMMS, "window %{public}d is nullptr or invisible", persistentId);
             return;
         }
         if (avoidAreaListenerSessionSet_.find(persistentId) == avoidAreaListenerSessionSet_.end()) {
-            TLOGND(WmsLogTag::WMS_IMMS, "window %{public}d has no listener, no need update", persistentId);
+            TLOGD(WmsLogTag::WMS_IMMS, "window %{public}d has no listener, no need update", persistentId);
             return;
         }
-        if (sceneSession->IsImmersiveType()) {
-            TLOGND(WmsLogTag::WMS_IMMS, "window %{public}d is immersive type", persistentId);
+        auto windowType = sceneSession->GetWindowType();
+        if (windowType == WindowType::WINDOW_TYPE_STATUS_BAR ||
+            windowType == WindowType::WINDOW_TYPE_NAVIGATION_BAR ||
+            windowType == WindowType::WINDOW_TYPE_INPUT_METHOD_FLOAT) {
+            TLOGD(WmsLogTag::WMS_IMMS, "window %{public}d is immersive type", persistentId);
             return;
         }
         auto avoidArea = sceneSession->GetAvoidAreaByType(type);
