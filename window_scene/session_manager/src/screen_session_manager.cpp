a0f8f3f81c88a964badfca2ed3752e7eea9a7058
李聪 <licong113@huawei.com>
2024-10-08 11:31:00 +0800

    解决获取displayinfo 卡死问题
    Signed-off-by: 李聪 <licong113@huawei.com>

--- window_scene/session_manager/src/screen_session_manager.cpp
+++ window_scene/session_manager/src/screen_session_manager.cpp
@@ -667,7 +667,8 @@ sptr<ScreenSession> ScreenSessionManager::GetDefaultScreenSession()
     return GetScreenSession(defaultScreenId_);
 }
 
-sptr<DisplayInfo> ScreenSessionManager::HookDisplayInfoByUid(sptr<DisplayInfo> displayInfo)
+sptr<DisplayInfo> ScreenSessionManager::HookDisplayInfoByUid(sptr<DisplayInfo> displayInfo,
+    const sptr<ScreenSession>& screenSession)
 {
     if (displayInfo == nullptr) {
         TLOGI(WmsLogTag::DMS, "ConvertToDisplayInfo error, displayInfo is nullptr.");
@@ -686,7 +687,6 @@ sptr<DisplayInfo> ScreenSessionManager::HookDisplayInfoByUid(sptr<DisplayInfo> d
         displayInfo->SetHeight(info.height_);
         displayInfo->SetVirtualPixelRatio(info.density_);
         if (info.enableHookRotation_) {
-            sptr<ScreenSession> screenSession = GetScreenSession(displayInfo->GetScreenId());
             if (screenSession) {
                 Rotation targetRotation = screenSession->ConvertIntToRotation(static_cast<int32_t>(info.rotation_));
                 displayInfo->SetRotation(targetRotation);
@@ -694,6 +694,9 @@ sptr<DisplayInfo> ScreenSessionManager::HookDisplayInfoByUid(sptr<DisplayInfo> d
                     FoldDisplayMode::UNKNOWN);
                 TLOGI(WmsLogTag::DMS, "tR: %{public}u, tO: %{public}u", targetRotation, targetOrientation);
                 displayInfo->SetDisplayOrientation(targetOrientation);
+            } else {
+                TLOGI(WmsLogTag::DMS, "ConvertToDisplayInfo error, screenSession is nullptr.");
+                return nullptr;
             }
         }
     }
@@ -712,7 +715,7 @@ sptr<DisplayInfo> ScreenSessionManager::GetDefaultDisplayInfo()
             return nullptr;
         }
         // 在PC/PAD上安装的竖屏应用以及白名单中的应用在显示状态非全屏时需要hook displayinfo
-        displayInfo = HookDisplayInfoByUid(displayInfo);
+        displayInfo = HookDisplayInfoByUid(displayInfo, screenSession);
         return displayInfo;
     } else {
         TLOGE(WmsLogTag::DMS, "Get default screen session failed.");
@@ -738,7 +741,7 @@ sptr<DisplayInfo> ScreenSessionManager::GetDisplayInfoById(DisplayId displayId)
         }
         if (displayId == displayInfo->GetDisplayId()) {
             TLOGD(WmsLogTag::DMS, "GetDisplayInfoById success");
-            displayInfo = HookDisplayInfoByUid(displayInfo);
+            displayInfo = HookDisplayInfoByUid(displayInfo, screenSession);
             return displayInfo;
         }
     }
@@ -851,6 +854,7 @@ bool ScreenSessionManager::ConvertScreenIdToRsScreenId(ScreenId screenId, Screen
 
 void ScreenSessionManager::UpdateDisplayHookInfo(int32_t uid, bool enable, const DMHookInfo& hookInfo)
 {
+    TLOGD(WmsLogTag::DMS, "DisplayHookInfo will update");
     if (!SessionPermission::IsSystemCalling()) {
         TLOGE(WmsLogTag::DMS, "UpdateDisplayHookInfo permission denied!");
         return;
