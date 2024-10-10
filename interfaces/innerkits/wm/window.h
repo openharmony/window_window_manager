a5daa55694737d170e7f61819ba126ac5095be94
yyuehao <yangyuehao@huawei.com>
2024-10-10 12:53:12 +0000

    update interfaces/innerkits/wm/window.h. fix
    Signed-off-by: yyuehao <yangyuehao@huawei.com>

--- interfaces/innerkits/wm/window.h
+++ interfaces/innerkits/wm/window.h
@@ -1084,7 +1084,7 @@ public:
      */
     virtual WMError RequestFocusByClient(bool isFocused) const { return WMError::WM_ERROR_DEVICE_NOT_SUPPORT; }
     /**
-     * @brief Check current focus status.https://gitee.com/openharmony/window_window_manager/issues/IAW4II?from=project-issue
+     * @brief Check current focus status.
      *
      * @return True means window is focused, false means window is unfocused.
      */
