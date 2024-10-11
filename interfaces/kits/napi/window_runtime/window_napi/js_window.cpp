cec864aa2c574a6a66699d5be4f05aac139ec55d
万孝国 <wanxiaoguo@huawei.com>
2024-10-11 15:41:27 +0800

    子子窗创建错误码整改
    Signed-off-by: 万孝国 <wanxiaoguo@huawei.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
+++ interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
@@ -6861,7 +6861,6 @@ napi_value JsWindow::OnCreateSubWindowWithOptions(napi_env env, napi_callback_in
     }
     if (!windowToken_->IsPcOrPadCapabilityEnabled()) {
         TLOGE(WmsLogTag::WMS_SUB, "device not support");
-        napi_throw(env, JsErrUtils::CreateJsError(env, WmErrorCode::WM_ERROR_DEVICE_NOT_SUPPORT));
         return NapiGetUndefined(env);
     }
     size_t argc = 4;
