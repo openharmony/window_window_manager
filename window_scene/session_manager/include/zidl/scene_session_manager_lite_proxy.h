3e6f49d4e90243674a7aa4f9a323d4805b3eb212
lcaidm <lichen139@huawei.com>
2024-10-12 18:38:08 +0800

    bugfix Signed-off-by:lcaidm<lichen139@huawei.com>
    Signed-off-by: lcaidm <lichen139@huawei.com>

--- window_scene/session_manager/include/zidl/scene_session_manager_lite_proxy.h
+++ window_scene/session_manager/include/zidl/scene_session_manager_lite_proxy.h
@@ -71,10 +71,10 @@ public:
     WMError CheckWindowId(int32_t windowId, int32_t &pid) override;
     WMError GetVisibilityWindowInfo(std::vector<sptr<WindowVisibilityInfo>>& infos) override;
     WMError GetWindowModeType(WindowModeType& windowModeType) override;
+    WSError RaiseWindowToTop(int32_t persistentId) override;
     WMError GetMainWindowInfos(int32_t topNum, std::vector<MainWindowInfo>& topNInfo) override;
     WMError GetAllMainWindowInfos(std::vector<MainWindowInfo>& infos) override;
     WMError ClearMainSessions(const std::vector<int32_t>& persistentIds, std::vector<int32_t>& clearFailedIds) override;
-    WSError RaiseWindowToTop(int32_t persistentId) override;
     WSError RegisterIAbilityManagerCollaborator(int32_t type,
         const sptr<AAFwk::IAbilityManagerCollaborator>& impl) override;
     WSError UnregisterIAbilityManagerCollaborator(int32_t type) override;
