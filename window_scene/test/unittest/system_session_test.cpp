021153406d6c4bc5da1f12eb2295ba174b1e3f02
张航鸣 <zhanghangming3@h-partners.com>
2024-10-12 16:11:44 +0800

    商分代码同步
    Signed-off-by: 张航鸣 <zhanghangming3@h-partners.com>

--- window_scene/test/unittest/system_session_test.cpp
+++ window_scene/test/unittest/system_session_test.cpp
@@ -103,88 +103,6 @@ HWTEST_F(SystemSessionTest, TransferKeyEvent02, Function | SmallTest | Level1)
 }
 
 /**
- * @tc.name: TransferKeyEvent03
- * @tc.desc: check func TransferKeyEvent
- * @tc.type: FUNC
- */
-HWTEST_F(SystemSessionTest, TransferKeyEvent03, Function | SmallTest | Level1)
-{
-    systemSession_->state_ = SessionState::STATE_CONNECT;
-    std::shared_ptr<MMI::KeyEvent> keyEvent = MMI::KeyEvent::Create();
-
-    sptr<WindowSessionProperty> windowSessionProperty = new (std::nothrow) WindowSessionProperty();
-    EXPECT_NE(nullptr, windowSessionProperty);
-    systemSession_->property_ = windowSessionProperty;
-    EXPECT_NE(WSError::WS_OK, systemSession_->TransferKeyEvent(keyEvent));
-}
-
-/**
- * @tc.name: CheckKeyEventDispatch01
- * @tc.desc: check func CheckKeyEventDispatch
- * @tc.type: FUNC
- */
-HWTEST_F(SystemSessionTest, CheckKeyEventDispatch01, Function | SmallTest | Level1)
-{
-    std::shared_ptr<MMI::KeyEvent> keyEvent = MMI::KeyEvent::Create();
-    systemSession_->isRSVisible_ = false;
-    systemSession_->winRect_ = {1, 1, 1, 1};
-    ASSERT_EQ(false, systemSession_->CheckKeyEventDispatch(keyEvent));
-
-    systemSession_->isRSVisible_ = true;
-    systemSession_->winRect_ = {1, 1, 1, 1};
-    ASSERT_EQ(false, systemSession_->CheckKeyEventDispatch(keyEvent));
-
-    systemSession_->winRect_ = {1, 1, 1, 0};
-    ASSERT_EQ(false, systemSession_->CheckKeyEventDispatch(keyEvent));
-
-    systemSession_->isRSVisible_ = true;
-    systemSession_->winRect_ = {1, 1, 0, 0};
-    ASSERT_EQ(false, systemSession_->CheckKeyEventDispatch(keyEvent));
-}
-
-/**
- * @tc.name: CheckKeyEventDispatch02
- * @tc.desc: check func CheckKeyEventDispatch
- * @tc.type: FUNC
- */
-HWTEST_F(SystemSessionTest, CheckKeyEventDispatch02, Function | SmallTest | Level1)
-{
-    std::shared_ptr<MMI::KeyEvent> keyEvent;
-    systemSession_->isRSVisible_ = true;
-    systemSession_->winRect_ = {1, 1, 0, 0};
-
-    SessionInfo info;
-    info.abilityName_ = "ParamSystemSession1";
-    info.moduleName_ = "ParamSystemSession2";
-    info.bundleName_ = "ParamSystemSession3";
-    sptr<Session> session = new (std::nothrow) Session(info);
-    systemSession_->parentSession_ = session;
-
-    systemSession_->parentSession_->state_ = SessionState::STATE_CONNECT;
-    systemSession_->state_ = SessionState::STATE_FOREGROUND;
-    ASSERT_EQ(false, systemSession_->CheckKeyEventDispatch(keyEvent));
-
-    systemSession_->state_ = SessionState::STATE_ACTIVE;
-    ASSERT_EQ(false, systemSession_->CheckKeyEventDispatch(keyEvent));
-
-    systemSession_->state_ = SessionState::STATE_CONNECT;
-    systemSession_->parentSession_->state_ = SessionState::STATE_FOREGROUND;
-    ASSERT_EQ(false, systemSession_->CheckKeyEventDispatch(keyEvent));
-
-    systemSession_->parentSession_->state_ = SessionState::STATE_ACTIVE;
-    ASSERT_EQ(false, systemSession_->CheckKeyEventDispatch(keyEvent));
-
-    systemSession_->parentSession_->state_ = SessionState::STATE_CONNECT;
-    systemSession_->state_ = SessionState::STATE_CONNECT;
-    ASSERT_EQ(false, systemSession_->CheckKeyEventDispatch(keyEvent));
-
-    systemSession_->parentSession_->state_ = SessionState::STATE_FOREGROUND;
-    systemSession_->state_ = SessionState::STATE_FOREGROUND;
-    ASSERT_EQ(false, systemSession_->CheckKeyEventDispatch(keyEvent));
-}
-
-
-/**
  * @tc.name: ProcessBackEvent01
  * @tc.desc: check func ProcessBackEvent
  * @tc.type: FUNC
@@ -271,35 +189,6 @@ HWTEST_F(SystemSessionTest, ProcessPointDownSession, Function | SmallTest | Leve
     int32_t posY = 3;
     auto ret = systemSession_->ProcessPointDownSession(posX, posY);
     ASSERT_EQ(WSError::WS_OK, ret);
-
-    sptr<WindowSessionProperty> windowSessionProperty = new (std::nothrow) WindowSessionProperty();
-    EXPECT_NE(nullptr, windowSessionProperty);
-
-    systemSession_->property_ = windowSessionProperty;
-    ret = systemSession_->ProcessPointDownSession(posX, posY);
-    ASSERT_EQ(WSError::WS_OK, ret);
-
-    windowSessionProperty->raiseEnabled_ = false;
-    ret = systemSession_->ProcessPointDownSession(posX, posY);
-    ASSERT_EQ(WSError::WS_OK, ret);
-}
-
-/**
- * @tc.name: GetMissionId
- * @tc.desc: test function : GetMissionId
- * @tc.type: FUNC
- */
-HWTEST_F(SystemSessionTest, GetMissionId, Function | SmallTest | Level1)
-{
-    ASSERT_TRUE(systemSession_ != nullptr);
-    SessionInfo info;
-    info.abilityName_ = "testSystemSession1";
-    info.moduleName_ = "testSystemSession2";
-    info.bundleName_ = "testSystemSession3";
-    sptr<Session> session = new (std::nothrow) Session(info);
-    systemSession_->parentSession_ = session;
-    auto ret = systemSession_->GetMissionId();
-    ASSERT_EQ(0, ret);
 }
 
 /**
