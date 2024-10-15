69cc831ab6542e0fcff5356597d94e197867ce09
imp9527 <liumingxin4@h-partners.com>
2024-10-15 15:52:56 +0800

    在头文件增加方法声明
    Signed-off-by: imp9527 <liumingxin4@h-partners.com>

--- window_scene/session/host/include/scene_session.h
+++ window_scene/session/host/include/scene_session.h
@@ -526,6 +526,8 @@ private:
     void GetAINavigationBarArea(WSRect rect, AvoidArea& avoidArea) const;
     void HandleStyleEvent(MMI::WindowArea area) override;
     WSError HandleEnterWinwdowArea(int32_t windowX, int32_t windowY);
+    bool CheckIdentityToken(const std::string& identityToken);
+    bool CheckPid();
 
     // session lifecycle funcs
     WSError ForegroundTask(const sptr<WindowSessionProperty>& property);
