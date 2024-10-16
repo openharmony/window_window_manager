fdfc0d9bae8e1aa80cd8ff5833e618af17fb2d0c
flybear <lichao436@huawei.com>
2024-10-16 10:56:25 +0000

    update window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp.
    Signed-off-by: flybear <lichao436@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
@@ -2751,7 +2751,7 @@ void JsSceneSession::PendingSessionActivation(SessionInfo& info)
         bool isCalledRightlyByCallerId = ((info.callerToken_ == callerSession->GetAbilityToken()) &&
           info.bundleName_ == "");
         TLOGI(WmsLogTag::WMS_SCB,
-            "update isCalledRightlyByCallerId result is from :%{public}d to false", info.isCalledRightlyByCallerId_);
+            "update isCalledRightlyByCallerId from :%{public}d to false", info.isCalledRightlyByCallerId_);
         info.isCalledRightlyByCallerId_ = false;
     }
     std::shared_ptr<SessionInfo> sessionInfo = std::make_shared<SessionInfo>(info);
