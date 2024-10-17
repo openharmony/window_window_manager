6332df191643e302271b1bd568bcc1433b0275c0
lpf <liaopengfei@h-partners.com>
2024-10-17 19:21:39 +0800

    窗口布局层级焦点低级编码问题排查, 放在static_cast后面判断
    Signed-off-by: lpf <liaopengfei@h-partners.com>

--- window_scene/session/host/src/zidl/session_stub.cpp
+++ window_scene/session/host/src/zidl/session_stub.cpp
@@ -628,11 +628,11 @@ int SessionStub::HandleUpdateSessionRect(MessageParcel& data, MessageParcel& rep
         TLOGE(WmsLogTag::WMS_LAYOUT, "read changeReason failed");
         return ERR_INVALID_DATA;
     }
-    if (SizeChangeReason::UNDEFINED > changeReason || changeReason > SizeChangeReason::END) {
+    SizeChangeReason reason = static_cast<SizeChangeReason>(changeReason);
+    if (SizeChangeReason::UNDEFINED > reason || reason > SizeChangeReason::END) {
         TLOGE(WmsLogTag::WMS_LAYOUT, "Unknown reason");
         return ERR_INVALID_DATA;
     }
-    SizeChangeReason reason = static_cast<SizeChangeReason>(changeReason);
     bool isGlobal = false;
     if (!data.ReadBool(isGlobal)) {
         TLOGE(WmsLogTag::WMS_LAYOUT, "read isGlobal failed");
