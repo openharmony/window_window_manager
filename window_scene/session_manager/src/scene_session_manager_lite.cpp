5912513ce0998fad2893121143111058ceae0b27
李鹏 <lipeng364@huawei.com>
2024-10-17 14:35:07 +0800

    sessionManagerLite 多线程优化
    Signed-off-by: 李鹏 <lipeng364@huawei.com>

--- window_scene/session_manager/src/scene_session_manager_lite.cpp
+++ window_scene/session_manager/src/scene_session_manager_lite.cpp
@@ -80,7 +80,7 @@ WSError SceneSessionManagerLite::GetSessionInfoByContinueSessionId(
     return SceneSessionManager::GetInstance().GetSessionInfoByContinueSessionId(continueSessionId, sessionInfo);
 }
 
-WSError SceneSessionManagerLite::RegisterSessionListener(const sptr<ISessionListener>& listener)
+WSError SceneSessionManagerLite::RegisterSessionListener(const sptr<ISessionListener>& listener, bool isRecover)
 {
     WLOGFD("in");
     return SceneSessionManager::GetInstance().RegisterSessionListener(listener);
