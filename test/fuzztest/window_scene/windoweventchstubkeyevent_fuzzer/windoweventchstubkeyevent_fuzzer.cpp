ededf98d6e4eb7e0db18aa58bab07d8cc226e56d
tanyuhang <tanyuhang@huawei.com>
2024-10-21 03:06:36 +0000

    update windoweventchstubkeyevent_fuzzer.cpp.
    Signed-off-by: tanyuhang <tanyuhang@huawei.com>

--- test/fuzztest/window_scene/windoweventchstubkeyevent_fuzzer/windoweventchstubkeyevent_fuzzer.cpp
+++ test/fuzztest/window_scene/windoweventchstubkeyevent_fuzzer/windoweventchstubkeyevent_fuzzer.cpp
@@ -74,14 +74,6 @@ bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
     eventStub->OnRemoteRequest(
         static_cast<uint32_t>(Rosen::WindowEventInterfaceCode::TRANS_ID_TRANSFER_ACCESSIBILITY_DUMP_CHILD_INFO),
         parcel, reply, option);
-    parcel.RewindRead(0);
-    eventStub->OnRemoteRequest(
-        static_cast<uint32_t>(Rosen::WindowEventInterfaceCode::TRANS_ID_TRANSFER_ACCESSIBILITY_CHILD_TREE_REGISTER),
-        parcel, reply, option);
-    parcel.RewindRead(0);
-    eventStub->OnRemoteRequest(
-        static_cast<uint32_t>(Rosen::WindowEventInterfaceCode::TRANS_ID_TRANSFER_ACCESSIBILITY_DUMP_CHILD_INFO),
-        parcel, reply, option);
     return true;
 }
 } // namespace.OHOS
