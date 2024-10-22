43ce518a988792e3aec4b28299c39eb171eff1dd
许杨杨 <xuyangyang29@huawei.com>
2024-10-22 08:01:50 +0000

    update wmserver/test/unittest/window_controller_test.cpp.
    Signed-off-by: 许杨杨 <xuyangyang29@huawei.com>

--- wmserver/test/unittest/window_controller_test.cpp
+++ wmserver/test/unittest/window_controller_test.cpp
@@ -1023,9 +1023,9 @@ HWTEST_F(WindowControllerTest, ChangeMouseStyle2, Function | SmallTest | Level3)
     sptr<MoveDragProperty> moveDragProperty;
     WMError res = windowController_->ChangeMouseStyle(windowId, moveDragProperty);
     if (SceneBoardJudgement::IsSceneBoardEnabled()) {
-        ASSERT_EQ(WMError::WM_ERROR_INVALID_OPERATION, res);
-    } else {
         ASSERT_EQ(WMError::WM_OK, res);
+    } else {
+        ASSERT_EQ(WMError::WM_ERROR_INVALID_OPERATION, res);
     }
 }
 
