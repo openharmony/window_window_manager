4a71fa2e1774eb5903a6b9b586c85d7422400506
giteeOrange <zhuying79@huawei.com>
2024-10-26 11:18:33 +0800

    修改魔鬼数字。
    Signed-off-by: giteeOrange <zhuying79@huawei.com>

--- wm/test/unittest/window_manager_lite_test.cpp
+++ wm/test/unittest/window_manager_lite_test.cpp
@@ -57,11 +57,13 @@ public:
     void OnWindowVisibilityChanged(const std::vector<sptr<WindowVisibilityInfo>>& windowVisibilityInfo) override {};
 };
 
+
 class IWMSConnectionChangedListenerSon : public IWMSConnectionChangedListener {
 public:
     void OnConnected(int32_t userId, int32_t screenId) override {};
     void OnDisconnected(int32_t userId, int32_t screenId) override {};
-}
+};
+
 
 class TestWindowStyleChangedListener : public IWindowStyleChangedListener {
 public:
@@ -261,6 +263,7 @@ HWTEST_F(WindowManagerLiteTest, Test01, Function | SmallTest | Level2)
     WMError res10 = WindowManagerLite::GetInstance().UnregisterWMSConnectionChangedListener();
     ASSERT_EQ(WMError::WM_OK, res10);
 }
+
 
 /**
  * @tc.name: Test04
@@ -867,6 +870,7 @@ HWTEST_F(WindowManagerLiteTest, TerminateSessionByPersistentId002, Function | Sm
     auto errorCode = WindowManagerLite::GetInstance().TerminateSessionByPersistentId(persistentId);
     ASSERT_EQ(WMError::WM_ERROR_INVALID_PARAM, errorCode);
 }
+
 
 /**
  * @tc.name: OnRemoteDied01
