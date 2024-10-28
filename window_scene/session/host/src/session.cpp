bbb580bd26c084b83b0d1772dd3fdcf84f4b681e
x00856704 <1049772496@qq.com>
2024-10-28 10:41:50 +0800

    Signed-off-by: x00856704 <1049772496@qq.com> （cherry picked commit from <gitee.com//openharmony/window_window_manager/commit/a8107f70eb8698f7259854940ac4d4f55ba6dd9f>

--- window_scene/session/host/src/session.cpp
+++ window_scene/session/host/src/session.cpp
@@ -2409,11 +2409,6 @@ WSError Session::UpdateWindowMode(WindowMode mode)
         property->SetWindowMode(mode);
         if (mode == WindowMode::WINDOW_MODE_SPLIT_PRIMARY || mode == WindowMode::WINDOW_MODE_SPLIT_SECONDARY) {
             property->SetMaximizeMode(MaximizeMode::MODE_RECOVER);
-            if (mode == WindowMode::WINDOW_MODE_SPLIT_PRIMARY) {
-                surfaceNode_->MarkUifirstNode(false);
-            }
-        } else {
-            surfaceNode_->MarkUifirstNode(true);
         }
         UpdateGestureBackEnabled();
         UpdateGravityWhenUpdateWindowMode(mode);
