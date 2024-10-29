909006c02959a876613bad80676e92c7f3d50b77
yangyuanzhou <yangyuanzhou@huawei.com>
2024-10-29 20:12:46 +0800

    调用setDefaultDensityEnabled时通知其他模块
    Signed-off-by: yangyuanzhou <yangyuanzhou@huawei.com>

--- window_scene/session/host/src/zidl/session_proxy.cpp
+++ window_scene/session/host/src/zidl/session_proxy.cpp
@@ -605,6 +605,33 @@ WSError SessionProxy::OnLayoutFullScreenChange(bool isLayoutFullScreen)
     return static_cast<WSError>(ret);
 }
 
+WSError SessionProxy::OnDefaultDensityEnabled(bool isDefaultDensityEnabled)
+{
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option(MessageOption::TF_ASYNC);
+    if (!data.WriteInterfaceToken(GetDescriptor())) {
+        TLOGE(WmsLogTag::WMS_LAYOUT, "WriteInterfaceToken failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    if (!data.WriteBool(isDefaultDensityEnabled)) {
+        TLOGE(WmsLogTag::WMS_LAYOUT, "Write isDefaultDensityEnabled failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    sptr<IRemoteObject> remote = Remote();
+    if (remote == nullptr) {
+        TLOGE(WmsLogTag::WMS_LAYOUT, "remote is null");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    if (remote->SendRequest(static_cast<uint32_t>(SessionInterfaceCode::TRANS_ID_DEFAULT_DENSITY_ENABLED),
+        data, reply, option) != ERR_NONE) {
+        TLOGE(WmsLogTag::WMS_LAYOUT, "SendRequest failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    int32_t ret = reply.ReadInt32();
+    return static_cast<WSError>(ret);
+}
+
 WSError SessionProxy::UpdateSessionRect(const WSRect& rect, const SizeChangeReason reason,
     bool isGlobal, bool isFromMoveToGlobal)
 {
