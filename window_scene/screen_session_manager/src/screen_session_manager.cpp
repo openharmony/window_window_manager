cf685ee851fa2d0f82dacce6ec5b47fdef07baf9
宋佳伟 <songjiawei9@h-partners.com>
2024-10-30 10:45:09 +0800

    Description:dpi向上取整 IssueNo:https://gitee.com/openharmony/window_window_manager/issues/IAZO2V Binary Source:No Signed-off-by: songjiawei9 <songjiawei9@h-partners.com>

--- window_scene/screen_session_manager/src/screen_session_manager.cpp
+++ window_scene/screen_session_manager/src/screen_session_manager.cpp
@@ -1467,6 +1467,21 @@ void ScreenSessionManager::CreateScreenProperty(ScreenId screenId, ScreenPropert
     property.SetPhyBounds(screenBounds);
     property.SetBounds(screenBounds);
     property.SetAvailableArea({0, 0, screenMode.GetScreenWidth(), screenMode.GetScreenHeight()});
+    if (isDensityDpiLoad_) {
+        if (screenId == SCREEN_ID_MAIN) {
+            TLOGI(WmsLogTag::DMS, "subDensityDpi_ = %{public}f", subDensityDpi_);
+            property.SetVirtualPixelRatio(subDensityDpi_);
+            property.SetDefaultDensity(subDensityDpi_);
+            property.SetDensityInCurResolution(subDensityDpi_);
+        } else {
+            TLOGI(WmsLogTag::DMS, "densityDpi_ = %{public}f", densityDpi_);
+            property.SetVirtualPixelRatio(densityDpi_);
+            property.SetDefaultDensity(densityDpi_);
+            property.SetDensityInCurResolution(densityDpi_);
+        }
+    } else {
+        property.UpdateVirtualPixelRatio(screenBounds);
+    }
     property.SetRefreshRate(screenRefreshRate);
     property.SetDefaultDeviceRotationOffset(defaultDeviceRotationOffset_);
 
@@ -1478,32 +1493,27 @@ void ScreenSessionManager::CreateScreenProperty(ScreenId screenId, ScreenPropert
     property.CalcDefaultDisplayOrientation();
 }
 
-void ScreenSessionManager::InitScreenDensity(sptr<ScreenSession> session, const ScreenProperty& property)
+void ScreenSessionManager::InitExtendScreenDensity(sptr<ScreenSession> session, ScreenProperty property)
 {
-    if (session->GetScreenProperty().GetScreenType() == ScreenType::REAL && !session->isInternal_) {
-        // 表示拓展屏
-        float extendDensity = CalcDefaultExtendScreenDensity(property);
-        TLOGI(WmsLogTag::DMS, "extendDensity = %{public}f", extendDensity);
-        session->SetVirtualPixelRatio(extendDensity);
-        session->SetDefaultDensity(extendDensity);
-        session->SetDensityInCurResolution(extendDensity);
+    if (session->GetScreenProperty().GetScreenType() != ScreenType::REAL || session->isInternal_) {
+        // 表示非拓展屏
+        TLOGI(WmsLogTag::DMS, "Not expandable screen, no need to set dpi");
         return;
     }
-    if (isDensityDpiLoad_) {
-        if (session->GetScreenId() == SCREEN_ID_MAIN) {
-            TLOGI(WmsLogTag::DMS, "subDensityDpi_ = %{public}f", subDensityDpi_);
-            session->SetVirtualPixelRatio(subDensityDpi_);
-            session->SetDefaultDensity(subDensityDpi_);
-            session->SetDensityInCurResolution(subDensityDpi_);
-        } else {
-            TLOGI(WmsLogTag::DMS, "densityDpi_ = %{public}f", densityDpi_);
-            session->SetVirtualPixelRatio(densityDpi_);
-            session->SetDefaultDensity(densityDpi_);
-            session->SetDensityInCurResolution(densityDpi_);
-        }
-    } else {
-        session->UpdateVirtualPixelRatio(property.GetBounds());
+    float extendDensity = CalcDefaultExtendScreenDensity(property);
+    TLOGI(WmsLogTag::DMS, "extendDensity = %{public}f", extendDensity);
+    session->SetVirtualPixelRatio(extendDensity);
+    session->SetDefaultDensity(extendDensity);
+    session->SetDensityInCurResolution(extendDensity);
+    ScreenId screenId = session->GetScreenId();
+    property.SetVirtualPixelRatio(extendDensity);
+    property.SetDefaultDensity(extendDensity);
+    property.SetDensityInCurResolution(extendDensity);
+    {
+        std::lock_guard<std::recursive_mutex> lock_phy(phyScreenPropMapMutex_);
+        phyScreenPropMap_[screenId] = property;
     }
+    return;
 }
 
 float ScreenSessionManager::CalcDefaultExtendScreenDensity(const ScreenProperty& property)
@@ -1561,6 +1571,7 @@ sptr<ScreenSession> ScreenSessionManager::GetOrCreateScreenSession(ScreenId scre
         return session;
     }
     session->RegisterScreenChangeListener(this);
+    InitExtendScreenDensity(session, property);
     InitAbstractScreenModesInfo(session);
     session->groupSmsId_ = 1;
     {
@@ -1573,7 +1584,6 @@ sptr<ScreenSession> ScreenSessionManager::GetOrCreateScreenSession(ScreenId scre
     screenEventTracker_.RecordEvent("create screen session success.");
     SetHdrFormats(screenId, session);
     SetColorSpaces(screenId, session);
-    InitScreenDensity(session, property);
     RegisterRefreshRateChangeListener();
     TLOGI(WmsLogTag::DMS, "CreateScreenSession success. ScreenId: %{public}" PRIu64 "", screenId);
     return session;
