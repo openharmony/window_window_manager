15dd74018788e8519b3cc0887eee5b15d1d6fd5b
fukun <214202095@qq.com>
2024-10-30 13:57:49 +0800

    应用启动瞬间，侧滑退出，不再直接terminate应用，而是minimize应用，与默认的back侧滑退出行为保持一致 Signed-off-by: fukun <fukun11@huawei.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -3205,13 +3205,7 @@ WSError SceneSessionManager::ProcessBackEvent()
         WLOGFI("ProcessBackEvent session persistentId:%{public}d needBlock::%{public}d",
             focusedSessionId_, needBlockNotifyFocusStatusUntilForeground_);
         if (needBlockNotifyFocusStatusUntilForeground_) {
-            WLOGFD("RequestSessionBack when start session");
-            if (session->GetSessionInfo().abilityInfo != nullptr &&
-                session->GetSessionInfo().abilityInfo->unclearableMission) {
-                TLOGI(WmsLogTag::WMS_MAIN, "backPress unclearableMission");
-                return WSError::WS_OK;
-            }
-            session->RequestSessionBack(false);
+            session->RequestSessionBack(true);
             return WSError::WS_OK;
         }
         if (session->GetSessionInfo().isSystem_ && rootSceneProcessBackEventFunc_) {
