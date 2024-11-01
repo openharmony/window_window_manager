e2a4f1db9faa580626c21271ef18932415e5b1ad
jjxxx123 <jiangxue32@h-partners.com>
2024-11-01 14:28:05 +0800

    窗口事件TDD用例整改
    Signed-off-by: jjxxx123 <jiangxue32@h-partners.com>

--- wm/test/unittest/input_transfer_station_test.cpp
+++ wm/test/unittest/input_transfer_station_test.cpp
@@ -115,9 +115,10 @@ HWTEST_F(InputTransferStationTest, OnInputEvent1, Function | SmallTest | Level2)
     keyEvent = nullptr;
     listener->OnInputEvent(keyEvent);
     keyEvent = tempKeyEvent;
-    auto channel = InputTransferStation::GetInstance().GetInputChannel(1);
+    InputTransferStation::GetInstance().destroyed_ = true;
+    auto channel = InputTransferStation::GetInstance().GetInputChannel(0);
     listener->OnInputEvent(keyEvent);
-    EXPECT_NE(nullptr, channel);
+    ASSERT_EQ(channel, nullptr);
 }
 
 /**
@@ -161,9 +162,10 @@ HWTEST_F(InputTransferStationTest, OnInputEvent3, Function | SmallTest | Level2)
     pointerEvent->SetAgentWindowId(0);
     listener->OnInputEvent(pointerEvent);
     pointerEvent->SetAgentWindowId(static_cast<uint32_t>(-1));
-    auto channel = InputTransferStation::GetInstance().GetInputChannel(1);
+    InputTransferStation::GetInstance().destroyed_ = true;
+    auto channel = InputTransferStation::GetInstance().GetInputChannel(0);
     listener->OnInputEvent(pointerEvent);
-    EXPECT_NE(nullptr, channel);
+    ASSERT_EQ(channel, nullptr);
 }
 
 /**
