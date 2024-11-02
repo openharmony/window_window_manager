7aa2beb020b590b76e9f53fb1023a1f29ee07c16
c30063259 <chenjunliang26@h-partners.com>
2024-11-02 10:17:37 +0800

    删除空行 Signed-off-by: c30063259 <chenjunliang26@h-partners.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window_listener.cpp
+++ interfaces/kits/napi/window_runtime/window_napi/js_window_listener.cpp
@@ -127,7 +127,6 @@ void JsWindowListener::OnSystemBarPropertyChange(DisplayId displayId, const Syst
     std::unique_ptr<NapiAsyncTask::CompleteCallback> complete = std::make_unique<NapiAsyncTask::CompleteCallback> (
         [self = weakRef_, displayId, tints, eng = env_] (napi_env env,
             NapiAsyncTask& task, int32_t status) {
-
             auto thisListener = self.promote();
             if (thisListener == nullptr || eng == nullptr) {
                 WLOGFE("[NAPI]this listener or eng is nullptr");
