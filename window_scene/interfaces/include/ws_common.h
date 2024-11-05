30756a1cf2496a053850919020f2fad918a23bc6
zhaoqi209 <18872749497@163.com>
2024-11-05 10:17:26 +0800

    PAD产品应用unspecified默认行为配置
    Signed-off-by: zhaoqi209 <18872749497@163.com>

--- window_scene/interfaces/include/ws_common.h
+++ window_scene/interfaces/include/ws_common.h
@@ -600,6 +600,7 @@ struct AppWindowSceneConfig {
 
 struct DeviceScreenConfig {
     std::string rotationPolicy_ = "11"; // default use phone policy
+    std::string defaultRotationPolicy_ = ""; // default unspecified policy
     bool isRightPowerButton_ = true;
 };
 
