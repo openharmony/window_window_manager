2ce946a111832fb107b5b7fb004d4a84c35156d3
zhoukai <zhoukai94@huawei.com>
2024-11-07 16:40:30 +0800

    编码规范同步
    Signed-off-by: zhoukai <zhoukai94@huawei.com>

--- window_scene/session/host/src/move_drag_controller.cpp
+++ window_scene/session/host/src/move_drag_controller.cpp
@@ -416,10 +416,9 @@ void MoveDragController::CalcDragTargetRect(const std::shared_ptr<MMI::PointerEv
         static_cast<uint64_t>(pointerEvent->GetTargetDisplayId()) == moveDragStartDisplayId_) {
         std::pair<int32_t, int32_t> trans = CalcUnifiedTranslate(pointerEvent);
         moveDragProperty_.targetRect_ =
-            MathHelper::GreatNotEqual(aspectRatio_, NEAR_ZERO)
-            ? CalcFixedAspectRatioTargetRect(type_, trans.first, trans.second, aspectRatio_,
-            moveDragProperty_.originalRect_)
-            : CalcFreeformTargetRect(type_, trans.first, trans.second, moveDragProperty_.originalRect_);
+            MathHelper::GreatNotEqual(aspectRatio_, NEAR_ZERO) ? CalcFixedAspectRatioTargetRect(type_, trans.first, 
+            trans.second, aspectRatio_, moveDragProperty_.originalRect_):
+            CalcFreeformTargetRect(type_, trans.first, trans.second, moveDragProperty_.originalRect_);
     }
     TLOGD(WmsLogTag::WMS_LAYOUT, "drag rect: %{public}s", moveDragProperty_.targetRect_.ToString().c_str());
 }
