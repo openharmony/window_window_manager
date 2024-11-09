67afcec8a2525541ad3373dc9d51066aa438554b
Hayden Lee <lihanyuan@huawei.com>
2024-11-09 12:16:10 +0000

    完善子窗id打印
    Signed-off-by: Hayden Lee <lihanyuan@huawei.com>

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -1596,17 +1596,19 @@ WMError WindowSessionImpl::SetRaiseByClickEnabled(bool raiseEnabled)
     auto parentId = GetParentId();
     if (parentId == INVALID_SESSION_ID) {
         TLOGE(WmsLogTag::WMS_HIERARCHY, "Window id: %{public}d Parent id is invalid!",
-        GetPersistentId());
+              GetPersistentId());
         return WMError::WM_ERROR_INVALID_PARENT;
     }
 
     if (!WindowHelper::IsSubWindow(GetType())) {
-        TLOGE(WmsLogTag::WMS_HIERARCHY, "Must be app sub window!");
+        TLOGE(WmsLogTag::WMS_HIERARCHY, "Window id: %{public}d Must be app sub window!",
+              GetPersistentId());
         return WMError::WM_ERROR_INVALID_CALLING;
     }
 
     if (state_ != WindowState::STATE_SHOWN) {
-        TLOGE(WmsLogTag::WMS_HIERARCHY, "The sub window must be shown!");
+        TLOGE(WmsLogTag::WMS_HIERARCHY, "Window id: %{public}d The sub window must be shown!",
+              GetPersistentId());
         return WMError::WM_DO_NOTHING;
     }
     if (IsWindowSessionInvalid()) {
