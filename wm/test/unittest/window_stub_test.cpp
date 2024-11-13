5812d0f476fbdd4ded4bc07990eeb89a28a2c305
卢宇豪 <luyuhao6@huawei.com>
2024-11-13 17:32:15 +0800

    change1
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- wm/test/unittest/window_stub_test.cpp
+++ wm/test/unittest/window_stub_test.cpp
@@ -460,7 +460,7 @@ HWTEST_F(WindowStubTest, OnRemoteRequest18, Function | SmallTest | Level2)
 
     uint32_t code = static_cast<uint32_t>(IWindow::WindowMessage::TRANS_ID_UPDATE_WINDOW_STATE);
     int res = windowStub_->OnRemoteRequest(code, data, reply, option);
-    EXPECT_EQ(res, static_cast<int>(ERR_NONE));
+    EXPECT_EQ(res, static_cast<int>(ERR_INVALID_DATA));
 }
 
 /**
