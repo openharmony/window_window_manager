928067a8931c3fcacf45c3dbc401921f3b3983b1
红书包指挥官 <gongjingbo2@huawei.com>
2024-11-13 14:57:47 +0800

    fix codes
    Signed-off-by: 红书包指挥官 <gongjingbo2@huawei.com>

--- window_scene/session_manager/src/scene_input_manager.cpp
+++ window_scene/session_manager/src/scene_input_manager.cpp
@@ -262,13 +262,23 @@ void SceneInputManager::FlushFullInfoToMMI(const std::vector<MMI::DisplayInfo>& 
 void SceneInputManager::FlushEmptyInfoToMMI()
 {
     auto task = [this]() {
-        TLOGI(WmsLogTag::WMS_EVENT, "Flush empty info to MMI");
+        std::vector<MMI::DisplayInfo> displayInfos;
+        ConstructDisplayInfos(displayInfos);
+        int mainScreenWidth = 0;
+        int mainScreenHeight = 0;
+        if (!displayInfos.empty()) {
+            mainScreenWidth = displayInfos[0].width;
+            mainScreenHeight = displayInfos[0].height;
+        }
         MMI::DisplayGroupInfo displayGroupInfo = {
-            .width = 0,
-            .height = 0,
+            .width = mainScreenWidth,
+            .height = mainScreenHeight,
             .focusWindowId = EMPTY_FOCUS_WINDOW_ID,
             .currentUserId = currentUserId_,
+            .displaysInfo = displayInfos
         };
+        TLOGI(WmsLogTag::WMS_EVENT, "currUserId:%{public}d width:%{public}d height:%{public}d",
+            currentUserId_, mainScreenWidth, mainScreenHeight);
         MMI::InputManager::GetInstance()->UpdateDisplayInfo(displayGroupInfo);
     };
     if (eventHandler_) {
