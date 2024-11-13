6a936ba40f56b6ef25aed6c10599975fe53fe7ff
huangjunlin <huangjunlin9@h-partners.com>
2024-11-13 11:03:44 +0800

    格式修改
    Signed-off-by: huangjunlin <huangjunlin9@h-partners.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
@@ -2492,7 +2492,7 @@ napi_value JsSceneSession::OnSetSystemSceneForceUIFirst(napi_env env, napi_callb
     return NapiGetUndefined(env);
 }
 
-napi_value JsSceneSession::OnMarkSystemSceneUIFirst(napi_env env, napi_callback_info info) 
+napi_value JsSceneSession::OnMarkSystemSceneUIFirst(napi_env env, napi_callback_info info)
 {
     size_t argc = ARG_COUNT_4;
     napi_value argv[ARG_COUNT_4] = {nullptr};
