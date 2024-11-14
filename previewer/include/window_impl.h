10a904177f38673ba4f2921c218c48cf8b3666c0
yangyibo <yangyibo6@huawei.com>
2024-11-14 14:41:03 +0800

    priviewer模块下windowImpl实现SetUIContentByName方法，deveco使用对外接口loadcontentByName正常模拟
    Signed-off-by: yangyibo <yangyibo6@huawei.com>

--- previewer/include/window_impl.h
+++ previewer/include/window_impl.h
@@ -175,7 +175,9 @@ public:
     virtual void UpdateConfiguration(const std::shared_ptr<AppExecFwk::Configuration>& configuration) override;
     void UpdateAvoidArea(const sptr<AvoidArea>& avoidArea, AvoidAreaType type) override;
     void NotifyTouchDialogTarget(int32_t posX = 0, int32_t posY = 0) override;
-
+    
+    virtual WMError SetUIContentByName(const std::string& contentInfo, napi_env env, napi_value storage,
+        AppExecFwk::Ability* ability) override;
     virtual WMError NapiSetUIContent(const std::string& contentInfo, napi_env env, napi_value storage,
         BackupAndRestoreType type, sptr<IRemoteObject> token, AppExecFwk::Ability* ability) override;
     virtual std::string GetContentInfo(BackupAndRestoreType type = BackupAndRestoreType::CONTINUATION) override;
