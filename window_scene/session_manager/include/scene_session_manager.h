d8430d42b28a6f0dc06023b9a4cc948911c26301
ZihaoWU <wuzihao11@huawei.com>
2024-11-14 21:23:45 +0800

    add code
    Signed-off-by: ZihaoWU <wuzihao11@huawei.com>

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -1055,7 +1055,7 @@ private:
         int32_t uid_ = 0;
     };
 
-    std::shared_mutex lastDrawingDataMutex_;
+    std::mutex lastDrawingDataMutex_;
     std::vector<DrawingSessionIdInfo> lastDrawingData_;
 
     /**
