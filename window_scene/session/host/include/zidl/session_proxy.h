2d34461bb9c5fa18e2b3d8dbb39129241ec0aada
赵聪 <zhaocong33@h-partners.com>
2024-11-15 10:13:20 +0800

    去空格
    Signed-off-by: 赵聪 <zhaocong33@h-partners.com>

--- window_scene/session/host/include/zidl/session_proxy.h
+++ window_scene/session/host/include/zidl/session_proxy.h
@@ -104,7 +104,7 @@ public:
     WMError SetSystemWindowEnableDrag(bool enableDrag) override;
     WSError RequestFocus(bool isFocused) override;
     void NotifyExtensionDetachToDisplay() override;
-  
+
     WSError OnSessionModalTypeChange(SubWindowModalType subWindowModalType) override;
     WSError OnMainSessionModalTypeChange(bool isModal) override;
     
