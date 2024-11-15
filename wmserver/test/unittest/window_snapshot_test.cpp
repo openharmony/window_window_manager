ae92b1e4f26204e06603b653ac18135818b1812d
tanyuhang <tanyuhang@huawei.com>
2024-11-15 01:15:26 +0000

    update wmserver/test/unittest/window_snapshot_test.cpp.
    Signed-off-by: tanyuhang <tanyuhang@huawei.com>

--- wmserver/test/unittest/window_snapshot_test.cpp
+++ wmserver/test/unittest/window_snapshot_test.cpp
@@ -149,8 +149,8 @@ HWTEST_F(WindowSnapshotTest, GetSnapshot05, Function | SmallTest | Level3)
     node->SetSnapshot(CommonTestUtils::CreatePixelMap());
 
     sptr<SnapshotController> snapshotController = new SnapshotController(root, handler);
-    AAFwk::Snapshot snapshot_;
-    ASSERT_EQ(static_cast<int32_t>(WMError::WM_OK), snapshotController->GetSnapshot(iRemoteObjectMocker, snapshot_));
+    AAFwk::Snapshot snapshot;
+    ASSERT_EQ(static_cast<int32_t>(WMError::WM_OK), snapshotController->GetSnapshot(iRemoteObjectMocker, snapshot));
 }
 }
 } // namespace Rosen
