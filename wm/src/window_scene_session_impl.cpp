7f5fa86223617788ea3420e896bc96713406a8c0
zhoukai <zhoukai94@huawei.com>
2024-11-19 10:53:09 +0800

    window-错误码描述优化
    Signed-off-by: zhoukai <zhoukai94@huawei.com>

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -1501,9 +1501,9 @@ WMError WindowSceneSessionImpl::MoveWindowToGlobal(int32_t x, int32_t y)
     }
 
     if (GetMode() != WindowMode::WINDOW_MODE_FLOATING) {
-        TLOGW(WmsLogTag::WMS_LAYOUT, "FullScreen window should not move, winId:%{public}u, mode:%{public}u",
+        TLOGW(WmsLogTag::WMS_LAYOUT, "window should not move, winId:%{public}u, mode:%{public}u",
             GetWindowId(), GetMode());
-        return WMError::WM_ERROR_OPER_FULLSCREEN_FAILED;
+        return WMError::WM_ERROR_INVALID_OP_IN_CUR_STATUS;
     }
 
     if (property_->GetWindowType() == WindowType::WINDOW_TYPE_PIP) {
@@ -1551,9 +1551,9 @@ WMError WindowSceneSessionImpl::MoveToAsync(int32_t x, int32_t y)
     }
 
     if (GetMode() != WindowMode::WINDOW_MODE_FLOATING) {
-        TLOGW(WmsLogTag::WMS_LAYOUT, "FullScreen window should not move, winId:%{public}u, mode:%{public}u",
+        TLOGW(WmsLogTag::WMS_LAYOUT, "window should not move, winId:%{public}u, mode:%{public}u",
             GetWindowId(), GetMode());
-        return WMError::WM_ERROR_OPER_FULLSCREEN_FAILED;
+        return WMError::WM_ERROR_INVALID_OP_IN_CUR_STATUS;
     }
     auto ret = MoveTo(x, y);
     if (state_ == WindowState::STATE_SHOWN) {
@@ -1749,9 +1749,9 @@ WMError WindowSceneSessionImpl::ResizeAsync(uint32_t width, uint32_t height)
     }
 
     if (GetMode() != WindowMode::WINDOW_MODE_FLOATING) {
-        TLOGW(WmsLogTag::WMS_LAYOUT, "Fullscreen window should not resize, winId:%{public}u, mode:%{public}u",
+        TLOGW(WmsLogTag::WMS_LAYOUT, "window should not resize, winId:%{public}u, mode:%{public}u",
             GetWindowId(), GetMode());
-        return WMError::WM_ERROR_OPER_FULLSCREEN_FAILED;
+        return WMError::WM_ERROR_INVALID_OP_IN_CUR_STATUS;
     }
     auto ret = Resize(width, height);
     if (state_ == WindowState::STATE_SHOWN) {
