fefc3d25a73f3cf8fa1141c292f3ee9aa873e5be
韦国宁 <weiguoning2@huawei.com>
2024-11-21 13:13:40 +0800

    startAbility支持supportWindowMode
    Signed-off-by: 韦国宁 <weiguoning2@huawei.com>

--- window_scene/session/host/src/session.cpp
+++ window_scene/session/host/src/session.cpp
@@ -1088,7 +1088,7 @@ void Session::InitSessionPropertyWhenConnect(const sptr<WindowSessionProperty>& 
         property->SetWindowRect(rect);
         property->SetPersistentId(GetPersistentId());
         property->SetFullScreenStart(GetSessionInfo().fullScreenStart_);
-        property->SetWindowModeSupportType(GetSessionInfo().windowModeSupportType);
+        property->SetSupportWindowModes(GetSessionInfo().supportWindowModes);
     }
     if (sessionProperty && property) {
         property->SetRequestedOrientation(sessionProperty->GetRequestedOrientation());
