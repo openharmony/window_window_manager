10977367fc58365cdfcb6e973db29a9e5d0c1d65
fox <tanhong6@h-partners.com>
2024-11-21 20:44:38 +0800

    i
    Change-Id: Ia06b11462a13fc3ee83bbff269b0621e66a2c690
    Signed-off-by: fox <tanhong6@h-partners.com>

--- window_scene/test/unittest/scene_session_test5.cpp
+++ window_scene/test/unittest/scene_session_test5.cpp
@@ -1152,6 +1152,8 @@ HWTEST_F(SceneSessionTest5, HandleUpdatePropertyByAction, Function | SmallTest |
     action = WSPropertyChangeAction::ACTION_UPDATE_FLAGS;
     res = session->HandleUpdatePropertyByAction(property, action);
     EXPECT_EQ(WMError::WM_OK, res);
+    auto prop = sceneSession->GetSessionProperty();
+    EXPECT_EQ(prop->GetSystemBarProperty(), property->GetSystemBarProperty());
 }
 
 /**
