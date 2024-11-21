1e60b1f2c99f9b2ac6b3fb517b342e6e9c55c021
thks <tanhongkang@h-partners.com>
2024-11-21 14:28:51 +0800

    Float权限变更-tdd
    Signed-off-by: thks <tanhongkang@h-partners.com>

--- window_scene/test/unittest/scene_session_manager_test5.cpp
+++ window_scene/test/unittest/scene_session_manager_test5.cpp
@@ -1532,7 +1532,7 @@ HWTEST_F(SceneSessionManagerTest5, CreateAndConnectSpecificSession02, Function |
     property->SetWindowType(WindowType::WINDOW_TYPE_FLOAT);
     res = ssm_->CreateAndConnectSpecificSession(sessionStage, eventChannel, node, property, id, session,
         systemConfig, token);
-    ASSERT_EQ(WSError::WS_OK, res);
+    ASSERT_EQ(WSError::WS_ERROR_NOT_SYSTEM_APP, res);
     property->SetWindowType(WindowType::WINDOW_TYPE_APP_SUB_WINDOW);
     res = ssm_->CreateAndConnectSpecificSession(sessionStage, eventChannel, node, property, id, session,
         systemConfig, token);
