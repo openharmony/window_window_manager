718932de54679b26a2475ff994e5c49f525276bf
cxy20001123 <chenxingyu34@h-partners.com>
2024-11-22 11:34:17 +0800

    同步代码整改
    Signed-off-by: cxy20001123 <chenxingyu34@h-partners.com>

--- window_scene/session_manager/include/screen_session_manager.h
+++ window_scene/session_manager/include/screen_session_manager.h
@@ -220,6 +220,7 @@ public:
     void SetScreenPowerForFold(ScreenId screenId, ScreenPowerStatus status);
     void TriggerDisplayModeUpdate(FoldDisplayMode targetDisplayMode);
     void CallRsSetScreenPowerStatusSync(ScreenId screenId, ScreenPowerStatus status);
+    void CallRsSetScreenPowerStatusSyncForFold(ScreenPowerStatus status);
     void TryToRecoverFoldDisplayMode(ScreenPowerStatus status);
 
     void SetKeyguardDrawnDoneFlag(bool flag);
