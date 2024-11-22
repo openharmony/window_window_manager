e0f80f23d556a01112503ed22c6e881ef50995c1
wangziyao <wangziyao13@huawei.com>
2024-11-22 09:43:23 +0800

    Quick opening and close of DMS
    Signed-off-by: wangziyao <wangziyao13@huawei.com>

--- window_scene/screen_session_manager/src/fold_screen_controller/single_display_fold_policy.cpp
+++ window_scene/screen_session_manager/src/fold_screen_controller/single_display_fold_policy.cpp
@@ -234,7 +234,6 @@ void SingleDisplayFoldPolicy::ReportFoldStatusChangeBegin(int32_t offScreen, int
 
 void SingleDisplayFoldPolicy::ChangeScreenDisplayModeToMain(sptr<ScreenSession> screenSession)
 {
-    RSInterfaces::GetInstance().SetScreenSwitching(true);
     if (onBootAnimation_) {
         ChangeScreenDisplayModeToMainOnBootAnimation(screenSession);
         return;
@@ -286,7 +285,6 @@ void SingleDisplayFoldPolicy::ChangeScreenDisplayModeToMain(sptr<ScreenSession> 
 
 void SingleDisplayFoldPolicy::ChangeScreenDisplayModeToFull(sptr<ScreenSession> screenSession)
 {
-    RSInterfaces::GetInstance().SetScreenSwitching(true);
     if (onBootAnimation_) {
         ChangeScreenDisplayModeToFullOnBootAnimation(screenSession);
         return;
