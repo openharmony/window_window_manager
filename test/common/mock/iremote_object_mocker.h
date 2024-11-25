cc610f956d5f1d26e8a70b0865f2fe169304a980
lijiyang <lijiyang@huawei.com>
2024-11-25 21:08:19 +0800

    TDD用例补充 Signed-off-by: lijiyang <lijiyang@huawei.com>

--- test/common/mock/iremote_object_mocker.h
+++ test/common/mock/iremote_object_mocker.h
@@ -149,7 +149,7 @@ public:
     MOCK_METHOD(bool, RemoveDeathRecipient, (const sptr<DeathRecipient>& recipient), (override));
     MOCK_METHOD(bool, Marshalling, (Parcel& parcel), (const, override));
     MOCK_METHOD(sptr<IRemoteBroker>, AsInterface, (), (override));
-    MOCK_METHOD(int, Dump, (int fd, const std::vector<std::u16string>& args, (override));
+    MOCK_METHOD(int, Dump, (int fd, const std::vector<std::u16string>& args), (override));
 }
 }
 }
