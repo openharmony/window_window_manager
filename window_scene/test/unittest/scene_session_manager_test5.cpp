90a5250b38a9b80b4c05e78efd898e65767abb56
闫瀚林0424 <yanhanlin6@huawei.com>
2024-11-26 14:38:37 +0800

    update TDD ut case crash scene_session_manager_test5.cpp
    Signed-off-by: 闫瀚林0424 <yanhanlin6@huawei.com>
    Change-Id: I5e98730d33fe15cba1b31729822206fb15a8278b

--- window_scene/test/unittest/scene_session_manager_test5.cpp
+++ window_scene/test/unittest/scene_session_manager_test5.cpp
@@ -1536,33 +1536,7 @@ HWTEST_F(SceneSessionManagerTest5, CreateAndConnectSpecificSession02, Function |
     property->SetWindowType(WindowType::WINDOW_TYPE_APP_SUB_WINDOW);
     res = ssm_->CreateAndConnectSpecificSession(sessionStage, eventChannel, node, property, id, session,
         systemConfig, token);
-    ASSERT_EQ(WSError::WS_OK, res);
-}
-
-/**
- * @tc.name: CheckSubSessionStartedByExtensionAndSetDisplayId
- * @tc.desc: CheckSubSessionStartedByExtensionAndSetDisplayId
- * @tc.type: FUNC
- */
-HWTEST_F(SceneSessionManagerTest5, CheckUIExtensionAndSetDisplayId01, Function | SmallTest | Level3)
-{
-    ASSERT_NE(ssm_, nullptr);
-    sptr<SessionStageMocker> sessionStage = sptr<SessionStageMocker>::MakeSptr();
-    sptr<WindowSessionProperty> property = sptr<WindowSessionProperty>::MakeSptr();
-    sptr<IRemoteObject> token;
-    SessionInfo info;
-    sptr<SceneSession::SpecificSessionCallback> callback = ssm_->CreateSpecificSessionCallback();
-    sptr<SceneSession> parentSession = sptr<SceneSession>::MakeSptr(info, callback);
-    ssm_->sceneSessionMap_.insert({ parentSession->GetPersistentId(), parentSession });
-    EXPECT_EQ(ssm_->CheckSubSessionStartedByExtensionAndSetDisplayId(token, property, sessionStage),
-        WSError::WS_ERROR_NULLPTR);
-    int64_t displayId = 1234;
-    property->SetParentPersistentId(parentSession->GetPersistentId());
-    parentSession->GetSessionProperty()->SetDisplayId(displayId);
-    EXPECT_CALL(*sessionStage, UpdateDisplayId(displayId)).Times(1);
-    EXPECT_EQ(ssm_->CheckSubSessionStartedByExtensionAndSetDisplayId(token, property, sessionStage),
-        WSError::WS_OK);
-    EXPECT_EQ(property->GetDisplayId(), displayId);
+    ASSERT_EQ(WSError::WS_ERROR_NULLPTR, res);
 }
 
 /**
