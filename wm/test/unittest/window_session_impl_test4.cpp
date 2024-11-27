12918e53b9e43a086a9f3bc63dbc1a3443a4b78f
卢宇豪 <luyuhao6@huawei.com>
2024-11-28 02:03:08 +0800

    change4
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- wm/test/unittest/window_session_impl_test4.cpp
+++ wm/test/unittest/window_session_impl_test4.cpp
@@ -2291,7 +2291,7 @@ HWTEST_F(WindowSessionImplTest4, NotifyWindowDisplayIdChange01, Function | Small
     sptr<SessionMocker> session = new(std::nothrow) SessionMocker(sessioninfo);
     ASSERT_NE(session, nullptr);
     ASSERT_EQ(WMError::WM_OK, window->Create(nullptr, session));
-    displayId = 12;
+    DisplayId displayId = 12;
     ret = window->NotifyWindowDisplayIdChange(displayId);
     ASSERT_EQ(WSError::WS_OK, ret);
     ASSERT_EQ(WMError::WM_ERROR_INVALID_WINDOW, window->Destroy());
