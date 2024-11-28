fae1ac96921df75df2b80ab62f336e45d4b17c70
卢宇豪 <luyuhao6@huawei.com>
2024-11-28 11:35:23 +0800

    change17
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window_listener.cpp
+++ interfaces/kits/napi/window_runtime/window_napi/js_window_listener.cpp
@@ -468,7 +468,7 @@ void JsWindowListener::OnDisplayIdChangedCallback(DisplayId displayId)
 {
     TLOGD(WmsLogTag::DEFAULT, "CALLED");
     std::unique_ptr<NapiAsyncTask::CompleteCallback> complete = std::make_unique<NapiAsyncTask::CompleteCallback>(
-        [self = weakRef_, displayIdValue, eng = env_] (napi_env env, NapiAsyncTask& task, int32_t status) {
+        [self = weakRef_, displayId, eng = env_] (napi_env env, NapiAsyncTask& task, int32_t status) {
             auto thisListener = self.promote();
             if (thisListener == nullptr || eng == nullptr) {
                 WLOGFE("This listener or eng is nullptr");
