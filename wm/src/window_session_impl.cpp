6de6e99e2bed08c789f133a2dbb0a29d81c04786
ZihaoWU <wuzihao11@huawei.com>
2024-11-29 17:51:06 +0800

    add code
    Signed-off-by: ZihaoWU <wuzihao11@huawei.com>

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -688,14 +688,14 @@ int32_t WindowSessionImpl::UpdateSuperFoldRectPosY(const WSRect& rect)
     property_->SetDisplayId(SUPER_FOLD_UPPER_DISPLAY_ID);
     auto superFoldStatus = SingletonContainer::Get<DisplayManager>().GetFoldStatus();
     if (superFoldStatus != FoldStatus::HALF_FOLD) {
-        TLOGI(WmsLogTag::WMS_LAYOUT, "window: %{public}d, superFoldStatus: %{public}u", GetPersistentId(),
+        TLOGI(WmsLogTag::WMS_LAYOUT, "windowId: %{public}d, superFoldStatus: %{public}u", GetPersistentId(),
               superFoldStatus);
         return rect.posY_;
     }
     auto curAllDisplayIds = SingletonContainer::Get<DisplayManager>().GetAllDisplayIds();
     auto it = std::find(curAllDisplayIds.begin(), curAllDisplayIds.end(), SUPER_FOLD_LOWER_DISPLAY_ID);
     if (it == curAllDisplayIds.end()) {
-        TLOGI(WmsLogTag::WMS_LAYOUT, "window: %{public}d, KEYBOARD Status", GetPersistentId());
+        TLOGI(WmsLogTag::WMS_LAYOUT, "windowId: %{public}d, KEYBOARD Status", GetPersistentId());
         return rect.posY_;
     }
     // Get Hopper's CreaseRects, Calculate coordinates, determine which screen the window is located on, and notify.
