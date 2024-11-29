2664eeca8ac626f23ccae204ba7f2d7d20e2c061
lpf <liaopengfei@h-partners.com>
2024-11-29 09:29:25 +0800

    窗口日志等级降为debug
    Signed-off-by: lpf <liaopengfei@h-partners.com>

--- wm/src/window_input_channel.cpp
+++ wm/src/window_input_channel.cpp
@@ -96,7 +96,8 @@ __attribute__((no_sanitize("cfi"))) void WindowInputChannel::HandleKeyEvent(
         };
         auto ret = MiscServices::InputMethodController::GetInstance()->DispatchKeyEvent(keyEvent, callback);
         if (ret != 0) {
-            WLOGFD("DispatchKeyEvent failed, ret:%{public}d, id:%{public}d", ret, keyEvent->GetId());
+            TLOGI(WmsLogTag::WMS_EVENT, "DispatchKeyEvent failed, ret:%{public}d, id:%{public}d",
+                ret, keyEvent->GetId());
             DispatchKeyEventCallback(keyEvent, false);
         }
         return;
