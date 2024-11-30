6e316e6ff3f6bd5f3a928041598987dcc427070b
lijiyang <lijiyang@huawei.com>
2024-11-30 20:05:53 +0800

    属性UT用例规整 Signed-off-by: lijiyang <lijiyang@huawei.com>

--- window_scene/test/unittest/session_proxy_test.cpp
+++ window_scene/test/unittest/session_proxy_test.cpp
@@ -247,44 +247,6 @@ HWTEST_F(SessionProxyTest, SetAspectRatio, Function | SmallTest | Level2)
 }
 
 /**
- * @tc.name: UpdateSessionPropertyByAction01
- * @tc.desc: UpdateSessionPropertyByAction
- * @tc.type: FUNC
- */
-HWTEST_F(SessionProxyTest, UpdateSessionPropertyByAction01, Function | SmallTest | Level2)
-{
-    GTEST_LOG_(INFO) << "SessionProxyTest: UpdateSessionPropertyByAction01 start";
-    sptr<IRemoteObject> iRemoteObjectMocker = new IRemoteObjectMocker();
-    ASSERT_NE(iRemoteObjectMocker, nullptr);
-    SessionProxy* sProxy = new(std::nothrow) SessionProxy(iRemoteObjectMocker);
-    ASSERT_NE(sProxy, nullptr);
-    WMError res = sProxy->UpdateSessionPropertyByAction(nullptr,
-        WSPropertyChangeAction::ACTION_UPDATE_KEEP_SCREEN_ON);
-    ASSERT_EQ(res, WMError::WM_OK);
-    delete sProxy;
-    GTEST_LOG_(INFO) << "SessionProxyTest: UpdateSessionPropertyByAction01 end";
-}
-
-/**
- * @tc.name: UpdateSessionPropertyByAction02
- * @tc.desc: UpdateSessionPropertyByAction
- * @tc.type: FUNC
- */
-HWTEST_F(SessionProxyTest, UpdateSessionPropertyByAction02, Function | SmallTest | Level2)
-{
-    GTEST_LOG_(INFO) << "SessionProxyTest: UpdateSessionPropertyByAction02 start";
-    sptr<IRemoteObject> iRemoteObjectMocker = new IRemoteObjectMocker();
-    ASSERT_NE(iRemoteObjectMocker, nullptr);
-    SessionProxy* sProxy = new(std::nothrow) SessionProxy(iRemoteObjectMocker);
-    ASSERT_NE(sProxy, nullptr);
-    WMError res = sProxy->UpdateSessionPropertyByAction(nullptr,
-        WSPropertyChangeAction::ACTION_UPDATE_TURN_SCREEN_ON);
-    ASSERT_EQ(res, WMError::WM_OK);
-    delete sProxy;
-    GTEST_LOG_(INFO) << "SessionProxyTest: UpdateSessionPropertyByAction02 end";
-}
-
-/**
  * @tc.name: UpdateWindowSceneAfterCustomAnimation
  * @tc.desc: normal function
  * @tc.type: FUNC
