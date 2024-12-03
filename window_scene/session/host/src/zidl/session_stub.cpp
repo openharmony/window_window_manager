5c130e0f1adce9ee48bfe29da86b0a70080ba6aa
yangyuanzhou <yangyuanzhou@huawei.com>
2024-12-03 20:58:36 +0800

    代码同步
    Signed-off-by: yangyuanzhou <yangyuanzhou@huawei.com>

--- window_scene/session/host/src/zidl/session_stub.cpp
+++ window_scene/session/host/src/zidl/session_stub.cpp
@@ -479,12 +479,12 @@ int SessionStub::HandleDefaultDensityEnabled(MessageParcel& data, MessageParcel&
 {
     bool isDefaultDensityEnabled = false;
     if (!data.ReadBool(isDefaultDensityEnabled)) {
-        TLOGE(WmsLogTag::WMS_LAYOUT, "Read isDefaultDensityEnabled failed");
+        TLOGE(WmsLogTag::WMS_LAYOUT, "Read isDefaultDensityEnabled failed.");
         return ERR_INVALID_DATA;
     }
     TLOGD(WmsLogTag::WMS_LAYOUT, "isDefaultDensityEnabled: %{public}d", isDefaultDensityEnabled);
     WSError errCode = OnDefaultDensityEnabled(isDefaultDensityEnabled);
-    reply.WriteUint32(static_cast<uint32_t>(errCode));
+    reply.WriteInt32(static_cast<int32_t>(errCode));
     return ERR_NONE;
 }
 
