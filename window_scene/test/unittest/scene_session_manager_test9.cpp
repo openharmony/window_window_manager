293db9880e3a4d5591b7af3fa04e7baf85838f0f
许静怡 <xujingyi19@huawei.com>
2024-12-03 12:17:29 +0000

    update window_scene/test/unittest/scene_session_manager_test9.cpp.
    Signed-off-by: 许静怡 <xujingyi19@huawei.com>

--- window_scene/test/unittest/scene_session_manager_test9.cpp
+++ window_scene/test/unittest/scene_session_manager_test9.cpp
@@ -849,7 +849,8 @@ HWTEST_F(SceneSessionManagerTest9, CheckUIExtensionCreation, Function | SmallTes
     int32_t windowId = 5;
     uint32_t callingTokenId = 0;
     int32_t pid = 0;
-    auto ret = ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, pid);
+    AppExecFwk::ExtensionAbilityType extensionAbilityTyp = AppExecFwk::ExtensionAbilityType::ACTION;
+    auto ret = ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, extensionAbilityTyp, pid);
     ASSERT_EQ(ret, WMError::WM_ERROR_INVALID_WINDOW);
 
     SessionInfo info;
@@ -860,11 +861,11 @@ HWTEST_F(SceneSessionManagerTest9, CheckUIExtensionCreation, Function | SmallTes
 
     Session session(info);
     session.getStateFromManagerFunc_ = getStateFalse;
-    ret = ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, pid);
+    ret = ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, extensionAbilityTyp, pid);
     ASSERT_EQ(ret, WMError::WM_OK);
 
     session.getStateFromManagerFunc_ = getStateTrue;
-    ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, pid);
+    ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, extensionAbilityTyp, pid);
 }
 
 /**
@@ -879,6 +880,7 @@ HWTEST_F(SceneSessionManagerTest9, CheckUIExtensionCreation01, Function | SmallT
     int32_t windowId = 0;
     uint32_t callingTokenId = 0;
     int32_t pid = 0;
+    AppExecFwk::ExtensionAbilityType extensionAbilityTyp = AppExecFwk::ExtensionAbilityType::ACTION;
 
     SessionInfo info;
     sptr<SceneSession::SpecificSessionCallback> callback = new SceneSession::SpecificSessionCallback();
@@ -886,15 +888,15 @@ HWTEST_F(SceneSessionManagerTest9, CheckUIExtensionCreation01, Function | SmallT
     ssm_->sceneSessionMap_.insert(std::pair<int32_t, sptr<SceneSession>>(0, sceneSession));
     Session session(info);
     session.getStateFromManagerFunc_ = getStateTrue;
-    auto ret = ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, pid);
+    auto ret = ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, extensionAbilityTyp, pid);
 
     session.property_ = nullptr;
-    ret = ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, pid);
+    ret = ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, extensionAbilityTyp, pid);
     ASSERT_EQ(ret, WMError::WM_OK);
 
     sceneSession->IsShowOnLockScreen(0);
     session.zOrder_ = 1;
-    ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, pid);
+    ssm_->CheckUIExtensionCreation(windowId, callingTokenId, element, extensionAbilityTyp, pid);
     ASSERT_EQ(ret, WMError::WM_OK);
 }
 
