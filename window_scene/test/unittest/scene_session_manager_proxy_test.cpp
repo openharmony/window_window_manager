a87b1ac0378f9307c41d051a7dac90593f04bb5f
qianli22 <qianli22@huawei.com>
2024-12-03 20:33:21 +0800

    添加拖拽类型接口
    Signed-off-by: qianli22 <qianli22@huawei.com>

--- window_scene/test/unittest/scene_session_manager_proxy_test.cpp
+++ window_scene/test/unittest/scene_session_manager_proxy_test.cpp
@@ -1134,9 +1134,7 @@ HWTEST_F(sceneSessionManagerProxyTest, GetGlobalDragResizeType, Function | Small
     sptr<SceneSessionManagerProxy> sceneSessionManagerProxy =
         new SceneSessionManagerProxy(iRemoteObjectMocker);
     ASSERT_TRUE(sceneSessionManagerProxy != nullptr);
-    DragResizeType toDragResizeType = DragResizeType::RESIZE_EACH_FRAME;
     DragResizeType dragResizeType = DragResizeType::RESIZE_TYPE_UNDEFINED;
-    SceneSession::globalDragResizeType_ = toDragResizeType;
     sceneSessionManagerProxy->GetGlobalDragResizeType(dragResizeType);
 }
 
