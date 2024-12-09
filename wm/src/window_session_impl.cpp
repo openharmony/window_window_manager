76b68c983721e3f43d33d7e7dc1f6ebe347dff88
kuangmei <kuangmei1@huawei.com>
2024-12-09 21:47:51 +0800

    使能/禁止窗口拖拽缩放
    Signed-off-by: kuangmei <kuangmei1@huawei.com>

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -1552,15 +1552,17 @@ WMError WindowSessionImpl::SetResizeByDragEnabled(bool dragEnabled)
         return WMError::WM_ERROR_INVALID_WINDOW;
     }
 
-    WLOGFD("set dragEnabled");
+    TLOGD(WmsLogTag::DEFAULT, "%{public}d", dragEnabled);
     if (IsWindowSessionInvalid()) {
         return WMError::WM_ERROR_INVALID_WINDOW;
     }
-
-    if (WindowHelper::IsMainWindow(GetType())) {
+    if (WindowHelper::IsSubWindow(GetType()) && !windowOption_->GetSubWindowDecorEnable()) {
+        return WMError::WM_OK;
+    }
+    if (WindowHelper::IsMainWindow(GetType()) || WindowHelper::IsSubWindow(GetType())) {
         property_->SetDragEnabled(dragEnabled);
     } else {
-        WLOGFE("This is not main window.");
+        TLOGE(WmsLogTag::DEFAULT, "This is not main window or sub window.");
         return WMError::WM_ERROR_INVALID_TYPE;
     }
     return UpdateProperty(WSPropertyChangeAction::ACTION_UPDATE_DRAGENABLED);
