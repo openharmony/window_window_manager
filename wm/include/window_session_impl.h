03395b832adaad360c11e500e322727b9daf73cd
崔洪斌 <cuihongbin@huawei.com>
2024-12-09 14:50:58 +0800

    按代码检视意见修复
    Signed-off-by: 崔洪斌 <cuihongbin@huawei.com>

--- wm/include/window_session_impl.h
+++ wm/include/window_session_impl.h
@@ -50,8 +50,6 @@ using EnableIfSame = typename std::enable_if<std::is_same_v<T1, T2>, Ret>::type;
  */
 const std::string SET_UICONTENT_TIMEOUT_LISTENER_TASK_NAME = "SetUIContentTimeoutListener";
 constexpr int64_t SET_UICONTENT_TIMEOUT_TIME_MS = 4000;
-const std::string SET_UIEXTENSION_DESTROY_TIMEOUT_LISTENER_TASK_NAME = "SetUIExtDestroyTimeoutListener";
-constexpr int64_t SET_UIEXTENSION_DESTROY_TIMEOUT_TIME_MS = 4000;
 constexpr int64_t SET_UICONTENT_TIMEOUT_TIME_AFTER_FREEZE_MS = 5000;
 }
 
