37f413a37b00d91174d2c66dbd0b28ecfaf43929
ZihaoWU <wuzihao11@huawei.com>
2024-12-12 17:51:43 +0800

    add code
    Signed-off-by: ZihaoWU <wuzihao11@huawei.com>

--- interfaces/kits/cj/window_runtime/window_register_manager.h
+++ interfaces/kits/cj/window_runtime/window_register_manager.h
@@ -54,11 +54,11 @@ private:
         bool isRegister);
     WmErrorCode ProcessDisplayIdChangeRegister(const sptr<CjWindowListener>& listener, const sptr<Window>& window,
         bool isRegister);
+    WmErrorCode ProcessSystemDensityChangeRegister(const sptr<CjWindowListener>& listener, const sptr<Window>& window,
+        bool isRegister);
     WmErrorCode ProcessWindowStatusChangeRegister(sptr<CjWindowListener> listener, sptr<Window> window,
         bool isRegister);
     WmErrorCode ProcessWindowTitleButtonRectChangeRegister(sptr<CjWindowListener> listener, sptr<Window> window,
-        bool isRegister);
-    WmErrorCode ProcessSystemDensityChangeRegister(const sptr<CjWindowListener>& listener, const sptr<Window>& window,
         bool isRegister);
     using Func = std::function<WmErrorCode(sptr<CjWindowListener>, sptr<Window>, bool)>;
     std::map<std::string, std::map<int64_t, sptr<CjWindowListener>>> cjCbMap_;
