171ba1bdb032d0c4671a038393e876430ee5775c
thks <tanhongkang@h-partners.com>
2024-12-25 09:33:50 +0800

    Revert "悬浮窗权限变更"
    This reverts commit 3e3d1cc385a15237a7eded9fa20ffd078da6d770.
    
    Signed-off-by: thks <tanhongkang@h-partners.com>

--- wm/test/unittest/window_scene_session_impl_test4.cpp
+++ wm/test/unittest/window_scene_session_impl_test4.cpp
@@ -859,9 +859,9 @@ HWTEST_F(WindowSceneSessionImplTest4, MoveToAsync02, Function | SmallTest | Leve
 {
     sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
     option->SetWindowName("MoveToAsync02");
-    option->SetWindowType(WindowType::WINDOW_TYPE_TOAST);
+    option->SetWindowType(WindowType::WINDOW_TYPE_FLOAT);
     option->SetWindowMode(WindowMode::WINDOW_MODE_FLOATING);
-
+    
     sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
     ASSERT_NE(window->property_, nullptr);
     window->property_->SetPersistentId(10001);
@@ -919,9 +919,9 @@ HWTEST_F(WindowSceneSessionImplTest4, ResizeAsync02, Function | SmallTest | Leve
 {
     sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
     option->SetWindowName("ResizeAsync02");
-    option->SetWindowType(WindowType::WINDOW_TYPE_TOAST);
+    option->SetWindowType(WindowType::WINDOW_TYPE_FLOAT);
     option->SetWindowMode(WindowMode::WINDOW_MODE_FLOATING);
-
+    
     sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
 
     Rect rect;
