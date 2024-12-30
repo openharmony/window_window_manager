e740b1520d9a7f27380a76c8ca1323d581b94e25
Peng fafu <pengfafu@huawei.com>
2024-12-30 10:05:31 +0800

    PC don't hide NoneSecure floating indow
    Signed-off-by: Peng fafu <pengfafu@huawei.com>

--- window_scene/test/unittest/scene_session_manager_test5.cpp
+++ window_scene/test/unittest/scene_session_manager_test5.cpp
@@ -1250,9 +1250,9 @@ HWTEST_F(SceneSessionManagerTest5, CreateAndConnectSpecificSession02, Function |
     ASSERT_EQ(WSError::WS_ERROR_NOT_SYSTEM_APP, res);
 
     property->SetWindowType(WindowType::WINDOW_TYPE_FLOAT);
-    property->FloatingWindowAppType(true);
+    property->SetFloatingWindowAppType(true);
     ssm_->shouldHideNonSecureFloatingWindows_.store(true);
-    ssm_->systemConfig_.windowUIType = WindowUIType::PC_WINDOW;
+    ssm_->systemConfig_.windowUIType_ = WindowUIType::PC_WINDOW;
     res = ssm_->CreateAndConnectSpecificSession(sessionStage, eventChannel, node, property, id, session,
         systemConfig, token);
     ASSERT_EQ(WSError::WS_ERROR_NOT_SYSTEM_APP, res);
