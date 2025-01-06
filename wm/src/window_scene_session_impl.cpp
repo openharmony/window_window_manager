081c6785c28d0cf89cebfa90762752f670783534
苏嵋岩 <sumeiyan@huawei.com>
2025-01-06 18:51:40 +0800

    使maximize在窗口隐藏时不生效，其他状态生效
    Signed-off-by: 苏嵋岩 <sumeiyan@huawei.com>

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -2255,7 +2255,7 @@ WMError WindowSceneSessionImpl::Maximize()
         TLOGE(WmsLogTag::WMS_LAYOUT_PC, "session is invalid");
         return WMError::WM_ERROR_INVALID_WINDOW;
     }
-    if (GetTargetAPIVersion() >= 15 && state_ != WindowState::STATE_SHOWN) { // 15: isolated version
+    if (GetTargetAPIVersion() >= 15 && state_ == WindowState::STATE_HIDDEN) { // 15: isolated version
         TLOGW(WmsLogTag::WMS_LAYOUT_PC, "window is not shown, id:%{public}d", GetPersistentId());
         return WMError::WM_OK;
     }
@@ -2283,7 +2283,7 @@ WMError WindowSceneSessionImpl::Maximize(MaximizePresentation presentation)
         TLOGW(WmsLogTag::WMS_LAYOUT_PC, "The device is not supported");
         return WMError::WM_OK;
     }
-    if (GetTargetAPIVersion() >= 15 && state_ != WindowState::STATE_SHOWN) { // 15: isolated version
+    if (GetTargetAPIVersion() >= 15 && state_ == WindowState::STATE_HIDDEN) { // 15: isolated version
         TLOGW(WmsLogTag::WMS_LAYOUT_PC, "window is not shown, id:%{public}d", GetPersistentId());
         return WMError::WM_OK;
     }
