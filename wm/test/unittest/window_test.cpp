d0849d8084febc0cbc21ccb3e781cf0aac776d86
tanchenghao <tanchenghao@h-partners.com>
2025-01-08 10:39:30 +0800

    补充tdd
    Signed-off-by: tanchenghao <tanchenghao@h-partners.com>

--- wm/test/unittest/window_test.cpp
+++ wm/test/unittest/window_test.cpp
@@ -2192,6 +2192,8 @@ HWTEST_F(WindowTest, SetWindowLimits, Function | SmallTest | Level2)
     WindowLimits windowLimits;
     auto ret = window->SetWindowLimits(windowLimits, false);
     ASSERT_EQ(WMError::WM_ERROR_DEVICE_NOT_SUPPORT, ret);
+    ret = window->SetWindowLimits(windowLimits, true);
+    ASSERT_EQ(WMError::WM_ERROR_DEVICE_NOT_SUPPORT, ret);
     ASSERT_EQ(WMError::WM_OK, window->Destroy());
 }
 
