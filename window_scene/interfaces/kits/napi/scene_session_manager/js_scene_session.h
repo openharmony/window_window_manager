8d2e2215c9bbcb2f8e0dc731ca59367cfe0ff9bf
chengzhen <chengzhen25@huawei.com>
2025-01-11 03:51:36 +0000

    update window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.h.
    Signed-off-by: chengzhen <chengzhen25@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.h
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.h
@@ -161,7 +161,7 @@ private:
     static napi_value SyncScenePanelGlobalPosition(napi_env env, napi_callback_info info);
     static napi_value UnSyncScenePanelGlobalPosition(napi_env env, napi_callback_info info);
     static napi_value SetNeedSyncSessionRect(napi_env env, napi_callback_info info);
-    static napi_value MaskingEnterWaterfallMode(napi_env env, napi_callback_info info);
+    static napi_value MaskSupportEnterWaterfallMode(napi_env env, napi_callback_info info);
     static void BindNativeMethod(napi_env env, napi_value objValue, const char* moduleName);
     static void BindNativeMethodForKeyboard(napi_env env, napi_value objValue, const char* moduleName);
     static void BindNativeMethodForCompatiblePcMode(napi_env env, napi_value objValue, const char* moduleName);
@@ -253,7 +253,7 @@ private:
     napi_value OnSaveSnapshotSync(napi_env env, napi_callback_info info);
     napi_value OnSetBehindWindowFilterEnabled(napi_env env, napi_callback_info info);
     napi_value OnSetFreezeImmediately(napi_env env, napi_callback_info info);
-    napi_value OnMaskingEnterWaterfallMode(napi_env env, napi_callback_info info);
+    napi_value OnMaskSupportEnterWaterfallMode(napi_env env, napi_callback_info info);
 
     bool IsCallbackRegistered(napi_env env, const std::string& type, napi_value jsListenerObject);
     void ProcessChangeSessionVisibilityWithStatusBarRegister();
