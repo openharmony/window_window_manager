03d493030dcf3553ef7fbddfd21ba49b3bb7b8fd
dengbing <dengbing9@huawei.com>
2025-01-13 22:28:03 +0800

    检视意见修改
    Signed-off-by: dengbing <dengbing9@huawei.com>

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -2900,6 +2900,7 @@ WSError WindowSessionImpl::SendContainerModalEvent(const std::string& eventName,
             TLOGNE(WmsLogTag::WMS_EVENT, "uiContent is null!");
             return;
         }
+        TLOGNI(WmsLogTag::WMS_EVENT, "name: %{public}s, value: %{public}s", eventName, eventValue);
         uiContent->OnContainerModalEvent(eventName, eventValue);
     };
     handler_->PostTask(task, "WMS_WindowSessionImpl_SendContainerModalEvent");
