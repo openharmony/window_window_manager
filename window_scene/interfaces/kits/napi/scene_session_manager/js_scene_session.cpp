e889ecdc153f9e329e2eb6ee23dfc271f49dd0b4
dengbing <dengbing9@huawei.com>
2025-01-13 22:28:03 +0800

    检视意见修改
    Signed-off-by: dengbing <dengbing9@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.cpp
@@ -5799,7 +5799,7 @@ napi_value JsSceneSession::OnSendContainerModalEvent(napi_env env, napi_callback
     size_t argc = ARGC_FOUR;
     napi_value argv[ARGC_FOUR] = {nullptr};
     napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
-    if (argc < ARGC_ONE) {
+    if (argc < ARGC_TWO) {
         TLOGE(WmsLogTag::WMS_EVENT, "Argc count is invalid: %{public}zu", argc);
         napi_throw(env, CreateJsError(env, static_cast<int32_t>(WSErrorCode::WS_ERROR_INVALID_PARAM),
                                       "Input parameter is missing or invalid"));
@@ -5813,7 +5813,7 @@ napi_value JsSceneSession::OnSendContainerModalEvent(napi_env env, napi_callback
         return NapiGetUndefined(env);
     }
     std::string eventValue;
-    if (!ConvertFromJsValue(env, argv[1], eventValue)) {
+    if (!ConvertFromJsValue(env, argv[1], eventValue) || eventValue.empty()) {
         TLOGE(WmsLogTag::WMS_EVENT, "Failed to convert parameter to string");
         napi_throw(env, CreateJsError(env, static_cast<int32_t>(WSErrorCode::WS_ERROR_INVALID_PARAM),
                                       "Input parameter is missing or invalid"));
@@ -5823,6 +5823,10 @@ napi_value JsSceneSession::OnSendContainerModalEvent(napi_env env, napi_callback
     auto session = weakSession_.promote();
     if (session == nullptr) {
         TLOGE(WmsLogTag::WMS_EVENT, "session is nullptr, id:%{public}d", persistentId_);
+        return NapiGetUndefined(env);
+    }
+    if (!session->IsPcWindow()) {
+        TLOGE(WmsLogTag::WMS_EVENT, "device type not support");
         return NapiGetUndefined(env);
     }
     session->SendContainerModalEvent(eventName, eventValue);
