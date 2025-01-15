bccb841a7aec83309f5e677f54ae8bb6caa13828
ZihaoWU <wuzihao11@huawei.com>
2025-01-15 19:28:28 +0800

    add code
    Signed-off-by: ZihaoWU <wuzihao11@huawei.com>

--- wm/test/unittest/window_inspector_test.cpp
+++ wm/test/unittest/window_inspector_test.cpp
@@ -50,18 +50,6 @@ HWTEST_F(WindowInspetorTest, InitConnectServer, Function | SmallTest | Level2)
 }
 
 /**
- * @tc.name: UnregisterCallback01
- * @tc.desc: UnregisterCallback
- * @tc.type: FUNC
- */
-HWTEST_F(WindowInspetorTest, UnregisterCallback, Function | SmallTest | Level2)
-{
-    WindowInspector::GetInstance().UnregisterCallback();
-    EXPECT_EQ(nullptr, WindowInspector::GetInstance().setWMSCallback_);
-    EXPECT_EQ(nullptr, WindowInspector::GetInstance().sendMessage_);
-}
-
-/**
  * @tc.name: ProcessArkUIInspectorMessage01
  * @tc.desc: ProcessArkUIInspectorMessage
  * @tc.type: FUNC
@@ -92,9 +80,8 @@ HWTEST_F(WindowInspetorTest, TransformDataToJson, Function | SmallTest | Level2)
     std::vector<WindowListsInfo> windowListsInfo;
     windowListsInfo.push_back({ "test01", 1, 1, windowRect});
     WindowInspector::GetInstance().TransformDataToJson(windowListsInfo);
-    std::string ret =
-        "{\"type\":\"window\",\"content\":[{\"windowName\":\"test01\",\"winId\":\"1\",\"type\":\"1\",\"rect\":[{"
-        "\"startX\":\"100\",\"startY\":\"100\",\"width\":\"100\",\"height\":\"100\"}]}]}";
+    std::string ret = "{\"type\":\"window\",\"content\":[{\"windowName\":\"test01\",\"winId\":\"1\",\"type\":\"1\","
+                      "\"rect\":{\"startX\":\"100\",\"startY\":\"100\",\"width\":\"100\",\"height\":\"100\"}}]}";
     EXPECT_EQ(ret, WindowInspector::GetInstance().jsonWindowListsInfoStr);
 }
 } // namespace
