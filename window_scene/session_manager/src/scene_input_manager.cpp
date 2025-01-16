cb3c26765f9f7b5ee50ceb2e000a91ead78c025d
qianli22 <qianli22@huawei.com>
2025-01-16 14:37:38 +0800

    修改隐式转换
    Signed-off-by: qianli22 <qianli22@huawei.com>

--- window_scene/session_manager/src/scene_input_manager.cpp
+++ window_scene/session_manager/src/scene_input_manager.cpp
@@ -234,7 +234,7 @@ void SceneInputManager::ConstructDisplayInfos(std::vector<MMI::DisplayInfo>& dis
             .screenRealWidth = screenProperty.GetScreenRealWidth(),
             .screenRealHeight = screenProperty.GetScreenRealHeight(),
             .screenRealPPI = screenProperty.GetScreenRealPPI(),
-            .screenRealDPI = screenProperty.GetScreenRealDPI(),
+            .screenRealDPI = static_cast<int32_t>(screenProperty.GetScreenRealDPI()),
             .screenCombination = static_cast<MMI::ScreenCombination>(screenCombination)};
         displayInfos.emplace_back(displayInfo);
     }
