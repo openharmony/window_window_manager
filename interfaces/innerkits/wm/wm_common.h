6f552cc919006403db5fefc33d1efdf512f0db9c
卢宇豪 <luyuhao6@huawei.com>
2025-01-20 00:49:22 +0800

    change1
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- interfaces/innerkits/wm/wm_common.h
+++ interfaces/innerkits/wm/wm_common.h
@@ -1256,11 +1256,11 @@ enum class WindowInfoFilterOption : WindowInfoFilterOptionType {
 
 inline WindowInfoFilterOption operator|(WindowInfoFilterOption lhs, WindowInfoFilterOption rhs) {
     return static_cast<WindowInfoFilterOption>(static_cast<WindowInfoFilterOptionType>(lhs) |
-        static_cast<WindowInfoFilterOptionType>(rhs))
+        static_cast<WindowInfoFilterOptionType>(rhs));
 }
 
 inline bool IsChosenOption(WindowInfoFilterOption options, WindowInfoFilterOption option) {
-    return (static_cast<WindowInfoFilterOptionType>(options) & static_cast<WindowInfoFilterOptionType>(option)) != 0
+    return (static_cast<WindowInfoFilterOptionType>(options) & static_cast<WindowInfoFilterOptionType>(option)) != 0;
 }
 
 using WindowInfoTypeOptionType = uint8_t
@@ -1280,11 +1280,11 @@ enum class WindowInfoTypeOption : WindowInfoTypeOptionType {
 
 inline WindowInfoTypeOption operator|(WindowInfoTypeOption lhs, WindowInfoTypeOption rhs) {
     return static_cast<WindowInfoTypeOption>(static_cast<WindowInfoTypeOptionType>(lhs) |
-        static_cast<WindowInfoTypeOptionType>(rhs))
+        static_cast<WindowInfoTypeOptionType>(rhs));
 }
 
 inline bool IsChosenOption(WindowInfoTypeOption options, WindowInfoTypeOption option) {
-    return (static_cast<WindowInfoTypeOptionType>(options) & static_cast<WindowInfoTypeOptionType>(option)) != 0
+    return (static_cast<WindowInfoTypeOptionType>(options) & static_cast<WindowInfoTypeOptionType>(option)) != 0;
 }
 
 /**
