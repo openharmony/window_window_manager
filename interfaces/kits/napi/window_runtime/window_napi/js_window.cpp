2ec0f080905e3dccee0737bde2b896756aaf093f
卢宇豪 <luyuhao6@huawei.com>
2025-01-21 09:21:53 +0800

    change6
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
+++ interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
@@ -4408,7 +4408,7 @@ napi_value JsWindow::OnSetRaiseByClickEnabled(napi_env env, napi_callback_info i
 napi_value JsWindow::OnHideNonSystemFloatingWindows(napi_env env, napi_callback_info info)
 {
     size_t argc = FOUR_PARAMS_SIZE;
-    napi_value argv[INDEX_FOUR] = { nullptr }; 
+    napi_value argv[INDEX_FOUR] = { nullptr };
     napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
     if (argc < ONE_PARAMS_SIZE || argc > TWO_PARAMS_SIZE) { // 2: maximum params num
         TLOGE(WmsLogTag::WMS_ATTRIBUTE, "Argc is invalid: %{public}zu", argc);
@@ -4445,7 +4445,7 @@ napi_value JsWindow::OnHideNonSystemFloatingWindows(napi_env env, napi_callback_
                 task->Reject(env, JsErrUtils::CreateJsError(env, WM_JS_TO_ERROR_CODE_MAP.at(ret),
                     "Hide non-system floating windows failed"));
                 return;
-            } 
+            }
             task->Resolve(env, NapiGetUndefined(env));
             TLOGNI(WmsLogTag::WMS_ATTRIBUTE,
                 "%{public}s Window [%{public}u, %{public}s] hide non-system floating windows end",
