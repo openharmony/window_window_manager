e07897778d5f3f6c71347fbc794392655a9bf503
Peng fafu <pengfafu@huawei.com>
2025-02-08 09:21:16 +0800

    cross axis
    Signed-off-by: Peng fafu <pengfafu@huawei.com>

--- wm/test/unittest/window_session_impl_test.cpp
+++ wm/test/unittest/window_session_impl_test.cpp
@@ -1484,8 +1484,8 @@ HWTEST_F(WindowSessionImplTest, SetAPPWindowIcon, Function | SmallTest | Level2)
     ASSERT_EQ(res, WMError::WM_ERROR_NULLPTR);
 
     Media::InitializationOptions opts;
-    opts.size.width = 200;  // 200£º test width
-    opts.size.height = 300; // 300£º test height
+    opts.size.width = 200;  // 200ï¼š test width
+    opts.size.height = 300; // 300ï¼š test height
     opts.pixelFormat = Media::PixelFormat::ARGB_8888;
     opts.alphaType = Media::AlphaType::IMAGE_ALPHA_TYPE_OPAQUE;
     std::unique_ptr<Media::PixelMap> pixelMapPtr = Media::PixelMap::Create(opts);
