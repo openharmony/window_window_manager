5da8b16895141cebdbe0bae787360a9cc5290477
Peng fafu <pengfafu@huawei.com>
2025-02-08 16:51:26 +0800

    cross axis
    Signed-off-by: Peng fafu <pengfafu@huawei.com>

--- wm/src/window_extension_session_impl.cpp
+++ wm/src/window_extension_session_impl.cpp
@@ -1396,7 +1396,7 @@ WMError WindowExtensionSessionImpl::OnCrossAxisStateChange(AAFwk::Want&& data, s
         TLOGE(WmsLogTag::WMS_UIEXT, "invalid CrossAxisState:%{public}d", state);
         return WMError::WM_ERROR_INVALID_PARAM;
     }
-    crossAxisState_ = static_cast<CrossAxisState>(state);;
+    crossAxisState_ = static_cast<CrossAxisState>(state);
     if (auto uiContent = GetUIContentSharedPtr()) {
         uiContent->SendUIExtProprty(static_cast<uint32_t>(Extension::Businesscode::SYNC_CROSS_AXIS_STATE),
             data, static_cast<uint8_t>(SubSystemId::WM_UIEXT));
