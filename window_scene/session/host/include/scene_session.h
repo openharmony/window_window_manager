922cd340b55cf74ba007066f1b2ad2837dd83091
liangwei <liangwei49@huawei.com>
2025-02-14 16:20:52 +0800

    liangwei49@huawei.com
    Signed-off-by: liangwei <liangwei49@huawei.com>

--- window_scene/session/host/include/scene_session.h
+++ window_scene/session/host/include/scene_session.h
@@ -595,8 +595,7 @@ public:
     DragResizeType GetAppDragResizeType() const { return appDragResizeType_; }
     void RegisterSessionEventCallback(NotifySessionEventFunc&& callback);
     void SetWindowMovingCallback(NotifyWindowMovingFunc&& func);
-    int32_t CalcAvoidAreaHeightForStatusBar();
-    int32_t CalcAvoidAreaWidthForStatusBar();
+    DMRect CalcRectForStatusBar();
     WSError SetMoveAvailableArea(DisplayId displayId);
 
     /*
