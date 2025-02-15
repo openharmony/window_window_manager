3ae1ba7f843b953e068628ce38aa7ddf2c24b22d
wangqingjie <wangqingjie5@huawei.com>
2025-02-15 11:32:05 +0800

    去除日志中多余空格
    Signed-off-by: wangqingjie <wangqingjie5@huawei.com>

--- window_scene/session/host/src/main_session.cpp
+++ window_scene/session/host/src/main_session.cpp
@@ -215,7 +215,7 @@ void MainSession::RectCheck(uint32_t curWidth, uint32_t curHeight)
 
 void MainSession::SetExitSplitOnBackground(bool isExitSplitOnBackground)
 {
-    TLOGD(WmsLogTag::WMS_MULTI_WINDOW, "id: %{public}d, isExitSplitOnBackground: %{public}d", persistentId_,
+    TLOGD(WmsLogTag::WMS_MULTI_WINDOW, "id:%{public}d, isExitSplitOnBackground:%{public}d", persistentId_,
         isExitSplitOnBackground);
     isExitSplitOnBackground_ = isExitSplitOnBackground;
 }
