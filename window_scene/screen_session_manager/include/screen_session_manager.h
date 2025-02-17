dae4aef055edf553e123392f7b3e3c5b87f08541
dqwdgit <dengqingwen1@h-partners.com>
2025-02-17 11:26:43 +0800

    Description:打开注册motion，灭屏解注册motion IssueNo:https://gitee.com/openharmony/window_window_manager/issues/IBMPCE Binary Source:No Signed-off-by:dqwdgit <dengqingwen1@h-partners.com>

--- window_scene/screen_session_manager/include/screen_session_manager.h
+++ window_scene/screen_session_manager/include/screen_session_manager.h
@@ -441,6 +441,7 @@ private:
     std::string TransferPropertyChangeTypeToString(ScreenPropertyChangeType type) const;
     void CheckAndSendHiSysEvent(const std::string& eventName, const std::string& bundleName) const;
     void HandlerSensor(ScreenPowerStatus status, PowerStateChangeReason reason);
+    void UnregisterInHandlerSensorWithPowerOff(PowerStateChangeReason reason);
     bool GetPowerStatus(ScreenPowerState state, PowerStateChangeReason reason, ScreenPowerStatus& status);
     DMError CheckDisplayMangerAgentTypeAndPermission(
         const sptr<IDisplayManagerAgent>& displayManagerAgent, DisplayManagerAgentType type);
