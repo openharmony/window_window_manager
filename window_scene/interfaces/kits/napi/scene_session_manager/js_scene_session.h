ac1a3b8779596e11801d5b3614ce4eee64a26ed6
xuzihan129 <xuzihan11@huawei.com>
2025-02-18 23:33:09 +0800

    remove space
    Signed-off-by: xuzihan129 <xuzihan11@huawei.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.h
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session.h
@@ -338,7 +338,7 @@ private:
     /*
      * PC Window Layout
      */
-    void OnSetSupportedWindowModes(std::vectorAppExecFwk::SupportWindowMode&& supportedWindowModes);
+    void OnSetSupportedWindowModes(AppExecFwk::SupportWindowMode&& supportedWindowModes);
 
     void UpdateSessionLabelAndIcon(const std::string& label, const std::shared_ptr<Media::PixelMap>& icon);
     void OnKeyboardStateChange(SessionState state, KeyboardViewMode mode);
