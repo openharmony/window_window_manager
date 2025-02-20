08da98fc95682ea70c9c5ed7e978a6eef5b0de16
tanchenghao <zhouchengchao5@huawei.com>
2025-02-20 20:17:08 +0800

    compatibleMode in pc
    Signed-off-by: tanchenghao <zhouchengchao5@huawei.com>

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -1434,7 +1434,7 @@ WMError WindowSessionImpl::InitUIContent(const std::string& contentInfo, napi_en
         // compatibleMode app in pc, need change decor title to float title bar
         if (property_->GetCompatibleModeInPc()) {
             uiContent_->SetContainerModalTitleVisible(false, true);
-            uiContent_->IncreaseDecorEventProority(true);
+            uiContent_->EnableContainerModalCustomGesture(true);
         }
         WLOGFI("Initialized, isUIExtensionSubWindow:%{public}d, isUIExtensionAbilityProcess:%{public}d",
             uiContent_->IsUIExtensionSubWindow(), uiContent_->IsUIExtensionAbilityProcess());
