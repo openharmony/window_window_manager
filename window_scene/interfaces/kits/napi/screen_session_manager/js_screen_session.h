1cc607c61dad330fde48e5fd6441e9b5e1f5b27b
dqwdgit <dengqingwen1@h-partners.com>
2025-02-26 20:29:27 +0800

    Description:确保销毁 IssueNo:https://gitee.com/openharmony/window_window_manager/issues/IBP7PT Binary Source:No Signed-off-by:dqwdgit <dengqingwen1@h-partners.com>

--- window_scene/interfaces/kits/napi/screen_session_manager/js_screen_session.h
+++ window_scene/interfaces/kits/napi/screen_session_manager/js_screen_session.h
@@ -26,10 +26,10 @@
 namespace OHOS::Rosen {
 class JsScreenSession : public IScreenChangeListener {
 public:
-    JsScreenSession(napi_env env, const sptr<ScreenSession>& screenSession);
+    JsScreenSession(napi_env env, const sptr<ScreenSession>& screenSession, const int& type);
     virtual ~JsScreenSession();
 
-    static napi_value Create(napi_env env, const sptr<ScreenSession>& screenSession);
+    static napi_value Create(napi_env env, const sptr<ScreenSession>& screenSession, const int& type);
     static void Finalizer(napi_env env, void* data, void* hint);
 
 private:
