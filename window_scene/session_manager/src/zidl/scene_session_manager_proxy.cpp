6521a3d9c052582ccdc53b69336cebea7c284c59
xiazhiqi <xiazhiqi1@huawei.com>
2025-02-27 08:26:03 +0000

    sync
    Signed-off-by: xiazhiqi <xiazhiqi1@huawei.com>
    Change-Id: Iaa251e1d0d214edc094c5b91677c08e3e6010115

--- window_scene/session_manager/src/zidl/scene_session_manager_proxy.cpp
+++ window_scene/session_manager/src/zidl/scene_session_manager_proxy.cpp
@@ -100,6 +100,12 @@ WSError SceneSessionManagerProxy::CreateAndConnectSpecificSession(const sptr<ISe
         return WSError::WS_ERROR_IPC_FAILED;
     }
     property->SetWindowCornerRadius(cornerRadius);
+    uint64_t displayId = 0;
+    if (!reply.ReadUint64(displayId)) {
+        TLOGE(WmsLogTag::WMS_LIFE, "Read displayId failed");
+        return WSError::WS_ERROR_IPC_FAILED;
+    }
+    property->SetDisplayId(displayId);
     int32_t ret = reply.ReadInt32();
     return static_cast<WSError>(ret);
 }
