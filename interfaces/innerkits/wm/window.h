44f3b209aebe20d9bac72c18d41d0a79ebe20d06
ZihaoWU <wuzihao11@huawei.com>
2025-02-27 16:14:28 +0800

    add code
    Signed-off-by: ZihaoWU <wuzihao11@huawei.com>

--- interfaces/innerkits/wm/window.h
+++ interfaces/innerkits/wm/window.h
@@ -2603,8 +2603,7 @@ public:
      * @param densityInfo the struct representing system density, default density and custom density.
      * @return WMError.
      */
-    virtual WMError GetWindowDensityInfo(
-        WindowDensityInfo& densityInfo) { return WMError::WM_ERROR_DEVICE_NOT_SUPPORT; }
+    virtual WMError GetWindowDensityInfo(WindowDensityInfo& densityInfo) { return WMError::WM_OK; }
 
     /**
      * @brief Get virtual pixel ratio.
