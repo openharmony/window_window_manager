b2ceca423783ec5a58510aefc9a65fbe3c1ea45e
万孝国 <wanxiaoguo@huawei.com>
2025-02-27 20:09:58 +0800

    执行悬浮窗调用minmize
    Signed-off-by: 万孝国 <wanxiaoguo@huawei.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
+++ interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
@@ -5846,8 +5846,8 @@ napi_value JsWindow::OnMinimize(napi_env env, napi_callback_info info)
     if (errCode == WmErrorCode::WM_ERROR_INVALID_PARAM) {
         return NapiThrowError(env, WmErrorCode::WM_ERROR_INVALID_PARAM);
     }
-    if (errCode == WmErrorCode::WM_OK && WindowHelper::IsSubWindow(windowToken_->GetType())) {
-        WLOGFE("subWindow hide");
+    if (errCode == WmErrorCode::WM_OK && WindowHelper::IsFloatOrSubWindow(windowToken_->GetType())) {
+        TLOGI(WmsLogTag::WMS_LAYOUT, "subWindow or float window use hide");
         return HideWindowFunction(env, info, WmErrorCode::WM_OK);
     }
 
