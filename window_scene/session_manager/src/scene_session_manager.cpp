e32a98b0ef95a14b17646be73f15b669b5891b00
xiazhiqi <xiazhiqi1@huawei.com>
2025-02-28 02:30:03 +0000

    fix cache
    Signed-off-by: xiazhiqi <xiazhiqi1@huawei.com>
    Change-Id: I919dde8a499ad3ee90d502c1cde538bdc57b9016

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -3636,6 +3636,7 @@ void SceneSessionManager::NotifyCreateSubSession(int32_t persistentId, sptr<Scen
     }
     auto iter = createSubSessionFuncMap_.find(persistentId);
     if (iter == createSubSessionFuncMap_.end()) {
+        recoverSubSessionCacheMap_[persistentId].push_back(session);
         TLOGW(WmsLogTag::WMS_LIFE, "Can't find CreateSubSessionListener, parentId: %{public}d", persistentId);
         return;
     }
