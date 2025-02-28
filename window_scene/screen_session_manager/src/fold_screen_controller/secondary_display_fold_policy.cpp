43ef03bcf421562099500a3feb49da983eb9f24e
吴永鑫 <wuyongxin5@huawei.com>
2025-02-28 11:20:17 +0800

    双击不亮屏问题
    Signed-off-by: 吴永鑫 <wuyongxin5@huawei.com>

--- window_scene/screen_session_manager/src/fold_screen_controller/secondary_display_fold_policy.cpp
+++ window_scene/screen_session_manager/src/fold_screen_controller/secondary_display_fold_policy.cpp
@@ -124,6 +124,7 @@ void SecondaryDisplayFoldPolicy::SetOnBootAnimation(bool onBootAnimation)
     onBootAnimation_ = onBootAnimation;
     if (!onBootAnimation_) {
         TLOGW(WmsLogTag::DMS, "when boot animation finished, change display mode");
+        isChangeScreenWhenBootCompleted = true;
         RecoverWhenBootAnimationExit();
     }
 }
@@ -188,9 +189,12 @@ void SecondaryDisplayFoldPolicy::ChangeSuperScreenDisplayMode(sptr<ScreenSession
 {
     {
         std::lock_guard<std::recursive_mutex> lock_mode(displayModeMutex_);
-        if (currentDisplayMode_ == displayMode) {
+        if (currentDisplayMode_ == displayMode && !isChangeScreenWhenBootCompleted) {
             TLOGW(WmsLogTag::DMS, "already in displayMode %{public}d", displayMode);
             return;
+        }
+        if (isChangeScreenWhenBootCompleted) {
+            isChangeScreenWhenBootCompleted = false;
         }
     }
     SetSecondaryDisplayModeChangeStatus(true);
@@ -248,7 +252,8 @@ void SecondaryDisplayFoldPolicy::SetStatusFullActiveRectAndTpFeature(ScreenPrope
         .h = screenParams_[FULL_STATUS_WIDTH],
     };
     if (!onBootAnimation_) {
-        RSInterfaces::GetInstance().SetScreenActiveRect(0, rectCur);
+        auto response = RSInterfaces::GetInstance().SetScreenActiveRect(0, rectCur);
+        TLOGI(WmsLogTag::DMS, "rs response is %{public}ld", static_cast<long>(response));
     }
 #ifdef TP_FEATURE_ENABLE
     RSInterfaces::GetInstance().SetTpFeatureConfig(TP_TYPE, STATUS_FULL, TpFeatureConfigType::AFT_TP_FEATURE);
@@ -268,7 +273,8 @@ void SecondaryDisplayFoldPolicy::SetStatusMainActiveRectAndTpFeature(ScreenPrope
         .h = screenParams_[MAIN_STATUS_WIDTH],
     };
     if (!onBootAnimation_) {
-        RSInterfaces::GetInstance().SetScreenActiveRect(0, rectCur);
+        auto response = RSInterfaces::GetInstance().SetScreenActiveRect(0, rectCur);
+        TLOGI(WmsLogTag::DMS, "rs response is %{public}ld", static_cast<long>(response));
     }
 #ifdef TP_FEATURE_ENABLE
     RSInterfaces::GetInstance().SetTpFeatureConfig(TP_TYPE, STATUS_MAIN, TpFeatureConfigType::AFT_TP_FEATURE);
@@ -288,7 +294,8 @@ void SecondaryDisplayFoldPolicy::SetStatusGlobalFullActiveRectAndTpFeature(Scree
         .h = screenParams_[GLOBAL_FULL_STATUS_WIDTH],
     };
     if (!onBootAnimation_) {
-        RSInterfaces::GetInstance().SetScreenActiveRect(0, rectCur);
+        auto response = RSInterfaces::GetInstance().SetScreenActiveRect(0, rectCur);
+        TLOGI(WmsLogTag::DMS, "rs response is %{public}ld", static_cast<long>(response));
     }
 #ifdef TP_FEATURE_ENABLE
     RSInterfaces::GetInstance().SetTpFeatureConfig(TP_TYPE, STATUS_GLOBAL_FULL, TpFeatureConfigType::AFT_TP_FEATURE);
