bfe5401fcfdaa5022a1b6cad7844b2b7448937d0
tanyuhang <tanyuhang@huawei.com>
2025-02-28 17:36:10 +0800

    fix_tdd_5.0.3
    Signed-off-by: tanyuhang <tanyuhang@huawei.com>

--- wm/test/unittest/window_session_impl_test4.cpp
+++ wm/test/unittest/window_session_impl_test4.cpp
@@ -200,15 +200,17 @@ HWTEST_F(WindowSessionImplTest4, SetMainWindowTopmost, Function | SmallTest | Le
     sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
     option->SetWindowName("SetMainWindowTopmost");
     sptr<WindowSessionImpl> window = sptr<WindowSessionImpl>::MakeSptr(option);
+    WMError res = window->SetMainWindowTopmost(false);
+
     window->property_->SetPersistentId(1);
     SessionInfo sessionInfo = {"CreateTestBundle", "CreateTestModule", "CreateTestAbility"};
     sptr<SessionMocker> session = sptr<SessionMocker>::MakeSptr(sessionInfo);
     window->hostSession_ = session;
     window->state_ = WindowState::STATE_CREATED;
     window->windowSystemConfig_.uiType_ = UI_TYPE_PC;
-    window->property_->SetWindowType(WindowType::APP_MAIN_WINDOW_BASE);
-    WMError res = window->SetMainWindowTopmost(true);
-    ASSERT_EQ(WMError::WM_ERROR_DEVICE_NOT_SUPPORT, res);
+    wndow->property_->SetWindowType(WindowType::APP_MAIN_WINDOW_BASE);
+    res = window->SetMainWindowTopmost(true);
+    ASSERT_EQ(WMError::WM_OK, res);
 }
 
 /**
