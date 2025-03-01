fb334441687bb4df8e90f6915937e1ed092c87cd
dqwdgit <dengqingwen1@h-partners.com>
2025-03-01 15:30:07 +0800

    Description:解决折展后DPI恢复默认值的问题 IssueNo:https://gitee.com/openharmony/window_window_manager/issues/IBPWNI Binary Source:No Signed-off-by:dqwdgit <dengqingwen1@h-partners.com>

--- window_scene/screen_session_manager/src/screen_session_manager.cpp
+++ window_scene/screen_session_manager/src/screen_session_manager.cpp
@@ -6517,17 +6517,10 @@ void ScreenSessionManager::NotifyFoldStatusChanged(FoldStatus foldStatus)
             screenSession->SetDefaultDeviceRotationOffset(defaultDeviceRotationOffset_);
         }
     }
-    if (screenSession != nullptr && FoldScreenStateInternel::IsSingleDisplayPocketFoldDevice()) {
-        // 维护外屏独立dpi
-        if (foldStatus == FoldStatus::FOLDED) {
+    if (screenSession != nullptr && FoldScreenStateInternel::IsSingleDisplayPocketFoldDevice() &&
+        foldStatus == FoldStatus::FOLDED) {
             // sub screen default rotation off set
             screenSession->SetDefaultDeviceRotationOffset(270);
-            auto property = screenSession->GetScreenProperty();
-            densityDpi_ = property.GetDensity();
-            SetVirtualPixelRatio(GetDefaultScreenId(), subDensityDpi_);
-        } else {
-            SetVirtualPixelRatio(GetDefaultScreenId(), densityDpi_);
-        }
     }
     auto agents = dmAgentContainer_.GetAgentsByType(DisplayManagerAgentType::FOLD_STATUS_CHANGED_LISTENER);
     TLOGI(WmsLogTag::DMS, "foldStatus:%{public}d, agent size: %{public}u",
