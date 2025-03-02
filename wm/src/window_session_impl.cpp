a7058734ff7cedfd36a1a46e7b38906acc66acd1
chengzhen <chengzhen25@huawei.com>
2025-03-02 18:12:25 +0800

    clean code
    Signed-off-by: chengzhen <chengzhen25@huawei.com>

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -1442,8 +1442,8 @@ WMError WindowSessionImpl::InitUIContent(const std::string& contentInfo, napi_en
         uiContent_ = std::move(uiContent);
         // compatibleMode app in pc, need change decor title to float title bar
         if (property_->GetCompatibleModeInPc()) {
-            uiContent_->EnableContainerModalCustomGesture(true);
             uiContent_->SetContainerModalTitleVisible(false, true);
+            uiContent_->EnableContainerModalCustomGesture(true);
         }
         WLOGFI("Initialized, isUIExtensionSubWindow:%{public}d, isUIExtensionAbilityProcess:%{public}d",
             uiContent_->IsUIExtensionSubWindow(), uiContent_->IsUIExtensionAbilityProcess());
