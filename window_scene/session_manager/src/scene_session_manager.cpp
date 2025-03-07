2858449645f2eb4e09bef92f2a0f8001b8e34d1e
liaoqingxing <qingxingliao@huawei.com>
2025-03-07 20:42:19 +0800

    修改检视意见
    Signed-off-by: liaoqingxing <qingxingliao@huawei.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -10377,12 +10377,12 @@ WSError SceneSessionManager::UpdateSessionDisplayId(int32_t persistentId, uint64
     const auto& keyboardSessionVec = GetSceneSessionVectorByType(WindowType::WINDOW_TYPE_INPUT_METHOD_FLOAT);
     for (const auto& keyboardSession : keyboardSessionVec) {
         if (keyboardSession) {
-            TLOGI(WmsLogTag::WMS_KEYBOARD, "isSystemKeyboard: %{public}d, callingSessionId: %{public}d",
+            TLOGD(WmsLogTag::WMS_KEYBOARD, "isSystemKeyboard: %{public}d, callingSessionId: %{public}d",
                 keyboardSession->IsSystemKeyboard(), keyboardSession->GetCallingSessionId());
         }
         if (keyboardSession && !keyboardSession->IsSystemKeyboard() &&
             static_cast<int32_t>(keyboardSession->GetCallingSessionId()) == persistentId) {
-            CallingWindowInfo callingWindowInfo(persistentId, sceneSession->GetCallingPid(), screenId, sceneSession->GetCallingUid());
+            CallingWindowInfo callingWindowInfo(persistentId, sceneSession->GetCallingPid(), screenId, GetUserIdByUid(getuid()));
             SessionManagerAgentController::GetInstance().NotifyCallingWindowDisplayChanged(callingWindowInfo);
             break;
         }
@@ -12156,19 +12156,17 @@ WMError SceneSessionManager::GetMainWindowInfos(int32_t topNum, std::vector<Main
 
 WMError SceneSessionManager::GetCallingWindowInfo(CallingWindowInfo& callingWindowInfo)
 {
-    auto sceneSession = GetSceneSession(callingWindowInfo.windowId_);
-    if (sceneSession == nullptr) {
-        TLOGE(WmsLogTag::WMS_KEYBOARD, "sceneSession is nullptr,id: %{public}d", callingWindowInfo.windowId_);
-        return WMError::WM_ERROR_NULLPTR;
-    }
-
-    int32_t curUserId = sceneSession->GetCallingUid();
+    int32_t curUserId = GetUserIdByUid(getuid());
     if (curUserId != callingWindowInfo.userId_) {
         TLOGE(WmsLogTag::WMS_KEYBOARD, "Target user not exists,targetUserId: %{public}d, curUserId: %{public}d, id: %{public}d",
             callingWindowInfo.userId_, curUserId, callingWindowInfo.windowId_);
         return WMError::WM_ERROR_INVALID_PARAM;
     }
-
+    auto sceneSession = GetSceneSession(callingWindowInfo.windowId_);
+    if (sceneSession == nullptr) {
+        TLOGE(WmsLogTag::WMS_KEYBOARD, "sceneSession is nullptr,id: %{public}d", callingWindowInfo.windowId_);
+        return WMError::WM_ERROR_NULLPTR;
+    }
     callingWindowInfo.callingPid_ = sceneSession->GetCallingPid();
     callingWindowInfo.displayId_ = sceneSession->GetSessionProperty()->GetDisplayId();
     TLOGI(WmsLogTag::WMS_KEYBOARD, "callingWindow id: %{public}d,pid: %{public}d,displayId: %{public}" PRIu64" , curUserId: %{public}d",
