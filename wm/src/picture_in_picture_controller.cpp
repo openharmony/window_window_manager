fcddddd46062dae0e8d1ae996d299e994082b286
刘阳 <liuyang972@huawei.com>
2025-03-07 17:31:05 +0800

    画中画新增testApi
    Signed-off-by: 刘阳 <liuyang972@huawei.com>

--- wm/src/picture_in_picture_controller.cpp
+++ wm/src/picture_in_picture_controller.cpp
@@ -14,15 +14,14 @@
  */
 
 #include "picture_in_picture_controller.h"
-#include "parameters.h"
 
 #include <refbase.h>
 #include <transaction/rs_sync_transaction_controller.h>
+#include "window_adapter.h"
 #include "picture_in_picture_manager.h"
 #include "window_manager_hilog.h"
 #include "window_option.h"
 #include "singleton_container.h"
-#include "window_adapter.h"
 
 namespace OHOS {
 namespace Rosen {
