58114e98e41e196622db78d86216f41fb6faf06d
zyu3695 <14508223+zyu3695@user.noreply.gitee.com>
2025-03-10 20:57:10 +0800

    应用锁支持仅管控多任务的参数
    Signed-off-by: 张余 <1459392684@qq.com>

--- window_scene/session_manager/src/zidl/scene_session_manager_lite_proxy.cpp
+++ window_scene/session_manager/src/zidl/scene_session_manager_lite_proxy.cpp
@@ -1454,7 +1454,7 @@ WSError SceneSessionManagerLiteProxy::NotifyAppUseControlList(
 
     for (const auto& control : controlList) {
         if (!data.WriteString(control.bundleName_) || !data.WriteInt32(control.appIndex_) ||
-            !data.WriteBool(control.isNeedControl_)) {
+            !data.WriteBool(control.isNeedControl_) || !data.WriteBool(control.isControlRecentOnly_)) {
             TLOGE(WmsLogTag::WMS_LIFE, "Write controlList failed");
             return WSError::WS_ERROR_INVALID_PARAM;
         }
