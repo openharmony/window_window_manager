072991ecd29dc1bdb01f8b1726f1cd4a6ecdcb4a
许杨杨 <xuyangyang29@huawei.com>
2025-03-13 07:20:41 +0000

    update window_scene/session_manager/include/scene_session_manager.h.
    Signed-off-by: 许杨杨 <xuyangyang29@huawei.com>

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -618,7 +618,8 @@ private:
     std::atomic<bool> enterRecent_ { false };
     bool isKeyboardPanelEnabled_ = false;
     static sptr<SceneSessionManager> CreateInstance();
-    static inline bool isNotCurrentScreen(sptr<SceneSession> sceneSession, ScreenId screenId) {
+    static inline bool isNotCurrentScreen(sptr<SceneSession> sceneSession, ScreenId screenId)
+    {
             return sceneSession->GetSessionInfo().screenId_ != screenId &&
                    sceneSession->GetSessionInfo().screenId_ != SCREEN_ID_INVALID;
     }
