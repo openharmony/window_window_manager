e0b85b907f2e527730425b2a6ae47c3ca0726a72
giteeOrange <zhuying79@huawei.com>
2025-03-14 04:11:00 +0000

    update wm/src/window_session_impl.cpp.
    Signed-off-by: giteeOrange <zhuying79@huawei.com>

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -2052,7 +2052,7 @@ float WindowSessionImpl::GetBrightness() const
     return property_->GetBrightness();
 }
 
-void WindowSessionImpl::SetRequestedOrientation(Orientation orientation, bool animation)
+void WindowSessionImpl::SetRequestedOrientation(Orientation orientation, bool needAnimation)
 {
     if (IsWindowSessionInvalid()) {
         TLOGE(WmsLogTag::DEFAULT, "windowSession is invalid");
