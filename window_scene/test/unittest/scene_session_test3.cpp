815a9952a392a379e7a0f4a4fb574ebd81be24cb
liaoqingxing <qingxingliao@huawei.com>
2025-03-15 16:58:43 +0800

    修改检视意见-删除无效变量和UT用例
    Signed-off-by: liaoqingxing <qingxingliao@huawei.com>

--- window_scene/test/unittest/scene_session_test3.cpp
+++ window_scene/test/unittest/scene_session_test3.cpp
@@ -1680,32 +1680,6 @@ HWTEST_F(SceneSessionTest3, GetKeyboardOccupiedAreaWithRotation2, Function | Sma
     ASSERT_EQ(1, areaSize);
     ASSERT_EQ(true, avoidAreas[0].first);
 }
-
-/**
- * @tc.name: GetKeyboardOccupiedAreaWithRotation3
- * @tc.desc: GetKeyboardOccupiedAreaWithRotation
- * @tc.type: FUNC
- */
-HWTEST_F(SceneSessionTest3, GetKeyboardOccupiedAreaWithRotation3, Function | SmallTest | Level2)
-{
-    SessionInfo info;
-    info.abilityName_ = "GetKeyboardOccupiedAreaWithRotation";
-    info.bundleName_ = "GetKeyboardOccupiedAreaWithRotation";
-    sptr<SceneSession> sceneSession = sptr<SceneSession>::MakeSptr(info, nullptr);
-    sptr<SceneSession::SpecificSessionCallback> specificCallback =
-        sptr<SceneSession::SpecificSessionCallback>::MakeSptr();
-    sceneSession->specificCallback_ = sptr<SceneSession::SpecificSessionCallback>::MakeSptr();
-
-    int32_t persistentId = 1;
-    uint32_t rotation = 90;
-    std::vector<std::pair<bool, WSRect>> avoidAreas;
-    std::pair<bool, WSRect> keyboardOccupiedArea = {false, {0, 0, 0, 0}};
-    avoidAreas.emplace_back(keyboardOccupiedArea);
-    sceneSession->GetKeyboardOccupiedAreaWithRotation(persistentId, rotation, avoidAreas);
-    uint32_t areaSize = static_cast<uint32_t>(avoidAreas.size());
-    ASSERT_EQ(1, areaSize);
-    ASSERT_EQ(false, avoidAreas[0].first);
-}
 }
 }
 }
