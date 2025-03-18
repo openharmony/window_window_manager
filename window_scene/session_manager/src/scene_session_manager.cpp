8dc3ef4bd8c2e4a671389929e3669ff778e01fa2
胡晗 <huhan25@huawei.com>
2025-03-18 10:29:57 +0800

    修复WPS在前台时，WIN_D焦点切换异常问题
    Signed-off-by: 胡晗 <huhan25@huawei.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -6141,7 +6141,7 @@ sptr<SceneSession> SceneSessionManager::GetTopFocusableNonAppSession()
             return false;
         }
         if (session->IsAppSession()) {
-            return true;
+            return session->GetZOrder() == 0 ? false :true;
         }
         if (session->CheckFocusable() && IsSessionVisibleForeground(session)) {
             ret = session;
