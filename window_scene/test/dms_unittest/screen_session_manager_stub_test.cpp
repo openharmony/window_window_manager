50eed710cffed75cdd2717f3a037f28da0fd94dd
Baijian Chen <b84299469@china.huawei.com>
2025-03-18 20:57:24 -0400

    Fix review comments
    Signed-off-by: Baijian Chen <baijian.chen@h-partners.com>

--- window_scene/test/dms_unittest/screen_session_manager_stub_test.cpp
+++ window_scene/test/dms_unittest/screen_session_manager_stub_test.cpp
@@ -3160,7 +3160,7 @@ HWTEST_F(ScreenSessionManagerStubTest, OnRemoteRequest148, Function | SmallTest 
 
 /**
  * @tc.name: OnRemoteRequest149
- * @tc.desc: normal function, TRANS_ID_SET_SYSTEM_KEYBOARD_ON test
+ * @tc.desc: normal function, TRANS_ID_SET_SYSTEM_KEYBOARD_STATUS test
  * @tc.type: FUNC
  */
 HWTEST_F(ScreenSessionManagerStubTest, OnRemoteRequest149, Function | SmallTest | Level2)
@@ -3168,10 +3168,10 @@ HWTEST_F(ScreenSessionManagerStubTest, OnRemoteRequest149, Function | SmallTest 
     MessageParcel data;
     MessageParcel reply;
     MessageOption option;
-    bool isOn = true;
+    bool isTpKeyboardOn = true;
     data.WriteInterfaceToken(ScreenSessionManagerStub::GetDescriptor());
-    data.WriteBool(isOn);
-    uint32_t code = static_cast<uint32_t>(DisplayManagerMessage::TRANS_ID_SET_SYSTEM_KEYBOARD_ON);
+    data.WriteBool(isTpKeyboardOn);
+    uint32_t code = static_cast<uint32_t>(DisplayManagerMessage::TRANS_ID_SET_SYSTEM_KEYBOARD_STATUS);
     int res = stub_->OnRemoteRequest(code, data, reply, option);
     EXPECT_EQ(res, 0);
 }
