2b7f76bfdb8f027493df1c7d72f45db8e93edfd9
Peng fafu <pengfafu@huawei.com>
2025-03-20 19:32:02 +0800

    resync extension config
    Signed-off-by: Peng fafu <pengfafu@huawei.com>

--- wm/test/unittest/ui_extension/window_extension_session_impl_test.cpp
+++ wm/test/unittest/ui_extension/window_extension_session_impl_test.cpp
@@ -2318,6 +2318,27 @@ HWTEST_F(WindowExtensionSessionImplTest, OnWaterfallModeChange, Function | Small
     EXPECT_EQ(WMError::WM_OK, window_->OnWaterfallModeChange(std::move(want), reply));
     EXPECT_TRUE(window_->IsWaterfallModeEnabled());
 }
+
+/**
+ * @tc.name: OnResyncExtensionConfig
+ * @tc.desc: OnResyncExtensionConfig Test
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowExtensionSessionImplTest, OnResyncExtensionConfig, Function | SmallTest | Level3)
+{
+    AAFwk::Want want;
+    AAFwk::WantParams configParam;
+    AAFwk::WantParams wantParam(want->GetParams());
+    configParam.SetParam(Extension::CROSS_AXIS_FIELD,
+        AAFwk::Integer::Box(static_cast<int32_t>(CrossAxisState::STATE_CROSS)));
+    configParam.SetParam(Extension::WATERFALL_MODE_FIELD, AAFwk::Integer::Box(static_cast<int32_t>(1)));
+    wantParam.SetParam(Extension::UIEXTENSION_CONFIG_FIELD, AAFwk::WantParamWrapper::Box(configParam));
+    want.SetParams(wantParam);
+    std::optional<AAFwk::Want> reply = std::make_optional<AAFwk::Want>();
+    EXPECT_EQ(WMError::WM_OK, window_->OnResyncExtensionConfig(std::move(want), reply));
+    EXPECT_EQ(CrossAxisState::STATE_CROSS, window_->crossAxisState_.load());
+    EXPECT_TRUE(window_->IsWaterfallModeEnabled());
+}
 }
 } // namespace Rosen
 } // namespace OHOS
