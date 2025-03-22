10fbe2951fe6d3be2d1cce17d8a087ee4ad3c921
卢宇豪 <luyuhao6@huawei.com>
2025-03-23 01:55:38 +0800

    change1
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- window_scene/session_manager/src/scene_session_dirty_manager.cpp
+++ window_scene/session_manager/src/scene_session_dirty_manager.cpp
@@ -743,7 +743,7 @@ std::pair<MMI::WindowInfo, std::shared_ptr<Media::PixelMap>> SceneSessionDirtyMa
         .pixelMap = pixelMap.get(),
         .windowInputType = static_cast<MMI::WindowInputType>(sceneSession->GetSessionInfo().windowInputType_),
         .windowType = static_cast<int32_t>(windowType),
-        .isSkipEventOnCastPlus = sceneSession->GetSessionProperty()->GetSkipEventOnCastPlus(),
+        .isSkipSelfWhenShowOnVirtualScreen = sceneSession->GetSessionProperty()->GetSkipEventOnCastPlus(),
     };
     UpdateWindowFlags(displayId, sceneSession, windowInfo);
     if (windowSessionProperty->GetWindowFlags() & static_cast<uint32_t>(WindowFlag::WINDOW_FLAG_HANDWRITING)) {
