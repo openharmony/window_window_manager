6280af3591ecf9c0118fb6ea68d6217ce6008e07
许杨杨 <xuyangyang29@huawei.com>
2025-03-24 18:07:43 +0800

    检视意见整改 & bugfix 1、SetFollowParentMultiScreenPolicyTask函数命名整改 2、GetFollowParentMultiScreenPolicyTask入参使用引用 3、RemoveSufaceNodeFromScreen函数名拼写错误整改 4、修复独立恢复场景 5、修复子窗跟随松手后位置异常问题
    Signed-off-by: 白钰胜 <baiyusheng@h-partners.com>

--- window_scene/common/src/window_session_property.cpp
+++ window_scene/common/src/window_session_property.cpp
@@ -1042,6 +1042,9 @@ bool WindowSessionProperty::MarshallingSessionInfo(Parcel& parcel) const
     if (hasWant && !parcel.WriteParcelable(want.get())) {
         return false;
     }
+    if (!parcel.WriteBool(sessionInfo_.isFollowParentMultiScreenPolicy)) {
+        return false;
+    }
     return true;
 }
 
@@ -1080,6 +1083,12 @@ bool WindowSessionProperty::UnmarshallingSessionInfo(Parcel& parcel, WindowSessi
         }
         info.want = want;
     }
+    bool isFollowParentMultiScreenPolicy = false;
+    if (!parcel.ReadBool(isFollowParentMultiScreenPolicy)) {
+        TLOGE(WmsLogTag::DEFAULT, "Failed to read isFollowParentMultiScreenPolicy!");
+        return false;
+    }
+    info.isFollowParentMultiScreenPolicy = isFollowParentMultiScreenPolicy;
     property->SetSessionInfo(info);
     return true;
 }
