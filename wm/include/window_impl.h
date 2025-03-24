ec961a84a419f56e5073e7bbf8980e026b3887a3
zhoukai <zhoukai94@huawei.com>
2025-03-24 14:20:21 +0800

    virtualPixelRatio由ipc变为缓存机制
    Signed-off-by: zhoukai <zhoukai94@huawei.com>

--- wm/include/window_impl.h
+++ wm/include/window_impl.h
@@ -546,7 +546,7 @@ private:
     bool escKeyEventTriggered_ = false;
     bool enableImmersiveMode_ = false;
     std::shared_ptr<VsyncStation> vsyncStation_ = nullptr;
-
+    std::atomic<float> virtualPixelRatio_ = 1.0f;
     std::string restoredRouterStack_; // It was set and get in same thread, which is js thread.
 };
 } // namespace Rosen
