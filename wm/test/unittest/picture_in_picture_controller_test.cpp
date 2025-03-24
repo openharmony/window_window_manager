5874c71f19bab519487ae414b60fdfd82fd6248b
刘阳 <liuyang972@huawei.com>
2025-03-24 22:57:48 +0800

    画中画设备判断能力优化
    Signed-off-by: 刘阳 <liuyang972@huawei.com>

--- wm/test/unittest/picture_in_picture_controller_test.cpp
+++ wm/test/unittest/picture_in_picture_controller_test.cpp
@@ -1198,7 +1198,8 @@ HWTEST_F(PictureInPictureControllerTest, GetPipPossible, Function | SmallTest | 
     auto pipControl = sptr<PictureInPictureController>::MakeSptr(option, mw, 100, nullptr);
 
     const std::string multiWindowUIType = system::GetParameter("const.window.multiWindowUIType", "");
-    bool isDeviceSupported = multiWindowUIType == "HandsetSmartWindow" || multiWindowUIType == "TabletSmartWindow";
+    bool isDeviceSupported = multiWindowUIType == "HandsetSmartWindow" || multiWindowUIType == "FreeFormMultiWindow" ||
+        multiWindowUIType == "TabletSmartWindow";
 
     bool pipSupported = false;
     pipControl->pipOption_ = option;
