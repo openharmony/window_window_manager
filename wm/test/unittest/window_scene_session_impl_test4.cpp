428d9325d15d6cd7cf8f16bc6a43d7ec5dd3f1f3
jingbotao <jingbotao3@huawei.com>
2025-03-27 14:48:35 +0800

    Solve the issue of incorrect position of the input method sub-window pop-up
    Signed-off-by: jingbotao <jingbotao3@huawei.com>

--- wm/test/unittest/window_scene_session_impl_test4.cpp
+++ wm/test/unittest/window_scene_session_impl_test4.cpp
@@ -1126,6 +1126,13 @@ HWTEST_F(WindowSceneSessionImplTest4, PreLayoutOnShow01, Function | SmallTest | 
     window->PreLayoutOnShow(WindowType::WINDOW_TYPE_APP_SUB_WINDOW, displayInfo);
     window->hostSession_ = session;
     window->PreLayoutOnShow(WindowType::WINDOW_TYPE_APP_SUB_WINDOW, displayInfo);
+    Rect originRect = window->GetRect();
+    Rect testRect = {10, 20, 100, 200};
+    window->GetProperty()->SetRequestRect(testRect);
+    window->PreLayoutOnShow(WindowType::WINDOW_TYPE_INPUT_METHOD_FLOAT, displayInfo);
+    ASSERT_EQ(window->GetRect(), originRect);
+    window->PreLayoutOnShow(WindowType::WINDOW_TYPE_APP_SUB_WINDOW, displayInfo);
+    ASSERT_EQ(window->GetRect(), testRect);
 }
 
 /**
