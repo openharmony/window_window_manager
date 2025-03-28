837c2fd9538e55795730d8633e4541dfb97819e4
wangyaohui <wangyaohui17@huawei.com>
2025-03-28 10:24:34 +0800

    [feature] PiPWindow adds optional parameter: localStorage
    Signed-off-by: wangyaohui <wangyaohui17@huawei.com>

--- wm/include/picture_in_picture_option.h
+++ wm/include/picture_in_picture_option.h
@@ -53,6 +53,8 @@ public:
     napi_ref GetNodeControllerRef() const;
     void SetTypeNodeRef(napi_ref ref);
     napi_ref GetTypeNodeRef() const;
+    void SetStorageRef(napi_ref ref);
+    napi_ref GetStorageRef() const;
     void SetTypeNodeEnabled(bool enable);
     bool IsTypeNodeEnabled() const;
 private:
@@ -69,6 +71,7 @@ private:
     napi_ref customNodeController_ = nullptr;
     napi_ref typeNode_ = nullptr;
     bool useTypeNode_ = false;
+    napi_ref storage_ = nullptr;
 };
 }
 }
