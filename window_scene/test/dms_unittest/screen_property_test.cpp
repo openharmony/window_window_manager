8f6ad3ce855e3d0ef54f4152ae68295820503300
MirMort <chenjunliang26@h-partners.com>
2025-03-31 21:36:20 +0800

    解决输入法截断问题
    Signed-off-by: MirMort <chenjunliang26@h-partners.com>

--- window_scene/test/dms_unittest/screen_property_test.cpp
+++ window_scene/test/dms_unittest/screen_property_test.cpp
@@ -518,6 +518,30 @@ HWTEST_F(ScreenPropertyTest, SetDensityInCurResolution, Function | SmallTest | L
     delete property;
     GTEST_LOG_(INFO) << "ScreenPropertyTest: SetDensityInCurResolution end";
 }
+
+/**
+ * @tc.name: SetRotationAndScreenRotationOnly
+ * @tc.desc: SetRotationAndScreenRotationOnly test
+ * @tc.type: FUNC
+ */
+HWTEST_F(ScreenPropertyTest, SetRotationAndScreenRotationOnly, Function | SmallTest | Level2)
+{
+    GTEST_LOG_(INFO) << "ScreenPropertyTest: SetRotationAndScreenRotationOnly start";
+    std::shared_ptr<ScreenProperty> property = std::make_shared<ScreenProperty>();
+    ASSERT_NE(property, nullptr);
+    property->SetRotationAndScreenRotationOnly(Rotation::ROTATION_0);
+    ASSERT_EQ(property->GetScreenRotation(), Rotation::ROTATION_0);
+    
+    property->SetRotationAndScreenRotationOnly(Rotation::ROTATION_90);
+    ASSERT_EQ(property->GetScreenRotation(), Rotation::ROTATION_90);
+
+    property->SetRotationAndScreenRotationOnly(Rotation::ROTATION_180);
+    ASSERT_EQ(property->GetScreenRotation(), Rotation::ROTATION_180);
+
+    property->SetRotationAndScreenRotationOnly(Rotation::ROTATION_270);
+    ASSERT_EQ(property->GetScreenRotation(), Rotation::ROTATION_270);
+    GTEST_LOG_(INFO) << "ScreenPropertyTest: SetRotationAndScreenRotationOnly end";
+}
 } // namespace
 } // namespace Rosen
 } // namespace OHOS
