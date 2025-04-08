882841e6e7e5d697a79982b37c32c5fe678b1dbf
cxy20001123 <chenxingyu34@h-partners.com>
2025-04-08 16:47:27 +0800

    代码优化
    Signed-off-by: cxy20001123 <chenxingyu34@h-partners.com>

--- dm/src/display_manager_adapter.cpp
+++ dm/src/display_manager_adapter.cpp
@@ -940,17 +940,17 @@ DMError DisplayManagerAdapter::SetVirtualScreenSecurityExemption(ScreenId screen
     return displayManagerServiceProxy_->SetVirtualScreenSecurityExemption(screenId, pid, windowIdList);
 }
 
-bool ScreenManagerAdapter::SetVirtualScreenStatus(ScreenId screenId, VirtualScreenStatus screenStatus)
-{
-    INIT_PROXY_CHECK_RETURN(false);
-    return displayManagerServiceProxy_->SetVirtualScreenStatus(screenId, screenStatus);
-}
-
 DMError ScreenManagerAdapter::SetVirtualScreenMaxRefreshRate(ScreenId id, uint32_t refreshRate,
     uint32_t& actualRefreshRate)
 {
     INIT_PROXY_CHECK_RETURN(DMError::DM_ERROR_INIT_DMS_PROXY_LOCKED);
     return displayManagerServiceProxy_->SetVirtualScreenMaxRefreshRate(id, refreshRate, actualRefreshRate);
+}
+
+bool ScreenManagerAdapter::SetVirtualScreenStatus(ScreenId screenId, VirtualScreenStatus screenStatus)
+{
+    INIT_PROXY_CHECK_RETURN(false);
+    return displayManagerServiceProxy_->SetVirtualScreenStatus(screenId, screenStatus);
 }
 
 std::shared_ptr<Media::PixelMap> DisplayManagerAdapter::GetScreenCapture(const CaptureOption& captureOption,
