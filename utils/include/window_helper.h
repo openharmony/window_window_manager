84c2594180b2902525db09064c446029cf325762
xxx <xiaozhiwen7@h-partners.com>
2025-04-08 04:37:44 +0000

    update utils/include/window_helper.h.
    Signed-off-by: xxx <xiaozhiwen7@h-partners.com>

--- utils/include/window_helper.h
+++ utils/include/window_helper.h
@@ -629,7 +629,11 @@ public:
                decorButtonStyle.spacingBetweenButtons >= MIN_SPACING_BETWEEN_BUTTONS &&
                decorButtonStyle.spacingBetweenButtons <= MAX_SPACING_BETWEEN_BUTTONS &&
                decorButtonStyle.colorMode >= MIN_COLOR_MODE &&
-               decorButtonStyle.colorMode <= MAX_COLOR_MODE;
+               decorButtonStyle.colorMode <= MAX_COLOR_MODE &&
+               decorButtonStyle.buttonBackgroundCornerRedius >= MIN_BUTTON_BACKGROUND_CORNER_RADIUS &&
+               decorButtonStyle.buttonBackgroundCornerRedius <= MAX_BUTTON_BACKGROUND_CORNER_RADIUS &&
+               decorButtonStyle.spacingBetweenButtons +
+                decorButtonStyle.buttonBackgroundSize >= MIN_BUTTON_BETWEEN;
     }
 
 private:
