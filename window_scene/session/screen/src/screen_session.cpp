b501b39b1db1b3fb9b77e68af7504e5175ce0dcf
MirMort <chenjunliang26@h-partners.com>
2025-04-08 11:43:48 +0800

    提前通知旋转时，同时通知图像
    Signed-off-by: MirMort <chenjunliang26@h-partners.com>

--- window_scene/session/screen/src/screen_session.cpp
+++ window_scene/session/screen/src/screen_session.cpp
@@ -1019,6 +1019,20 @@ void ScreenSession::UpdatePropertyAfterRotation(RRect bounds, int rotation,
     ReportNotifyModeChange(displayOrientation);
 }
 
+void ScreenSession::UpdateDisplayNodeRotation(int rotation)
+{
+    Rotation targetRotation = ConvertIntToRotation(rotation);
+    auto transactionProxy = RSTransactionProxy::GetInstance();
+    if (transactionProxy != nullptr) {
+        transactionProxy->Begin();
+        {
+            std::shared_lock<std::shared_mutex> displayNodeLock(displayNodeMutex_);
+            displayNode_->SetScreenRotation(static_cast<uint32_t>(property_.GetDeviceRotation()));
+        }
+        transactionProxy->Commit();
+    }
+}
+
 void ScreenSession::UpdatePropertyOnly(RRect bounds, int rotation, FoldDisplayMode foldDisplayMode, bool isChanged)
 {
     OptimizeSecondaryDisplayMode(bounds, foldDisplayMode);
