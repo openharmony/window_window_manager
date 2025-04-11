fae36e118e72789cb6baeb88d127395cbb994220
MirMort <chenjunliang26@h-partners.com>
2025-04-11 12:05:02 +0800

    add
    Signed-off-by: MirMort <chenjunliang26@h-partners.com>

--- window_scene/screen_session_manager/src/screen_session_manager.cpp
+++ window_scene/screen_session_manager/src/screen_session_manager.cpp
@@ -8855,7 +8855,7 @@ DMError ScreenSessionManager::SetScreenSkipProtectedWindow(const std::vector<Scr
                 TLOGI(WmsLogTag::DMS, "virtualScreenId:%{public}" PRIu64 "",screenId);
                 HITRACE_METER_FMT(HITRACE_TAG_WINDOW_MANAGER,
                     "SetCastScreenEnableSkipWindow(%" PRIu64")", screenId);
-                rsInterface_.SetCastScreenEnableSkipWindow(rsScreenId, requiredSkipWindow);
+                rsInterface_.SetCastScreenEnableSkipWindow(rsScreenId, isEnable);
             }
         }
     }
