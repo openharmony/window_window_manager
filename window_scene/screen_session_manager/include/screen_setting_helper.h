d6524f264d2749f0ebf1de68f962e66aee28dd71
MirMort <chenjunliang26@h-partners.com>
2025-04-11 11:06:54 +0800

    去除屏幕共享保护设置开关限制
    Signed-off-by: MirMort <chenjunliang26@h-partners.com>

--- window_scene/screen_session_manager/include/screen_setting_helper.h
+++ window_scene/screen_session_manager/include/screen_setting_helper.h
@@ -55,10 +55,6 @@ public:
     static bool UpdateScreenMode(MultiScreenInfo& info, uint32_t mode, bool isExternal);
     static bool GetScreenRelativePosition(MultiScreenInfo& info, const std::string& inputString);
     static ScreenShape GetScreenShape(ScreenId screenId);
-    static void RegisterSettingscreenSkipProtectedWindowObserver(SettingObserver::UpdateFunc func);
-    static void UnregisterSettingscreenSkipProtectedWindowObserver();
-    static bool GetSettingscreenSkipProtectedWindow(bool& enable,
-        const std::string& key = SETTING_SCREEN_SHARE_PROTECT_KEY);
     static void RegisterSettingWireCastObserver(SettingObserver::UpdateFunc func);
     static void UnregisterSettingWireCastObserver();
     static void RegisterSettingExtendScreenDpiObserver(SettingObserver::UpdateFunc func);
@@ -71,12 +67,10 @@ private:
     static const constexpr char* SETTING_ROTATION_KEY {"screen_rotation_value"};
     static const constexpr char* SETTING_ROTATION_SCREEN_ID_KEY {"screen_rotation_screen_id_value"};
     static const constexpr char* SETTING_SCREEN_MODE_KEY {"user_set_screen_mode_edid"};
-    static const constexpr char* SETTING_SCREEN_SHARE_PROTECT_KEY {"spamshield_screenshare_protect"};
     static const constexpr char* SETTING_EXTEND_DPI_KEY {"user_set_dpi_extend"};
     static sptr<SettingObserver> dpiObserver_;
     static sptr<SettingObserver> castObserver_;
     static sptr<SettingObserver> rotationObserver_;
-    static sptr<SettingObserver> screenSkipProtectedWindowObserver_;
     static sptr<SettingObserver> wireCastObserver_;
     static sptr<SettingObserver> extendScreenDpiObserver_;
 };
