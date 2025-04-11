5a90b7a8f0817eda120a7b559c4b53b04766392c
MirMort <chenjunliang26@h-partners.com>
2025-04-11 11:33:58 +0800

    add Signed-off-by: MirMort <chenjunliang26@h-partners.com>

--- window_scene/screen_session_manager/src/screen_session_manager.cpp
+++ window_scene/screen_session_manager/src/screen_session_manager.cpp
@@ -8852,9 +8852,9 @@ DMError ScreenSessionManager::SetScreenSkipProtectedWindow(const std::vector<Scr
                     TLOGE(WmsLogTag::DMS, "No corresponding rsId.");
                     continue;
                 }
-                TLOGI(WmsLogTag::DMS, "virtualScreenId:%{public}" PRIu64 "", sessionIt.first);
+                TLOGI(WmsLogTag::DMS, "virtualScreenId:%{public}" PRIu64 "",screenId);
                 HITRACE_METER_FMT(HITRACE_TAG_WINDOW_MANAGER,
-                    "SetCastScreenEnableSkipWindow(%" PRIu64")", sessionIt.first);
+                    "SetCastScreenEnableSkipWindow(%" PRIu64")", screenId);
                 rsInterface_.SetCastScreenEnableSkipWindow(rsScreenId, requiredSkipWindow);
             }
         }
