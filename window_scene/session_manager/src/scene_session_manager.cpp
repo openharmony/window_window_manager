71698481e0bd2ac8ea55e67653318db3e86f217d
wangzilin <wangzilin17@huawei.com>
2025-04-16 17:23:56 +0800

    fix bug
    Signed-off-by: wangzilin <wangzilin17@huawei.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -10390,7 +10390,7 @@ WSError SceneSessionManager::NotifyStatusBarShowStatus(int32_t persistentId, boo
     return WSError::WS_OK;
 }
 
-WSError SceneSessionManager::NotifyStatusBarConstantlyShow(uint64_t screenId, bool isVisible)
+void SceneSessionManager::NotifyStatusBarConstantlyShow(uint64_t screenId, bool isVisible)
 {
     TLOGD(WmsLogTag::WMS_IMMS, "screenId %{public}" PRIu64 " isVisible %{public}u", screenId, isVisible);
     const char* const where = __func__;
