998d4507217ad893c91bbbd585ff82297d541022
xxx <xiaozhiwen7@h-partners.com>
2025-04-21 14:23:55 +0000

    update wm/src/window_session_impl.cpp.
    Signed-off-by: xxx <xiaozhiwen7@h-partners.com>

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -2795,7 +2795,7 @@ WMError WindowSessionImpl::SetDecorButtonStyle(const DecorButtonStyle& decorButt
     decorStr.emplace("closeButtonRightMargin", decorButtonStyle.closeButtonRightMargin);
     decorStr.emplace("colorMode", decorButtonStyle.colorMode);
     decorStr.emplace("spacingBetweenButtons", decorButtonStyle.spacingBetweenButtons);
-    uiContent->onContainerModalEvent("decor_button_style_change", decorStr.dump());
+    uiContent->OnContainerModalEvent("decor_button_style_change", decorStr.dump());
     decorButtonStyle_ = decorButtonStyle;
     return WMError::WM_OK;
 }
