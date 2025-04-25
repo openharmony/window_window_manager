cb5ef0d2f08a69b6feb4c0446385256066e804d5
l30067243 <leipeng42@h-partners.com>
2025-04-25 15:08:55 +0800

    fix uint64 value
    Signed-off-by: l30067243 <leipeng42@h-partners.com>

--- interfaces/innerkits/wm/wm_common.h
+++ interfaces/innerkits/wm/wm_common.h
@@ -1693,8 +1693,8 @@ struct WindowMetaInfo : public Parcelable {
     int32_t pid = -1;
     WindowType windowType = WindowType::WINDOW_TYPE_APP_MAIN_WINDOW;
     int32_t parentWindowId = -1;
-    uint64_t surfaceNodeId = -1;
-    uint64_t leashWinSurfaceNodeId = -1;
+    uint64_t surfaceNodeId = 0;
+    uint64_t leashWinSurfaceNodeId = 0;
     bool isPrivacyMode = false;
 
     bool Marshalling(Parcel& parcel) const override
