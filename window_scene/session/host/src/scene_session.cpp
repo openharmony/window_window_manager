493b8836391f55375b19194ba38020f18f42282e
giteeOrange <zhuying79@huawei.com>
2025-04-26 16:29:31 +0800

    fix callback is null.
    Signed-off-by: giteeOrange <zhuying79@huawei.com>

--- window_scene/session/host/src/scene_session.cpp
+++ window_scene/session/host/src/scene_session.cpp
@@ -1186,8 +1186,7 @@ void SceneSession::RegisterFollowScreenChangeCallback(NotifyFollowScreenChangeFu
             return;
         }
         session->specificCallback_->onUpdateFollowScreenChange_ = std::move(callback);
-        auto property = session->GetSessionProperty();
-        session->specificCallback_->onUpdateFollowScreenChange_(property->GetFollowScreenChange());
+        session->specificCallback_->onUpdateFollowScreenChange_(session->GetFollowScreenChange());
     }, __func__);
 }
 
@@ -4490,6 +4489,16 @@ bool SceneSession::IsNeedDefaultAnimation() const
     return needDefaultAnimationFlag_;
 }
 
+bool SceneSession::GetFollowScreenChange() const
+{
+    return isFollowScreenChange_;
+}
+
+void SceneSession::SetFollowScreenChange(bool isFollowScreenChange)
+{
+    isFollowScreenChange_ = isFollowScreenChange;
+}
+
 bool SceneSession::IsAppSession() const
 {
     if (GetWindowType() == WindowType::WINDOW_TYPE_APP_MAIN_WINDOW) {
@@ -5737,6 +5746,7 @@ WMError SceneSession::HandleActionUpdateFollowScreenChange(const sptr<WindowSess
 
 WSError SceneSession::UpdateFollowScreenChange(bool isFollowScreenChange)
 {
+    SetFollowScreenChange(isFollowScreenChange);
     auto task = [weakThis = wptr(this), isFollowScreenChange] {
         auto session = weakThis.promote();
         if (!session || !session->specificCallback_) {
