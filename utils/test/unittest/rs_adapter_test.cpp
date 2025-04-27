ac80cc95e43a0c1aa1814064c2aba09a29786d27
liuanguang <liuanguang@huawei.com>
2025-04-27 23:32:47 +0800

    test: add rs adpter tests
    Signed-off-by: liuanguang <liuanguang@huawei.com>
    Change-Id: Ib52a5b1d79b719d21f2f9085116acd639ca62f24

---
+++ utils/test/unittest/rs_adapter_test.cpp
@@ -0,0 +1,326 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rs_adapter.h"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "hilog/log.h"
+#include "ui/rs_surface_node.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Rosen {
+namespace {
+std::string logMsg;
+
+void TestLogCallback(const LogType type, const LogLevel level, const unsigned int domain, const char* tag,
+                     const char* msg)
+{
+    logMsg += msg;
+}
+}
+
+class RSAdapterTest : public Test {
+public:
+    static void SetUpTestCase()
+    {
+        LOG_SetCallback(TestLogCallback);
+
+        rsUIDirector_ = RSUIDirector::Create();
+        rsUIDirector_->Init(true, true);
+        rsUIContext_ = rsUIDirector_->GetRSUIContext();
+        struct RSSurfaceNodeConfig config;
+        rsNode_ = RSSurfaceNode::Create(config, true, rsUIContext_);
+    }
+
+    static void TearDownTestCase()
+    {
+        LOG_SetCallback(nullptr);
+    }
+
+    void SetUp() override
+    {
+        logMsg.clear();
+    }
+
+    void TearDown() override
+    {
+        logMsg.clear();
+    }
+
+private:
+    static std::shared_ptr<RSNode> rsNode_;
+    static std::shared_ptr<RSUIContext> rsUIContext_;
+    static std::shared_ptr<RSUIDirector> rsUIDirector_;
+};
+
+std::shared_ptr<RSNode> RSAdapterTest::rsNode_ = nullptr;
+std::shared_ptr<RSUIContext> RSAdapterTest::rsUIContext_ = nullptr;
+std::shared_ptr<RSUIDirector> RSAdapterTest::rsUIDirector_ = nullptr;
+
+/**
+ * @tc.name: RSTransactionAdapterConstructor
+ * @tc.desc: Verify RSTransactionAdapter constructed with RSUIContext.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSTransactionAdapterConstructor, Function | SmallTest | Level1)
+{
+    RSTransactionAdapter adapter(rsUIContext_);
+    if (rsUIContext_) {
+        EXPECT_NE(adapter.rsTransHandler_, nullptr);
+        EXPECT_EQ(adapter.rsTransProxy_, nullptr);
+    } else {
+        EXPECT_EQ(adapter.rsTransHandler_, nullptr);
+        EXPECT_NE(adapter.rsTransProxy_, nullptr);
+    }
+}
+
+/**
+ * @tc.name: RSTransactionAdapterBegin
+ * @tc.desc: Verify RSTransactionAdapter::Begin works.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSTransactionAdapterBegin, Function | SmallTest | Level1)
+{
+    RSTransactionAdapter adapter(rsUIContext_);
+    adapter.Begin();
+    if (rsUIContext_) {
+        EXPECT_EQ(RSTransactionAdapter::invokerType_, InvokerType::RS_TRANSACTION_HANDLER);
+    } else {
+        EXPECT_EQ(RSTransactionAdapter::invokerType_, InvokerType::RS_TRANSACTION_PROXY);
+    }
+}
+
+/**
+ * @tc.name: RSTransactionAdapterCommit
+ * @tc.desc: Verify RSTransactionAdapter::Commit works.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSTransactionAdapterCommit, Function | SmallTest | Level1)
+{
+    RSTransactionAdapter adapter(rsUIContext_);
+    adapter.Commit();
+    if (rsUIContext_) {
+        EXPECT_EQ(RSTransactionAdapter::invokerType_, InvokerType::RS_TRANSACTION_HANDLER);
+    } else {
+        EXPECT_EQ(RSTransactionAdapter::invokerType_, InvokerType::RS_TRANSACTION_PROXY);
+    }
+}
+
+/**
+ * @tc.name: RSTransactionAdapterFlushImplicitTransactionMember
+ * @tc.desc: Verify RSTransactionAdapter::FlushImplicitTransaction (member function).
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSTransactionAdapterFlushImplicitTransactionMember, Function | SmallTest | Level1)
+{
+    RSTransactionAdapter adapter(rsUIContext_);
+    adapter.FlushImplicitTransaction();
+    if (rsUIContext_) {
+        EXPECT_EQ(RSTransactionAdapter::invokerType_, InvokerType::RS_TRANSACTION_HANDLER);
+    } else {
+        EXPECT_EQ(RSTransactionAdapter::invokerType_, InvokerType::RS_TRANSACTION_PROXY);
+    }
+}
+
+/**
+ * @tc.name: RSTransactionAdapterFlushImplicitTransactionStatic
+ * @tc.desc: Verify static FlushImplicitTransaction with RSUIContext.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSTransactionAdapterFlushImplicitTransactionStatic, Function | SmallTest | Level1)
+{
+    RSTransactionAdapter::FlushImplicitTransaction(rsUIContext_);
+    if (rsUIContext_) {
+        EXPECT_EQ(RSTransactionAdapter::invokerType_, InvokerType::RS_TRANSACTION_HANDLER);
+    } else {
+        EXPECT_EQ(RSTransactionAdapter::invokerType_, InvokerType::RS_TRANSACTION_PROXY);
+    }
+}
+
+/**
+ * @tc.name: AutoRSTransactionLifecycle
+ * @tc.desc: Verify AutoRSTransaction lifecycle (Begin + Commit).
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, AutoRSTransactionLifecycle, Function | SmallTest | Level1)
+{
+    {
+        AutoRSTransaction autoTransaction(rsNode_, true);
+        EXPECT_NE(logMsg.find("Begin transaction"), std::string::npos);
+    }
+    EXPECT_NE(logMsg.find("Commit transaction"), std::string::npos);
+}
+
+/**
+ * @tc.name: RSSyncTransactionAdapterConstructor
+ * @tc.desc: Verify RSSyncTransactionAdapter constructed with RSUIContext.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSSyncTransactionAdapterConstructor, Function | SmallTest | Level1)
+{
+    RSSyncTransactionAdapter adapter(rsUIContext_);
+    if (rsUIContext_) {
+        EXPECT_NE(adapter.rsSyncTransHandler_, nullptr);
+        EXPECT_EQ(adapter.rsSyncTransController_, nullptr);
+    } else {
+        EXPECT_EQ(adapter.rsSyncTransHandler_, nullptr);
+        EXPECT_NE(adapter.rsSyncTransController_, nullptr);
+    }
+}
+
+/**
+ * @tc.name: RSSyncTransactionAdapterGetRSTransaction
+ * @tc.desc: Verify RSSyncTransactionAdapter::GetRSTransaction.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSSyncTransactionAdapterGetRSTransaction, Function | SmallTest | Level1)
+{
+    RSSyncTransactionAdapter adapter(rsUIContext_);
+    adapter.GetRSTransaction();
+    if (rsUIContext_) {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_HANDLER);
+    } else {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_CONTROLLER);
+    }
+}
+
+/**
+ * @tc.name: RSSyncTransactionAdapterOpenTransaction
+ * @tc.desc: Verify RSSyncTransactionAdapter::OpenSyncTransaction.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSSyncTransactionAdapterOpenTransaction, Function | SmallTest | Level1)
+{
+    RSSyncTransactionAdapter adapter(rsUIContext_);
+    adapter.OpenSyncTransaction();
+    if (rsUIContext_) {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_HANDLER);
+    } else {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_CONTROLLER);
+    }
+}
+
+/**
+ * @tc.name: RSSyncTransactionAdapterCloseTransaction
+ * @tc.desc: Verify RSSyncTransactionAdapter::CloseSyncTransaction.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSSyncTransactionAdapterCloseTransaction, Function | SmallTest | Level1)
+{
+    RSSyncTransactionAdapter adapter(rsUIContext_);
+    adapter.CloseSyncTransaction();
+    if (rsUIContext_) {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_HANDLER);
+    } else {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_CONTROLLER);
+    }
+}
+
+/**
+ * @tc.name: RSSyncTransactionAdapterStaticGetRSTransaction
+ * @tc.desc: Verify static GetRSTransaction with RSUIContext.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSSyncTransactionAdapterStaticGetRSTransaction, Function | SmallTest | Level1)
+{
+    RSSyncTransactionAdapter::GetRSTransaction(rsUIContext_);
+    if (rsUIContext_) {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_HANDLER);
+    } else {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_CONTROLLER);
+    }
+}
+
+/**
+ * @tc.name: RSSyncTransactionAdapterStaticOpenSyncTransaction
+ * @tc.desc: Verify static OpenSyncTransaction with RSUIContext.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSSyncTransactionAdapterStaticOpenSyncTransaction, Function | SmallTest | Level1)
+{
+    RSSyncTransactionAdapter::OpenSyncTransaction(rsUIContext_);
+    if (rsUIContext_) {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_HANDLER);
+    } else {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_CONTROLLER);
+    }
+}
+
+/**
+ * @tc.name: RSSyncTransactionAdapterStaticCloseSyncTransaction
+ * @tc.desc: Verify static CloseSyncTransaction with RSUIContext.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSSyncTransactionAdapterStaticCloseSyncTransaction, Function | SmallTest | Level1)
+{
+    RSSyncTransactionAdapter::CloseSyncTransaction(rsUIContext_);
+    if (rsUIContext_) {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_HANDLER);
+    } else {
+        EXPECT_EQ(RSSyncTransactionAdapter::invokerType_, InvokerType::RS_SYNC_TRANSACTION_CONTROLLER);
+    }
+}
+
+/**
+ * @tc.name: AutoRSSyncTransactionLifecycle
+ * @tc.desc: Verify AutoRSSyncTransaction lifecycle (Flush + Open + Close).
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, AutoRSSyncTransactionLifecycle, Function | SmallTest | Level1)
+{
+    {
+        AutoRSSyncTransaction autoSyncTransaction(rsNode_);
+        EXPECT_NE(logMsg.find("Open sync transaction"), std::string::npos);
+    }
+    EXPECT_NE(logMsg.find("Close sync transaction"), std::string::npos);
+}
+
+/**
+ * @tc.name: AllowRSMultiInstanceLifecycle
+ * @tc.desc: Verify AllowRSMultiInstance lifecycle (skip check toggle).
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, AllowRSMultiInstanceLifecycle, Function | SmallTest | Level1)
+{
+    {
+        AllowRSMultiInstance allowMulti(rsNode_);
+        EXPECT_NE(logMsg.find("Skip check in RS multi-instance"), std::string::npos);
+    }
+    EXPECT_NE(logMsg.find("Reopen check in RS multi-instance"), std::string::npos);
+}
+
+/**
+ * @tc.name: RSAdapterUtilToStringMethods
+ * @tc.desc: Verify RSAdapterUtil ToString methods.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSAdapterTest, RSAdapterUtilToStringMethods, Function | SmallTest | Level1)
+{
+    auto nodeStr = RSAdapterUtil::RSNodeToStr(rsNode_);
+    EXPECT_THAT(nodeStr, HasSubstr("RSNode"));
+
+    auto ctxStr = RSAdapterUtil::RSUIContextToStr(rsUIContext_);
+    EXPECT_THAT(ctxStr, HasSubstr("RSUIContext"));
+
+    auto directorStr = RSAdapterUtil::RSUIDirectorToStr(rsUIDirector_);
+    EXPECT_THAT(directorStr, HasSubstr("RSUIDirector"));
+}
+} // namespace Rosen
+} // namespace OHOS
