2bcbff6b01c9155282738de48c654cd1f67b5a80
rsyys <renzhao5@h-partners.com>
2025-04-28 12:12:35 +0800

    fix: parseZIndex
    Signed-off-by: rsyys <renzhao5@h-partners.com>

--- interfaces/kits/napi/window_runtime/window_manager_napi/js_window_manager.cpp
+++ interfaces/kits/napi/window_runtime/window_manager_napi/js_window_manager.cpp
@@ -498,6 +498,9 @@ bool JsWindowManager::ParseConfigOption(napi_env env, napi_value jsObject,
     if (!isConfigOptionWindowTypeValid(env, option)) {
         return false;
     }
+    if (!ParseZIndex(env, jsObject, option)) {
+        return false;
+    }
     napi_value value = nullptr;
     napi_get_named_property(env, jsObject, "ctx", &value);
     if (GetType(env, value) == napi_undefined) {
@@ -533,10 +536,6 @@ bool JsWindowManager::ParseConfigOption(napi_env env, napi_value jsObject,
     int64_t parentId = -1;
     if (ParseJsValue(jsObject, env, "parentId", parentId)) {
         option.SetParentId(parentId);
-    }
-
-    if (!ParseZIndex(env, jsObject, option)) {
-        return false;
     }
 
     return true;
