35c23e62482e32b0197fd39aa7376bb8cc9c9e38
ZihaoWU <wuzihao11@huawei.com>
2025-05-06 13:05:58 +0800

    add code
    Signed-off-by: ZihaoWU <wuzihao11@huawei.com>

--- previewer/include/window.h
+++ previewer/include/window.h
@@ -402,6 +402,10 @@ public:
     {
         return WMError::WM_ERROR_DEVICE_NOT_SUPPORT;
     }
+    virtual WMError SetMainWindowContainerColor(const std::string& activeColor, const std::string& inactiveColor)
+    {
+        return WMError::WM_ERROR_DEVICE_NOT_SUPPORT;
+    }
     virtual WMError RegisterWindowTitleButtonRectChangeListener(
         const sptr<IWindowTitleButtonRectChangedListener>& listener)
     {
