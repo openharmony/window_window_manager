3109370cfdbe72f844bd95e4c5a94f638d4cdf3b
second_89ed <l2311057382@gmail.com>
2025-05-09 12:01:34 +0800

    fix a vkb typing problem during preview window
    Signed-off-by: second_89ed <l2311057382@gmail.com>

--- window_scene/session_manager/src/scene_session_dirty_manager.cpp
+++ window_scene/session_manager/src/scene_session_dirty_manager.cpp
@@ -40,6 +40,8 @@ constexpr float ZORDER_UIEXTENSION_INDEX = 0.1;
 constexpr int WINDOW_NAME_TYPE_UNKNOWN = 0;
 constexpr int WINDOW_NAME_TYPE_SCREENSHOT = 1;
 const std::string SCREENSHOT_WINDOW_NAME_PREFIX = "ScreenShotWindow";
+const std::string PREVIEW_WINDOW_NAME_PREFIX = "PreviewWindow";
+
 } // namespace
 
 static bool operator==(const MMI::Rect left, const MMI::Rect right)
@@ -744,8 +746,10 @@ std::pair<MMI::WindowInfo, std::shared_ptr<Media::PixelMap>> SceneSessionDirtyMa
     UpdateHotAreas(sceneSession, touchHotAreas, pointerHotAreas);
     int windowNameType = WINDOW_NAME_TYPE_UNKNOWN;
     std::string windowName = sceneSession->GetWindowNameAllType();
-    std::string prefix = SCREENSHOT_WINDOW_NAME_PREFIX;
-    if (windowName.size() >= prefix.size() && windowName.substr(0, prefix.size()) == prefix) {
+    std::string screenshotPrefix = SCREENSHOT_WINDOW_NAME_PREFIX;
+    std::string previewPrefix = PREVIEW_WINDOW_NAME_PREFIX;
+    if ((windowName.size() >= screenshotPrefix.size() && windowName.substr(0, screenshotPrefix.size()) == screenshotPrefix)||
+        (windowName.size() >= previewPrefix.size() && windowName.substr(0, previewPrefix.size()) == previewPrefix)) {
         windowNameType = WINDOW_NAME_TYPE_SCREENSHOT;
     }
     auto pixelMap = windowSessionProperty->GetWindowMask();
