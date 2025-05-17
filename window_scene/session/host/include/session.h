b455edd2cc83998255342c98072451598c9a143b
卢宇豪 <luyuhao6@huawei.com>
2025-05-18 03:52:45 +0800

    change1
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- window_scene/session/host/include/session.h
+++ window_scene/session/host/include/session.h
@@ -672,6 +672,7 @@ public:
     WindowMetaInfo GetWindowMetaInfoForWindowInfo() const;
     uint32_t GetPropertyDirtyFlags() const { return propertyDirtyFlags_; };
     void SetPropertyDirtyFlags(uint32_t dirtyFlags) { propertyDirtyFlags_ = dirtyFlags; }
+    void AddPropertyDirtyFlags(SessionPropertyFlag dirtyFlag) { propertyDirtyFlags_ |= dirtyFlags; }
 
     /*
      * Window Pattern
