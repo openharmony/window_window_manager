ee1d7f9f9589ff07c95304d076a061889594ac30
wangyaohui <wangyaohui17@huawei.com>
2025-05-17 12:44:40 +0800

    [fix] 改为static函数
    Signed-off-by: wangyaohui <wangyaohui17@huawei.com>

--- wm/include/picture_in_picture_controller_base.h
+++ wm/include/picture_in_picture_controller_base.h
@@ -75,7 +75,6 @@ public:
     void PipSizeChange(double width, double height, double scale);
     void PreRestorePictureInPicture();
     void GetPipPossible(bool& pipPossible);
-    void GetPipEnabled(bool& isPipEnabled);
     WMError RegisterPiPLifecycle(const sptr<IPiPLifeCycle>& listener);
     WMError RegisterPiPActionObserver(const sptr<IPiPActionObserver>& listener);
     WMError RegisterPiPControlObserver(const sptr<IPiPControlObserver>& listener);
@@ -100,6 +99,7 @@ public:
     PiPWindowState GetControllerState() const;
     void OnPictureInPictureStart();
     void SetSurfaceId(std::string surfaceId);
+    static bool GetPipEnabled();
     std::string GetSurfaceId() const;
     bool isWeb_ = false;
 
