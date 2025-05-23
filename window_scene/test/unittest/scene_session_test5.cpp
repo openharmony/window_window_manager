670bc15549d2f5a51148e2e9788e69cf98075a6f
卢宇豪 <luyuhao6@huawei.com>
2025-05-23 14:56:16 +0800

    change1
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- window_scene/test/unittest/scene_session_test5.cpp
+++ window_scene/test/unittest/scene_session_test5.cpp
@@ -1218,7 +1218,7 @@ HWTEST_F(SceneSessionTest5, HandleActionUpdateSetBrightness01, Function | SmallT
     WSPropertyChangeAction action = WSPropertyChangeAction::ACTION_UPDATE_SET_BRIGHTNESS;
     auto res = session->HandleActionUpdateSetBrightness(property, session, action);
     EXPECT_EQ(WMError::WM_OK, res);
-} 
+}
 
 /**
  * @tc.name: HandleActionUpdateMaximizeState
