c6656e2317626c6e4b15d5762571d221bf1d7c4d
ZihaoWU <wuzihao11@huawei.com>
2025-05-26 17:55:35 +0800

    add code
    Signed-off-by: ZihaoWU <wuzihao11@huawei.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window_utils.h
+++ interfaces/kits/napi/window_runtime/window_napi/js_window_utils.h
@@ -388,6 +388,7 @@ public:
     napi_value ModalityTypeInit(napi_env env);
     napi_value CreateJsDecorButtonStyleObj(napi_env env, DecorButtonStyle decorButtonStyle);
     napi_value ConvertWindowDensityInfoToJsValue(napi_env env, const WindowDensityInfo& windowDensityInfo);
+    napi_value ConvertScreenshotEventTypeToJsValue(napi_env env, ScreenshotEventType type);
     bool ConvertDecorButtonStyleFromJs(napi_env env, napi_value jsObject, DecorButtonStyle& decorButtonStyle);
     bool GetAPI7Ability(napi_env env, AppExecFwk::Ability* &ability);
     bool GetWindowMaskFromJsValue(napi_env env, napi_value jsObject, std::vector<std::vector<uint32_t>>& windowMask);
