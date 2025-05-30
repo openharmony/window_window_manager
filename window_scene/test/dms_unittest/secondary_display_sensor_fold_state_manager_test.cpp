f4ed697f865d66a60ac0a7bc4ca542008237fa3f
wangjiaqi <wangjiaqi91@huawei.com>
2025-05-30 16:57:22 +0800

    屏幕闪烁问题修复
    Signed-off-by: wangjiaqi <wangjiaqi91@huawei.com>

--- window_scene/test/dms_unittest/secondary_display_sensor_fold_state_manager_test.cpp
+++ window_scene/test/dms_unittest/secondary_display_sensor_fold_state_manager_test.cpp
@@ -32,7 +32,7 @@ namespace {
     void MyLogCallback(const LogType type, const LogLevel level, const unsigned int domain, const char *tag,
         const char *msg)
     {
-    g_errLog = msg;
+        g_errLog = msg;
     }
 }
 class SecondaryDisplaySensorFoldStateManagerTest : public testing::Test {
