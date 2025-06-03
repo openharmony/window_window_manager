905b4c5e89c248347c56960be2de4941fa99085e
ZihaoWU <wuzihao11@huawei.com>
2025-06-03 18:18:58 +0800

    add code
    Signed-off-by: ZihaoWU <wuzihao11@huawei.com>

--- wm/test/unittest/window_stub_test.cpp
+++ wm/test/unittest/window_stub_test.cpp
@@ -615,7 +615,13 @@ HWTEST_F(WindowStubTest, OnRemoteRequest_TRANS_ID_NOTIFY_SCREEN_SHOT, TestSize.L
 
     uint32_t code = static_cast<uint32_t>(IWindow::WindowMessage::TRANS_ID_NOTIFY_SCREEN_SHOT_APP_EVENT);
     int res = windowStub_->OnRemoteRequest(code, data, reply, option);
-    EXPECT_EQ(res, 0);
+    EXPECT_EQ(res, ERR_INVALID_DATA);
+
+    data.WriteInt32(0);
+    data.WriteInterfaceToken(WindowStub::GetDescriptor());
+    code = static_cast<uint32_t>(IWindow::WindowMessage::TRANS_ID_NOTIFY_SCREEN_SHOT_APP_EVENT);
+    res = windowStub_->OnRemoteRequest(code, data, reply, option);
+    EXPECT_EQ(res, ERR_TRANSACTION_FAILED);
 }
 
 /**
