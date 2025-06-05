330759cca95f44004fa8beeced928a9ee392dc30
赵聪 <zhaocong33@h-partners.com>
2025-06-05 21:38:35 +0800

    格式调整
    Signed-off-by: 赵聪 <zhaocong33@h-partners.com>

--- window_scene/test/unittest/scene_session_manager_test12.cpp
+++ window_scene/test/unittest/scene_session_manager_test12.cpp
@@ -591,7 +591,7 @@ HWTEST_F(SceneSessionManagerTest12, CreateAndConnectSpecificSession05, TestSize.
     parentSession->UpdateExtWindowFlags(parentSession->GetPersistentId(), extWindowFlags, extWindowFlags);
     parentSession->SetSessionState(SessionState::STATE_FOREGROUND);
     ssm_->sceneSessionMap_.insert( {parentSession->GetPersistentId(), parentSession} );
-    res = ssm_->CreateAndConnectSpecificSession(
+    WSError res = ssm_->CreateAndConnectSpecificSession(
         sessionStage, eventChannel, node, property, id, session, systemConfig, token);
     ASSERT_EQ(WSError::WS_ERROR_INVALID_OPERATION, res);
 
