44d6dfcde16e0b18799229441a3e0384c0032914
l30067243 <leipeng42@h-partners.com>
2025-06-05 17:26:47 +0800

    add mock
    Signed-off-by: l30067243 <leipeng42@h-partners.com>

--- window_scene/test/mock/mock_session_stage.h
+++ window_scene/test/mock/mock_session_stage.h
@@ -47,6 +47,7 @@ public:
     MOCK_METHOD0(NotifyScreenshot, void(void));
     MOCK_METHOD0(NotifyTouchOutside, WSError(void));
     MOCK_METHOD1(UpdateWindowMode, WSError(WindowMode mode));
+    MOCK_METHOD1(GetTopNavDestinationName, WSError(std::string& topNavDestName));
     MOCK_METHOD1(NotifyLayoutFinishAfterWindowModeChange, WSError(WindowMode mode));
     MOCK_METHOD1(NotifyForegroundInteractiveStatus, void(bool interactive));
     MOCK_METHOD1(UpdateMaximizeMode, WSError(MaximizeMode mode));
