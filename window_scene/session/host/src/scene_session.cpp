c6bf0ff86726b2887e577f47a2c2bbfac5d5aa63
DingYi <dingyi54@huawei.com>
2025-06-07 20:33:59 +0800

    bugfix: 限制多设备预览器虚拟屏切换
    Signed-off-by: DingYi <dingyi54@huawei.com>

--- window_scene/session/host/src/scene_session.cpp
+++ window_scene/session/host/src/scene_session.cpp
@@ -4030,6 +4030,10 @@ void SceneSession::HandleMoveDragSurfaceNode(SizeChangeReason reason)
     }
     const auto startDisplayId = moveDragController_->GetMoveDragStartDisplayId();
     auto startScreenSession = ScreenSessionManagerClient::GetInstance().GetScreenSessionById(startDisplayId);
+    if (startScreenSession == nullptr) {
+        TLOGD(WmsLogTag::WMS_LAYOUT, "startScreenSession is null, startDisplayId: %{public}" PRIu64, startDisplayId);
+        return;
+    }
     if (reason == SizeChangeReason::DRAG || reason == SizeChangeReason::DRAG_MOVE) {
         AutoRSTransaction trans(GetRSUIContext());
         for (const auto displayId : moveDragController_->GetNewAddedDisplayIdsDuringMoveDrag()) {
@@ -4047,7 +4051,8 @@ void SceneSession::HandleMoveDragSurfaceNode(SizeChangeReason reason)
                 || screenSession->GetSourceMode() == ScreenSourceMode::SCREEN_EXTEND;
             // Not main to extend or extend to main or extend to extend, no need to add cross parent child
             if (!(isStartScreenMainOrExtend && isDestScreenMainOrExtend)) {
-                TLOGD(WmsLogTag::WMS_LAYOUT, "No need to add cross-parent child elements for out-of-scope situations");
+                TLOGD(WmsLogTag::WMS_LAYOUT, "No need to add cross-parent child elements for out-of-scope situations,
+                    DisplayId: %{public}"  PRIu64, displayId);
                 continue;
             }
             movedSurfaceNode->SetPositionZ(MOVE_DRAG_POSITION_Z);
@@ -4071,7 +4076,8 @@ void SceneSession::HandleMoveDragSurfaceNode(SizeChangeReason reason)
                 || screenSession->GetSourceMode() == ScreenSourceMode::SCREEN_EXTEND;
             // Not main to extend or extend to main or extend to extend, no need to add cross parent child
             if (!(isStartScreenMainOrExtend && isDestScreenMainOrExtend)) {
-                TLOGD(WmsLogTag::WMS_LAYOUT, "No need to add cross-parent child elements for out-of-scope situations");
+                TLOGD(WmsLogTag::WMS_LAYOUT, "No need to add cross-parent child elements for out-of-scope situations,
+                    DisplayId: %{public}"  PRIu64, displayId);
                 continue;
             }
             movedSurfaceNode->SetPositionZ(moveDragController_->GetOriginalPositionZ());
