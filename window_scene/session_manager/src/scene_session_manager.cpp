a6367851908d088c838188de4aac28f27fc4add6
崔洪斌 <cuihongbin@huawei.com>
2025-06-14 10:24:33 +0800

    新增NotifySessionTouchOutside用例
    Signed-off-by: 崔洪斌 <cuihongbin@huawei.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -4439,7 +4439,7 @@ bool SceneSessionManager::IsSameDisplayGroupId(const sptr<SceneSession>& session
 {
     DisplayId displayGroupId = 0;
     if (GetDisplayGroupIdFromSceneSession(session, displayGroupId) != WSError::WS_OK) {
-        TLOGD(WmsLogTag::WMS_EVENT, "session is null");
+        TLOGD(WmsLogTag::WMS_EVENT, "session is null.");
         return false;
     }
     return displayGroupId == touchDisplayGroupId;
