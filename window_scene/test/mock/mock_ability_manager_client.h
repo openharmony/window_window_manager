5e8b6268a4f8eb05b03518f3f516154d4b968ba1
赵聪 <zhaocong33@h-partners.com>
2025-06-17 19:34:30 +0800

    uiextension创建子窗管控修改
    Signed-off-by: 赵聪 <zhaocong33@h-partners.com>

---
+++ window_scene/test/mock/mock_ability_manager_client.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MOCK_ABILITY_RUNTIME_ABILITY_MANAGER_CLIENT_H
+#define MOCK_ABILITY_RUNTIME_ABILITY_MANAGER_CLIENT_H
+
+#include <ability_manager_client.h>
+
+namespace OHOS {
+namespace AAFwk {
+class MockAbilityManagerClient {
+public:
+    static void ClearAll();
+    static void SetUIExtensionRootHostInfo(const UIExtensionHostInfo& hostInfo);
+    static void SetUIExtensionSessionInfo(const UIExtensionSessionInfo& sessionInfo);
+};
+}  // namespace AAFwk
+}  // namespace OHOS
+#endif MOCK_ABILITY_RUNTIME_ABILITY_MANAGER_CLIENT_H
