f70063e0d2a16c3ce2882800843526286b9e976a
fox <tanhong6@h-partners.com>
2025-06-20 12:04:57 +0800

    ii
    Signed-off-by: fox <tanhong6@h-partners.com>

--- window_scene/test/unittest/scene_session_test.cpp
+++ window_scene/test/unittest/scene_session_test.cpp
@@ -873,14 +873,14 @@ HWTEST_F(SceneSessionTest, SetSystemBarProperty03, TestSize.Level0)
     sceneSession->onSystemBarPropertyChange_ =[](
         const std::unordered_map<WindowType, SystemBarProperty>& propertyMap) {};
     SystemBarProperty statusBarProperty;
-    ASSERT_EQ(sceneSession->SetSystemBarProperty(WindowType::WINDOW_TYPE_STATUS_BAR, statusBarProperty),
+    EXPECT_EQ(sceneSession->SetSystemBarProperty(WindowType::WINDOW_TYPE_STATUS_BAR, statusBarProperty),
         WSError::WS_OK);
     auto propMap = sceneSession->property_->GetSystemBarProperty();
-    ASSERT_EQ(statusBarProperty, propMap[WindowType::WINDOW_TYPE_STATUS_BAR]);
-    ASSERT_EQ(sceneSession->SetSystemBarProperty(WindowType::WINDOW_TYPE_NAVIGATION_INDICATOR, statusBarProperty),
+    EXPECT_EQ(statusBarProperty, propMap[WindowType::WINDOW_TYPE_STATUS_BAR]);
+    EXPECT_EQ(sceneSession->SetSystemBarProperty(WindowType::WINDOW_TYPE_NAVIGATION_INDICATOR, statusBarProperty),
         WSError::WS_OK);
     sceneSession->specificCallback_->onNotifyWindowSystemBarPropertyChangeFunc_ = nullptr;
-    ASSERT_EQ(sceneSession->SetSystemBarProperty(WindowType::WINDOW_TYPE_STATUS_BAR, statusBarProperty),
+    EXPECT_EQ(sceneSession->SetSystemBarProperty(WindowType::WINDOW_TYPE_STATUS_BAR, statusBarProperty),
         WSError::WS_OK);
 }
 
