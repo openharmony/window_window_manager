7784f3a7d74c141ddd2a72aee2a1ea90e2cd501b
Hayden Lee <lihanyuan@huawei.com>
2025-06-23 16:47:42 +0800

    【检视意见修改2】解注册入口及成功日志扩展agentType信息
    Signed-off-by: Hayden Lee <lihanyuan@huawei.com>

--- wm/src/window_adapter.cpp
+++ wm/src/window_adapter.cpp
@@ -142,7 +142,7 @@ WMError WindowAdapter::RegisterWindowManagerAgent(WindowManagerAgentType type,
 WMError WindowAdapter::UnregisterWindowManagerAgent(WindowManagerAgentType type,
     const sptr<IWindowManagerAgent>& windowManagerAgent)
 {
-    TLOGI(WmsLogTag::DEFAULT, "called");
+    TLOGI(WmsLogTag::DEFAULT, "called, WindowManagerAgentType: ", type);
     INIT_PROXY_CHECK_RETURN(WMError::WM_ERROR_SAMGR);
 
     auto wmsProxy = GetWindowManagerServiceProxy();
@@ -162,7 +162,7 @@ WMError WindowAdapter::UnregisterWindowManagerAgent(WindowManagerAgentType type,
         return ret;
     }
     agentSet.erase(agent);
-    TLOGI(WmsLogTag::DEFAULT, "success");
+    TLOGI(WmsLogTag::DEFAULT, "success, WindowManagerAgentType: ", type);
 
     return ret;
 }
