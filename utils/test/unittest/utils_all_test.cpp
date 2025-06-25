d2c217aa73db7892e6f0831660d08295fd841b3d
罗堡文 <14489174+robber-script@user.noreply.gitee.com>
2025-06-25 16:16:46 +0800

    fix
    Signed-off-by: 罗堡文 <luobaowen@huawei.com>

--- utils/test/unittest/utils_all_test.cpp
+++ utils/test/unittest/utils_all_test.cpp
@@ -19,13 +19,13 @@
 #include <iremote_object.h>
 
 #include "agent_death_recipient.h"
-#include "iremote_object_mocker.h"
-#include "wm_common.h"
 #include "color_parser.h"
+#include "iremote_object_mocker.h"
 #include "perform_reporter.h"
 #include "singleton_container.h"
 #include "surface_reader_handler_impl.h"
 #include "sys_cap_util.h"
+#include "wm_common.h"
 
 using namespace testing;
 using namespace testing::ext;
@@ -215,22 +215,22 @@ HWTEST_F(UtilsAllTest, SysCapUtilGetClientName, TestSize.Level1)
 }
 
 /**
- * @tc.name: FindCodeByError
- * @tc.desc: test FindCodeByError
+ * @tc.name: ConvertErrorToCode
+ * @tc.desc: test ConvertErrorToCode
  * @tc.type: FUNC
  */
-HWTEST_F(UtilsAllTest, FindCodeByError, TestSize.Level1)
+HWTEST_F(UtilsAllTest, ConvertErrorToCode, TestSize.Level1)
 {
-    EXPECT_EQ(FindCodeByError(WMError::WM_OK), WmErrorCode::WM_OK);
-    EXPECT_EQ(FindCodeByError(WMError::WM_ERROR_FB_RESTORE_MAIN_WINDOW_FAILED),
+    EXPECT_EQ(ConvertErrorToCode(WMError::WM_OK), WmErrorCode::WM_OK);
+    EXPECT_EQ(ConvertErrorToCode(WMError::WM_ERROR_FB_RESTORE_MAIN_WINDOW_FAILED),
         WmErrorCode::WM_ERROR_FB_RESTORE_MAIN_WINDOW_FAILED);
     WMError error = static_cast<WMError>(static_cast<int>(WMError::WM_ERROR_FB_RESTORE_MAIN_WINDOW_FAILED) + 1);
-    EXPECT_EQ(FindCodeByError(error), WmErrorCode::WM_ERROR_SYSTEM_ABNORMALLY);
+    EXPECT_EQ(ConvertErrorToCode(error), WmErrorCode::WM_ERROR_SYSTEM_ABNORMALLY);
 }
 
 /**
- * @tc.name: FindCodeByError
- * @tc.desc: test FindCodeByError
+ * @tc.name: IsValidColorNoAlpha
+ * @tc.desc: test IsValidColorNoAlpha
  * @tc.type: FUNC
  */
 HWTEST_F(UtilsAllTest, IsValidColorNoAlpha, TestSize.Level1)
