ac21a005ef5102f74fbd332cda8cb62a43b7e507
fox <tanhong6@h-partners.com>
2025-06-25 07:25:15 +0000

    update wm/src/oh_window.cpp.
    Signed-off-by: fox <tanhong6@h-partners.com>

--- wm/src/oh_window.cpp
+++ wm/src/oh_window.cpp
@@ -48,7 +48,6 @@ inline bool IsMainWindow(WindowType type)
     return (type >= WindowType::APP_MAIN_WINDOW_BASE && type < WindowType::APP_MAIN_WINDOW_END);
 }
 
-
 inline bool IsMainWindowAndNotShown(WindowType type, WindowState state)
 {
     return (IsMainWindow(type) && state != WindowState::STATE_SHOWN);
