ccbd7a90d9dc4510c9b432042f60cfcd3a00aa1d
chengzhen <chengzhen25@huawei.com>
2025-06-26 14:47:34 +0800

    change review name
    Signed-off-by: chengzhen <chengzhen25@huawei.com>

--- window_scene/common/include/session_helper.h
+++ window_scene/common/include/session_helper.h
@@ -170,7 +170,6 @@ public:
 
     static AreaType GetAreaTypeForScaleResize(int32_t pointWinX, int32_t pointWinY, int outside, const WSRect& rect)
     {
-
         constexpr uint32_t HALF = 2;
         int32_t leftOut = -outside;
         int32_t rightOut = rect.width_ + outside;
