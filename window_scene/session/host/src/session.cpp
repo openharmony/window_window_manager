eafbf2dda581c93c50f67293ab4243648e0e31b3
tangrui <tangrui45@h-partners.com>
2025-06-26 14:10:18 +0800

    修改告警
    Signed-off-by: tangrui <tangrui45@h-partners.com>

--- window_scene/session/host/src/session.cpp
+++ window_scene/session/host/src/session.cpp
@@ -2597,19 +2597,18 @@ std::shared_ptr<Media::PixelMap> Session::Snapshot(bool runInFfrt, float scalePa
 
 uint32_t Session::GetBackgroundColor() const
 {
-    uint32_t backGroundColor = COLOR_WHITE;
     std::shared_ptr<AbilityRuntime::ApplicationContext> appContext = AbilityRuntime::Context::GetApplicationContext();
     if (appContext != nullptr) {
         std::shared_ptr<AppExecFwk::Configuration> appConfig = appContext->GetConfiguration();
         if (appConfig != nullptr) {
             std::string colorMode = appConfig->GetItem(AAFwk::GlobalConfigurationKey::SYSTEM_COLORMODE);
-            backGroundColor = (colorMode == AppExecFwk::ConfigurationInner::COLOR_MODE_DARK) ? COLOR_BLACK : COLOR_WHITE;
-            TLOGI(WmsLogTag::WMS_PATTERN, ":%{public}u", backGroundColor);
+            return (colorMode == AppExecFwk::ConfigurationInner::COLOR_MODE_DARK) ? COLOR_BLACK : COLOR_WHITE;
+            TLOGI(WmsLogTag::WMS_PATTERN, ":%{public}s", colorMode.c_str());
         }
     } else {
         TLOGE(WmsLogTag::WMS_PATTERN, "app context is nullptr, use default backGroundColor WHITE");
     }
-    return backGroundColor;
+    return COLOR_WHITE;
 }
 
 void Session::ResetSnapshot()
