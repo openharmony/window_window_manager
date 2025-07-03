0dcd29d3773cb332572f34006cc530b5907d20fb
陈树山 <css353929243@163.com>
2025-07-03 11:42:55 +0800

    修改返回值
    Signed-off-by: 陈树山 <css353929243@163.com>

--- window_scene/session/host/src/zidl/session_stub.cpp
+++ window_scene/session/host/src/zidl/session_stub.cpp
@@ -801,7 +801,7 @@ int SessionStub::HandlePendingSessionActivation(MessageParcel& data, MessageParc
     }
     if (!data.ReadInt32(abilitySessionInfo->scenarios)) {
         TLOGE(WmsLogTag::WMS_LIFE, "Read scenarios failed.");
-        return WSError::ERR_INVALID_DATA;
+        return ERR_INVALID_DATA;
     }
     abilitySessionInfo->windowCreateParams.reset(data.ReadParcelable<WindowCreateParams>());
     WSError errCode = PendingSessionActivation(abilitySessionInfo);
