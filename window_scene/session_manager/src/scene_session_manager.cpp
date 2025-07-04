18f2251ad82aaed292c9d263cf2e83d82965ee44
zhouyang <zhouyang273@huawei.com>
2025-07-04 16:14:46 +0800

    bugfix
    Signed-off-by: zhouyang <zhouyang273@huawei.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -14712,17 +14712,6 @@ WMError SceneSessionManager::MakeScreenFoldData(const std::vector<std::string>& 
         return WMError::WM_DO_NOTHING;
     }
     screenFoldData.SetFocusedPkgName(element.GetURI());
-    int32_t ret_z = HiSysEventWrite(
-        OHOS::HiviewDFX::HiSysEvent::Domain::WINDOW_MANAGER,
-        "FOCUS_WINDOW",
-        OHOS::HiviewDFX::HiSysEvent::EventType::BEHAVIOR,
-        "PID", getpid(),
-        "UID", getuid(),
-        "BUNDLE_NAME", sceneSession->GetSessionInfo().bundleName_,
-        "WINDOW_TYPE", static_cast<uint32_t>(sceneSession->GetWindowType()));
-    if (ret_z != 0) {
-        TLOGE(WmsLogTag::DMS, "Write FOCUS_WINDOW HiSysEvent error, ret_z: %{public}d.", ret_z);
-    }
     return WMError::WM_OK;
 }
 
