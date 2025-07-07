664888811ad1cec2a8bfb434eee3235947e5f2bf
崔洪斌 <cuihongbin@huawei.com>
2025-07-07 07:01:39 +0000

    update wm/include/window_input_channel.h.
    Signed-off-by: 崔洪斌 <cuihongbin@huawei.com>

--- wm/include/window_input_channel.h
+++ wm/include/window_input_channel.h
@@ -36,7 +36,7 @@ public:
 private:
     bool IsKeyboardEvent(const std::shared_ptr<MMI::KeyEvent>& keyEvent) const;
     void DispatchKeyEventCallback(std::shared_ptr<MMI::KeyEvent>& keyEvent, bool consumed);
-    void ProcAncoEvent(const std::shared_ptr<MMI::PointerEvent>& pointerEvent);
+    void ProcAncoEvent(std::shared_ptr<MMI::PointerEvent>& pointerEvent);
     std::mutex mtx_;
     sptr<Window> window_;
     bool isAvailable_;
