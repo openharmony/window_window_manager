6ce9daca936d8e1d1227226db2d94079a5b0d805
ZihaoWU <wuzihao11@huawei.com>
2025-07-14 11:02:41 +0800

    add code
    Signed-off-by: ZihaoWU <wuzihao11@huawei.com>

--- window_scene/test/unittest/session_proxy_test.cpp
+++ window_scene/test/unittest/session_proxy_test.cpp
@@ -1613,6 +1613,47 @@ HWTEST_F(SessionProxyTest, IsMainWindowFullScreenAcrossDisplays, Function | Smal
 }
 
 /**
+ * @tc.name: OnUpdateAbilityColorMode
+ * @tc.desc: OnUpdateAbilityColorMode test
+ * @tc.type: FUNC
+ */
+HWTEST_F(SessionProxyTest, OnUpdateAbilityColorMode, Function | SmallTest | Level2)
+{
+    std::string colorMode = "DARK";
+    bool hasDarkRes = true;
+    auto sProxy = sptr<SessionProxy>::MakeSptr(nullptr);
+    ASSERT_NE(sProxy, nullptr);
+    auto ret = sProxy->OnUpdateAbilityColorMode(colorMode, hasDarkRes);
+    EXPECT_EQ(ret, WMError::WM_ERROR_IPC_FAILED);
+
+    sptr<MockIRemoteObject> remoteMocker = sptr<MockIRemoteObject>::MakeSptr();
+    sProxy = sptr<SessionProxy>::MakeSptr(remoteMocker);
+    ASSERT_NE(sProxy, nullptr);
+    ret = sProxy->OnUpdateAbilityColorMode(colorMode, hasDarkRes);
+    EXPECT_EQ(ret, WMError::WM_OK);
+
+    remoteMocker->SetRequestResult(ERR_INVALID_DATA);
+    ret = sProxy->OnUpdateAbilityColorMode(colorMode, hasDarkRes);
+    EXPECT_EQ(ret, WMError::WM_ERROR_IPC_FAILED);
+    remoteMocker->SetRequestResult(ERR_NONE);
+
+    MockMessageParcel::SetWriteBoolErrorFlag(true);
+    ret = sProxy->OnUpdateAbilityColorMode(colorMode, hasDarkRes);
+    EXPECT_EQ(ret, WMError::WM_ERROR_IPC_FAILED);
+
+    MockMessageParcel::SetWriteStringErrorFlag(true);
+    ret = sProxy->OnUpdateAbilityColorMode(colorMode, hasDarkRes);
+    EXPECT_EQ(ret, WMError::WM_ERROR_IPC_FAILED);
+
+    MockMessageParcel::SetWriteInterfaceTokenErrorFlag(true);
+    ret = sProxy->OnUpdateAbilityColorMode(colorMode, hasDarkRes);
+    EXPECT_EQ(ret, WMError::WM_ERROR_IPC_FAILED);
+    MockMessageParcel::SetWriteBoolErrorFlag(false);
+    MockMessageParcel::SetWriteStringErrorFlag(false);
+    MockMessageParcel::SetWriteInterfaceTokenErrorFlag(false);
+}
+
+/**
  * @tc.name: GetTargetOrientationConfigInfo
  * @tc.desc: GetTargetOrientationConfigInfo test
  * @tc.type: FUNC
