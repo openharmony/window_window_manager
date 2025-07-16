ccd2aee7ca340ab25150895a4edb95b973184760
卢宇豪 <luyuhao6@huawei.com>
2025-07-16 19:45:18 +0800

    change1
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- window_scene/test/unittest/scene_session_manager_test8.cpp
+++ window_scene/test/unittest/scene_session_manager_test8.cpp
@@ -1438,20 +1438,24 @@ HWTEST_F(SceneSessionManagerTest8, NotifyOnAttachToFrameNode01, TestSize.Level1)
     EXPECT_EQ(ssm_->sessionBlackListInfoMap_.size(), 0);
     EXPECT_EQ(ssm_->sessionRSBlackListConfigSet_.size(), 0);
 
-    session = sptr<Session>::MakeSptr();
-    session->SetWindowType(WindowType::APP_MAIN_WINDOW_BASE);
+    SessionInfo info;
+    session = sptr<Session>::MakeSptr(info);
+    session->GetSessionProperty()->SetWindowType(WindowType::APP_MAIN_WINDOW_BASE);
     ssm_->NotifyOnAttachToFrameNode(session);
     EXPECT_EQ(ssm_->sessionBlackListInfoMap_.size(), 0);
     EXPECT_EQ(ssm_->sessionRSBlackListConfigSet_.size(), 0);
 
-    session->SetWindowType(WindowType::APP_SUB_WINDOW_BASE);
+    session->GetSessionProperty()->SetWindowType(WindowType::APP_SUB_WINDOW_BASE);
     session->SetSurfaceNode(nullptr);
     ssm_->NotifyOnAttachToFrameNode(session);
     EXPECT_EQ(ssm_->sessionBlackListInfoMap_.size(), 0);
     EXPECT_EQ(ssm_->sessionRSBlackListConfigSet_.size(), 0);
 
-    session->SetWindowType(WindowType::APP_SUB_WINDOW_BASE);
-    session->SetSurfaceNode(sptr<RSSurfaceNode>::MakeSptr());
+    session->GetSessionProperty()->SetWindowType(WindowType::APP_SUB_WINDOW_BASE);
+    struct RSSurfaceNodeConfig config;
+    std::shared_ptr<RSSurfaceNode> surfaceNode = RSSurfaceNode::Create(config);
+    ASSERT_NE(nullptr, surfaceNode);
+    session->SetSurfaceNode(surfaceNode);
     ssm_->NotifyOnAttachToFrameNode(session);
     EXPECT_EQ(ssm_->sessionBlackListInfoMap_.size(), 0);
     EXPECT_EQ(ssm_->sessionRSBlackListConfigSet_.size(), 0);
