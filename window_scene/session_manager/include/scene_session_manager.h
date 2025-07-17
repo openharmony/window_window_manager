01b805b77068b1e32d34df2b5044919a44277d69
FangWuxing <fangwuxing@h-partners.com>
2025-07-17 03:43:50 +0000

    应用投屏场景刷新亮屏锁
    Signed-off-by: FangWuxing <fangwuxing@h-partners.com>

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -781,6 +781,7 @@ public:
         const WindowAnimationOption& animationOption) override;
 
     std::vector<sptr<SceneSession>> GetSceneSessions(ScreenId screenId);
+    WMError UpdateScreenLockState(const TransferSessionInfo& info);
 
 protected:
     SceneSessionManager();
