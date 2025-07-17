01fff2eed8b46fadc4b1aff568207f922c796852
yangxu <yangxu69@hisilicon.com>
2025-07-18 00:00:07 +0800

    fix secure
    Signed-off-by: yangxu <yangxu69@hisilicon.com>

--- window_scene/session/screen/include/screen_session.h
+++ window_scene/session/screen/include/screen_session.h
@@ -460,6 +460,7 @@ private:
         sptr<ScreenSession> defaultScreenSession);
 
     std::map<ScreenId, std::pair<sptr<ScreenSession>, Point>> screenSessionMap_;
+    mutable std::mutex screenSessionMapMutex_;
 };
 
 } // namespace OHOS::Rosen
