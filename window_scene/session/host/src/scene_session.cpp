ee76d841aab7cfb9064f9bd7a69e4fbbbe6b5070
qianli22 <qianli22@huawei.com>
2025-07-18 18:44:34 +0800

    添加dragend时的rect信息传递
    Signed-off-by: qianli22 <qianli22@huawei.com>

--- window_scene/session/host/src/scene_session.cpp
+++ window_scene/session/host/src/scene_session.cpp
@@ -932,7 +932,13 @@ void SceneSession::HandleSessionDragEvent(SessionEvent event)
         Gravity gravity = moveDragController_->GetGravity();
         SetSessionEventParam({rect.posX_, rect.posY_, rect.width_, rect.height_,
             static_cast<uint32_t>(dragResizeType), static_cast<uint32_t>(gravity)});
-    } else if (event == SessionEvent::EVENT_END_MOVE) {
+    } else if (moveDragController_ && event == SessionEvent::EVENT_END_MOVE) {
+        auto lastDragEndRect = moveDragController_->GetLastDragEndRect();
+        SetSessionEventParam({lastDragEndRect.posX_, lastDragEndRect.posY_,
+            lastDragEndRect.width_, lastDragEndRect.height_,
+            static_cast<uint32_t>(GetDragResizeTypeDuringDrag()),
+            static_cast<uint32_t>(moveDragController_->GetGravity())});
+        moveDragController_->SetLastDragEndRect({ 0, 0, 0, 0 });
         SetDragResizeTypeDuringDrag(dragResizeType);
     }
 }
@@ -3584,6 +3590,7 @@ void SceneSession::HandleMoveDragEnd(WSRect& rect, SizeChangeReason reason)
         NotifySubSessionRectChangeByAnchor(rect, SizeChangeReason::UNDEFINED, endDisplayId);
         CheckSubSessionShouldFollowParent(endDisplayId);
     }
+    moveDragController_->SetLastDragEndRect(rect);
     moveDragController_->ResetCrossMoveDragProperty();
     OnSessionEvent(SessionEvent::EVENT_END_MOVE);
 }
