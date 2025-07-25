407449b4f047f49e403876b4f96c035b299f1524
赵聪 <zhaocong33@h-partners.com>
2025-07-25 16:00:44 +0800

    回退
    Signed-off-by: 赵聪 <zhaocong33@h-partners.com>

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -2797,7 +2797,7 @@ WMError WindowSessionImpl::SetWindowType(WindowType type)
     TLOGD(WmsLogTag::DEFAULT, "%{public}u type %{public}u", GetWindowId(), static_cast<uint32_t>(type));
     if (type != WindowType::WINDOW_TYPE_SYSTEM_ALARM_WINDOW && !SessionPermission::IsSystemCalling()) {
         TLOGE(WmsLogTag::DEFAULT, "permission denied!");
-        return WSError::WS_ERROR_NOT_SYSTEM_APP;
+        return WMError::WM_ERROR_NOT_SYSTEM_APP;
     }
     if (IsWindowSessionInvalid()) {
         TLOGE(WmsLogTag::DEFAULT, "Session is invalid");
