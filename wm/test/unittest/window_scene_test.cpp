a7f9fae78f5784596043dde3ed34224bb96e4421
shuangshuangliu <liushuangshuang14@h-partners.com>
2025-07-28 14:09:42 +0800

    TDD修改
    Signed-off-by: shuangshuangliu <liushuangshuang14@h-partners.com>

--- wm/test/unittest/window_scene_test.cpp
+++ wm/test/unittest/window_scene_test.cpp
@@ -16,6 +16,7 @@
 #include <configuration.h>
 #include <gtest/gtest.h>
 #include "ability_context_impl.h"
+#include "modifier_render_thread/rs_modifiers_draw_thread.h"
 #include "mock_static_call.h"
 #include "singleton_mocker.h"
 #include "window_impl.h"
@@ -41,7 +42,12 @@ public:
 };
 void WindowSceneTest::SetUpTestCase() {}
 
-void WindowSceneTest::TearDownTestCase() {}
+void WindowSceneTest::TearDownTestCase() 
+{
+#ifdef RS_ENABLE_VK
+    RSModifiersDrawThread::Destroy();
+#endif
+}
 
 void WindowSceneTest::SetUp()
 {
