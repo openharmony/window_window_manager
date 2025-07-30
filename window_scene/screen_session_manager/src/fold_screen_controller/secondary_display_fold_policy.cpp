5e6b26e2359b32ec95d1e925cfade5d043500e02
chaos <zhongchao26@huawei.com>
2025-07-30 18:04:30 +0800

    test
    Signed-off-by: chaos <zhongchao26@huawei.com>

--- window_scene/screen_session_manager/src/fold_screen_controller/secondary_display_fold_policy.cpp
+++ window_scene/screen_session_manager/src/fold_screen_controller/secondary_display_fold_policy.cpp
@@ -478,10 +478,11 @@ void SecondaryDisplayFoldPolicy::SendPropertyChangeResult(sptr<ScreenSession> sc
     }
 
     screenSession->UpdatePropertyByFoldControl(screenProperty_, displayMode);
+    auto oldScreenProperty = screenSession->GetScreenProperty();
     if (displayMode == FoldDisplayMode::MAIN) {
         screenSession->SetRotationAndScreenRotationOnly(Rotation::ROTATION_0);
     }
-    screenSession->PropertyChange(screenSession->GetScreenProperty(), reason);
+    screenSession->PropertyChange(oldScreenProperty, reason);
     TLOGI(WmsLogTag::DMS, "screenBounds : width_= %{public}f, height_= %{public}f",
         screenSession->GetScreenProperty().GetBounds().rect_.width_,
         screenSession->GetScreenProperty().GetBounds().rect_.height_);
