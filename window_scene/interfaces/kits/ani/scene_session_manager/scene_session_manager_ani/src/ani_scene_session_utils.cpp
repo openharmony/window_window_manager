6d46dcc4034ddf9ccc9e89abbcf0c4c3d9755933
zhongqihong <zhongqihong2@huawei.com>
2025-08-06 16:10:16 +0800

    updata
    Signed-off-by: zhongqihong <zhongqihong2@huawei.com>

--- window_scene/interfaces/kits/ani/scene_session_manager/scene_session_manager_ani/src/ani_scene_session_utils.cpp
+++ window_scene/interfaces/kits/ani/scene_session_manager/scene_session_manager_ani/src/ani_scene_session_utils.cpp
@@ -12,9 +12,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#include "ani.h"
 #include "ani_err_utils.h"
 #include "ani_scene_session_utils.h"
-#include "ani.h"
 #include "window_manager_hilog.h"
 
 namespace OHOS {
