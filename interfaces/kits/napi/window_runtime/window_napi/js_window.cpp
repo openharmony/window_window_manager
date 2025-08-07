e13c32052554f41aa0f1f3989f70bdcf6f032c8c
gcw_bkPrirku <zhuying79@huawei.com>
2025-08-07 16:55:03 +0800

    fix compile
    Signed-off-by: gcw_bkPrirku <zhuying79@huawei.com>

--- interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
+++ interfaces/kits/napi/window_runtime/window_napi/js_window.cpp
@@ -3851,7 +3851,7 @@ napi_value JsWindow::OnGetPreferredOrientation(napi_env env, napi_callback_info 
     if (windowToken_ == nullptr) {
         TLOGE(WmsLogTag::WMS_ROTATION, "window is nullptr");
         napi_throw(env, JsErrUtils::CreateJsError(env, WmErrorCode::WM_ERROR_STATE_ABNORMALLY,
-            "[window][getPreferredOrientation]msg: window is nullptr"))
+            "[window][getPreferredOrientation]msg: window is nullptr"));
         return NapiGetUndefined(env);
     }
     Orientation requestedOrientation = windowToken_->GetRequestedOrientation();
