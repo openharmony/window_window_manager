e5b54e708093e9ad81bc57edbe214568748cad6a
WenJunYuan <1903700887@qq.com>
2025-08-11 17:01:19 +0800

    feature: multi screen multi user
    Signed-off-by: WenJunYuan <1903700887@qq.com>

--- wmserver/include/mock_session_manager_service.h
+++ wmserver/include/mock_session_manager_service.h
@@ -125,7 +125,7 @@ private:
      */
     int32_t defaultWMSUserId_;
     int32_t defaultScreenId_;
-    std::shared_mutex screenId2UserIdMapLock_;
+    std::mutex screenId2UserIdMapLock_;
     std::map<int32_t, int32_t> screenId2UserIdMap_;
     std::shared_mutex smsDeathRecipientMapLock_;
     std::map<int32_t, sptr<SMSDeathRecipient>> smsDeathRecipientMap_;
