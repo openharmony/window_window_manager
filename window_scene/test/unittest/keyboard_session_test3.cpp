8e9aebef53e35e027bbf2bbb8a674ca745280fdb
“zhongzijie” <zhongzijie2@huawei.com>
2025-08-12 10:06:40 +0800

    修改3
    Signed-off-by: “zhongzijie” <zhongzijie2@huawei.com>

--- window_scene/test/unittest/keyboard_session_test3.cpp
+++ window_scene/test/unittest/keyboard_session_test3.cpp
@@ -763,16 +763,16 @@ HWTEST_F(KeyboardSessionTest3, NotifyOccupiedAreaChanged, Function | SmallTest |
     EXPECT_TRUE(g_logMsg.find("Calling id:") != std::string::npos);
 
     g_logMsg.clear();
-    CallingSession->sessionInfo_.isSystem_ = false;
+    callingSession->sessionInfo_.isSystem_ = false;
     sptr<SessionStageMocker> mockSessionStage = sptr<SessionStageMocker>::MakeSptr();
     ASSERT_NE(mockSessionStage, nullptr);
-    CallingSession->sessionStage_ = mockSessionStage;
+    callingSession->sessionStage_ = mockSessionStage;
     EXPECT_CALL(*mockSessionStage, NotifyOccupiedAreaChangeInfo(_, _, _, _)).Times(2);
     keyboardSession->NotifyOccupiedAreaChanged(callingSession, occupiedAreaInfo, false, nullptr);
     EXPECT_TRUE(g_logMsg.find("Calling id:") != std::string::npos);
 
     g_logMsg.clear();
-    CallingSession->sessionInfo_.isSystem_ = false;
+    callingSession->sessionInfo_.isSystem_ = false;
     keyboardSession->NotifyOccupiedAreaChanged(callingSession, occupiedAreaInfo, true, nullptr);
     EXPECT_TRUE(g_logMsg.find("size of avoidAreas: 5") != std::string::npos);
 }
