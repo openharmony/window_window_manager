9f3d5153c00be6d6f3f28714fa6f3f67844b8544
l30033162 <liuliwan@huawei.com>
2025-08-13 15:38:38 +0800

    Hooper B面投屏问题修复。1.鼠标光标可从内屏穿越至外屏
    Signed-off-by: l30033162 <liuliwan@huawei.com>

--- window_scene/screen_session_manager/include/screen_session_manager.h
+++ window_scene/screen_session_manager/include/screen_session_manager.h
@@ -609,6 +609,9 @@ private:
     void HandleSuperFoldStatusLocked(bool isLocked);
     void CalculateRotatedDisplay(Rotation rotation, const DMRect& screenRegion, DMRect& displayRegion, DMRect& displayArea);
     void CalculateScreenArea(const DMRect& displayRegion, const DMRect& displayArea, const DMRect& screenRegion, DMRect& screenArea);
+    void DisConnectScreenIfScreenInfoNull(sptr<ScreenSession>& screenSession);
+    void RecoverDefaultScreenModeInner(ScreenId innerRsId, ScreenId externalRsId);
+    void SetDefaultScreenModeWhenCreateMirror(sptr<ScreenSession>& screenSession);
 #ifdef DEVICE_STATUS_ENABLE
     void SetDragWindowScreenId(ScreenId screenId, ScreenId displayNodeScreenId);
 #endif // DEVICE_STATUS_ENABLE
