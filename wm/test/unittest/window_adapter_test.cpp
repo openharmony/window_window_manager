6a3221bd83cd2a2c23c5126f88fc3275dc81c3c7
卢宇豪 <luyuhao6@huawei.com>
2025-08-13 17:35:22 +0800

    change1
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- wm/test/unittest/window_adapter_test.cpp
+++ wm/test/unittest/window_adapter_test.cpp
@@ -510,6 +510,23 @@ HWTEST_F(WindowAdapterTest, GetVisibilityWindowInfo, TestSize.Level1)
 }
 
 /**
+ * @tc.name: RecoverWindowPropertyChangeFlag
+ * @tc.desc: WindowAdapter/RecoverWindowPropertyChangeFlag
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowAdapterTest, RecoverWindowPropertyChangeFlag01, TestSize.Level1)
+{
+    WindowAdapter windowAdapter;
+    auto ret = windowAdapter.RecoverWindowPropertyChangeFlag();
+    EXPECT_EQ(WMError::WM_ERROR_NULLPTR, ret);
+
+    windowAdapter.InitWMSProxy();
+    ASSERT_NE(windowManagerServiceProxy_, false);
+    ret = windowAdapter.RecoverWindowPropertyChangeFlag();
+    EXPECT_EQ(WMError::WM_OK, ret);
+}
+
+/**
  * @tc.name: ReregisterWindowManagerAgent
  * @tc.desc: WindowAdapter/ReregisterWindowManagerAgent
  * @tc.type: FUNC
