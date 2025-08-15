dbd39a604d20f321af597ddae730f3f04e384eaf
陈树山 <css353929243@163.com>
2025-08-15 17:32:39 +0800

    createCreateJsError增加描述信息
    Signed-off-by: 陈树山 <css353929243@163.com>

--- interfaces/kits/napi/window_runtime/window_manager_napi/js_window_manager.cpp
+++ interfaces/kits/napi/window_runtime/window_manager_napi/js_window_manager.cpp
@@ -782,8 +782,8 @@ napi_value JsWindowManager::OnFindWindowSync(napi_env env, napi_callback_info in
     } else {
         sptr<Window> window = Window::Find(windowName);
         if (window == nullptr) {
-            napi_throw(env, JsErrUtils::CreateJsError(env, WmErrorCode::WM_ERROR_STATE_ABNORMALLY),
-                "[windowLifecycle][OnFindWindowSync]msg: window is nullptr");
+            napi_throw(env, JsErrUtils::CreateJsError(env, WmErrorCode::WM_ERROR_STATE_ABNORMALLY,
+                "[window][OnFindWindowSync]msg: window is nullptr"));
             return NapiGetUndefined(env);
         } else {
             return CreateJsWindowObject(env, window);
