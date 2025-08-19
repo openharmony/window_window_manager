40eab27206707ed3fe1f86e28202d6e05ab2197e
ZhaoLiang <zhaoliang50@huawei.com>
2025-08-19 22:07:48 +0800

    add set fold displayMode error message
    Signed-off-by: ZhaoLiang <zhaoliang50@huawei.com>

--- interfaces/innerkits/dm/dm_common.h
+++ interfaces/innerkits/dm/dm_common.h
@@ -43,6 +43,14 @@ constexpr int DOT_PER_INCH_MAXIMUM_VALUE = 1000;
 constexpr int DOT_PER_INCH_MINIMUM_VALUE = 80;
 constexpr uint32_t BASELINE_DENSITY = 160;
 constexpr uint32_t HALF_SCREEN_PARAM = 2;
+const std::string DM_ERROR_MSG_NOT_SUPPORT_COOR_WHEN_WIRED_CASTING =
+    "[wired casting]not support coordination when wired casting.";
+const std::string DM_ERROR_MSG_NOT_SUPPORT_COOR_WHEN_WIRLESS_CASTING =
+    "[wireless casting]not support coordination when wireless casting.";
+const std::string DM_ERROR_MSG_NOT_SUPPORT_COOR_WHEN_RECORDING =
+    "[recording]not support coordination when wired recording.";
+const std::string DM_ERROR_MSG_NOT_SUPPORT_COOR_WHEN_TENTMODE =
+    "[tentmode]not support coordination when tentmode.";
 }
 
 /**
@@ -207,6 +215,10 @@ enum class DMError : int32_t {
     DM_ERROR_DEVICE_NOT_SUPPORT = 801,
     DM_ERROR_UNKNOWN = -1,
     DM_ERROR_ILLEGAL_PARAM = 1400004,
+    DM_ERROR_NOT_SUPPORT_COOR_WHEN_WIRED_CASTING = 100001,
+    DM_ERROR_NOT_SUPPORT_COOR_WHEN_WIRLESS_CASTING = 100002,
+    DM_ERROR_NOT_SUPPORT_COOR_WHEN_RECORDING = 100003,
+    DM_ERROR_NOT_SUPPORT_COOR_WHEN_TENTMODE = 100004,
 };
 
 /**
@@ -228,24 +240,35 @@ enum class DmErrorCode : int32_t {
  * @brief Constructs the mapping of the DM errors to the DM error codes.
  */
 const std::map<DMError, DmErrorCode> DM_JS_TO_ERROR_CODE_MAP {
-    {DMError::DM_OK,                                    DmErrorCode::DM_OK                          },
-    {DMError::DM_ERROR_INVALID_PERMISSION,              DmErrorCode::DM_ERROR_NO_PERMISSION         },
-    {DMError::DM_ERROR_INIT_DMS_PROXY_LOCKED,           DmErrorCode::DM_ERROR_SYSTEM_INNORMAL       },
-    {DMError::DM_ERROR_IPC_FAILED,                      DmErrorCode::DM_ERROR_SYSTEM_INNORMAL       },
-    {DMError::DM_ERROR_REMOTE_CREATE_FAILED,            DmErrorCode::DM_ERROR_SYSTEM_INNORMAL       },
-    {DMError::DM_ERROR_NULLPTR,                         DmErrorCode::DM_ERROR_INVALID_SCREEN        },
-    {DMError::DM_ERROR_INVALID_PARAM,                   DmErrorCode::DM_ERROR_INVALID_PARAM         },
-    {DMError::DM_ERROR_WRITE_INTERFACE_TOKEN_FAILED,    DmErrorCode::DM_ERROR_SYSTEM_INNORMAL       },
-    {DMError::DM_ERROR_DEATH_RECIPIENT,                 DmErrorCode::DM_ERROR_SYSTEM_INNORMAL       },
-    {DMError::DM_ERROR_INVALID_MODE_ID,                 DmErrorCode::DM_ERROR_SYSTEM_INNORMAL       },
-    {DMError::DM_ERROR_WRITE_DATA_FAILED,               DmErrorCode::DM_ERROR_SYSTEM_INNORMAL       },
-    {DMError::DM_ERROR_RENDER_SERVICE_FAILED,           DmErrorCode::DM_ERROR_SYSTEM_INNORMAL       },
-    {DMError::DM_ERROR_UNREGISTER_AGENT_FAILED,         DmErrorCode::DM_ERROR_SYSTEM_INNORMAL       },
-    {DMError::DM_ERROR_INVALID_CALLING,                 DmErrorCode::DM_ERROR_INVALID_CALLING       },
-    {DMError::DM_ERROR_NOT_SYSTEM_APP,                  DmErrorCode::DM_ERROR_NOT_SYSTEM_APP        },
-    {DMError::DM_ERROR_UNKNOWN,                         DmErrorCode::DM_ERROR_SYSTEM_INNORMAL       },
-    {DMError::DM_ERROR_DEVICE_NOT_SUPPORT,              DmErrorCode::DM_ERROR_DEVICE_NOT_SUPPORT    },
-    {DMError::DM_ERROR_ILLEGAL_PARAM,                   DmErrorCode::DM_ERROR_ILLEGAL_PARAM    },
+    {DMError::DM_OK,                                             DmErrorCode::DM_OK                              },
+    {DMError::DM_ERROR_INVALID_PERMISSION,                       DmErrorCode::DM_ERROR_NO_PERMISSION             },
+    {DMError::DM_ERROR_INIT_DMS_PROXY_LOCKED,                    DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_IPC_FAILED,                               DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_REMOTE_CREATE_FAILED,                     DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_NULLPTR,                                  DmErrorCode::DM_ERROR_INVALID_SCREEN            },
+    {DMError::DM_ERROR_INVALID_PARAM,                            DmErrorCode::DM_ERROR_INVALID_PARAM             },
+    {DMError::DM_ERROR_WRITE_INTERFACE_TOKEN_FAILED,             DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_DEATH_RECIPIENT,                          DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_INVALID_MODE_ID,                          DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_WRITE_DATA_FAILED,                        DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_RENDER_SERVICE_FAILED,                    DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_UNREGISTER_AGENT_FAILED,                  DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_INVALID_CALLING,                          DmErrorCode::DM_ERROR_INVALID_CALLING           },
+    {DMError::DM_ERROR_NOT_SYSTEM_APP,                           DmErrorCode::DM_ERROR_NOT_SYSTEM_APP            },
+    {DMError::DM_ERROR_UNKNOWN,                                  DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_DEVICE_NOT_SUPPORT,                       DmErrorCode::DM_ERROR_DEVICE_NOT_SUPPORT        },
+    {DMError::DM_ERROR_ILLEGAL_PARAM,                            DmErrorCode::DM_ERROR_ILLEGAL_PARAM             },
+    {DMError::DM_ERROR_NOT_SUPPORT_COOR_WHEN_WIRED_CASTING,      DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_NOT_SUPPORT_COOR_WHEN_WIRLESS_CASTING,    DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_NOT_SUPPORT_COOR_WHEN_RECORDING,          DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+    {DMError::DM_ERROR_NOT_SUPPORT_COOR_WHEN_TENTMODE,           DmErrorCode::DM_ERROR_SYSTEM_INNORMAL           },
+};
+
+const std::map<DMError, std::string> DM_ERROR_JS_TO_ERROR_MESSAGE_MAP {
+    {DMError::DM_ERROR_NOT_SUPPORT_COOR_WHEN_WIRED_CASTING,      DM_ERROR_MSG_NOT_SUPPORT_COOR_WHEN_WIRED_CASTING   },
+    {DMError::DM_ERROR_NOT_SUPPORT_COOR_WHEN_WIRLESS_CASTING,    DM_ERROR_MSG_NOT_SUPPORT_COOR_WHEN_WIRLESS_CASTING },
+    {DMError::DM_ERROR_NOT_SUPPORT_COOR_WHEN_RECORDING,          DM_ERROR_MSG_NOT_SUPPORT_COOR_WHEN_RECORDING       },
+    {DMError::DM_ERROR_NOT_SUPPORT_COOR_WHEN_TENTMODE,           DM_ERROR_MSG_NOT_SUPPORT_COOR_WHEN_TENTMODE        },
 };
 
 using DisplayStateCallback = std::function<void(DisplayState)>;
