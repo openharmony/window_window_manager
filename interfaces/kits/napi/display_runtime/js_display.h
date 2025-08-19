5460489915135732c31d40e88bf2206fc4218714
chenpeng <chenpeng299@huawei.com>
2025-08-19 21:09:02 +0800

    检视意见修改
    Signed-off-by: chenpeng <chenpeng299@huawei.com>

--- interfaces/kits/napi/display_runtime/js_display.h
+++ interfaces/kits/napi/display_runtime/js_display.h
@@ -78,7 +78,7 @@ public:
     static napi_value GetDisplayCapability(napi_env env, napi_callback_info info);
     static napi_value RegisterDisplayManagerCallback(napi_env env, napi_callback_info info);
     static napi_value UnregisterDisplayManagerCallback(napi_env env, napi_callback_info info);
-    sptr<Display> GetDisplay() { return display_; }
+    sptr<Display> GetDisplay() const { return display_; }
 
 private:
     sptr<Display> display_ = nullptr;
