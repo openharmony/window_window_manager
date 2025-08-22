c0815cea23a1e506f515e0a4d69fb493bcc944eb
lly920173971 <920173971@qq.com>
2025-08-22 15:14:28 +0800

    蓝黄同步
    Signed-off-by: lly920173971 <920173971@qq.com>

--- window_scene/session_manager/src/zidl/scene_session_manager_proxy.cpp
+++ window_scene/session_manager/src/zidl/scene_session_manager_proxy.cpp
@@ -3567,7 +3567,11 @@ WMError SceneSessionManagerProxy::MinimizeByWindowId(const std::vector<int32_t>&
         TLOGE(WmsLogTag::WMS_LIFE, "SendRequest failed");
         return WMError::WM_ERROR_IPC_FAILED;
     }
-    return static_cast<WMError>(reply.ReadInt32());
+    int32_t ret = 0;
+    if (!reply.ReadInt32(ret)) {
+        return WMError::WM_ERROR_IPC_FAILED;
+    }
+    return static_cast<WMError>(ret);
 }
 
 WMError SceneSessionManagerProxy::SetForegroundWindowNum(uint32_t windowNum)
@@ -3598,7 +3602,7 @@ WMError SceneSessionManagerProxy::SetForegroundWindowNum(uint32_t windowNum)
 
 WSError SceneSessionManagerProxy::UseImplicitAnimation(int32_t hostWindowId, bool useImplicit)
 {
-    TLOGD(WmsLogTag::WMS_UIEXT, "run SceneSessionManagerProxy::GetHostWindowRect");
+    TLOGD(WmsLogTag::WMS_UIEXT, "Run SceneSessionManagerProxy::UseImplicitAnimation");
     MessageParcel data;
     MessageParcel reply;
     MessageOption option;
