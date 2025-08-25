426d9bc15b745aa86a84c0b1bff11cba5bd49b1c
SelinaJing <jiangjing23@hisilicon.com>
2025-08-25 21:15:04 +0800

    fix char
    Signed-off-by: SelinaJing <jiangjing23@hisilicon.com>

--- window_scene/session/host/src/scene_persistence.cpp
+++ window_scene/session/host/src/scene_persistence.cpp
@@ -89,7 +89,7 @@ ScenePersistence::ScenePersistence(const std::string& bundleName, int32_t persis
         snapshotFfrtHelper_ = std::make_shared<WSFFRTHelper>();
     }
     const std::string multiWindowUIType= system::GetParameter("const.window.multiWindowUIType","HandsetSmartWindow");
-    if (multiWindowUIType == "FreeFormMutiWindow") {
+    if (multiWindowUIType == "FreeFormMultiWindow") {
         isPcWindow_ = true;
     } else {
         isPcWindow_ = false;
