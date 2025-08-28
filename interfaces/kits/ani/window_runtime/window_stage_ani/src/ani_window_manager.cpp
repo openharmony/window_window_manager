fbf942dba3d7ac13f9573f4ed9a218fa315b36b9
yanxinyu7 <yanxinyu7@h-partners.com>
2025-08-28 16:05:52 +0800

    bugfix
    Signed-off-by: yanxinyu7 <yanxinyu7@h-partners.com>

--- interfaces/kits/ani/window_runtime/window_stage_ani/src/ani_window_manager.cpp
+++ interfaces/kits/ani/window_runtime/window_stage_ani/src/ani_window_manager.cpp
@@ -231,10 +231,10 @@ ani_ref CreateAniSystemWindow(ani_env* env, void* contextPtr, sptr<WindowOption>
     WMError wmError = WMError::WM_OK;
     sptr<Window> window = Window::Create(windowOption->GetWindowName(), windowOption, context->lock(), wmError);
     WmErrorCode wmErrorCode = WM_JS_TO_ERROR_CODE_MAP.at(wmError);
-    if (window == nullptr && wmErrorCode != WmErrorCode::WM_OK) {
-        return AniWindowUtils::AniThrowError(env, WmErrorCode::WM_ERROR_STATE_ABNORMALLY);
-    } else {
+    if (window != nullptr && wmErrorCode == WmErrorCode::WM_OK) {
         return CreateAniWindowObject(env, window);
+    } else {
+        return AniWindowUtils::AniThrowError(env, wmErrorCode, "Create window failed");
     }
 }
 
