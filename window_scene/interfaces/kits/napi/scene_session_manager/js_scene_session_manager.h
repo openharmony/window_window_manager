49c4034c8eb5244f6ba99601e351981ecf4c31b8
rsyys <renzhao5@h-partners.com>
2025-09-03 14:50:30 +0800

    fix: visible focus tdd
    Signed-off-by: rsyys <renzhao5@h-partners.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session_manager.h
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session_manager.h
@@ -73,7 +73,6 @@ public:
     static napi_value UpdateRotateAnimationConfig(napi_env env, napi_callback_info info);
     static napi_value ProcessBackEvent(napi_env env, napi_callback_info info);
     static napi_value CheckSceneZOrder(napi_env env, napi_callback_info info);
-    static napi_value UpdateFocus(napi_env env, napi_callback_info info);
     static napi_value SetVmaCacheStatus(napi_env env, napi_callback_info info);
     static napi_value InitWithRenderServiceAdded(napi_env env, napi_callback_info info);
     static napi_value GetAllWindowVisibilityInfos(napi_env env, napi_callback_info info);
@@ -200,7 +199,6 @@ private:
     napi_value OnUpdateRotateAnimationConfig(napi_env env, napi_callback_info info);
     napi_value OnProcessBackEvent(napi_env env, napi_callback_info info);
     napi_value OnCheckSceneZOrder(napi_env env, napi_callback_info info);
-    napi_value OnUpdateFocus(napi_env env, napi_callback_info info);
     napi_value OnSetVmaCacheStatus(napi_env env, napi_callback_info info);
     napi_value OnInitWithRenderServiceAdded(napi_env env, napi_callback_info info);
     napi_value OnGetAllWindowVisibilityInfos(napi_env env, napi_callback_info info);
