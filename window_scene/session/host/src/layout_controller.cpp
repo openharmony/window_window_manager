890b154676c4d5cca2063eb3adff9607f6300bdc
liuanguang <liuanguang@h-partners.com>
2025-09-11 11:23:43 +0800

    fix(wms-layout): revise by code review 2
    Signed-off-by: liuanguang <liuanguang@h-partners.com>

--- window_scene/session/host/src/layout_controller.cpp
+++ window_scene/session/host/src/layout_controller.cpp
@@ -161,7 +161,7 @@ WSRect LayoutController::AdjustRectByAspectRatio(const WSRect& rect, const Windo
     float vpr = display->GetVirtualPixelRatio();
     auto limits = sessionProperty_->GetWindowLimits();
     SessionUtils::CalcFloatWindowRectLimits(getSystemConfigFunc_().maxFloatingWindowSize_, vpr, limits);
-    WSRect adjustedRect = SessionUtils::AdjustRectByAspectRatio(rect, limits, decoration, aspectRatio_);
+    const WSRect& adjustedRect = SessionUtils::AdjustRectByAspectRatio(rect, limits, decoration, aspectRatio_);
     TLOGD(WmsLogTag::WMS_LAYOUT, "After adjustment, windowId: %{public}d, rect: %{public}s",
         windowId, adjustedRect.ToString().c_str());
     return adjustedRect;
