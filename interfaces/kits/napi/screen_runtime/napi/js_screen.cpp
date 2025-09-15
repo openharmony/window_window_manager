73e3d1efa7c998e1eab27428f147bffc891e9ab0
hejunfei <hejunfei@huawei.com>
2025-09-15 15:01:05 +0800

    add check
    Signed-off-by: hejunfei <hejunfei@huawei.com>

--- interfaces/kits/napi/screen_runtime/napi/js_screen.cpp
+++ interfaces/kits/napi/screen_runtime/napi/js_screen.cpp
@@ -287,7 +287,11 @@ napi_value CreateJsScreenObject(napi_env env, sptr<Screen>& screen)
         objValue = jsScreenObj->GetNapiValue();
     }
     if (objValue == nullptr) {
-        napi_create_object(env, &objValue);
+        auto status = napi_create_object(env, &objValue);
+        if ((status != napi_ok) || (objValue == nullptr)) {
+            TLOGE(WmsLogTag::DMS, "failed to create js obj, error:%{public}d", status);
+            return NapiGetUndefined(env);
+        }
     }
     if (objValue == nullptr) {
         TLOGE(WmsLogTag::DMS, "Failed to convert prop to jsObject");
