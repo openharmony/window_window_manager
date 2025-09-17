845d73d59c4cb26b92f9e693c03878de6f7d928a
陈树山 <css353929243@163.com>
2025-09-17 21:32:14 +0800

    minimizeAll整改
    Signed-off-by: 陈树山 <css353929243@163.com>

--- window_scene/test/unittest/scene_session_manager_lifecycle_test2.cpp
+++ window_scene/test/unittest/scene_session_manager_lifecycle_test2.cpp
@@ -219,7 +219,7 @@ HWTEST_F(SceneSessionManagerLifecycleTest2, MinimizeAllWindow, TestSize.Level1)
     sptr<SceneSession> sceneSession = sptr<SceneSession>::MakeSptr(info, nullptr);
     ASSERT_NE(nullptr, sceneSession);
 
-    DisplayId displayId =0;
+    DisplayId displayId = 0;
     MockAccesstokenKit::MockIsSACalling(false);
     MockAccesstokenKit::MockIsSystemApp(false);
     EXPECT_EQ(ssm_->MinimizeAllWindow(displayId), WMError::WM_OK);
@@ -250,7 +250,7 @@ HWTEST_F(SceneSessionManagerLifecycleTest2, MinimizeAllWindow01, TestSize.Level1
     sptr<SceneSession> sceneSession = sptr<SceneSession>::MakeSptr(info, nullptr);
     ASSERT_NE(nullptr, sceneSession);
 
-    DisplayId displayId =0;
+    DisplayId displayId = 0;
     MockAccesstokenKit::MockIsSACalling(true);
     MockAccesstokenKit::MockIsSystemApp(true);
     sptr<WindowSessionProperty> property = sptr<WindowSessionProperty>::MakeSptr();
@@ -263,7 +263,6 @@ HWTEST_F(SceneSessionManagerLifecycleTest2, MinimizeAllWindow01, TestSize.Level1
     sceneSession->SetScreenId(0);
     ssm_->sceneSessionMap_.insert({1, nullptr});
     ssm_->sceneSessionMap_.insert({9, sceneSession});
-    MockAccesstokenKit::MockAccessTokenKitRet(0);
     EXPECT_EQ(ssm_->MinimizeAllWindow(displayId), WMError::WM_OK);
 
     property->SetWindowType(WindowType::APP_MAIN_WINDOW_END);
