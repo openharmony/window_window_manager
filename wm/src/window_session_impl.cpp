81ecb85d0a238b137c30c67856ca2c22136eb078
jingbotao <jingbotao3@huawei.com>
2025-09-23 14:24:31 +0800

    布局维测日志优化
    Signed-off-by: jingbotao <jingbotao3@huawei.com>

--- wm/src/window_session_impl.cpp
+++ wm/src/window_session_impl.cpp
@@ -1091,10 +1091,9 @@ WSError WindowSessionImpl::UpdateRect(const WSRect& rect, SizeChangeReason reaso
     property_->SetWindowRect(wmRect);
     property_->SetRequestRect(wmRect);
 
-    TLOGI(WmsLogTag::WMS_LAYOUT, "%{public}s, preRect:%{public}s, reason:%{public}u,"
-        "[name:%{public}s, id:%{public}d], clientDisplayId: %{public}" PRIu64,
-        rect.ToString().c_str(), preRect.ToString().c_str(), wmReason,
-        GetWindowName().c_str(), GetPersistentId(), property_->GetDisplayId());
+    TLOGI(WmsLogTag::WMS_LAYOUT, "id:%{public}d name:%{public}s rect:%{public}s->%{public}s reason:%{public}u "
+        "displayId:%{public}" PRIu64, GetPersistentId(),GetWindowName().c_str(), preRect.ToString().c_str(),
+        rect.ToString().c_str(), wmReason, property_->GetDisplayId());
     HITRACE_METER_FMT(HITRACE_TAG_WINDOW_MANAGER,
         "WindowSessionImpl::UpdateRect id: %d [%d, %d, %u, %u] reason: %u hasRSTransaction: %u", GetPersistentId(),
         wmRect.posX_, wmRect.posY_, wmRect.width_, wmRect.height_, wmReason, config.rsTransaction_ != nullptr);
