c1df6f6137dd3813cd0f18638a4582b91662580d
wangzilin <wangzilin17@huawei.com>
2025-09-23 14:18:11 +0800

    window class transfer
    Signed-off-by: wangzilin <wangzilin17@huawei.com>

--- interfaces/kits/ani/window_runtime/window_stage_ani/include/ani_window.h
+++ interfaces/kits/ani/window_runtime/window_stage_ani/include/ani_window.h
@@ -37,6 +37,10 @@ public:
     ani_ref GetAniRef() { return aniRef_; }
     void SetAniRef(const ani_ref& aniRef) { aniRef_ = aniRef; }
 
+    /* transfer window class  */
+    static ani_object NativeTransferStatic(ani_env* aniEnv, ani_class cls, ani_object input);
+    static ani_object NativeTransferDynamic(ani_env* aniEnv, ani_class cls, ani_long nativeObj);
+
     /* window obj stored in ANI */
     static AniWindow* GetWindowObjectFromEnv(ani_env* env, ani_object obj);
 
