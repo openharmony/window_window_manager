137dc9d10c71df789f741c7ba4821419d30ad0b9
l30067243 <leipeng42@h-partners.com>
2025-09-24 18:24:53 +0800

    fix mock
    Signed-off-by: l30067243 <leipeng42@h-partners.com>

--- window_scene/test/mock/mock_session_stage.h
+++ window_scene/test/mock/mock_session_stage.h
@@ -59,6 +59,7 @@ public:
     MOCK_METHOD2(UpdateTitleInTargetPos, WSError(bool isShow, int32_t height));
     MOCK_METHOD2(NotifyDensityFollowHost, WSError(bool isFollowHost, float densityValue));
     MOCK_METHOD1(NotifyWindowVisibility, WSError(bool isVisible));
+    MOCK_METHOD1(NotifyWindowOcclusionState, WSError(const WindowVisibilityState state));
     MOCK_METHOD1(NotifyTransformChange, void(const Transform& transform));
     MOCK_METHOD1(NotifySingleHandTransformChange, void(const SingleHandTransform& singleHandTransform));
     MOCK_METHOD1(NotifyDialogStateChange, WSError(bool isForeground));
