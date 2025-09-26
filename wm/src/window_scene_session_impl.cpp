5b31847694dc466e0701888f450eb81e00932591
Huhan55 <huhan25@huawei.com>
2025-09-26 17:46:42 +0800

    fix error
    Signed-off-by: Huhan55 <huhan25@huawei.com>

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -492,7 +492,7 @@ WMError WindowSceneSessionImpl::RecoverAndReconnectSceneSession()
         property_->GetWindowMode(), property_->GetWindowType(), GetPersistentId(), state_, requestState_);
 
     if (isFocused_) {
-        UpdateFocus(false);
+        UpdateFocusState(false);
     }
     auto context = GetContext();
     auto abilityContext = AbilityRuntime::Context::ConvertTo<AbilityRuntime::AbilityContext>(context);
@@ -3544,10 +3544,10 @@ WMError WindowSceneSessionImpl::MaximizeFloating()
     }
     MaximizeEvent(hostSession);
     UpdateProperty(WSPropertyChangeAction::ACTION_UPDATE_MAXIMIZE_STATE);
- 
+
     return WMError::WM_OK;
 }
- 
+
 void WindowSceneSessionImpl::MaximizeEvent(const sptr<ISession> &hostSession)
 {
     if (hostSession == nullptr) {
@@ -5724,7 +5724,7 @@ void WindowSceneSessionImpl::PendingUpdateSupportWindowModesWhenSwitchMultiWindo
     uint32_t windowModeSupportType = pendingWindowModeSupportType_;
     TLOGI(WmsLogTag::WMS_LAYOUT_PC, "id: %{public}d, windowModeSupportType: %{public}u",
         GetPersistentId(), windowModeSupportType);
-    
+
     pendingWindowModeSupportType_ = WindowModeSupport::WINDOW_MODE_SUPPORT_ALL;
     property_->SetWindowModeSupportType(windowModeSupportType);
 
