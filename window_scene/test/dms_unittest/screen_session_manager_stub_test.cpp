cd6c18086ad196276b212b9bff1bb07e67fa818c
ZhaoLiang <zhaoliang50@huawei.com>
2025-10-10 21:06:57 +0800

    modify check test
    Signed-off-by: ZhaoLiang <zhaoliang50@huawei.com>

--- window_scene/test/dms_unittest/screen_session_manager_stub_test.cpp
+++ window_scene/test/dms_unittest/screen_session_manager_stub_test.cpp
@@ -2918,7 +2918,7 @@ HWTEST_F(ScreenSessionManagerStubTest, OnRemoteRequest13301, TestSize.Level1)
     data.WriteBool(true);
     uint32_t code = static_cast<uint32_t>(DisplayManagerMessage::TRANS_ID_GET_DISPLAY_CAPTURE);
     int res = stub_->OnRemoteRequest(code, data, reply, option);
-    EXPECT_TRUE(g_logMsg.find("Read node surfaceNodesList failed") != std::string::npos);
+    EXPECT_TRUE(g_logMsg.find("Read node blackWindowIdList failed") != std::string::npos);
     LOG_SetCallback(nullptr);
 }
 
