28a52d7bfde1b0b2b1c6dc8d9440ef28716cbad6
cuihongbin <cuihongbin@huawei.com>
2025-10-15 11:21:47 +0800

    补充TDD
    Signed-off-by: cuihongbin <cuihongbin@huawei.com>

--- wm/test/unittest/event_distribution/oh_window_event_test.cpp
+++ wm/test/unittest/event_distribution/oh_window_event_test.cpp
@@ -13,18 +13,9 @@
  * limitations under the License.
  */
 
-#include <configuration.h>
 #include <gtest/gtest.h>
 
-// #include "ability_context_impl.h"
-// #include "mock_static_call.h"
-// #include "mock_session.h"
-// #include "modifier_render_thread/rs_modifiers_draw_thread.h"
 #include "oh_window.h"
-// #include "singleton_mocker.h"
-#include "window_impl.h"
-#include "window_scene.h"
-#include "window_session_impl.h"
 
 struct Input_TouchEvent {
     int32_t action;
@@ -41,16 +32,12 @@ using namespace testing::ext;
 
 namespace OHOS {
 namespace Rosen {
-using Mocker = SingletonMocker<StaticCall, MockStaticCall>;
 class OHWindowEventTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
     void SetUp() override;
     void TearDown() override;
-
-    // sptr<WindowScene> scene_ = nullptr;
-    // std::shared_ptr<AbilityRuntime::AbilityContext> abilityContext_;
 };
 
 void OHWindowTest::SetUpTestCase()
@@ -59,28 +46,14 @@ void OHWindowTest::SetUpTestCase()
 
 void OHWindowTest::TearDownTestCase()
 {
-#ifdef RS_ENABLE_VK
-    RSModifiersDrawThread::Destroy();
-#endif
 }
 
 void OHWindowTest::SetUp()
 {
-    // abilityContext_ = std::make_shared<AbilityRuntime::AbilityContextImpl>();
-    // std::unique_ptr<Mocker> m = std::make_unique<Mocker>();
-    // sptr<WindowOption> option = new WindowOption();
-    // EXPECT_CALL(m->Mock(), CreateWindow(_, _, _)).Times(1).WillOnce(Return(new WindowImpl(option)));
-    // DisplayId displayId = 0;
-    // sptr<IWindowLifeCycle> listener = nullptr;
-    // scene_ = sptr<WindowScene>::MakeSptr();
-    // ASSERT_EQ(WMError::WM_OK, scene_->Init(displayId, abilityContext_, listener));
 }
 
 void OHWindowTest::TearDown()
 {
-    scene_->GoDestroy();
-    scene_ = nullptr;
-    abilityContext_ = nullptr;
 }
 
 namespace {
@@ -89,7 +62,7 @@ namespace {
  * @tc.desc: OH_WindowManager_InjectTouchEvent test
  * @tc.type: FUNC
  */
-HWTEST_F(OHWindowTest, OH_WindowManager_InjectTouchEvent, TestSize.Level0)
+HWTEST_F(OHWindowEventTest, OH_WindowManager_InjectTouchEvent, TestSize.Level0)
 {
     int32_t windowId = -1;
     int32_t windowX = 0;
@@ -124,38 +97,25 @@ HWTEST_F(OHWindowTest, OH_WindowManager_InjectTouchEvent, TestSize.Level0)
  * @tc.desc: OH_WindowManager_LockCursor test
  * @tc.type: FUNC
  */
-HWTEST_F(OHWindowTest, OH_WindowManager_LockCursor, TestSize.Level0)
+HWTEST_F(OHWindowEventTest, OH_WindowManager_LockCursor, TestSize.Level0)
 {
     int32_t windowId = 1;
     // Failed to obtain null window
     auto ret = OH_WindowManager_LockCursor(windowId, true);
     EXPECT_EQ(static_cast<int32_t>(WindowManager_ErrorCode::WINDOW_MANAGER_ERRORCODE_STATE_ABNORMAL), ret);
+}
 
-    // Failed to obtain eventHandler
-    auto eventHandler = g_eventHandler;
-    g_eventHandler = nullptr;
-    ret = OH_WindowManager_LockCursor(windowId, true);
-    EXPECT_EQ(static_cast<int32_t>(WindowManager_ErrorCode::WINDOW_MANAGER_ERRORCODE_SYSTEM_ABNORMAL), ret);
-    g_eventHandler = eventHandler;
-
-    // Window is not focused
-    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
-    option->SetWindowName("OH_WindowManager_LockCursor");
-    sptr<WindowSessionImpl> window = sptr<WindowSessionImpl>::MakeSptr(option);
-    window->property_->SetPersistentId(windowId);
-    window->property_->SetWindowType(WindowType::WINDOW_TYPE_APP_MAIN_WINDOW);
-    window->isFocused_ = false;
-    WindowSessionImpl::windowSessionMap_.clear();
-    WindowSessionImpl::windowSessionMap_.insert(std::make_pair(window->GetWindowName(),
-        std::pair<uint64_t, sptr<WindowSessionImpl>>(window->GetWindowId(), window)));
-    ret = OH_WindowManager_LockCursor(windowId, true);
+/**
+ * @tc.name: OH_WindowManager_UnlockCursor
+ * @tc.desc: OH_WindowManager_UnlockCursor test
+ * @tc.type: FUNC
+ */
+HWTEST_F(OHWindowEventTest, OH_WindowManager_UnlockCursor, TestSize.Level0)
+{
+    int32_t windowId = 1;
+    // Failed to obtain null window
+    auto ret = OH_WindowManager_UnlockCursor(windowId);
     EXPECT_EQ(static_cast<int32_t>(WindowManager_ErrorCode::WINDOW_MANAGER_ERRORCODE_STATE_ABNORMAL), ret);
-
-    // Normal
-    window->isFocused_ = true;
-    ret = OH_WindowManager_LockCursor(windowId, true);
-    EXPECT_EQ(0, ret);
-    WindowSessionImpl::windowSessionMap_.clear();
 }
 }
 } // namespace Rosen
