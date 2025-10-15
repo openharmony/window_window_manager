7f82cab71905f1d4988f2e7581c699a4d99e5801
cuihongbin <cuihongbin@huawei.com>
2025-10-15 11:21:47 +0800

    补充TDD
    Signed-off-by: cuihongbin <cuihongbin@huawei.com>

---
+++ wm/test/unittest/event_distribution/window_session_impl_event_test.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "window_session_impl.h"
+#include "mock_session.h"
+#include "mock_session_stub.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Rosen {
+class WindowSessionImplEventTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+};
+
+void WindowSessionImplEventTest::SetUpTestCase()
+{
+}
+
+void WindowSessionImplEventTest::TearDownTestCase()
+{
+}
+
+void WindowSessionImplEventTest::SetUp()
+{
+}
+
+void WindowSessionImplEventTest::TearDown()
+{
+}
+
+namespace {
+/**
+ * @tc.name: LockCursor
+ * @tc.desc: LockCursor
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSessionImplEventTest, LockCursor, TestSize.Level0)
+{
+    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
+    option->SetWindowName("LockCursor");
+    sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
+    window->hostSession_ = nullptr;
+    auto ret = window->LockCursor(1, true);
+    EXPECT_EQ(ret, WMError::WM_ERROR_INVALID_WINDOW);
+
+    window->property_->persistentId_ = 10;
+    window->state_ = WindowState::STATE_SHOW;
+    sptr<SessionMocker> session = sptr<SessionStubMocker>::MakeSptr();
+    window->hostSession_ = session;
+    ret = window->LockCursor(1, true);
+    EXPECT_EQ(ret, WMError::WM_OK);
+}
+
+/**
+ * @tc.name: LockCursor
+ * @tc.desc: LockCursor
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSessionImplEventTest, UnLockCursor, TestSize.Level0)
+{
+    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
+    option->SetWindowName("LockCursor");
+    sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
+    window->hostSession_ = nullptr;
+    auto ret = window->UnLockCursor(1);
+    EXPECT_EQ(ret, WMError::WM_ERROR_INVALID_WINDOW);
+
+    window->property_->persistentId_ = 10;
+    window->state_ = WindowState::STATE_SHOW;
+    sptr<SessionMocker> session = sptr<SessionStubMocker>::MakeSptr();
+    window->hostSession_ = session;
+    ret = window->UnLockCursor(1);
+    EXPECT_EQ(ret, WMError::WM_OK);
+}
+} // namespace
+} // namespace Rosen
+} // namespace OHOS
