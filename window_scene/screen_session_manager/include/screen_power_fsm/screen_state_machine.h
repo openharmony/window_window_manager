51169a92430d57cee66dfd6d95c6df2a415e864b
yyangxu <yangxu69@hisilicon.com>
2025-10-16 09:25:38 +0800

    update: 更新文件 screen_state_machine.h
    Signed-off-by: yyangxu <yangxu69@hisilicon.com>

--- window_scene/screen_session_manager/include/screen_power_fsm/screen_state_machine.h
+++ window_scene/screen_session_manager/include/screen_power_fsm/screen_state_machine.h
@@ -119,7 +119,7 @@ private:
     static bool DoSuspendBegin(ScreenPowerEvent event, const ScreenPowerInfoType& type);
     static bool DoWaitAodRequest(ScreenPowerEvent event, const ScreenPowerInfoType& type);
 
-    sptr<ScreenStateTimer> timer_;
+    ScreenStateTimer timer_;
     std::mutex mtx;
     bool isForceTrans_ = false;
     uint32_t initRefCnt_ = 0;
