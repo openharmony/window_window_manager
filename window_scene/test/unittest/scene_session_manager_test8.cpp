d70a35a6a6b7ffb9f5c48b66105b634c7741d95a
卢宇豪 <luyuhao6@huawei.com>
2025-10-26 18:07:27 +0800

    chaeg1
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- window_scene/test/unittest/scene_session_manager_test8.cpp
+++ window_scene/test/unittest/scene_session_manager_test8.cpp
@@ -1099,7 +1099,7 @@ HWTEST_F(SceneSessionManagerTest8, PackWindowPropertyChangeInfo01, TestSize.Leve
 
     std::unordered_map<WindowInfoKey, WindowChangeInfoType> windowPropertyChangeInfo;
     ssm_->PackWindowPropertyChangeInfo(sceneSession1, windowPropertyChangeInfo);
-    EXPECT_EQ(windowPropertyChangeInfo.size(), 10);
+    EXPECT_EQ(windowPropertyChangeInfo.size(), 11);
 }
 
 /**
