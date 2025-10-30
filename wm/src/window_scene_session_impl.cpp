708630c20c645b77f3bfc4a1f466a35f508a8d1a
yangwenxin <yangwenxin9@huawei.com>
2025-10-30 09:55:08 +0800

    Fix page system bar propert
    Signed-off-by: yangwenxin <yangwenxin9@huawei.com>

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -1014,7 +1014,8 @@ void WindowSceneSessionImpl::UpdateDefaultStatusBarColor()
     statusBarProp.settingFlag_ = static_cast<SystemBarSettingFlag>(
         static_cast<uint32_t>(statusBarProp.settingFlag_) |
         static_cast<uint32_t>(SystemBarSettingFlag::FOLLOW_SETTING));
-    SetSpecificBarProperty(WindowType::WINDOW_TYPE_STATUS_BAR, statusBarProp);
+    SystemBarPropertyFlag systemBarPropertyFlag = { false, false, true, false };
+    UpdateSystemBarPropertyForPage(WindowType::WINDOW_TYPE_STATUS_BAR, statusBarProp, systemBarPropertyFlag);
 }
 
 void WindowSceneSessionImpl::InitSubSessionDragEnable()
@@ -3399,25 +3400,22 @@ WMError WindowSceneSessionImpl::UpdateSystemBarProperties(
         auto property = GetSystemBarPropertyByType(systemBarType);
         property.enable_ = systemBarPropertyFlag.enableFlag ?
             systemBarProperties.at(systemBarType).enable_ : property.enable_;
+        property.settingFlag_ |= systemBarPropertyFlag.enableFlag ?
+            SystemBarSettingFlag::ENABLE_SETTING : SystemBarSettingFlag::DEFAULT_SETTING;
         property.backgroundColor_ = systemBarPropertyFlag.backgroundColorFlag ?
             systemBarProperties.at(systemBarType).backgroundColor_ : property.backgroundColor_;
         property.contentColor_ = systemBarPropertyFlag.contentColorFlag ?
             systemBarProperties.at(systemBarType).contentColor_ : property.contentColor_;
+        property.settingFlag_ |=
+            (systemBarPropertyFlag.backgroundColorFlag || systemBarPropertyFlag.contentColorFlag) ?
+            SystemBarSettingFlag::COLOR_SETTING : SystemBarSettingFlag::DEFAULT_SETTING;
         property.enableAnimation_ = systemBarPropertyFlag.enableAnimationFlag ?
             systemBarProperties.at(systemBarType).enableAnimation_ : property.enableAnimation_;
 
-        if (systemBarPropertyFlag.enableFlag) {
-            property.settingFlag_ |= SystemBarSettingFlag::ENABLE_SETTING;
-        }
-        if (systemBarPropertyFlag.backgroundColorFlag || systemBarPropertyFlag.contentColorFlag) {
-            property.settingFlag_ |= SystemBarSettingFlag::COLOR_SETTING;
-        }
-        if (systemBarPropertyFlag.enableFlag || systemBarPropertyFlag.backgroundColorFlag ||
-            systemBarPropertyFlag.contentColorFlag || systemBarPropertyFlag.enableAnimationFlag) {
-            auto err = SetSystemBarProperty(systemBarType, property);
-            if (err != WMError::WM_OK) {
-                return err;
-            }
+        auto ret = UpdateSystemBarPropertyForPage(systemBarType, property, systemBarPropertyFlag);
+        if (ret != WMError::WM_OK) {
+            TLOGE(WmsLogTag::WMS_IMMS, "set failed");
+            return ret;
         }
     }
     SystemBarProperty statusProperty = GetSystemBarPropertyByType(WindowType::WINDOW_TYPE_STATUS_BAR);
@@ -3425,6 +3423,34 @@ WMError WindowSceneSessionImpl::UpdateSystemBarProperties(
         GetSystemBarPropertyByType(WindowType::WINDOW_TYPE_NAVIGATION_INDICATOR);
     MobileAppInPadLayoutFullScreenChange(statusProperty.enable_, navigationIndicatorPorperty.enable_);
     return WMError::WM_OK;
+}
+
+WMError WindowSceneSessionImpl::UpdateSystemBarPropertyForPage(WindowType type,
+    const SystemBarProperty& systemBarProperty, const SystemBarPropertyFlag& systemBarPropertyFlag)
+{
+    bool isUsedPageEnabled = false;
+    {
+        std::lock_guard<std::mutex> lock(systemBarPropertyForPageMapMutex_);
+        auto iter = systemBarPropertyForPageMap_.find(type);
+        if (iter != systemBarPropertyForPageMap_.end() && iter->second.has_value()) {
+            auto& prop = *iter->second;
+            prop.enable_ =  systemBarPropertyFlag.enableFlag ? systemBarProperty.enable_ : prop.enable_;
+            prop.backgroundColor_ =  systemBarPropertyFlag.backgroundColorFlag ?
+                systemBarProperty.backgroundColor_ : prop.backgroundColor_;
+            prop.contentColor_ =  systemBarPropertyFlag.contentColorFlag ?
+                systemBarProperty.contentColor_ : prop.contentColor_;
+            prop.enableAnimation_ =  systemBarPropertyFlag.enableAnimationFlag ?
+                systemBarProperty.enableAnimation_ : prop.enableAnimation_;
+            prop.settingFlag_ |=  systemBarProperty.settingFlag_;
+            isUsedPageEnabled = true;
+        }
+    }
+    auto ret = SetSystemBarProperty(type,
+        isUsedPageEnabled ? systemBarPropertyForPageMap_[type].value() : systemBarProperty);
+    if (ret == WMError::WM_OK) {
+        property_->SetSystemBarProperty(type, systemBarProperty);
+    }
+    return ret;
 }
 
 void WindowSceneSessionImpl::MobileAppInPadLayoutFullScreenChange(bool statusBarEnable, bool navigationEnable)
@@ -3485,6 +3511,10 @@ WMError WindowSceneSessionImpl::SetSystemBarPropertyForPage(WindowType type, std
         return WMError::WM_DO_NOTHING;
     }
     if (property == std::nullopt) {
+        {
+            std::lock_guard<std::mutex> lock(systemBarPropertyForPageMapMutex_);
+            systemBarPropertyForPageMap_[type] = std::nullopt;
+        }
         TLOGI(WmsLogTag::WMS_IMMS, "win %{public}u property is nullptr use main window prop", GetWindowId());
         return NotifySpecificWindowSessionProperty(type, GetSystemBarPropertyByType(type));
     }
@@ -3493,6 +3523,10 @@ WMError WindowSceneSessionImpl::SetSystemBarPropertyForPage(WindowType type, std
         GetWindowId(), GetWindowName().c_str(), static_cast<uint32_t>(type),
         property->enable_, property->backgroundColor_,
         property->contentColor_, property->enableAnimation_, property->settingFlag_);
+    {
+        std::lock_guard<std::mutex> lock(systemBarPropertyForPageMapMutex_);
+        systemBarPropertyForPageMap_[type] = property;
+    }
     auto lastProperty = GetSystemBarPropertyByType(type);
     property_->SetSystemBarProperty(type, property.value());
     auto ret = NotifySpecificWindowSessionProperty(type, property.value());
