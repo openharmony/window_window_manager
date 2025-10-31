94aae51148175887185781f90259944c79041544
jjxxx123 <1720356274@qq.com>
2025-10-31 16:42:09 +0800

    fix printKeyCode
    Signed-off-by: jjxxx123 <1720356274@qq.com>

--- wm/src/window_input_channel.cpp
+++ wm/src/window_input_channel.cpp
@@ -67,7 +67,7 @@ __attribute__((no_sanitize("cfi"))) void WindowInputChannel::HandleKeyEvent(
         TLOGE(WmsLogTag::WMS_EVENT, "window_ is nullptr");
         return;
     }
-    WLOGFD("Receive key event, Id: %{public}u,", window_->GetWindowId());
+    WLOGFD("Receive key event, Id: %{public}u", window_->GetWindowId());
     if (window_->GetType() == WindowType::WINDOW_TYPE_DIALOG) {
         if (keyEvent->GetAgentWindowId() != keyEvent->GetTargetWindowId()) {
             window_->NotifyTouchDialogTarget();
