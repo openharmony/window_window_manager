548ecc9cb9b2e279f72d2dcab74444dc40d347c4
qianli22 <qianli22@h-partners.com>
2025-11-18 15:24:18 +0800

    add testcase
    Signed-off-by: qianli22 <qianli22@h-partners.com>

--- window_scene/test/unittest/session_stage_proxy_test.cpp
+++ window_scene/test/unittest/session_stage_proxy_test.cpp
@@ -1023,6 +1023,10 @@ HWTEST_F(SessionStageProxyTest, LinkKeyFrameNode, Function | SmallTest | Level1)
     ASSERT_NE(rsKeyFrameNode, nullptr);
     WSError res = sessionStage_->LinkKeyFrameNode(rsKeyFrameNode);
     ASSERT_EQ(WSError::WS_OK, res);
+
+    rsKeyFrameNode.reset();
+    WSError res = sessionStage_->LinkKeyFrameNode(rsKeyFrameNode);
+    ASSERT_EQ(WSError::WS_ERROR_IPC_FAILED, res);   
 }
 
 /**
