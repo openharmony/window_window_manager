ecf37f76f1786df3569f190c0f17142ad4dd278d
BCHMB <dengbing9@h-partners.com>
2025-11-19 10:58:26 +0800

    支持分屏拖拽还原成自由窗
    Signed-off-by: BCHMB <dengbing9@h-partners.com>

--- window_scene/session/host/src/scene_session.cpp
+++ window_scene/session/host/src/scene_session.cpp
@@ -944,7 +944,7 @@ WSError SceneSession::OnSessionEvent(SessionEvent event, const SessionEventParam
                 session->pcFoldScreenController_->RecordStartMoveRect(currRect, session->IsFullScreenMovable());
             }
             WSRect rect = session->GetMoveRectForWindowDrag();
-            if (session->IsFullScreenMovable()) {
+            if (session->IsFullScreenMovable()) {//
                 session->UpdateFullScreenWaterfallMode(false);
                 rect = session->moveDragController_->GetFullScreenToFloatingRect(session->GetSessionRect(),
                     session->GetSessionRequestRect());
