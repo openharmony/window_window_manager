8c67f185dbf0fd5a119d1baae8b594596c4f0555
f_j_m <fanjiaming10@h-partners.com>
2025-11-20 14:11:56 +0800

    fix5
    Signed-off-by:f_j_m <fanjiaming10@h-partners.com>

--- window_scene/screen_session_manager/include/screen_session_manager.h
+++ window_scene/screen_session_manager/include/screen_session_manager.h
@@ -650,7 +650,7 @@ private:
     bool GetIsCurrentInUseById(ScreenId screenId);
     bool CheckMultiScreenInfoMap(std::map<std::string, MultiScreenInfo> multiScreenInfoMap,
         const std::string& serialNumber);
-    bool CheckResolutionMode(std::map<std::string, MultiScreenInfo> multiScreenInfoMap,
+    bool CheckResolutionMode(std::map<std::string,SupportedScreenModes> resolutionMap,
         const std::string& serialNumber);
     void SetExtendedScreenFallbackPlanEvent(int32_t res);
     void SetInnerScreenFallbackPlan(sptr<ScreenSession> screenSession);
