295d8e18997372264e2e64294976d91f88c15630
f_j_m <fanjiaming10@h-partners.com>
2025-11-20 14:11:56 +0800

    fix5
    Signed-off-by:f_j_m <fanjiaming10@h-partners.com>

--- window_scene/screen_session_manager/src/screen_session_manager.cpp
+++ window_scene/screen_session_manager/src/screen_session_manager.cpp
@@ -1459,7 +1459,7 @@ bool ScreenSessionManager::CheckMultiScreenInfoMap(std::map<std::string, MultiSc
     return true;
 }
 
-bool ScreenSessionManager::CheckResolutionMode(std::map<std::string, MultiScreenInfo> resolutionMap,
+bool ScreenSessionManager::CheckResolutionMode(std::map<std::string, SupportedScreenModes> resolutionMap,
     const std::string& serialNumber)
 {
     if (resolutionMap.empty()) {
