69a4eddc811cac0f42d9c8693b009c1384cbbafd
Peng fafu <pengfafu@h-partners.com>
2025-11-22 15:55:54 +0800

    SetUIExtCustomDensity
    Signed-off-by: Peng fafu <pengfafu@h-partners.com>

--- interfaces/innerkits/wm/window.h
+++ interfaces/innerkits/wm/window.h
@@ -3574,6 +3574,15 @@ public:
     virtual float GetCustomDensity() const { return UNDEFINED_DENSITY; }
 
     /**
+     * @brief UIExtension window call to set custom density, once called this method to set custom density,
+     * UIExtension widnow will dinore FOLLOW_HOST_DPI and use specified density.
+     *
+     * @param density the custom density of UIExtension widnow.
+     * @return WM_OK means set success, others means failed.
+     */
+    virtual WMError SetUIExtCustomDensity(const float density) { return WMError::WM_OK; }
+
+    /**
      * @brief Get the window density of current window.
      *
      * @param densityInfo the struct representing system density, default density and custom density.
