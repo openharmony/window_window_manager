c5e00dd67e92228930a915ae37db811558ee1da3
cuihongbin <cuihongbin@huawei.com>
2025-11-22 10:24:17 +0800

    拆分TDD
    Signed-off-by: cuihongbin <cuihongbin@huawei.com>

--- window_scene/test/unittest/event_distribution/scene_session_dirty_manager_test2.cpp
+++ window_scene/test/unittest/event_distribution/scene_session_dirty_manager_test2.cpp
@@ -21,6 +21,7 @@
 #include "session_manager/include/scene_session_dirty_manager.h"
 #include "screen_session_manager_client/include/screen_session_manager_client.h"
 #include "scene_input_manager.h"
+#include "session/host/include/move_drag_controller.h"
 #include "session/host/include/scene_session.h"
 #include "session_manager/include/scene_session_manager.h"
 #include "transaction/rs_uiextension_data.h"
@@ -774,16 +775,6 @@ HWTEST_F(SceneSessionDirtyManagerTest2, UpdateWindowFlagsForLockCursor, TestSize
     session->SetSessionInfoAdvancedFeatureFlag(100, false);
     session->GetSessionInfoAdvancedFeatureFlag(100);
     session->SetSessionInfoAdvancedFeatureFlag(ADVANCED_FEATURE_BIT_LOCK_CURSOR, false);
-    sptr<MoveDragController> moveDragController;
-    moveDragController = sptr<MoveDragController>::MakeSptr(session->GetPersistentId(), session->GetWindowType());
-    session->moveDragController_ = moveDragController;
-    moveDragController->SetStartMoveFlag(false);
-    EXPECT_FALSE(moveDragController->GetStartMoveFlag());
-    EXPECT_FALSE(session->IsDragMoving());
-    moveDragController->SetStartDragFlag(false);
-    EXPECT_FALSE(moveDragController->GetStartDragFlag());
-    EXPECT_FALSE(session->IsDragZooming());
-
     manager_->UpdateWindowFlagsForLockCursor(session, windowInfo);
     EXPECT_EQ(windowInfo.flags, 0);
 
@@ -806,11 +797,34 @@ HWTEST_F(SceneSessionDirtyManagerTest2, UpdateWindowFlagsForLockCursor, TestSize
     session->SetSessionInfoAdvancedFeatureFlag(ADVANCED_FEATURE_BIT_CURSOR_FOLLOW_MOVEMENT, true);
     manager_->UpdateWindowFlagsForLockCursor(session, windowInfo);
     EXPECT_EQ(windowInfo.flags, MMI_FLAG_BIT_LOCK_CURSOR_FOLLOW_MOVEMENT);
+}
 
-    windowInfo.flags = 0;
-    session->SetSessionInfoCursorDragFlag(true);
+/**
+ * @tc.name: UpdateWindowFlagsForLockCursorInDrag
+ * @tc.desc: UpdateWindowFlagsForLockCursorInDrag
+ * @tc.type: FUNC
+ */
+HWTEST_F(SceneSessionDirtyManagerTest2, UpdateWindowFlagsForLockCursorInDrag, TestSize.Level2)
+{
+    SessionInfo info;
+    sptr<SceneSession> session = sptr<SceneSession>::MakeSptr(info, nullptr);
+    session->persistentId_ = 5;
+    session->UpdateFocus(false);
+    sptr<MoveDragController> moveDragController;
+    moveDragController = sptr<MoveDragController>::MakeSptr(session->GetPersistentId(), WindowType::WINDOW_TYPE_FLOAT);
+    session->moveDragController_ = moveDragController;
     session->SetSessionInfoAdvancedFeatureFlag(ADVANCED_FEATURE_BIT_LOCK_CURSOR, true);
     session->SetSessionInfoAdvancedFeatureFlag(ADVANCED_FEATURE_BIT_CURSOR_FOLLOW_MOVEMENT, true);
+    moveDragController->SetStartMoveFlag(false);
+    EXPECT_FALSE(moveDragController->GetStartMoveFlag());
+    EXPECT_FALSE(session->IsDragMoving());
+    moveDragController->SetStartDragFlag(false);
+    EXPECT_FALSE(moveDragController->GetStartDragFlag());
+    EXPECT_FALSE(session->IsDragZooming());
+
+    MMI::WindowInfo windowInfo;
+    windowInfo.flags = 0;
+    session->SetSessionInfoCursorDragFlag(true);
     manager_->UpdateWindowFlagsForLockCursor(session, windowInfo);
     manager_->UpdateWindowFlagsForLockCursor(session, windowInfo);
     EXPECT_EQ(windowInfo.flags, 0);
@@ -820,8 +834,6 @@ HWTEST_F(SceneSessionDirtyManagerTest2, UpdateWindowFlagsForLockCursor, TestSize
     moveDragController->SetStartMoveFlag(true);
     EXPECT_TRUE(moveDragController->GetStartMoveFlag());
     EXPECT_TRUE(session->IsDragMoving());
-    session->SetSessionInfoAdvancedFeatureFlag(ADVANCED_FEATURE_BIT_LOCK_CURSOR, true);
-    session->SetSessionInfoAdvancedFeatureFlag(ADVANCED_FEATURE_BIT_CURSOR_FOLLOW_MOVEMENT, true);
     manager_->UpdateWindowFlagsForLockCursor(session, windowInfo);
     EXPECT_EQ(windowInfo.flags, 0);
 
@@ -832,8 +844,6 @@ HWTEST_F(SceneSessionDirtyManagerTest2, UpdateWindowFlagsForLockCursor, TestSize
     moveDragController->SetStartDragFlag(true);
     EXPECT_TRUE(moveDragController->GetStartDragFlag());
     EXPECT_TRUE(session->IsDragZooming());
-    session->SetSessionInfoAdvancedFeatureFlag(ADVANCED_FEATURE_BIT_LOCK_CURSOR, true);
-    session->SetSessionInfoAdvancedFeatureFlag(ADVANCED_FEATURE_BIT_CURSOR_FOLLOW_MOVEMENT, true);
     manager_->UpdateWindowFlagsForLockCursor(session, windowInfo);
     EXPECT_EQ(windowInfo.flags, 0);
 }
