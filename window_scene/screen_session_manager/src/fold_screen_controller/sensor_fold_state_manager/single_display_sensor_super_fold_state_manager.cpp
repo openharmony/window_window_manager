16d7b077087832a12750e632c18c3e3788d0d7ff
chenjunliang26 <chenjunliang26@h-partners.com>
2025-11-23 10:18:20 +0800

    增加折叠策略适配
    Signed-off-by: chenjunliang26 <chenjunliang26@h-partners.com>

--- window_scene/screen_session_manager/src/fold_screen_controller/sensor_fold_state_manager/single_display_sensor_super_fold_state_manager.cpp
+++ window_scene/screen_session_manager/src/fold_screen_controller/sensor_fold_state_manager/single_display_sensor_super_fold_state_manager.cpp
@@ -13,15 +13,20 @@
  * limitations under the License.
  */
 
-#include "fold_screen_controller/sensor_fold_state_manager/single_display_sensor_super_fold_state_manager.h"
 #include <parameters.h>
+#include <hisysevent.h>
 
 #include "fold_screen_controller/fold_screen_policy.h"
 #include "fold_screen_controller/fold_screen_controller_config.h"
+#include "fold_screen_controller/sensor_fold_state_manager/single_display_sensor_super_fold_state_manager.h"
 #include "fold_screen_controller/sensor_fold_state_manager/sensor_fold_state_manager.h"
 #include "session/screen/include/screen_session.h"
+#include "screen_scene_config.h"
+#include "sensor_agent.h"
+#include "sensor_agent_type.h"
 
 #include "window_manager_hilog.h"
+#include "screen_session_manager.h"
 
 #ifdef POWER_MANAGER_ENABLE
 #include <power_mgr_client.h>
@@ -33,6 +38,10 @@ constexpr int32_t LARGER_BOUNDARY_FLAG = 1;
 constexpr int32_t SMALLER_BOUNDARY_FLAG = 0;
 constexpr int32_t HALL_THRESHOLD = 1;
 constexpr int32_t HALL_FOLDED_THRESHOLD = 0;
+constexpr float TENT_MODE_EXIT_MIN_THRESHOLD = 5.0F;
+constexpr float TENT_MODE_EXIT_MAX_THRESHOLD = 175.0F;
+constexpr int32_t TENT_MODE_OFF = 0;
+constexpr int32_t TENT_MODE_ON = 1;
 } // namespace
 
 SingleDisplaySensorSuperFoldStateManager::SingleDisplaySensorSuperFoldStateManager() {}
@@ -41,6 +50,10 @@ SingleDisplaySensorSuperFoldStateManager::~SingleDisplaySensorSuperFoldStateMana
 void SingleDisplaySensorSuperFoldStateManager::HandleAngleChange(float angle, int hall,
     sptr<FoldScreenPolicy> foldScreenPolicy)
 {
+    currentAngle = angle;
+    if (IsTentMode()) {
+        return TentModeHandleSensorChange(angle, hall, foldScreenPolicy);
+    }
     FoldStatus nextState = GetNextFoldState(angle, hall);
     HandleSensorChange(nextState, angle, foldScreenPolicy);
 }
@@ -48,6 +61,10 @@ void SingleDisplaySensorSuperFoldStateManager::HandleAngleChange(float angle, in
 void SingleDisplaySensorSuperFoldStateManager::HandleHallChange(float angle, int hall,
     sptr<FoldScreenPolicy> foldScreenPolicy)
 {
+    currentHall = hall;
+    if (IsTentMode()) {
+        return TentModeHandleSensorChange(angle, hall, foldScreenPolicy);
+    }
     FoldStatus nextState = GetNextFoldState(angle, hall);
     HandleSensorChange(nextState, angle, foldScreenPolicy);
 }
@@ -109,4 +126,102 @@ FoldStatus SingleDisplaySensorSuperFoldStateManager::GetNextFoldState(float angl
 }
 
 void SingleDisplaySensorSuperFoldStateManager::RegisterApplicationStateObserver() {}
+
+void SingleDisplaySensorPocketFoldStateManager::HandleTentChange(int tentType,
+    sptr<FoldScreenPolicy> foldScreenPolicy, int32_t hall)
+{
+    if (tentType == tentModeType_) {
+        TLOGI(WmsLogTag::DMS, "Repeat reporting tent mode:%{public}d, no processing", tentModeType_);
+        return;
+    }
+
+    SetTentMode(tentType);
+    if (foldScreenPolicy == nullptr) {
+        TLOGE(WmsLogTag::DMS, "foldScreenPolicy is nullptr");
+        return;
+    }
+    if (tentType != TENT_MODE_OFF) {
+        ReportTentStatusChange(ReportTentModeStatus::NORMAL_ENTER_TENT_MODE);
+        HandleSensorChange(FoldStatus::FOLDED, currentAngle, foldScreenPolicy);
+        foldScreenPolicy->ChangeOnTentMode(FoldStatus::FOLDED);
+        if (tentType == TENT_MODE_ON) {
+            SetDeviceStatusAndParam(static_cast<uint32_t>(DMDeviceStatus::STATUS_TENT));
+            ScreenRotationProperty::HandleHoverStatusEventInput(DeviceHoverStatus::TENT_STATUS);
+        }
+    } else {
+        if (hall == HALL_FOLDED_THRESHOLD) {
+            currentAngle = ANGLE_MIN_VAL;
+        }
+        FoldStatus nextState = FoldStatus::UNKNOWN;
+        if (hall == -1) {
+            nextState = GetNextFoldState(currentAngle, currentHall);
+        } else {
+            nextState = GetNextFoldState(currentAngle, hall);
+        }
+        if (nextState == FoldStatus::FOLDED) {
+            SetDeviceStatusAndParam(static_cast<uint32_t>(DMDeviceStatus::STATUS_FOLDED));
+        } else {
+            SetDeviceStatusAndParam(static_cast<uint32_t>(DMDeviceStatus::UNKNOWN));
+        }
+        HandleSensorChange(nextState, currentAngle, foldScreenPolicy);
+        ReportTentStatusChange(ReportTentModeStatus::NORMAL_EXIT_TENT_MODE);
+        foldScreenPolicy->ChangeOffTentMode();
+        ScreenRotationProperty::HandleHoverStatusEventInput(DeviceHoverStatus::TENT_STATUS_CANCEL);
+    }
+}
+
+bool SingleDisplaySensorPocketFoldStateManager::TriggerTentExit(float angle, int hall)
+{
+    if (hall == HALL_FOLDED_THRESHOLD) {
+        ReportTentStatusChange(ReportTentModeStatus::ABNORMAL_EXIT_TENT_MODE_DUE_TO_HALL);
+        TLOGI(WmsLogTag::DMS, "Exit tent mode due to hall sensor report folded");
+        return true;
+    }
+
+    if (std::isless(angle, TENT_MODE_EXIT_MIN_THRESHOLD) || std::isgreater(angle, TENT_MODE_EXIT_MAX_THRESHOLD)) {
+        ReportTentStatusChange(ReportTentModeStatus::ABNORMAL_EXIT_TENT_MODE_DUE_TO_ANGLE);
+        TLOGI(WmsLogTag::DMS, "Exit tent mode due to angle sensor report angle:%{public}f", angle);
+        return true;
+    }
+
+    return false;
+}
+
+void SingleDisplaySensorPocketFoldStateManager::TentModeHandleSensorChange(float angle, int hall,
+    sptr<FoldScreenPolicy> foldScreenPolicy)
+{
+    if (TriggerTentExit(angle, hall)) {
+        FoldStatus nextState = GetNextFoldState(angle, hall);
+        HandleSensorChange(nextState, angle, foldScreenPolicy);
+        TLOGI(WmsLogTag::DMS, "exit tent mode. angle: %{public}f, hall: %{public}d", angle, hall);
+        SetTentMode(TENT_MODE_OFF);
+        if (nextState == FoldStatus::FOLDED) {
+            SetDeviceStatusAndParam(static_cast<uint32_t>(DMDeviceStatus::STATUS_FOLDED));
+        } else {
+            SetDeviceStatusAndParam(static_cast<uint32_t>(DMDeviceStatus::UNKNOWN));
+        }
+        ScreenRotationProperty::HandleHoverStatusEventInput(DeviceHoverStatus::TENT_STATUS_CANCEL);
+    }
+}
+
+void SingleDisplaySensorPocketFoldStateManager::ReportTentStatusChange(ReportTentModeStatus tentStatus)
+{
+    int32_t status = static_cast<int32_t>(tentStatus);
+    TLOGI(WmsLogTag::DMS, "report tentStatus: %{public}d", status);
+    int32_t ret = HiSysEventWrite(
+        OHOS::HiviewDFX::HiSysEvent::Domain::WINDOW_MANAGER,
+        "FOLD_TENT_MODE",
+        OHOS::HiviewDFX::HiSysEvent::EventType::BEHAVIOR,
+        "FOLD_TENT_STATUS", status);
+    if (ret != 0) {
+        TLOGE(WmsLogTag::DMS, "Write HiSysEvent error, ret: %{public}d", ret);
+    }
+}
+
+void SingleDisplaySensorPocketFoldStateManager::SetDeviceStatusAndParam(uint32_t deviceStatus)
+{
+    TLOGI(WmsLogTag::DMS, "Set device status to: %{public}u", deviceStatus);
+    SetDeviceStatus(deviceStatus);
+    system::SetParameter("persist.dms.device.status", std::to_string(deviceStatus));
+}
 } // namespace OHOS::Rosen
