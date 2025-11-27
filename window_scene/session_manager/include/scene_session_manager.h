659690a2ddf85645f76116c904224276901f98de
wangzilin <wangzilin17@huawei.com>
2025-11-27 22:26:49 +0800

    support sync load starting window by config
    Signed-off-by: wangzilin <wangzilin17@huawei.com>

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -856,6 +856,7 @@ public:
     void ConfigSupportCacheLockedSessionSnapshot();
     void ConfigSupportPreloadStartingWindow();
     void PreLoadStartingWindow(sptr<SceneSession> sceneSession);
+    bool IsSyncLoadStartWindow() { return syncLoadStartingWindow_; };
 
     /*
      * Window Animation
@@ -1775,6 +1776,9 @@ private:
     std::unordered_map<std::string, std::unordered_set<std::string>> startingWindowFollowAppMap_;
     std::unordered_set<std::string> emptyStartupResource_;
     std::atomic<bool> delayRemoveSnapshot_ = false;
+    bool syncLoadStartingWindow_ = false;
+    void InitWindowPattern();
+    void InitStartingWindow();
     void InitStartingWindowRdb(const std::string& rdbPath);
     bool GetStartingWindowInfoFromCache(const SessionInfo& sessionInfo, StartingWindowInfo& startingWindowInfo,
         bool isDark);
