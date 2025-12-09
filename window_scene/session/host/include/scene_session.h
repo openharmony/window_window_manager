3bdab653d49db3e546779e3696753060bfb7998e
laiganlu <Laiganlu@huawei.com>
2025-12-09 20:50:29 +0800

    使用property的pricacymode做截图
    Signed-off-by: laiganlu <Laiganlu@huawei.com>

--- window_scene/session/host/include/scene_session.h
+++ window_scene/session/host/include/scene_session.h
@@ -938,7 +938,6 @@ public:
     */
     void NotifyWindowAttachStateListenerRegistered(bool registered) override;
     WMError NotifySnapshotUpdate() override;
-    bool GetIsPrivacyMode() const override { return isPrivacyMode_; };
     void SetAppControlInfo(ControlAppType type, ControlInfo controlInfo) override
     {
         std::lock_guard lock(appUseControlMapMutex_);
