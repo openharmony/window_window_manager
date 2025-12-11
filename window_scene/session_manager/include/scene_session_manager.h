9aa32681c3bbdfffa9c6b78d97f14ba067eca4ee
rsyys <renzhao5@h-partners.com>
2025-12-11 15:29:57 +0800

    fix: tdd
    Signed-off-by: rsyys <renzhao5@h-partners.com>

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -332,8 +332,6 @@ public:
     WSError ShiftAppWindowFocus(int32_t sourcePersistentId, int32_t targetPersistentId) override;
     void GetFocusWindowInfo(FocusChangeInfo& focusInfo, DisplayId displayId = DEFAULT_DISPLAY_ID) override;
     void GetFocusWindowInfoByAbilityToken(FocusChangeInfo& focusInfo,
-    const sptr<IRemoteObject>& abilityToken) override;
-    void GetFocusWindowInfoByAbilityToken(FocusChangeInfo& focusInfo,
         const sptr<IRemoteObject>& abilityToken) override;
     void GetAllGroupInfo(std::unordered_map<DisplayId, DisplayGroupId>& displayId2GroupIdMap,
                          std::vector<sptr<FocusChangeInfo>>& allFocusInfoList) override;
