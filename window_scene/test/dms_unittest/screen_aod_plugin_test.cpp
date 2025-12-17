c7ed608ed1a2d064f67a9adc35563b2f82ea7361
chenxinjie <chenxinjie11@h-partners.com>
2025-12-17 19:29:14 +0800

    q
    Signed-off-by: chenxinjie <chenxinjie11@h-partners.com>

---
+++ window_scene/test/dms_unittest/screen_aod_plugin_test.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "screen_session_manager.h"
+#include "screen_sensor_plugin.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Rosen {
+class IsInAodTest : public testing::Test {
+public:
+    void SetUp() override;
+    void TearDown() override;
+};
+
+void IsInAodTest::SetUp() {}
+
+void IsInAodTest::TearDown() {}
+
+namespace {
+
+/**
+ * @tc.name: NotifyTest01
+ * @tc.desc: NotifyTest01
+ * @tc.type: FUNC
+ */
+HWTEST_F(IsInAodTest, ATC_IsInAodAndStopAod01, TestSize.Level0)
+{
+    g_handle = nullptr;
+    EXPECT_FALSE(IsInAod());
+    EXPECT_FALSE(StopAod(1));
+    LoadAodLib();
+    EXPECT_TRUE(IsInAod());
+    EXPECT_TRUE(StopAod(1));
+    UnloadAodLib();
+}
+}
+} // namespace Rosen
+} // namespace OHOS
