bc181461cf27637d835a37cf32e83e9c8ef377a9
xujingyi <xujingyi19@h-partners.com>
2025-12-17 16:58:43 +0800

    alter fail test
    Signed-off-by: xujingyi <xujingyi19@h-partners.com>

--- window_scene/test/unittest/scene_session_manager_test6.cpp
+++ window_scene/test/unittest/scene_session_manager_test6.cpp
@@ -150,19 +150,6 @@ HWTEST_F(SceneSessionManagerTest6, UpdateSecSurfaceInfo, TestSize.Level1)
 }
 
 /**
- * @tc.name: SetBehindWindowFilterEnabled
- * @tc.desc: SetBehindWindowFilterEnabled
- * @tc.type: FUNC
- */
-HWTEST_F(SceneSessionManagerTest6, SetBehindWindowFilterEnabled, TestSize.Level1)
-{
-    int ret = 0;
-    ssm_->SetBehindWindowFilterEnabled(true);
-    ssm_->SetBehindWindowFilterEnabled(false);
-    ASSERT_EQ(ret, 0);
-}
-
-/**
  * @tc.name: GetWindowLayerChangeInfo
  * @tc.desc: Simulate window Layer change
  * @tc.type: FUNC
