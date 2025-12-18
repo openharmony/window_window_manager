ff40f9f240bf35ec2a77cfdc2a0d46f4353ebb46
lora8888 <louran@h-partners.com>
2025-12-18 11:16:25 +0800

    update: 更新文件 scene_session_manager_stub_test.cpp 判断设备是否支持画中画
    Signed-off-by: lora8888 <louran@h-partners.com>

--- window_scene/test/unittest/scene_session_manager_stub_test.cpp
+++ window_scene/test/unittest/scene_session_manager_stub_test.cpp
@@ -2862,13 +2862,13 @@ HWTEST_F(SceneSessionManagerStubTest, HandleGetPiPSettingSwitchStatus, Function 
 */
 HWTEST_F(SceneSessionManagerStubTest, HandleGetIsPipEnabled, Function | SmallTest | Level2)
 {
-MessageParcel data;
-MessageParcel reply;
-bool isPipEnabled = false;
-data.WriteBool(isPipEnabled);
-
-int res = stub_->HandleGetIsPipEnabled(data, reply);
-EXPECT_EQ(res, ERR_NONE);
+    MessageParcel data;
+    MessageParcel reply;
+    bool isPipEnabled = false;
+    data.WriteBool(isPipEnabled);
+    
+    int res = stub_->HandleGetIsPipEnabled(data, reply);
+    EXPECT_EQ(res, ERR_NONE);
 }
 
 /**
