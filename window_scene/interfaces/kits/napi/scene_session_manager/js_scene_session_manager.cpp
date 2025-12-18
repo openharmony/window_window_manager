b6f8f33a1dc14abd0da388d8bdf6156e091216a2
lora8888 <louran@h-partners.com>
2025-12-18 18:32:50 +0800

    update: 更新文件 js_scene_session_manager.cpp
    Signed-off-by: lora8888 <louran@h-partners.com>

--- window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session_manager.cpp
+++ window_scene/interfaces/kits/napi/scene_session_manager/js_scene_session_manager.cpp
@@ -5664,8 +5664,8 @@ napi_value JsSceneSessionManager::SetIsPipEnabled(napi_env env, napi_callback_in
  
 napi_value JsSceneSessionManager::OnSetIsPipEnabled(napi_env env, napi_callback_info info)
 {
-    size_t argc = ARGC_FOUR;
-    napi_value argv[ARGC_FOUR] = { nullptr };
+    size_t argc = ARGC_ONE;
+    napi_value argv[ARGC_ONE] = { nullptr };
     napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
     if (argc != ARGC_ONE) {
         TLOGE(WmsLogTag::WMS_PIP, "Argc is invalid: %{public}zu", argc);
