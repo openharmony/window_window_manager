9a6b24217b6a0781110632c166e38a959c2307f0
tanhon <tanhong6@h-partners.com>
2025-12-20 15:21:14 +0800

    ii
    Signed-off-by: tanhon <tanhong6@h-partners.com>

--- window_scene/session/host/src/scene_session.cpp
+++ window_scene/session/host/src/scene_session.cpp
@@ -3196,8 +3196,7 @@ int32_t SceneSession::GetUIExtPersistentIdBySurfaceNodeId(uint64_t surfaceNodeId
 void SceneSession::GetScaleInLSState(float& scaleX, float& scaleY) const
 {
     if (GetWindowMode() != WindowMode::WINDOW_MODE_FULLSCREEN || GetIsMidScene() ||
-        !specificCallback_ ||
-        !specificCallback_->onGetLSState_ || !specificCallback_->onGetLSState_()) {
+        !specificCallback_ || !specificCallback_->onGetLSState_ || !specificCallback_->onGetLSState_()) {
         TLOGD(WmsLogTag::WMS_IMMS, "win: %{public}d, not in LS state", GetPersistentId());
         return;
     }
