9c69b5c1bb1f89644938a36a2b051b94c1c74bf3
LuJingl30079059 <lujing63@h-partners.com>
2025-12-27 15:23:58 +0800

    update: 更新文件 screen_session_ability_connection.cpp
    Signed-off-by: LuJingl30079059 <lujing63@h-partners.com>

--- window_scene/screen_session_manager/src/connection/screen_session_ability_connection.cpp
+++ window_scene/screen_session_manager/src/connection/screen_session_ability_connection.cpp
@@ -156,8 +156,7 @@ int32_t ScreenSessionAbilityConnectionStub::SendMessageSync(int32_t transCode,
 }
 
 int32_t ScreenSessionAbilityConnectionStub::SendMessageSyncBlock(int32_t transCode,
-                                                                 MessageParcel& data,
-                                                                 MessageParcel& reply)
+    MessageParcel& data, MessageParcel& reply)
 {
     std::unique_lock<std::mutex> lock(connectedMutex_);
     if (!isConnected_) {
@@ -176,10 +175,8 @@ int32_t ScreenSessionAbilityConnectionStub::SendMessageSyncBlock(int32_t transCo
             TLOGE(WmsLogTag::DMS, "remoteObject is nullptr");
             return RES_FAILURE;
         }
-        TLOG(
-            WmsLogTag::DMS, "snapshot token: %{public}s", Str16ToStr8(remoteObject_->GetInterfaceDescriptor()).c_str());
         if (!data.WriteInterfaceToken(remoteObject_->GetInterfaceDescriptor())) {
-            TLOG(WmsLogTag::DMS, "WriteInterfaceToken failed");
+            TLOGE(WmsLogTag::DMS, "WriteInterfaceToken failed");
             return RES_FAILURE;
         }
         int32_t ret = remoteObject_->SendRequest(transCode, data, reply, option);
