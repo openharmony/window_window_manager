e1a56bcf4021da65b7e1cbdbcb0b87996dddd2fd
hxp2 <921993756@qq.com>
2025-12-27 23:20:44 +0800

    重构load_intention_event的实现
    Signed-off-by: hxp <haoxinpeng1@huawei.com>

--- wm/src/load_intention_event.cpp
+++ wm/src/load_intention_event.cpp
@@ -19,9 +19,14 @@ namespace OHOS {
 namespace Rosen {
 namespace {
     constexpr uint32_t SLEEP_TIME_US = 10000;
+    constexpr uint32_t RETRY_TIMES = 3;
 }
 
 static void *g_handle = nullptr;
+using CreateAndEnableInputEventListenerFunc =  bool (*)(
+    Ace::UIContent*, std::shared_ptr<AppExecFwk::EventHandler>, wptr<Window>);
+static CreateAndEnableInputEventListenerFunc g_createAndEnableInputEventListenerFunc = nullptr;
+
 
 bool LoadIntentionEvent(void)
 {
@@ -30,13 +35,17 @@ bool LoadIntentionEvent(void)
         return true;
     }
     int32_t cnt = 0;
-    int32_t retryTimes = 3;
+    const char* dlopenError = nullptr;
     do {
         cnt++;
         g_handle = dlopen(INTENTION_EVENT_SO_PATH.c_str(), RTLD_LAZY);
+        dlopenError = dlerror();
+        if (dlopenError) {
+            TLOGE(WmsLogTag::WMS_EVENT, "dlopen error: %{public}s", dlopenError);
+        }
         TLOGI(WmsLogTag::WMS_EVENT, "dlopen %{public}s, retry cnt: %{public}d", INTENTION_EVENT_SO_PATH.c_str(), cnt);
         usleep(SLEEP_TIME_US);
-    } while (!g_handle && cnt < retryTimes);
+    } while (!g_handle && cnt < RETRY_TIMES);
     return g_handle != nullptr;
 }
 
@@ -47,6 +56,7 @@ void UnloadIntentionEvent(void)
         dlclose(g_handle);
         g_handle = nullptr;
     }
+    g_createAndEnableInputEventListenerFunc = nullptr;
 }
 
 __attribute__((no_sanitize("cfi"))) bool EnableInputEventListener(
@@ -56,14 +66,25 @@ __attribute__((no_sanitize("cfi"))) bool EnableInputEventListener(
         TLOGE(WmsLogTag::WMS_EVENT, "g_handle is nullptr");
         return false;
     }
-    CreateAndEnableInputEventListenerPtr func =
-        (CreateAndEnableInputEventListenerPtr)(dlsym(g_handle, "CreateAndEnableInputEventListener"));
-    const char* dlsymError = dlerror();
-    if  (dlsymError) {
-        TLOGE(WmsLogTag::WMS_EVENT, "dlsym error: %{public}s", dlsymError);
+    if (g_createAndEnableInputEventListenerFunc == nullptr) {
+        int32_t cnt = 0;
+        const char* dlsymError = nullptr;
+        do {
+            cnt++;
+            g_createAndEnableInputEventListenerFunc = reinterpret_cast<CreateAndEnableInputEventListenerFunc>(
+                dlsym(g_handle, "CreateAndEnableInputEventListener"));
+            dlsymError = dlerror();
+            if (dlsymError) {
+                TLOGE(WmsLogTag::WMS_EVENT, "dlsym error: %{public}s", dlsymError);
+                usleep(SLEEP_TIME_US);
+            }
+            TLOGI(WmsLogTag::WMS_EVENT, "dlsym %{public}s, retry cnt: %{public}d", "CreateAndEnableInputEventListener", cnt);
+        } while (!g_createAndEnableInputEventListenerFunc && cnt < RETRY_TIMES);
+    }
+    if (g_createAndEnableInputEventListenerFunc == nullptr) {
         return false;
     }
-    return func(uiContent, eventHandler, window);
+    return g_createAndEnableInputEventListenerFunc(uiContent, eventHandler, window);
 }
 } // namespace Rosen
 } // namespace OHOS
