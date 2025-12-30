2988b4f7f4d02d36c570862e3dd310bdaaa22a8e
xujingyi <xujingyi19@h-partners.com>
2025-12-30 15:22:45 +0800

    update: 更新文件 window_option_event_test.cpp
    Signed-off-by: xujingyi <xujingyi19@h-partners.com>

--- wm/test/unittest/event_distribution/window_option_event_test.cpp
+++ wm/test/unittest/event_distribution/window_option_event_test.cpp
@@ -25,8 +25,8 @@ class WindowOptionEventTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
-    virtual void SetUp() override;
-    virtual void TearDown() override;
+    void SetUp() override;
+    void TearDown() override;
 };
 void WindowOptionEventTest::SetUpTestCase() {}
 
