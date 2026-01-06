88dab79bac621121ed8e5a1a88de2240da617022
chenxinjie253 <chenxinjie11@h-partners.com>
2026-01-06 10:17:27 +0800

    update: 更新文件 task_sequence_process_test.cpp
    Signed-off-by: chenxinjie253 <chenxinjie11@h-partners.com>

--- utils/test/unittest/task_sequence_process_test.cpp
+++ utils/test/unittest/task_sequence_process_test.cpp
@@ -47,7 +47,7 @@ namespace {
  */
 HWTEST_F(TaskSequenceProcessTest, ATC_TaskSequenceProcess01, TestSize.Level0)
 {
-    TaskSequenceProcess process = TaskSequenceProcess(0, 1000);
+    TaskSequenceProcess process = TaskSequenceProcess(0, 1000, "test1");
     EXPECT_EQ(process.maxQueueSize_, 1);
 }
 
@@ -58,7 +58,7 @@ HWTEST_F(TaskSequenceProcessTest, ATC_TaskSequenceProcess01, TestSize.Level0)
  */
 HWTEST_F(TaskSequenceProcessTest, ATC_TaskSequenceProcess02, TestSize.Level0)
 {
-    TaskSequenceProcess process = TaskSequenceProcess(10, 1000);
+    TaskSequenceProcess process = TaskSequenceProcess(10, 1000, "test2");
     EXPECT_EQ(process.maxQueueSize_, 10);
 }
 
@@ -69,7 +69,7 @@ HWTEST_F(TaskSequenceProcessTest, ATC_TaskSequenceProcess02, TestSize.Level0)
  */
 HWTEST_F(TaskSequenceProcessTest, ATC_AddTask01, TestSize.Level0)
 {
-    TaskSequenceProcess process = TaskSequenceProcess(1, 1000);
+    TaskSequenceProcess process = TaskSequenceProcess(1, 1000, "test3");
     bool taskCallback =  false;
     std::function<void()> task = [&taskCallback]() {
         taskCallback =true;
@@ -85,7 +85,7 @@ HWTEST_F(TaskSequenceProcessTest, ATC_AddTask01, TestSize.Level0)
  */
 HWTEST_F(TaskSequenceProcessTest, ATC_FinishTask01, TestSize.Level0)
 {
-    TaskSequenceProcess process = TaskSequenceProcess(1, 1000);
+    TaskSequenceProcess process = TaskSequenceProcess(1, 1000, "test4");
     bool taskCallback =  false;
     std::function<void()> task = [&taskCallback]() {
         taskCallback =true;
