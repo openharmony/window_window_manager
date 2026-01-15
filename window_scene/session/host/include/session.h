f0aea5c98e9bd3473cee6fa70c4dfa47a9dcf077
fwg5587 <fanweiguo@h-partners.com>
2026-01-15 15:27:07 +0800

    fix window alarm
    Signed-off-by: fwg5587 <fanweiguo@h-partners.com>

--- window_scene/session/host/include/session.h
+++ window_scene/session/host/include/session.h
@@ -631,11 +631,6 @@ public:
 
     virtual void SetTouchHotAreas(const std::vector<Rect>& touchHotAreas);
 
-    void SetVpr(float vpr)
-    {
-        vpr_ = vpr;
-    }
-
     bool operator==(const Session* session) const
     {
         if (session == nullptr) {
