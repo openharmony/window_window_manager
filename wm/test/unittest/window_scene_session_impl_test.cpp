56981c805a09a263e0f21a8631a58b022f8e9ccc
reccoder <jianhao2@h-partners.com>
2026-01-19 09:52:12 +0800

    revert unavailable testcases
    Signed-off-by: reccoder <jianhao2@h-partners.com>

--- wm/test/unittest/window_scene_session_impl_test.cpp
+++ wm/test/unittest/window_scene_session_impl_test.cpp
@@ -1725,6 +1725,31 @@ HWTEST_F(WindowSceneSessionImplTest, GetTopwindowWithId, TestSize.Level1)
 }
 
 /**
+ * @tc.name: GetAvoidAreaByType
+ * @tc.desc: GetAvoidAreaByType test
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSceneSessionImplImmersiveTest, GetAvoidAreaByType, TestSize.Level1)
+{
+    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
+    option->SetWindowMode(WindowMode::WINDOW_MODE_PIP);
+    option->SetWindowName("GetAvoidAreaByType");
+    sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
+
+    window->property_->SetPersistentId(1);
+    SessionInfo sessionInfo = { "CreateTestBundle", "CreateTestModule", "CreateTestAbility" };
+    sptr<SessionMocker> session = sptr<SessionMocker>::MakeSptr(sessionInfo);
+    AvoidArea avoidarea;
+    ASSERT_EQ(WMError::WM_ERROR_INVALID_WINDOW, window->GetAvoidAreaByType(AvoidAreaType::TYPE_CUTOUT, avoidarea));
+    window->hostSession_ = session;
+    ASSERT_EQ(WMError::WM_OK, window->GetAvoidAreaByType(AvoidAreaType::TYPE_CUTOUT, avoidarea));
+    window->property_->SetWindowType(WindowType::WINDOW_TYPE_FLOAT);
+    Rect rect;
+    ASSERT_EQ(WMError::WM_OK, window->GetAvoidAreaByType(AvoidAreaType::TYPE_CUTOUT, avoidarea, rect, 15));
+    ASSERT_EQ(WMError::WM_OK, window->GetAvoidAreaByType(AvoidAreaType::TYPE_CUTOUT, avoidarea, rect, 16));
+}
+
+/**
  * @tc.name: GetAvoidAreaByTypeIgnoringVisibility
  * @tc.desc: GetAvoidAreaByTypeIgnoringVisibility test
  * @tc.type: FUNC
