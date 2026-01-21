e70c3508dd3827e1ff562869916f0403b40893e8
chaos_xj <chaos_xj@noreply.gitcode.com>
2026-01-21 17:40:01 +0800

    fix feature
    Signed-off-by: chaos <zhongchao26@huawei.com>

--- utils/include/task_sequence_process.h
+++ utils/include/task_sequence_process.h
@@ -1,17 +1,17 @@
-/*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ /*
+* Copyright (c) 2025 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 #ifndef TASK_SEQUENCE_PROCESS_H
 #define TASK_SEQUENCE_PROCESS_H
@@ -22,30 +22,41 @@
 
 namespace OHOS::Rosen {
 
+struct TaskInfo {
+    uint64_t sn{0};
+    std::function<void()> task{nullptr};
+    TaskInfo(uint64_t sn, const std::function<void()>& task): sn(sn), task(task) {}
+};
+
 class TaskSequenceProcess {
 public:
     explicit TaskSequenceProcess(uint32_t maxQueueSize, uint64_t maxTimeInterval, std::string timerName);
+    explicit TaskSequenceProcess(uint32_t maxQueueSize, uint32_t maxQueueNumber, uint64_t maxTimeInterval,
+        std::string timerName);
     ~TaskSequenceProcess();
     void AddTask(const std::function<void()>& task);
+    void AddTask(uint64_t id, const std::function<void()>& task);
     void FinishTask();
 
 private:
+    bool FindMinSnTaskQueueId(uint64_t& minSnTaskQueueId);
     std::function<void()> PopFromQueue();
-    void PushToQueue(const std::function<void()>& task);
+    void PushToQueue(uint64_t sn, const TaskInfo& taskInfo);
     void ExecTask();
     bool CreateSysTimer();
     void DestroySysTimer();
     bool StartSysTimer();
     void StopSysTimer();
     uint32_t maxQueueSize_ {1};
+    uint32_t maxQueueNumber_ {1};
     uint64_t maxTimeInterval_ {1000};
     uint64_t taskTimerId_ {0};
     std::string timerName_;
-    std::queue<std::function<void()>> taskQueue_;
-    std::mutex queueMutex_;
+    std::map<uint64_t, std::queue<TaskInfo>> taskQueueMap_;
+    std::mutex taskQueueMapMutex_;
     std::mutex timerMutex_;
     std::atomic<bool> taskRunningFlag_ {false};
+    std::atomic<uint64_t> sn_{0};
     WindowSysTimer TaskTimer;
 };
 } //namespace OHOS::Rosen
-#endif
--- utils/src/task_sequence_process.cpp
+++ utils/src/task_sequence_process.cpp
@@ -1,28 +1,46 @@
-/*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ /*
+* Copyright (c) 2025 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 #include "task_sequence_process.h"
 #include "window_manager_hilog.h"
 
 namespace OHOS::Rosen {
+constexpr uint64_t INVALID_TASK_SN = UINT64_MAX;
+constexpr uint32_t INVALID_QUEUE_SIZE = 0;
+constexpr uint32_t INVALID_QUEUE_NUMBER = 0;
+constexpr uint64_t DEFAULT_QUEUE_ID = 0;
+constexpr uint64_t DEFAULT_TASK_SN = 0;
+constexpr uint32_t DEFAULT_QUEUE_SIZE = 1;
+constexpr uint32_t DEFAULT_QUEUE_NUMBER = 1;
+
 TaskSequenceProcess::TaskSequenceProcess(uint32_t maxQueueSize, uint64_t maxTimeInterval, std::string timerName)
     : maxQueueSize_(maxQueueSize), maxTimeInterval_(maxTimeInterval), timerName_(timerName)
 {
-    if (maxQueueSize_ <= 0) {
-        maxQueueSize_ = 1;
+    maxQueueSize_ = std::max(DEFAULT_QUEUE_SIZE, maxQueueSize);
+    if (!CreateSysTimer()) {
+        TLOGE(WmsLogTag::DMS, "TaskSequenceProcess created fail, maxQueueSize: %{public}u", maxQueueSize_);
     }
+}
+
+TaskSequenceProcess::TaskSequenceProcess(uint32_t maxQueueSize, uint32_t maxQueueNumber, uint64_t maxTimeInterval,
+    std::string timerName)
+    : maxQueueSize_(maxQueueSize), maxQueueNumber_(maxQueueNumber), maxTimeInterval_(maxTimeInterval),
+    timerName_(timerName)
+{
+    maxQueueSize_ = std::max(DEFAULT_QUEUE_SIZE, maxQueueSize);
+    maxQueueNumber_ = std::max(DEFAULT_QUEUE_NUMBER, maxQueueNumber);
     if (!CreateSysTimer()) {
         TLOGE(WmsLogTag::DMS, "TaskSequenceProcess created fail, maxQueueSize: %{public}u", maxQueueSize_);
     }
@@ -33,37 +51,72 @@ TaskSequenceProcess::~TaskSequenceProcess()
     DestroySysTimer();
 };
 
+bool TaskSequenceProcess::FindMinSnTaskQueueId(uint64_t& minSnTaskQueueId)
+{
+    if (taskQueueMap_.empty()) {
+        return false;
+    }
+    auto pair = taskQueueMap_.begin();
+    minSnTaskQueueId = pair->first;
+    uint64_t minSn = INVALID_TASK_SN;
+    for (auto it = taskQueueMap_.begin(); it != taskQueueMap_.end(); ++it) {
+        if (it->second.empty()) {
+            continue;
+        }
+        uint64_t currentSn = it->second.front().sn;
+        if (currentSn < minSn) {
+            minSn = currentSn;
+            minSnTaskQueueId = it->first;
+        }
+    }
+    return minSn == INVALID_TASK_SN ? false : true;
+}
+
 std::function<void()> TaskSequenceProcess::PopFromQueue()
 {
-    std::lock_guard<std::mutex> lock(queueMutex_);
-    if (taskQueue_.empty() || taskRunningFlag_.load()) {
+    std::lock_guard<std::mutex> lock(taskQueueMapMutex_);
+    uint64_t queueId = DEFAULT_QUEUE_ID;
+    if (!FindMinSnTaskQueueId(queueId)) {
+        TLOGE(WmsLogTag::DMS, "TaskSequenceProcess is empty");
+        return nullptr;
+    }
+    if (taskRunningFlag_.load()) {
         TLOGI(WmsLogTag::DMS, "TaskSequenceProcess do not pop");
         return nullptr;
     }
-    std::function<void()> task = taskQueue_.front();
+    std::function<void()> task = taskQueueMap_[queueId].front().task;
     taskRunningFlag_.store(true);
     if (!StartSysTimer()) {
         taskRunningFlag_.store(false);
         TLOGE(WmsLogTag::DMS, "TaskSequenceProcess do not StartSysTimer succ");
     }
-    taskQueue_.pop();
+    taskQueueMap_[queueId].pop();
     return task;
 }
 
-void TaskSequenceProcess::PushToQueue(const std::function<void()>& task)
+void TaskSequenceProcess::PushToQueue(uint64_t id, const TaskInfo& taskInfo)
 {
-    std::lock_guard<std::mutex> lock(queueMutex_);
-    if (taskQueue_.size() >= maxQueueSize_) {
-        taskQueue_.pop();
+    std::lock_guard<std::mutex> lock(taskQueueMapMutex_);
+    if (taskQueueMap_.size() >= maxQueueNumber_) {
+        for (auto it = taskQueueMap_.begin(); it != taskQueueMap_.end();) {
+            it = it->second.empty() ? taskQueueMap_.erase(it) : ++it;
+        }
     }
-    taskQueue_.push(task);
+    if (taskQueueMap_.find(id) == taskQueueMap_.end() && taskQueueMap_.size() >= maxQueueNumber_) {
+        TLOGW(WmsLogTag::DMS, "Task push fail, maxQueueNumber: %{public}u", maxQueueNumber_);
+        return;
+    }
+    if (taskQueueMap_[id].size() >= maxQueueSize_) {
+        taskQueueMap_[id].pop();
+    }
+    taskQueueMap_[id].push(taskInfo);
 }
 
 void TaskSequenceProcess::ExecTask()
 {
     std::function<void()> task = PopFromQueue();
     if (!task) {
-        TLOGD(WmsLogTag::DMS, "TaskSequenceProcess do not execute");
+        TLOGE(WmsLogTag::DMS, "TaskSequenceProcess do not execute");
         return;
     }
     task();
@@ -71,7 +124,15 @@ void TaskSequenceProcess::ExecTask()
 
 void TaskSequenceProcess::AddTask(const std::function<void()>& task)
 {
-    PushToQueue(task);
+    PushToQueue(DEFAULT_QUEUE_ID, {DEFAULT_TASK_SN, task});
+    ExecTask();
+}
+
+void TaskSequenceProcess::AddTask(uint64_t id, const std::function<void()>& task)
+{
+    uint64_t currentSn = sn_.fetch_add(1);
+    currentSn = (currentSn + 1) % INVALID_TASK_SN;
+    PushToQueue(id, {currentSn, task});
     ExecTask();
 }
 
@@ -92,7 +153,10 @@ bool TaskSequenceProcess::CreateSysTimer()
         return false;
     }
     std::shared_ptr<WindowSysTimer> taskSysTimer = std::make_unique<WindowSysTimer>(false, maxTimeInterval_, false);
-    std::function<void()> callback = [this]() { taskRunningFlag_.store(false); };
+    std::function<void()> callback = [this]() {
+        taskRunningFlag_.store(false);
+        ExecTask();
+    };
     taskSysTimer->SetCallbackInfo(callback);
     taskSysTimer->SetName(timerName_);
     taskTimerId_ = MiscServices::TimeServiceClient::GetInstance()->CreateTimer(taskSysTimer);
@@ -114,7 +178,7 @@ bool TaskSequenceProcess::StartSysTimer()
         TLOGW(WmsLogTag::DMS, "TaskTimerId is zero");
         return false;
     }
-    uint64_t currentTime = static_cast<uint64_t>(MiscServices::TimeServiceClient::GetInstance()->GetBootTimeMs());
+    auto currentTime = MiscServices::TimeServiceClient::GetInstance()->GetBootTimeMs();
     uint64_t triggerTime = currentTime + maxTimeInterval_;
     bool result = MiscServices::TimeServiceClient::GetInstance()->StartTimer(taskTimerId_, triggerTime);
     return result;
