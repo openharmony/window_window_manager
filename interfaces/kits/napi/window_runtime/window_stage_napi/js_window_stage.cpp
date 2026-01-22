1d2cb3e118d51a8722dbf64b4ac29d452c81bae8
fwg5587 <fanweiguo@h-partners.com>
2026-01-22 15:27:28 +0800

    add setImageForRecent log
    Signed-off-by: fwg5587 <fanweiguo@h-partners.com>

--- interfaces/kits/napi/window_runtime/window_stage_napi/js_window_stage.cpp
+++ interfaces/kits/napi/window_runtime/window_stage_napi/js_window_stage.cpp
@@ -1170,7 +1170,7 @@ napi_value JsWindowStage::OnSetImageForRecent(napi_env env, napi_callback_info i
         if (ret == WmErrorCode::WM_OK) {
             task->Resolve(env, NapiGetUndefined(env));
         } else {
-            TLOGE(WmsLogTag::WMS_PATTERN, "%{public}s set image for recent failed, ret=%{public}d,type=%{public}d", where, ret, pixelMap ? "pixelMap" : "resId");
+            TLOGE(WmsLogTag::WMS_PATTERN, "%{public}s set image for recent failed, ret=%{public}d, type=%{public}s",where, ret, pixelMap ? "pixelMap" : "resId");
             task->Reject(env, JsErrUtils::CreateJsError(env, ret, "set image for recent failed."));
         }
     };
