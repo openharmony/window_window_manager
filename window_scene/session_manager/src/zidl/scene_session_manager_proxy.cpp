2bbd0bc4403b96f05de4a93f3faecfcaaab8457d
卢宇豪 <luyuhao6@huawei.com>
2026-01-29 11:44:14 +0800

    chaneg1
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- window_scene/session_manager/src/zidl/scene_session_manager_proxy.cpp
+++ window_scene/session_manager/src/zidl/scene_session_manager_proxy.cpp
@@ -3501,9 +3501,9 @@ WMError SceneSessionManagerProxy::SetScreenPrivacyWindowTagSwitch(
         TLOGE(WmsLogTag::WMS_ATTRIBUTE, "write size failed");
         return WMError::WM_ERROR_IPC_FAILED;
     }
-    for (const auto privacyWidnowTag: privacyWindowTags) {
+    for (const auto& privacyWidnowTag: privacyWindowTags) {
         if (!data.WriteString(privacyWidnowTag)) {
-            TLOGE(WmsLogTag::WMS_ATTRIBUTE, "write privacyWidnowTag failed");
+            TLOGE(WmsLogTag::WMS_ATTRIBUTE, "write privacyWind   owTag failed");
             return WMError::WM_ERROR_IPC_FAILED;
         }
     }
@@ -4287,13 +4287,13 @@ WMError SceneSessionManagerProxy::AddSessionBlackList(
     }
     sptr<IRemoteObject> remote = Remote();
     if (remote == nullptr) {
-        TLOGE(WmsLogTag::WMS_ANIMATION, "Remote is null");
+        TLOGE(WmsLogTag::WMS_ATTRIBUTE, "Remote is null");
         return WMError::WM_ERROR_IPC_FAILED;
     }
     if (remote->SendRequest(
         static_cast<uint32_t>(SceneSessionManagerMessage::TRANS_ID_ADD_SESSION_BLACK_LIST),
         data, reply, option) != ERR_NONE) {
-        TLOGE(WmsLogTag::WMS_ANIMATION, "SendRequest failed");
+        TLOGE(WmsLogTag::WMS_ATTRIBUTE, "SendRequest failed");
         return WMError::WM_ERROR_IPC_FAILED;
     }
     int32_t ret = 0;
@@ -4336,13 +4336,13 @@ WMError SceneSessionManagerProxy::RemoveSessionBlackList(
     }
     sptr<IRemoteObject> remote = Remote();
     if (remote == nullptr) {
-        TLOGE(WmsLogTag::WMS_ANIMATION, "Remote is null");
+        TLOGE(WmsLogTag::WMS_ATTRIBUTE, "Remote is null");
         return WMError::WM_ERROR_IPC_FAILED;
     }
     if (remote->SendRequest(
         static_cast<uint32_t>(SceneSessionManagerMessage::TRANS_ID_REMOVE_SESSION_BLACK_LIST),
         data, reply, option) != ERR_NONE) {
-        TLOGE(WmsLogTag::WMS_ANIMATION, "SendRequest failed");
+        TLOGE(WmsLogTag::WMS_ATTRIBUTE, "SendRequest failed");
         return WMError::WM_ERROR_IPC_FAILED;
     }
     int32_t ret = 0;
