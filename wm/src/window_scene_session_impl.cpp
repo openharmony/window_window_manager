f9fe08596eda519dc35e582079d859b228420566
X__xx <xiaozhiwen7@h-partners.com>
2026-02-13 11:54:18 +0800

    update: 更新文件 window_scene_session_impl.cpp
    Signed-off-by: X__xx <xiaozhiwen7@h-partners.com>

--- wm/src/window_scene_session_impl.cpp
+++ wm/src/window_scene_session_impl.cpp
@@ -1434,34 +1434,15 @@ void WindowSceneSessionImpl::GetConfigurationFromAbilityInfo()
         UpdateWindowSizeLimits();
         UpdateProperty(WSPropertyChangeAction::ACTION_UPDATE_WINDOW_LIMITS);
         // get support modes configuration
-        uint32_t windowModeSupportType = 0;
-        std::vector<AppExecFwk::SupportWindowMode> supportedWindowModes;
-        property_->GetSupportedWindowModes(supportedWindowModes);
-        auto size = supportedWindowModes.size();
-        if (size > 0 && size <= WINDOW_SUPPORT_MODE_MAX_SIZE) {
-            windowModeSupportType = WindowHelper::ConvertSupportModesToSupportType(supportedWindowModes);
-        } else {
-            std::vector<AppExecFwk::SupportWindowMode> updateWindowModes =
-                ExtractSupportWindowModeFromMetaData(abilityInfo);
-            if (auto hostSession = GetHostSession()) {
-                hostSession->NotifySupportWindowModesChange(updateWindowModes);
-            };
-            windowModeSupportType = WindowHelper::ConvertSupportModesToSupportType(updateWindowModes);
-        }
-        if (windowModeSupportType == 0) {
-            TLOGI(WmsLogTag::WMS_LAYOUT_PC, "mode config param is 0, all modes is supported");
-            windowModeSupportType = WindowModeSupport::WINDOW_MODE_SUPPORT_ALL;
-        } else {
-            TLOGI(WmsLogTag::WMS_LAYOUT_PC, "winId: %{public}u, windowModeSupportType: %{public}u",
-                GetWindowId(), windowModeSupportType);
-        }
+        uint32_t windowModeSupportType = GetSupportedWindowModesConfiguration(abilityInfo);
         property_->SetWindowModeSupportType(windowModeSupportType);
         // anco support multiWindow config
-        static const bool isAncoSupportFreeWindow = system::GetIntParameter(IS_ANCO_SUPPORT_FREE_WINDOW, 0) == 1;
+        const bool isAncoSupportMultiWindow =
+            system::GetIntParameter("hmos_fusion.container.pc.freemode.captionbar", 0) == 1;
         bool isAncoInPcOrPcMode = IsAnco() && windowSystemConfig_.IsPcOrPcMode();
-        TLOGI(WmsLogTag::WMS_LAYOUT_PC, "windowId: %{public}u, windowModeSupportType: %{public}u, "
-            "isAncoSupportFreeWindow: %{public}d, isAncoInPcOrPcMode:%{public}d",
-            GetWindowId(), windowModeSupportType, isAncoSupportFreeWindow, isAncoInPcOrPcMode);
+        TLOGI(WmsLogTag::WMS_LAYOUT, "windowId: %{public}u, windowModeSupportType: %{public}u, "
+            "isAncoSupportMultiWindow: %{public}d, isAncoInPcOrPcMode:%{public}d",
+            GetWindowId(), windowModeSupportType, isAncoSupportMultiWindow, isAncoInPcOrPcMode);
         // update windowModeSupportType to server
         UpdateProperty(WSPropertyChangeAction::ACTION_UPDATE_MODE_SUPPORT_INFO);
         bool isWindowModeSupportFullscreen = GetTargetAPIVersion() < 15 ? // 15: isolated version
@@ -1471,7 +1452,7 @@ void WindowSceneSessionImpl::GetConfigurationFromAbilityInfo()
         bool isAncoInPhoneFreeMultiWindowMode = IsAnco() && IsFreeMultiWindowMode() &&
             windowSystemConfig_.IsPhoneWindow();
         bool onlySupportFullScreen = (isWindowModeSupportFullscreen ||
-            (isAncoInPcOrPcMode && !isAncoSupportFreeWindow)) || isAncoInPhoneFreeMultiWindowMode;
+            (isAncoInPcOrPcMode && !isAncoSupportMultiWindow)) || isAncoInPhoneFreeMultiWindowMode;
         bool compatibleDisableFullScreen = property_->IsFullScreenDisabled();
         if ((onlySupportFullScreen || property_->GetFullScreenStart()) && !compatibleDisableFullScreen) {
             TLOGI(WmsLogTag::WMS_LAYOUT_PC, "onlySupportFullScreen:%{public}d fullScreenStart:%{public}d",
