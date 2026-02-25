76bf7e7c94d6cea4c4b0c6a1bfb89b1fdfa4554c
tanhon <tanhong6@h-partners.com>
2026-02-25 17:49:19 +0800

    ii
    Signed-off-by: tanhon <tanhong6@h-partners.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -15104,13 +15104,18 @@ void SceneSessionManager::PostProcessFocus()
     }
 }
 
+// tanhong
 void SceneSessionManager::PostProcessProperty(uint32_t dirty)
 {
     HITRACE_METER_FMT(HITRACE_TAG_WINDOW_MANAGER, "SceneSessionManager::PostProcessProperty");
     if (dirty == static_cast<uint32_t>(SessionUIDirtyFlag::AVOID_AREA)) {
         // only trigger update avoid area
-        std::shared_lock<std::shared_mutex> lock(sceneSessionMapMutex_);
-        for (auto& iter : sceneSessionMap_) {
+        std::map<int32_t, sptr<SceneSession>> sceneSessionMapCopy;
+        {
+            std::shared_lock<std::shared_mutex> lock(sceneSessionMapMutex_);
+            sceneSessionMapCopy = sceneSessionMap_;
+        }
+        for (auto& iter : sceneSessionMapCopy) {
             auto session = iter.second;
             if (session == nullptr) {
                 continue;
@@ -15151,15 +15156,17 @@ void SceneSessionManager::PostProcessProperty(uint32_t dirty)
     }
 
     // update avoid area
+    std::map<int32_t, sptr<SceneSession>> sceneSessionMapCopy;
     {
         std::shared_lock<std::shared_mutex> lock(sceneSessionMapMutex_);
-        for (auto& iter : sceneSessionMap_) {
-            auto session = iter.second;
-            if (session == nullptr) {
-                continue;
-            }
-            session->PostProcessNotifyAvoidArea();
+        sceneSessionMapCopy = sceneSessionMap_;
+    }
+    for (auto& iter : sceneSessionMapCopy) {
+        auto session = iter.second;
+        if (session == nullptr) {
+            continue;
         }
+        session->PostProcessNotifyAvoidArea();
     }
 }
 
