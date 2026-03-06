17bbd20595e4e64d5ef42672ffde012b610e13af
fwg5587 <fanweiguo@h-partners.com>
2026-03-06 11:41:54 +0800

    增加按z序排列并补充TDD
    Co-Authored-By: Agent
    Signed-off-by: fwg5587 <fanweiguo@h-partners.com>

--- window_scene/session_manager/include/scene_session_manager.h
+++ window_scene/session_manager/include/scene_session_manager.h
@@ -926,6 +926,7 @@ public:
     WMError UpdateOutline(const sptr<IRemoteObject>& remoteObject, const OutlineParams& outlineParams) override;
 
     void NotifyRotationBegin(bool isStopDrag);
+    void SortVisibilityWindowInfos(std::vector<sptr<WindowVisibilityInfo>>& infos) const;
 
 protected:
     SceneSessionManager();
