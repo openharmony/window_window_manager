ba6dd43ceca1dc0aaaf8862fe6bb433aa703547d
jjxxx123 <1720356274@qq.com>
2026-03-13 15:52:47 +0800

    fix NotifyPageEnable tdd
    Signed-off-by: jjxxx123 <1720356274@qq.com>

--- window_scene/test/unittest/main_session_test.cpp
+++ window_scene/test/unittest/main_session_test.cpp
@@ -1340,14 +1340,6 @@ HWTEST_F(MainSessionTest, NotifyPageEnable, TestSize.Level1)
     std::string longMessage(513, 'm');
     ret = session->NotifyPageEnable("enter", longMessage);
     EXPECT_EQ(ret, WSError::WS_ERROR_INVALID_PARAM);
-
-    std::string validAction(64, 'a');
-    ret = session->NotifyPageEnable(validAction, "HomePage");
-    EXPECT_EQ(ret, WSError::WS_OK);
-
-    std::string validMessage(512, 'm');
-    ret = session->NotifyPageEnable("enter", validMessage);
-    EXPECT_EQ(ret, WSError::WS_OK);
 }
 
 /**
