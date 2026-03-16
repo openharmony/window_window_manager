a83691467778d8d5589df1d0b3f136676d3d3e07
王青青 <wangqingqing55@h-partners.com>
2026-03-16 18:11:10 +0800

    ROG 车机修复
    Signed-off-by: 王青青 <wangqingqing55@h-partners.com>

--- window_scene/screen_session_manager/src/screen_scene_config.cpp
+++ window_scene/screen_session_manager/src/screen_scene_config.cpp
@@ -577,16 +577,23 @@ RogResolution ScreenSceneConfig::GetRogResolution(uint32_t width, uint32_t heigh
                        GetRogDpi() > 0) {
                 SetRogResolution(RogResolution{ true, false, 0, rogDpi_, rogResolution_.width, rogResolution_.height });
             } else {
-                SetRogResolution(RogResolution{
-                    false, false, 0, static_cast<int32_t>(intNumbersConfig_[xmlNodeMap_[DPI]][0]), width, height });
+                uint32_t densityDpi = 0;
+                if (intNumbersConfig_.count(xmlNodeMap_[DPI]) != 0) {
+                    densityDpi = static_cast<uint32_t>(intNumbersConfig_[xmlNodeMap_[DPI]][0]);
+                }
+                SetRogResolution(RogResolution{ false, false, 0, static_cast<float>(densityDpi), width, height });
             }
             TLOGNFI(WmsLogTag::DMS, "rogResolution Init");
         } else {
             bool dmsSupport = (system::GetIntParameter<int32_t>("persist.window.screen.isSupportRog", 0) > 0);
             uint32_t dmsWidth = system::GetIntParameter<int32_t>("persist.window.screen.width", width);
             uint32_t dmsHeight = system::GetIntParameter<int32_t>("persist.window.screen.height", height);
-            float dmsDpi = static_cast<float>(
-                system::GetIntParameter<int32_t>("persist.window.screen.dpi", intNumbersConfig_[xmlNodeMap_[DPI]][0]));
+            uint32_t densityDpi = 0;
+            if (intNumbersConfig_.count(xmlNodeMap_[DPI]) != 0) {
+                densityDpi = static_cast<uint32_t>(intNumbersConfig_[xmlNodeMap_[DPI]][0]);
+            }
+            float dmsDpi =
+                static_cast<float>(system::GetIntParameter<int32_t>("persist.window.screen.dpi", densityDpi));
             SetRogResolution(RogResolution{ dmsSupport, false, 0, dmsDpi, dmsWidth, dmsHeight });
             TLOGNFI(WmsLogTag::DMS, "rogResolution Recover");
         }
