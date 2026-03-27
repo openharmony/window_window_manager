4c15bbfbd952980c643d1f7951def62a29b17d78
chenlang22 <chenlang48@h-partners.com>
2026-03-27 10:29:09 +0800

    add screenProperty.h
    Signed-off-by: chenlang22 <chenlang48@h-partners.com>

--- window_scene/test/unittest/session_test3.cpp
+++ window_scene/test/unittest/session_test3.cpp
@@ -28,6 +28,7 @@
 #include "session/host/include/session.h"
 #include "session_manager/include/scene_session_manager.h"
 #include "session_info.h"
+#include "session/screen/include/screen_property.h"
 #include "session/screen/include/screen_session.h"
 #include "screen_session_manager_client/include/screen_session_manager_client.h"
 #include "wm_common.h"
