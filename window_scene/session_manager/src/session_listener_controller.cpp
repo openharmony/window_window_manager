50be2da2aa4a2fbb93553ada51c8a7f1bd9e8308
vqshr <sit.liu99@gmail.com>
2026-04-02 14:20:33 +0800

    refactor: remove redundant session state retrieval in NotifySessionLifecycleEvent
    Signed-off-by: vqshr <sit.liu99@gmail.com>

--- window_scene/session_manager/src/session_listener_controller.cpp
+++ window_scene/session_manager/src/session_listener_controller.cpp
@@ -504,12 +504,6 @@ void SessionListenerController::NotifySessionLifecycleEvent(ISessionLifecycleLis
                 }
             }
         }, __func__);
-    SessionState state = sessionInfo.sessionState_;
-    auto sceneSession = SceneSessionManager::GetInstance().GetSceneSession(persistentId);
-    if (sceneSession != nullptr) {
-        state = sceneSession->GetSessionState();
-    }
-    NotifyAppInstanceLifecycleEvent(state, sessionInfo, reason);
 }
 
 void SessionListenerController::NotifyAppInstanceLifecycleEvent(SessionState state,
