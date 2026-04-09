df7ba2334128005c474508681aca852d69dfe0cd
zhouwenjie <zhouwenjie29@h-partners.com>
2026-04-09 20:05:33 +0800

    updatewhenboot
    Signed-off-by: zhouwenjie <zhouwenjie29@h-partners.com>

--- window_scene/screen_session_manager/include/screen_session_manager.h
+++ window_scene/screen_session_manager/include/screen_session_manager.h
@@ -391,7 +391,7 @@ public:
     void OnDisconnect(ScreenId screenId) override {}
     void OnPropertyChange(const ScreenProperty& newProperty, ScreenPropertyChangeReason reason,
         ScreenId screenId) override;
-    void UpdateDisplayOrientationWhenBootAnimation(ScreenId screenId);
+    void UpdateDisplayOrientationWhenBootAnimation(ScreenId screenId, const ScreenProperty& screenProperty);
     void OnFoldPropertyChange(ScreenId screenId, const ScreenProperty& newProperty, ScreenPropertyChangeReason reason,
             FoldDisplayMode displayMode) override;
     void OnPowerStatusChange(DisplayPowerEvent event, EventStatus status,
