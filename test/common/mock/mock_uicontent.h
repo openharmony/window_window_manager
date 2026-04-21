089040f1401c77fe61d2af3963db6b3bfce1cb9b
wujinhui7@huawei.com <wujinhui7@huawei.com>
2026-04-21 02:09:27 +0000

    bug fix tdd PreInitializeForm
    Signed-off-by: wujinhui7@huawei.com <wujinhui7@huawei.com>

--- test/common/mock/mock_uicontent.h
+++ test/common/mock/mock_uicontent.h
@@ -71,7 +71,11 @@ public:
     MOCK_METHOD1(NotifyMemoryLevel, void(int32_t level));
     MOCK_METHOD1(SetAppWindowTitle, void(const std::string& title));
     MOCK_METHOD1(SetAppWindowIcon, void(const std::shared_ptr<Media::PixelMap>& pixelMap));
-    MOCK_METHOD3(PreInitializeForm, void(OHOS::Rosen::Window* window, const std::string& url, napi_value storage));
+    MOCK_METHOD3(PreInitializeForm,
+                 void(OHOS::Rosen::Window* window,
+                      const std::string& url,
+                      napi_value storage,
+                      sptr<IRemoteObject> connectorToRender));
     MOCK_METHOD0(RunFormPage, void());
     MOCK_METHOD0(GetFormRootNode, std::shared_ptr<Rosen::RSSurfaceNode>());
     MOCK_METHOD1(UpdateFormData, void(const std::string& data));
