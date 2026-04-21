76f039d69eb8a25d4fc043f3afb2c831ef89ef39
chenpeng <chenpeng299@huawei.com>
2026-04-21 17:12:24 +0800

    解除外部依赖
    Signed-off-by: chenpeng <chenpeng299@huawei.com>

--- window_scene/screen_session_manager_client/include/screen_session_manager_client.h
+++ window_scene/screen_session_manager_client/include/screen_session_manager_client.h
@@ -26,6 +26,7 @@
 #include "display_change_info.h"
 #include "dm_common.h"
 #include "session/screen/include/screen_session.h"
+#include "session_option.h"
 #include "ffrt_queue_helper.h"
 #include "interfaces/include/ws_common.h"
 #include "wm_single_instance.h"
