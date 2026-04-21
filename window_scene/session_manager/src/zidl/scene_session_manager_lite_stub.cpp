f6eb7a593af1bc1e16f3364fe6fc518743e9bef2
l30067243 <leipeng42@h-partners.com>
2026-04-21 15:47:58 +0800

    update stub
    Signed-off-by: l30067243 <leipeng42@h-partners.com>

--- window_scene/session_manager/src/zidl/scene_session_manager_lite_stub.cpp
+++ window_scene/session_manager/src/zidl/scene_session_manager_lite_stub.cpp
@@ -1393,7 +1393,10 @@ int SceneSessionManagerLiteStub::HandleRecoverProcessWatermark(MessageParcel& da
         return ERR_INVALID_DATA;
     }
     WMError errCode = RecoverProcessWatermark(pid, watermarkName);
-    reply.WriteInt32(static_cast<int32_t>(errCode));
+    if (!reply.WriteInt32(static_cast<int32_t>(errCode))) {
+        TLOGE(WmsLogTag::WMS_ATTRIBUTE, "write error code failed");
+        return ERR_INVALID_DATA;
+    }
     return ERR_NONE;
 }
 
