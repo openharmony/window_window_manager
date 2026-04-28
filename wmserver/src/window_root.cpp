41861526ce3e52bdcb27fe414ed0cbb3f71201e6
tanhon <tanhong6@h-partners.com>
2026-04-28 19:41:34 +0800

    ii
    Signed-off-by: tanhon <tanhong6@h-partners.com>

--- wmserver/src/window_root.cpp
+++ wmserver/src/window_root.cpp
@@ -173,14 +173,12 @@ sptr<WindowNode> WindowRoot::GetWindowNodeByMissionId(uint32_t missionId) const
 
 sptr<WindowNode> WindowRoot::GetWindowNodeByWindowType(WindowType type) const
 {
-
     for (auto iter: windowNodeMap_) {
         if (iter->second) {
             TLOGE(WmsLogTag::WMS_ATTRIBUTE, "tanhong winId %{public}d windowType: %{public}d visible %{public}d",
                 iter->first, iter->second->GetWindowType(), iter->second->GetVisibilityState());
         }
     }
-
     using ValueType = const std::map<uint32_t, sptr<WindowNode>>::value_type&;
     auto it = std::find_if(windowNodeMap_.begin(), windowNodeMap_.end(), [type] (ValueType item) {
         return item.second && item.second->GetWindowType() == type;
