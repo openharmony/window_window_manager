3ccd9d6f29f15368bcd4e379d083e8c50b0cdb81
ZihaoWU <wuzihao11@huawei.com>
2026-04-28 14:28:38 +0800

    test(rotation): complete SetRotationLocked and NotifyRotationChangeResult UT coverage
    Add UT for WindowSceneSessionImpl.SetRotationLocked/GetRotationLocked (8 cases)
    Add UT for WindowSessionImpl.NotifyRotationChangeResult (4 cases)
    Achieve 100% rotation method coverage for wm module
    
    Co-Authored-By: Agent
    Signed-off-by: ZihaoWU <wuzihao11@huawei.com>

--- wm/test/unittest/rotation/window_scene_session_impl_rotation_test.cpp
+++ wm/test/unittest/rotation/window_scene_session_impl_rotation_test.cpp
@@ -100,6 +100,190 @@ HWTEST_F(WindowSceneSessionImplRotationTest, CheckAndModifyWindowRect, Function 
         EXPECT_EQ(WMError::WM_ERROR_INVALID_OPERATION, ret);
     }
 }
+
+/**
+ * @tc.name: SetRotationLocked_InvalidWindow
+ * @tc.desc: Test SetRotationLocked with invalid window state
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSceneSessionImplRotationTest, SetRotationLocked_InvalidWindow, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: SetRotationLocked_InvalidWindow start";
+    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
+    option->SetWindowName("SetRotationLocked_InvalidWindow");
+    sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
+    window->state_ = WindowState::STATE_DESTROYED;
+    auto ret = window->SetRotationLocked(true);
+    EXPECT_EQ(ret, WMError::WM_ERROR_INVALID_WINDOW);
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: SetRotationLocked_InvalidWindow end";
+}
+
+/**
+ * @tc.name: SetRotationLocked_DeviceNotSupport
+ * @tc.desc: Test SetRotationLocked with device not supporting rotation lock
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSceneSessionImplRotationTest, SetRotationLocked_DeviceNotSupport, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: SetRotationLocked_DeviceNotSupport start";
+    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
+    option->SetWindowName("SetRotationLocked_DeviceNotSupport");
+    option->SetWindowType(WindowType::WINDOW_TYPE_STATUS_BAR);
+    sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
+    SessionInfo sessionInfo = { "TestBundle", "TestModule", "TestAbility" };
+    sptr<SessionMocker> session = sptr<SessionMocker>::MakeSptr(sessionInfo);
+    window->hostSession_ = session;
+    window->property_->SetPersistentId(1);
+    window->state_ = WindowState::STATE_CREATED;
+    window->windowSystemConfig_.windowUIType_ = WindowUIType::INVALID_WINDOW;
+    auto ret = window->SetRotationLocked(true);
+    EXPECT_EQ(ret, WMError::WM_ERROR_DEVICE_NOT_SUPPORT);
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: SetRotationLocked_DeviceNotSupport end";
+}
+
+/**
+ * @tc.name: SetRotationLocked_InvalidWindowType
+ * @tc.desc: Test SetRotationLocked with non-system window type
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSceneSessionImplRotationTest, SetRotationLocked_InvalidWindowType, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: SetRotationLocked_InvalidWindowType start";
+    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
+    option->SetWindowName("SetRotationLocked_InvalidWindowType");
+    option->SetWindowType(WindowType::WINDOW_TYPE_APP_MAIN_WINDOW);
+    sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
+    SessionInfo sessionInfo = { "TestBundle", "TestModule", "TestAbility" };
+    sptr<SessionMocker> session = sptr<SessionMocker>::MakeSptr(sessionInfo);
+    window->hostSession_ = session;
+    window->property_->SetPersistentId(1);
+    window->state_ = WindowState::STATE_CREATED;
+    window->windowSystemConfig_.windowUIType_ = WindowUIType::PHONE_WINDOW;
+    auto ret = window->SetRotationLocked(true);
+    EXPECT_EQ(ret, WMError::WM_ERROR_INVALID_WINDOW_TYPE);
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: SetRotationLocked_InvalidWindowType end";
+}
+
+/**
+ * @tc.name: SetRotationLocked_Success
+ * @tc.desc: Test SetRotationLocked with valid configuration
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSceneSessionImplRotationTest, SetRotationLocked_Success, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: SetRotationLocked_Success start";
+    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
+    option->SetWindowName("SetRotationLocked_Success");
+    option->SetWindowType(WindowType::WINDOW_TYPE_STATUS_BAR);
+    sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
+    SessionInfo sessionInfo = { "TestBundle", "TestModule", "TestAbility" };
+    sptr<SessionMocker> session = sptr<SessionMocker>::MakeSptr(sessionInfo);
+    window->hostSession_ = session;
+    window->property_->SetPersistentId(1);
+    window->state_ = WindowState::STATE_CREATED;
+    window->windowSystemConfig_.windowUIType_ = WindowUIType::PHONE_WINDOW;
+    auto ret = window->SetRotationLocked(true);
+    EXPECT_EQ(ret, WMError::WM_OK);
+    ret = window->SetRotationLocked(false);
+    EXPECT_EQ(ret, WMError::WM_OK);
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: SetRotationLocked_Success end";
+}
+
+/**
+ * @tc.name: GetRotationLocked_InvalidWindow
+ * @tc.desc: Test GetRotationLocked with invalid window state
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSceneSessionImplRotationTest, GetRotationLocked_InvalidWindow, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: GetRotationLocked_InvalidWindow start";
+    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
+    option->SetWindowName("GetRotationLocked_InvalidWindow");
+    sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
+    window->state_ = WindowState::STATE_DESTROYED;
+    bool locked = false;
+    auto ret = window->GetRotationLocked(locked);
+    EXPECT_EQ(ret, WMError::WM_ERROR_INVALID_WINDOW);
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: GetRotationLocked_InvalidWindow end";
+}
+
+/**
+ * @tc.name: GetRotationLocked_DeviceNotSupport
+ * @tc.desc: Test GetRotationLocked with device not supporting rotation lock
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSceneSessionImplRotationTest, GetRotationLocked_DeviceNotSupport, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: GetRotationLocked_DeviceNotSupport start";
+    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
+    option->SetWindowName("GetRotationLocked_DeviceNotSupport");
+    option->SetWindowType(WindowType::WINDOW_TYPE_STATUS_BAR);
+    sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
+    SessionInfo sessionInfo = { "TestBundle", "TestModule", "TestAbility" };
+    sptr<SessionMocker> session = sptr<SessionMocker>::MakeSptr(sessionInfo);
+    window->hostSession_ = session;
+    window->property_->SetPersistentId(1);
+    window->state_ = WindowState::STATE_CREATED;
+    window->windowSystemConfig_.windowUIType_ = WindowUIType::INVALID_WINDOW;
+    bool locked = false;
+    auto ret = window->GetRotationLocked(locked);
+    EXPECT_EQ(ret, WMError::WM_ERROR_DEVICE_NOT_SUPPORT);
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: GetRotationLocked_DeviceNotSupport end";
+}
+
+/**
+ * @tc.name: GetRotationLocked_InvalidWindowType
+ * @tc.desc: Test GetRotationLocked with non-system window type
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSceneSessionImplRotationTest, GetRotationLocked_InvalidWindowType, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: GetRotationLocked_InvalidWindowType start";
+    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
+    option->SetWindowName("GetRotationLocked_InvalidWindowType");
+    option->SetWindowType(WindowType::WINDOW_TYPE_APP_MAIN_WINDOW);
+    sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
+    SessionInfo sessionInfo = { "TestBundle", "TestModule", "TestAbility" };
+    sptr<SessionMocker> session = sptr<SessionMocker>::MakeSptr(sessionInfo);
+    window->hostSession_ = session;
+    window->property_->SetPersistentId(1);
+    window->state_ = WindowState::STATE_CREATED;
+    window->windowSystemConfig_.windowUIType_ = WindowUIType::PHONE_WINDOW;
+    bool locked = false;
+    auto ret = window->GetRotationLocked(locked);
+    EXPECT_EQ(ret, WMError::WM_ERROR_INVALID_WINDOW_TYPE);
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: GetRotationLocked_InvalidWindowType end";
+}
+
+/**
+ * @tc.name: GetRotationLocked_Success
+ * @tc.desc: Test GetRotationLocked with valid configuration
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSceneSessionImplRotationTest, GetRotationLocked_Success, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: GetRotationLocked_Success start";
+    sptr<WindowOption> option = sptr<WindowOption>::MakeSptr();
+    option->SetWindowName("GetRotationLocked_Success");
+    option->SetWindowType(WindowType::WINDOW_TYPE_STATUS_BAR);
+    sptr<WindowSceneSessionImpl> window = sptr<WindowSceneSessionImpl>::MakeSptr(option);
+    SessionInfo sessionInfo = { "TestBundle", "TestModule", "TestAbility" };
+    sptr<SessionMocker> session = sptr<SessionMocker>::MakeSptr(sessionInfo);
+    window->hostSession_ = session;
+    window->property_->SetPersistentId(1);
+    window->state_ = WindowState::STATE_CREATED;
+    window->windowSystemConfig_.windowUIType_ = WindowUIType::PHONE_WINDOW;
+    window->property_->SetRotationLocked(true);
+    bool locked = false;
+    auto ret = window->GetRotationLocked(locked);
+    EXPECT_EQ(ret, WMError::WM_OK);
+    EXPECT_EQ(locked, true);
+    window->property_->SetRotationLocked(false);
+    ret = window->GetRotationLocked(locked);
+    EXPECT_EQ(ret, WMError::WM_OK);
+    EXPECT_EQ(locked, false);
+    GTEST_LOG_(INFO) << "WindowSceneSessionImplRotationTest: GetRotationLocked_Success end";
+}
 } // namespace
 } // namespace Rosen
 } // namespace OHOS
