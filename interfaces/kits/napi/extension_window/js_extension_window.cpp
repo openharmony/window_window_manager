1dc6aa7d7b8b28183a042ede28afac45434ac38b
jjxxx123 <1720356274@qq.com>
2026-05-20 09:32:13 +0800

    fix FindJsExtensionWindow
    Co-Authored-By: Agent
    Signed-off-by: jjxxx123 <1720356274@qq.com>

--- interfaces/kits/napi/extension_window/js_extension_window.cpp
+++ interfaces/kits/napi/extension_window/js_extension_window.cpp
@@ -102,6 +102,8 @@ napi_value FindJsExtensionWindow(napi_env env, sptr<Rosen::Window> window)
 {
     if (window == nullptr) {
         TLOGE(WmsLogTag::WMS_UIEXT, "window is nullptr");
+        napi_throw(env, JsErrUtils::CreateJsError(env, WmErrorCode::WM_ERROR_STATE_ABNORMALLY, 
+            "The extensionWindow is destroyed."));
         return NapiGetUndefined(env);
     }
     int32_t id = window->GetWindowPersistentId();
