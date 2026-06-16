38755d6fd6f29fdc3af7a28e53436d09b19579a0
aurifolia <207265976@qq.com>
2026-06-16 19:48:50 +0800

    sceneboard独立恢复时还原hook信息 Signed-off-by: aurifolia <207265976@qq.com>

--- window_scene/common/src/window_session_property.cpp
+++ window_scene/common/src/window_session_property.cpp
@@ -1931,6 +1931,7 @@ void WindowSessionProperty::CopyFrom(const sptr<WindowSessionProperty>& property
     statusBarHeightInImmersive_ = property->statusBarHeightInImmersive_;
     pageCompatibleMode_ = property->pageCompatibleMode_;
     isCrossProcessWindow_ = property->isCrossProcessWindow_;
+    hookWindowInfo_ = property->hookWindowInfo_;
 }
 
 bool WindowSessionProperty::Write(Parcel& parcel, WSPropertyChangeAction action)
