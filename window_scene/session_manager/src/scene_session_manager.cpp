6ebef6751d8bd54a546c73a2b18a970170f60ed3
yekai28 <yekai28@h-partners.com>
2026-07-25 10:14:40 +0800

    updateScreenSupportMultiWindow同步到服务端和客户端
    Signed-off-by: yekai28 <yekai28@h-partners.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -16058,16 +16058,13 @@ WSError SceneSessionManager::UpdateScreenSupportMultiWindow(uint64_t screenId, S
     }
     {
         std::shared_lock<std::shared_mutex> lock(sceneSessionMapMutex_);
-        std::vector<std::shared_ptr<SceneSession>> sessions;
         for (const auto& [_, sceneSession] : sceneSessionMap_) {
-            if (sceneSession != nullptr) {
-                sessions.push_back(sceneSession);
+            if (sceneSession == nullptr) {
+                continue;
             }
+            sceneSession->UpdateSupportMultiWindowScreenSet(systemConfig_.supportMultiWindowScreenSet_);
+            sceneSession->UpdateScreenSupportMultiWindowToClient();
         }
-    }
-    for (const auto& sceneSession : sessions) {
-        sceneSession->UpdateSupportMultiWindowScreenSet(systemConfig_.supportMultiWindowScreenSet_);
-        sceneSession->UpdateScreenSupportMultiWindowToClient();
     }
     return WSError::WS_OK;
 }
