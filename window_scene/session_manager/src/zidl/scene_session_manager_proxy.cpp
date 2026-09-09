a28f88818d9b5d427acd40f47985c6f6e5de4763
赵聪 <zhaocong33@h-partners.com>
2026-09-09 17:31:11 +0800

    错误信息优化
    Signed-off-by: 赵聪 <zhaocong33@h-partners.com>

--- window_scene/session_manager/src/zidl/scene_session_manager_proxy.cpp
+++ window_scene/session_manager/src/zidl/scene_session_manager_proxy.cpp
@@ -54,41 +54,36 @@ WSErrorResult SceneSessionManagerProxy::CreateAndConnectSpecificSession(const sp
     MessageParcel reply;
     if (!data.WriteInterfaceToken(GetDescriptor())) {
         TLOGE(WmsLogTag::WMS_LIFE, "Write InterfaceToken failed!");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to write interface token, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, failed to write interface token"};
     }
     if (!data.WriteRemoteObject(sessionStage->AsObject())) {
         TLOGE(WmsLogTag::WMS_LIFE, "Write ISessionStage failed!");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to write the ISessionStage remote object, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, "
+            "failed to write the ISessionStage remote object."};
     }
     if (!data.WriteRemoteObject(eventChannel->AsObject())) {
         TLOGE(WmsLogTag::WMS_LIFE, "Write IWindowEventChannel failed!");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to write IWindowEventChannel, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, "
+            "failed to write IWindowEventChannel."};
     }
     if (!data.WriteUint64(nodeId)) {
         TLOGE(WmsLogTag::WMS_LIFE, "Write nodeId failed");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to write node ID, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, failed to write node ID."};
     }
     if (!property || !data.WriteStrongParcelable(property)) {
         TLOGE(WmsLogTag::WMS_LIFE, "Write property failed");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to write property, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, failed to write property. "};
     }
     if (token != nullptr) {
         if (!data.WriteRemoteObject(token)) {
-            return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to write token, "
-                "Please contact official support for assistance."};
+            return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, failed to write token."};
         }
     }
 
     sptr<IRemoteObject> remote = Remote();
     if (remote == nullptr) {
         TLOGE(WmsLogTag::WMS_LIFE, "remote is null");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "The remote binder object is null, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, the remote binder object is null."};
     }
     int32_t errCode = ERR_NONE;
     std::string errMsg = "";
@@ -96,15 +91,14 @@ WSErrorResult SceneSessionManagerProxy::CreateAndConnectSpecificSession(const sp
         SceneSessionManagerMessage::TRANS_ID_CREATE_AND_CONNECT_SPECIFIC_SESSION), data, reply, option) != ERR_NONE ||
         !reply.ReadInt32(errCode) || !reply.ReadString(errMsg)) {
         TLOGE(WmsLogTag::WMS_LIFE, "SendRequest failed");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "IPC SendRequest invocation failed, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, IPC SendRequest invocation failed."};
     }
     persistentId = reply.ReadInt32();
     sptr<IRemoteObject> sessionObject = reply.ReadRemoteObject();
     if (sessionObject == nullptr) {
         TLOGE(WmsLogTag::WMS_LIFE, "Read RemoteObject failed");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to read the session remote object, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, "
+            "failed to read the session remote object."};
     }
     session = iface_cast<ISession>(sessionObject);
     sptr<SystemSessionConfig> config = reply.ReadParcelable<SystemSessionConfig>();
@@ -114,30 +108,29 @@ WSErrorResult SceneSessionManagerProxy::CreateAndConnectSpecificSession(const sp
     sptr<IRemoteObject> renderSessionObject = reply.ReadRemoteObject();
     if (renderSessionObject == nullptr) {
         TLOGE(WmsLogTag::WMS_LIFE, "Read RemoteObject failed");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to read the render session remote object, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, "
+            "failed to read the render session remote object."};
     }
     renderSession = renderSessionObject;
 
     surfaceNode = RSSurfaceNode::Unmarshalling(reply, false);
     if (!surfaceNode) {
         TLOGE(WmsLogTag::WMS_LIFE, "Read surfaceNode failed");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to read the RSSurfaceNode parcel data, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, "
+            "failed to read the RSSurfaceNode parcel data."};
     }
 
     uint32_t level = 0;
     if (!reply.ReadUint32(level)) {
         TLOGE(WmsLogTag::WMS_LIFE, "Read level failed");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to read the subwindow level value, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, "
+            "failed to read the subwindow level value"};
     }
     property->SetSubWindowLevel(level);
     uint64_t displayId = 0;
     if (!reply.ReadUint64(displayId)) {
         TLOGE(WmsLogTag::WMS_LIFE, "Read displayId failed");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to read the display ID, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, failed to read the display ID."};
     }
     if (property->GetDisplayId() != VIRTUAL_DISPLAY_ID) {
         property->SetDisplayId(displayId);
@@ -145,15 +138,15 @@ WSErrorResult SceneSessionManagerProxy::CreateAndConnectSpecificSession(const sp
     uint32_t windowType = 0;
     if (!reply.ReadUint32(windowType)) {
         TLOGE(WmsLogTag::WMS_LIFE, "Read windowType failed");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to read the window type value, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, "
+            "failed to read the window type value."};
     }
     property->SetWindowType(static_cast<WindowType>(windowType));
     bool isSystemCalling = false;
     if (!reply.ReadBool(isSystemCalling)) {
         TLOGE(WmsLogTag::WMS_LIFE, "Read isSystemCalling failed");
-        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Failed to read the system‑calling flag, "
-            "Please contact official support for assistance."};
+        return WSErrorResult{WSError::WS_ERROR_IPC_FAILED, "Internal task error, "
+            "failed to read the system‑calling flag."};
     }
     property->SetSystemCalling(isSystemCalling);
     return WSErrorResult{static_cast<WSError>(errCode), errMsg};
