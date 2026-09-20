f90a6f6fde68ac3905253121b6828f2b0dd9580e
Bigdemon <lianghanlin@h-partners.com>
2026-09-20 10:59:37 +0800

    update: 更新文件 scene_session_manager.cpp
    Signed-off-by: Bigdemon <lianghanlin@h-partners.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -13030,17 +13030,20 @@ void SceneSessionManager::StartAbilityBySpecified(const SessionInfo& sessionInfo
         TLOGNI(WmsLogTag::WMS_LIFE, "start specified ability by SCB result: %{public}d", result);
         if (result == ERR_OK) {
             return;
-        } else if (result == ERR_BLOCK_START_FIRST_BOOT_SCREEN_UNLOCK) {
-            TLOGNI(WmsLogTag::WMS_LIFE,
+        } else {
+            if (result == ERR_BLOCK_START_FIRST_BOOT_SCREEN_UNLOCK) {
+                TLOGNI(WmsLogTag::WMS_LIFE,
                    "start specified ability by SCB failed, errReason: %{public}s",
                    ERR_REASON_BLOCK_START_FIRST_BOOT_SCREEN_UNLOCK.c_str());
-        } else if (startUIAbilityErrorFunc_ && static_cast<WSError>(result) == WSError::WS_ERROR_EDM_CONTROLLED) {
-            startUIAbilityErrorFunc_(
-                static_cast<uint32_t>(WS_JS_TO_ERROR_CODE_MAP.at(WSError::WS_ERROR_EDM_CONTROLLED)));
+            }
+            if (startUIAbilityErrorFunc_ && static_cast<WSError>(result) == WSError::WS_ERROR_EDM_CONTROLLED) {
+                startUIAbilityErrorFunc_(
+                    static_cast<uint32_t>(WS_JS_TO_ERROR_CODE_MAP.at(WSError::WS_ERROR_EDM_CONTROLLED)));
+            }
+            auto sceneSession = GetSceneSession(sessionInfo.persistentId_);
+            RecordLifeCycleExceptionEvent(
+                sceneSession, result, WSErrorReason::WS_REASON_WINDOW_SPECIFIED_ERR, "start specified failed");
         }
-        auto sceneSession = GetSceneSession(sessionInfo.persistentId_);
-        RecordLifeCycleExceptionEvent(
-            sceneSession, result, WSErrorReason::WS_REASON_WINDOW_SPECIFIED_ERR, "start specified failed");
 
         auto task = [bundleName = sessionInfo.bundleName_, appInstanceKey] {
             MultiInstanceManager::GetInstance().DecreaseInstanceKeyRefCountByBundleNameAndInstanceKey(
