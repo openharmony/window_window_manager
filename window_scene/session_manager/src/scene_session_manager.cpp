af0c62b8c4f5871a97217d4ba3a177f46fe1701a
Bigdemon <lianghanlin@h-partners.com>
2026-09-20 11:00:55 +0800

    update: 更新文件 scene_session_manager.cpp
    Signed-off-by: Bigdemon <lianghanlin@h-partners.com>

--- window_scene/session_manager/src/scene_session_manager.cpp
+++ window_scene/session_manager/src/scene_session_manager.cpp
@@ -13032,8 +13032,7 @@ void SceneSessionManager::StartAbilityBySpecified(const SessionInfo& sessionInfo
             return;
         } else {
             if (result == ERR_BLOCK_START_FIRST_BOOT_SCREEN_UNLOCK) {
-                TLOGNI(WmsLogTag::WMS_LIFE,
-                   "start specified ability by SCB failed, errReason: %{public}s",
+                TLOGNI(WmsLogTag::WMS_LIFE, "start specified ability by SCB failed, errReason: %{public}s",
                    ERR_REASON_BLOCK_START_FIRST_BOOT_SCREEN_UNLOCK.c_str());
             }
             if (startUIAbilityErrorFunc_ && static_cast<WSError>(result) == WSError::WS_ERROR_EDM_CONTROLLED) {
@@ -13041,8 +13040,8 @@ void SceneSessionManager::StartAbilityBySpecified(const SessionInfo& sessionInfo
                     static_cast<uint32_t>(WS_JS_TO_ERROR_CODE_MAP.at(WSError::WS_ERROR_EDM_CONTROLLED)));
             }
             auto sceneSession = GetSceneSession(sessionInfo.persistentId_);
-            RecordLifeCycleExceptionEvent(
-                sceneSession, result, WSErrorReason::WS_REASON_WINDOW_SPECIFIED_ERR, "start specified failed");
+            RecordLifeCycleExceptionEvent(sceneSession, result, 
+                WSErrorReason::WS_REASON_WINDOW_SPECIFIED_ERR, "start specified failed");
         }
 
         auto task = [bundleName = sessionInfo.bundleName_, appInstanceKey] {
