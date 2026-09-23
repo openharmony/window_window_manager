0bfa6e861aa2b60592c10b7e819720e0961eb6ab
zym <841375420@qq.com>
2026-09-23 13:20:10 +0800

    startability 最小化启动窗口独立恢复后，窗口大小位置异常
    Signed-off-by: zym <841375420@qq.com>

--- window_scene/test/unittest/session_test4.cpp
+++ window_scene/test/unittest/session_test4.cpp
@@ -1605,8 +1605,8 @@ HWTEST_F(WindowSessionTest4, TestGetSessionScreenRelativeRect_002, TestSize.Leve
 
     session->SetMockLayoutController(layoutController);
     session_->UpdateSizeChangeReason(SizeChangeReason::DRAG_MOVE);
-    WSRect expectedRect = { 0, 0, 50, 50 };
-    WSRect winRect = { 0, 0, 50, 50 };
+    WSRect expectedRect = { 0, 0, 50, 50};
+    WSRect winRect = { 0, 0, 50, 50};
     session->SetSessionRect(winRect);
 
     EXPECT_CALL(*layoutController, ConvertGlobalRectToRelative(_, _)).Times(1).WillOnce(Return(expectedRect));
